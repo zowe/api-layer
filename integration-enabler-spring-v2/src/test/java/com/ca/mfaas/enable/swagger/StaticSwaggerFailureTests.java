dc2c6c48cc62cb0d26372f6a5b6a74408ad71d4e
kinda14 <kinda14@ca.com>
2018-11-08 13:49:14 +0100

    Remove unused import
    Signed-off-by: kinda14 <kinda14@ca.com>

--- integration-enabler-spring-v2/src/test/java/com/ca/mfaas/enable/swagger/StaticSwaggerFailureTests.java
+++ integration-enabler-spring-v2/src/test/java/com/ca/mfaas/enable/swagger/StaticSwaggerFailureTests.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RunWith(SpringRunner.class)
