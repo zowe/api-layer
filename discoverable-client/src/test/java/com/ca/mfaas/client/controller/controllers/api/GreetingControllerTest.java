de92e4524bc00a30a50d004d693924498a697f25
Vasyl Melnychuk <melva02@ca.com>
2018-11-12 06:09:51 +0100

    add more tests; fix hide api
    Signed-off-by: Vasyl Melnychuk <melva02@ca.com>

---
+++ discoverable-client/src/test/java/com/ca/mfaas/client/controller/controllers/api/GreetingControllerTest.java
@@ -0,0 +1,41 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package com.ca.mfaas.client.controller.controllers.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testGreetingWithoutDelay() throws Exception {
+        mvc.perform(get("/api/v1/greeting")
+            .contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("content", is("Hello, world!")));
+    }
+
+}
--- integration-tests/src/test/java/com/ca/mfaas/discoverableclient/GreetingApiIntegrationTest.java
+++ integration-tests/src/test/java/com/ca/mfaas/discoverableclient/GreetingApiIntegrationTest.java
@@ -9,20 +9,56 @@
  */
 package com.ca.mfaas.discoverableclient;
 
+import com.ca.mfaas.utils.config.ConfigReader;
+import com.ca.mfaas.utils.config.GatewayServiceConfiguration;
 import com.ca.mfaas.utils.http.HttpRequestUtils;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import io.restassured.RestAssured;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class GreetingApiIntegrationTest {
+    private static final String GREETING_ENDPOINT = "/api/v1/discoverableclient/greeting";
+
+    private GatewayServiceConfiguration serviceConfiguration;
+    private String scheme;
+    private String host;
+    private int port;
+
+    @BeforeClass
+    public static void beforeClass() {
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    @Before
+    public void setUp() {
+        serviceConfiguration = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration();
+        scheme = serviceConfiguration.getScheme();
+        host = serviceConfiguration.getHost();
+        port = serviceConfiguration.getPort();
+    }
+
+
     @Test
     public void shouldCallDiscoverableServiceApi() throws Exception {
+        given()
+            .when()
+            .get(String.format("%s://%s:%s%s", scheme, host, port, GREETING_ENDPOINT))
+            .then()
+            .statusCode(is(SC_OK))
+            .body("", is("%2fmy%2fvariable%2fpath"));
+
+
         // When
         final HttpResponse response = HttpRequestUtils.getResponse("/api/v1/discoverableclient/greeting", SC_OK);
         final String jsonResponse = EntityUtils.toString(response.getEntity());
