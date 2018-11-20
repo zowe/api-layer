41289ca50538c358fd85869393b709d7fb27ad0d
Dave King <david.king2@ca.com>
2018-11-20 16:44:19 +0100

    Merge with master Remove swaggerUI test as component cannot be loaded (easily)

--- api-catalog-ui/frontend/src/components/Swagger/Swagger.test.jsx
+++ api-catalog-ui/frontend/src/components/Swagger/Swagger.test.jsx
@@ -4,26 +4,6 @@ import { shallow } from "enzyme";
 import SwaggerUI from "./Swagger";
 
 describe(">>> Swagger component tests", () => {
-   xit("should render swagger if apiDoc is not null", () => {
-        const service = {
-            "serviceId": "testservice",
-            "title": "Spring Boot Enabler Service",
-            "description": "Dummy Service for enabling others",
-            "status": "UP",
-            "secured": false,
-            "homePageUrl": "http://localhost:10013/enabler/",
-            "apiDoc": "{\"swagger\": \"2.0\",\"info\": {\"description\": \"Sample\",\"version\": \"0.0.1\",\"title\": \"Sample API\"}  }"
-        };
-        const wrapper = shallow(
-            <div>
-                <SwaggerUI service={service}/>
-            </div>
-        );
-        const swaggerDiv = wrapper.find("#swaggerContainer").children();
-        console.log(swaggerDiv);
-        expect(swaggerDiv.length).toEqual(1);
-    });
-
     it("should not render swagger if apiDoc is null", () => {
         const service = {
             "serviceId": "testservice",
