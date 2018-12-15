5bfcc60e77c6f900246d8d8eb861871a039182ec
Petr Plavjanik <petr.plavjanik@broadcom.com>
2018-12-15 01:14:20 +0100

    Releasing

---
+++ scripts/publish_and_release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+set -ex
+
+RELEASE_TYPE=$1
+AUTH="-Pzowe.deploy.username=$USERNAME -Pzowe.deploy.password=$PASSWORD"
+
+case $RELEASE_TYPE in
+   "SNAPSHOT_RELEASE")
+   echo "Make SNAPSHOT release"
+   ./gradlew publishAllVersions $AUTH
+   ;;
+   "PATCH_RELEASE")
+   echo "Make PATCH release"
+   ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.scope=patch $AUTH
+   ;;
+   "MINOR_RELEASE")
+   echo "Make MINOR release"
+   ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.scope=minor $AUTH
+   ;;
+   "MAJOR_RELEASE")
+   echo "Make MAJOR release"
+   ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.scope=major $AUTH
+   ;;
+esac
+
+echo "End of publish and release"
