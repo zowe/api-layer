82fb9af1115b6acaeb0d7fbbf42786462b88ed2d
Petr Plavjanik <petr.plavjanik@broadcom.com>
2018-12-19 21:52:53 +0100

    Add setup action test

--- scripts/apiml_cm.test.js
+++ scripts/apiml_cm.test.js
@@ -1,13 +1,47 @@
-var nixt = require("nixt");
+const nixt = require("nixt");
+const path = require("path");
 
-let script = 'scripts/apiml_cm.sh';
+const script =  path.join(__dirname, "apiml_cm.sh");
+const testDir = path.join(__dirname, ".apiml_cm_test");
+console.log(testDir);
 
 describe(script, function() {
+    beforeEach(function(done) {
+        nixt()
+            .exec(`rm -Rf ${testDir}`).run(`mkdir ${testDir}`)
+            .end(done);
+    });
+
     describe("general", function() {
         it("should return usage when there is no argument", function(done) {
             nixt()
                 .run(script)
+                .code(1)
                 .stdout(/usage:/)
+                .end(done);
+        });
+        it("should return usage when there is wrong action", function(done) {
+            nixt()
+                .run(`${script} --action wrong`)
+                .code(1)
+                .stdout(/usage:/)
+                .stdout(/Called with: --action wrong/)
+                .end(done);
+        });
+    });
+    describe("setup", function() {
+        it("should create local CA and APIML stores", function(done) {
+            this.timeout(10000);
+            nixt()
+                .cwd(testDir)
+                .exec('mkdir -p keystore/local_ca')
+                .exec('mkdir -p keystore/localhost')
+                .run(`${script} --action setup`)
+                .code(0)
+                .exist('keystore/localhost/localhost.keystore.p12')
+                .exist('keystore/localhost/localhost.truststore.p12')
+                .exist('keystore/localhost/localhost.keystore.cer')
+                .exist('keystore/localhost/localhost.keystore.key')
                 .end(done);
         });
     });
