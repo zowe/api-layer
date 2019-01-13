b3b850d6d7bc73b22ebf051ddc56d3c6a8e1dbeb
Petr Plavjanik <petr.plavjanik@broadcom.com>
2019-01-13 22:38:32 +0100

    Fixes for APIML authentication plugin Zowe on z/OS (#187)
    Provides fixes for problems discovered during testing on z/OS.
    
    - `axios` is not a dependency of zLUX so it has been replaced by standard `https` library
    - better error handling
    
    Signed-off-by: Vasyl Melnychuk <vasyl.melnychuk@broadcom.com>

--- apiml-auth/lib/apimlAuth.js
+++ apiml-auth/lib/apimlAuth.js
@@ -8,19 +8,25 @@
   Copyright Contributors to the Zowe Project.
 */
 
-const axios = require('axios');
 const https = require('https');
 const fs = require('fs');
 
 function ApimlAuthenticator(pluginDef, pluginConf, serverConf) {
   this.authPluginID = pluginDef.identifier;
   this.pluginConf = pluginConf;
-  this.serverConf = serverConf;
-  this.gatewayUrl = `https://${serverConf.node.mediationLayer.server.hostname}:${serverConf.node.mediationLayer.server.gatewayPort}`;
-  this.httpsAgent = new https.Agent({
-    rejectUnauthorized: true,
-    ca: readFilesToArray(this.serverConf.node.https.certificateAuthorities)
-  });
+  this.apimlConf = serverConf.node.mediationLayer.server;
+  this.gatewayUrl = `https://${this.apimlConf.hostname}:${this.apimlConf.gatewayPort}`;
+  if (serverConf.node.https.certificateAuthorities === undefined) {
+    console.log("zLUX server is not configured with certificate authorities, APIML authentication plugin will not validate certificates");
+    this.httpsAgent = new https.Agent({
+      rejectUnauthorized: false
+    });
+  } else {
+    this.httpsAgent = new https.Agent({
+      rejectUnauthorized: true,
+      ca: readUtf8FilesToArray(serverConf.node.https.certificateAuthorities)
+    });
+  }
 }
 
 function deleteApimlAuthFromSession(sessionState) {
@@ -31,15 +37,17 @@ function deleteApimlAuthFromSession(sessionState) {
   delete sessionState.gatewayUrl;
 }
 
-function readFilesToArray(fileList) {
+function readUtf8FilesToArray(fileArray) {
   var contentArray = [];
-  fileList.forEach(function (filePath) {
+  for (var i = 0; i < fileArray.length; i++) {
+    const filePath = fileArray[i];
     try {
-      contentArray.push(fs.readFileSync(filePath));
+      contentArray.push(fs.readFileSync(filePath, 'utf8'));
     } catch (e) {
       console.log('Error when reading file=' + filePath + '. Error=' + e.message);
     }
-  });
+  }
+
   if (contentArray.length > 0) {
     return contentArray;
   } else {
@@ -58,53 +66,71 @@ ApimlAuthenticator.prototype = {
 
   /**
    * Should be called e.g. when the users enters credentials
-   * 
-   * Supposed to change the state of the client-server session. NOP for 
-   * stateless authentication (e.g. HTTP basic). 
-   * 
+   *
+   * Supposed to change the state of the client-server session. NOP for
+   * stateless authentication (e.g. HTTP basic).
+   *
    * `request` must be treated as read-only by the code. `sessionState` is this
    * plugin's private storage within the session (if stateful)
-   * 
-   * If auth doesn't fail, should return an object containing at least 
+   *
+   * If auth doesn't fail, should return an object containing at least
    * { success: true }. Should not reject the promise.
    */
   authenticate(request, sessionState) {
     return new Promise((resolve, reject) => {
       const gatewayUrl = this.gatewayUrl;
-      const authUrl = gatewayUrl + "/api/v1/gateway/auth/login";
-      const body = {
+      const data = JSON.stringify({
         username: request.body.username,
         password: request.body.password
-      };
-      axios.post(authUrl, body, { httpsAgent: this.httpsAgent }).then(function (response) {
-        let apimlCookie;
-        if (response.status == 200) {
-          if (typeof response.headers['set-cookie'] === 'object') {
-            for (const cookie of response.headers['set-cookie']) {
-              const content = cookie.split(';')[0];
-              if (content.indexOf('apimlAuthenticationToken') >= 0) {
-                apimlCookie = content;
+      });
+      const options = {
+        hostname: this.apimlConf.hostname,
+        port: this.apimlConf.gatewayPort,
+        path: '/api/v1/gateway/auth/login',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': data.length
+        },
+        agent: this.httpsAgent
+      }
+
+      const req = https.request(options, (res) => {
+        res.on('data', (d) => {
+          let apimlCookie;
+          if (res.statusCode == 200) {
+            if (typeof res.headers['set-cookie'] === 'object') {
+              for (const cookie of res.headers['set-cookie']) {
+                const content = cookie.split(';')[0];
+                if (content.indexOf('apimlAuthenticationToken') >= 0) {
+                  apimlCookie = content;
+                }
               }
             }
           }
-        }
-        if (apimlCookie) {
-          sessionState.apimlUsername = request.body.username;
-          sessionState.authenticated = true;
-          sessionState.apimlCookie = apimlCookie;
-          sessionState.apimlToken = apimlCookie.split("=")[1];
-          sessionState.gatewayUrl = gatewayUrl;
-          resolve({ success: true });
-        } else {
-          deleteApimlAuthFromSession(sessionState);
-          resolve({
-            success: false,
-            error: {
-              message: `${response.status} ${response.statusText}`
-            }
-          })
-        }
-      }).catch(function (error) {
+
+          if (apimlCookie) {
+            sessionState.apimlUsername = request.body.username;
+            sessionState.authenticated = true;
+            sessionState.apimlCookie = apimlCookie;
+            sessionState.apimlToken = apimlCookie.split("=")[1];
+            sessionState.gatewayUrl = gatewayUrl;
+            resolve({ success: true });
+          } else {
+            deleteApimlAuthFromSession(sessionState);
+            resolve({
+              success: false,
+              error: {
+                message: `${res.statusCode} ${res.statusMessage}`
+              }
+            })
+          }
+        });
+      });
+
+      req.on('error', (error) => {
+        console.log("APIML login has failed:");
+        console.log(error);
         var details = error.message;
         if ((error.response !== undefined) && (error.response.data !== undefined)) {
           details = error.response.data;
@@ -114,21 +140,24 @@ ApimlAuthenticator.prototype = {
           success: false,
           error: details
         });
-      })
+      });
+
+      req.write(data);
+      req.end();
     });
   },
 
   /**
    * Invoked for every service call by the middleware.
-   * 
+   *
    * Checks if the session is valid in a stateful scheme, or authenticates the
    * request in a stateless scheme. Then checks if the user can access the
    * resource.  Modifies the request if necessary.
-   * 
-   * `sessionState` is this plugin's private storage within the session (if 
+   *
+   * `sessionState` is this plugin's private storage within the session (if
    * stateful)
-   * 
-   * The promise should resolve to an object containing, at least, 
+   *
+   * The promise should resolve to an object containing, at least,
    * { authorized: true } if everything is fine. Should not reject the promise.
    */
   authorized(request, sessionState) {
