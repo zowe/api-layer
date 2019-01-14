f13fd6ac34a8b16748a0db9f06e8dbb9228ddd54
Andrea Tabone <andrea.tabone@ca.com>
2019-01-14 14:23:29 +0100

    Fix controller
    Signed-off-by: Vasyl Melnychuk <vasyl.melnychuk@broadcom.com>

--- discoverable-client/src/main/java/com/ca/mfaas/client/controller/controllers/api/MultipartController.java
+++ discoverable-client/src/main/java/com/ca/mfaas/client/controller/controllers/api/MultipartController.java
@@ -11,18 +11,14 @@ package com.ca.mfaas.client.controller.controllers.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
 public class MultipartController {
-    @RequestMapping(value = "api/v1/multipart", method = RequestMethod.PUT,
-        headers = "content-type=multipart/form-data",
-        consumes = { "multipart/form-data" })
+    @PutMapping(value = "api/v1/multipart",
+        headers = "content-type=multipart/form-data")
     public String submit(@RequestParam("file") MultipartFile file, ModelMap modelMap) {
         modelMap.addAttribute("file", file);
         return "fileUploadView";
