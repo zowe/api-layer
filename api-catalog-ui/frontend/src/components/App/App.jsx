0acd5a118aa47a75e71b1eae48fea0a8bdd4ff75
Vojta Oupicky <vojtech.oupicky@broadcom.com>
2019-01-24 13:57:32 +0100

    Show header and footer only on selected routes

--- api-catalog-ui/frontend/src/components/App/App.jsx
+++ api-catalog-ui/frontend/src/components/App/App.jsx
@@ -4,7 +4,6 @@ import { ToastContainer } from 'react-toastify';
 import BigShield from '../ErrorBoundary/BigShield/BigShield';
 import ErrorContainer from '../Error/ErrorContainer';
 import '../../webflow.css';
-import './App.css';
 import '../../assets/css/APIMReactToastify.css';
 import PageNotFound from '../PageNotFound/PageNotFound';
 import HeaderContainer from '../Header/HeaderContainer';
@@ -16,55 +15,56 @@ import Footer from '../Footer/Footer';
 
 class App extends Component {
     render() {
-        const { history, authentication } = this.props;
+        const { history } = this.props;
         const isLoading = true;
         return (
             <div className="App">
                 <BigShield history={history}>
                     <div className="content">
-                        {authentication.showHeader !== undefined &&
-                            authentication.showHeader === true && <HeaderContainer />}
                         <ToastContainer />
                         <ErrorContainer />
                         <Suspense fallback={<Spinner isLoading={isLoading} />}>
                             <Router history={history}>
-                                <Switch>
-                                    <Route path="/" exact render={() => <Redirect replace to="/login" />} />
-                                    <Route
-                                        path="/login"
-                                        exact
-                                        render={(props, state) => <LoginContainer {...props} {...state} />}
-                                    />
-                                    <Route
-                                        exact
-                                        path="/dashboard"
-                                        render={(props, state) => (
-                                            <BigShield>
-                                                <DashboardContainer {...props} {...state} />
-                                            </BigShield>
-                                        )}
-                                    />
-                                    <Route
-                                        path="/tile/:tileID"
-                                        render={(props, state) => (
-                                            <BigShield history={history}>
-                                                <DetailPageContainer {...props} {...state} />
-                                            </BigShield>
-                                        )}
-                                    />
-                                    <Route
-                                        render={(props, state) => (
-                                            <BigShield history={history}>
-                                                <PageNotFound {...props} {...state} />
-                                            </BigShield>
-                                        )}
-                                    />
-                                </Switch>
+                                <>
+                                    <Route path="/(dashboard|tile/.*)/" component={HeaderContainer} />
+                                    <Switch>
+                                        <Route path="/" exact render={() => <Redirect replace to="/login" />} />
+                                        <Route
+                                            path="/login"
+                                            exact
+                                            render={(props, state) => <LoginContainer {...props} {...state} />}
+                                        />
+                                        <Route
+                                            exact
+                                            path="/dashboard"
+                                            render={(props, state) => (
+                                                <BigShield>
+                                                    <DashboardContainer {...props} {...state} />
+                                                </BigShield>
+                                            )}
+                                        />
+                                        <Route
+                                            path="/tile/:tileID"
+                                            render={(props, state) => (
+                                                <BigShield history={history}>
+                                                    <DetailPageContainer {...props} {...state} />
+                                                </BigShield>
+                                            )}
+                                        />
+                                        <Route
+                                            render={(props, state) => (
+                                                <BigShield history={history}>
+                                                    <PageNotFound {...props} {...state} />
+                                                </BigShield>
+                                            )}
+                                        />
+                                    </Switch>
+                                    <Route path="/(dashboard|tile/.*)/" component={Footer} />
+                                </>
                             </Router>
-
                         </Suspense>
                     </div>
-                    {authentication.showHeader !== undefined && authentication.showHeader === true && <Footer />}
+                    {/* {authentication.showHeader !== undefined && authentication.showHeader === true && <Footer />} */}
                 </BigShield>
             </div>
         );
