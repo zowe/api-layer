b748a5bc58da7fc6032be8893e5bb94c57092fc2
oupvo01 <42371237+oupvo01@users.noreply.github.com>
2019-01-25 22:22:04 +0100

    Never show header on login screen (#212)
    * Allow easier mocked backend development
    * Show header and footer only on selected routes
    * Footer always at the bottom of the page
    * Cleanup AppContainer
    * Add test

--- api-catalog-ui/frontend/src/components/App/AppContainer.jsx
+++ api-catalog-ui/frontend/src/components/App/AppContainer.jsx
@@ -1,16 +1,4 @@
-import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import App from './App';
 
-const mapStateToProps = state => ({
-    authentication: state.authenticationReducer,
-});
-
-const mapDispatchToProps = {};
-
-export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(App)
-);
+export default withRouter(App);
