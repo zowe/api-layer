b2b4288b4b3462899cd998f6bd5676adcd3a2334
Ilkin <ilkinabdullayev6@gmail.com>
2019-01-29 14:01:26 +0100

    Add RestTemplate to the config bean
    Signed-off-by: Ilkin <ilkinabdullayev6@gmail.com>

--- api-catalog-services/src/main/java/com/ca/mfaas/apicatalog/config/BeanConfig.java
+++ api-catalog-services/src/main/java/com/ca/mfaas/apicatalog/config/BeanConfig.java
@@ -13,11 +13,24 @@ import com.ca.mfaas.error.ErrorService;
 import com.ca.mfaas.error.impl.ErrorServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
 
 @Configuration
 public class BeanConfig {
     @Bean
     public ErrorService errorService() {
         return new ErrorServiceImpl("/messages.yml");
+    }
+
+    @Bean
+    public RestTemplate apiRestClient() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters()
+            .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+
+        return restTemplate;
     }
 }
