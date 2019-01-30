c54c64779dbf3de1746460b43b6278c8695594c0
Ilkin <ilkin.abdullayev@broadcom.com>
2019-01-30 13:43:19 +0100

    hotfix/utf8 problem (#221)
    * Add RestTemplate to the config bean
    Signed-off-by: Ilkin <ilkinabdullayev6@gmail.com>

--- api-catalog-services/src/main/java/com/ca/mfaas/apicatalog/services/initialisation/InstanceRetrievalService.java
+++ api-catalog-services/src/main/java/com/ca/mfaas/apicatalog/services/initialisation/InstanceRetrievalService.java
@@ -27,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.retry.RetryException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
@@ -35,8 +36,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.constraints.NotBlank;
-
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Collection;
@@ -66,6 +67,8 @@ public class InstanceRetrievalService {
         this.propertiesContainer = propertiesContainer;
         this.cachedServicesService = cachedServicesService;
         this.restTemplate = restTemplate;
+
+        configureUnicode(restTemplate);
     }
 
     /**
@@ -173,7 +176,7 @@ public class InstanceRetrievalService {
 
         Pair<String, Pair<String, String>> requestInfo = constructServiceInfoQueryRequest(null, false);
 
-        // call Eureka REST endpoint to fetch single or all Instances
+        //  call Eureka REST endpoint to fetch single or all Instances
         ResponseEntity<String> response = queryDiscoveryForInstances(requestInfo);
 
         return extractApplications(requestInfo, response);
@@ -367,4 +370,8 @@ public class InstanceRetrievalService {
         log.info("API Catalog initialised with running services..");
     }
 
+    private void configureUnicode(RestTemplate restTemplate) {
+        restTemplate.getMessageConverters()
+            .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+    }
 }
