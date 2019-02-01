4b1de2d808f35af922fda3403e096d0c1833e7ea
Ilkin <ilkinabdullayev6@gmail.com>
2019-02-01 18:36:25 +0100

    Add unit tests
    Signed-off-by: Ilkin <ilkinabdullayev6@gmail.com>

--- api-catalog-services/src/test/java/com/ca/mfaas/apicatalog/services/status/LocalApiDocServiceTest.java
+++ api-catalog-services/src/test/java/com/ca/mfaas/apicatalog/services/status/LocalApiDocServiceTest.java
@@ -13,7 +13,11 @@ import com.ca.mfaas.apicatalog.services.initialisation.InstanceRetrievalService;
 import com.ca.mfaas.apicatalog.services.status.model.ApiDocNotFoundException;
 import com.ca.mfaas.product.constants.CoreService;
 import com.ca.mfaas.product.registry.CannotRegisterServiceException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.netflix.appinfo.InstanceInfo;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -24,14 +28,18 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class LocalApiDocServiceTest {
+
+    private static final String SERVICE1_ID = "service-1";
 
     @Mock
     private RestTemplate restTemplate;
@@ -43,6 +51,67 @@ public class LocalApiDocServiceTest {
     @InjectMocks
     private APIDocRetrievalService apiDocRetrievalService;
 
+    private InstanceInfo expectedGatewayInstance;
+    private InstanceInfo expectedServiceOneInstance;
+
+    @Before
+    public void setup() {
+        expectedGatewayInstance = getStandardInstance(CoreService.GATEWAY.getServiceId(), InstanceInfo.InstanceStatus.UP);
+        expectedServiceOneInstance = getStandardInstance(SERVICE1_ID, InstanceInfo.InstanceStatus.UP);
+    }
+
+    @Test
+    public void givenInstance_whenMetadataHasNotSwaggerURL_thenCheckParamsByMetadata() {
+        when(instanceRetrievalService.getInstanceInfo(CoreService.GATEWAY.getServiceId()))
+            .thenReturn(expectedGatewayInstance);
+
+        expectedServiceOneInstance.getMetadata().put("mfaas.discovery.service.title", "title");
+        expectedServiceOneInstance.getMetadata().put("mfaas.discovery.service.description", "description");
+
+        expectedServiceOneInstance.getMetadata().put("apiml.apiInfo.1.gatewayUrl", "/gatewayUrl");
+        expectedServiceOneInstance.getMetadata().put("apiml.apiInfo.1.version", "version");
+        expectedServiceOneInstance.getMetadata().put("apiml.apiInfo.1.documentationUrl", "/documentationUrl");
+
+        when(instanceRetrievalService.getInstanceInfo(SERVICE1_ID)).thenReturn(expectedServiceOneInstance);
+
+        ResponseEntity<String> actualResponse = apiDocRetrievalService.retrieveApiDoc(SERVICE1_ID, null);
+        assertNotNull(actualResponse);
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+
+        JsonObject jsonObject = new JsonParser().parse(actualResponse.getBody()).getAsJsonObject();
+        assertEquals("title", jsonObject.get("info").getAsJsonObject().get("title").getAsString());
+        assertEquals("description", jsonObject.get("info").getAsJsonObject().get("description").getAsString());
+        assertEquals("version", jsonObject.get("info").getAsJsonObject().get("version").getAsString());
+        assertEquals("/documentationUrl", jsonObject.get("externalDocs").getAsJsonObject().get("url").getAsString());
+        assertEquals("/gatewayUrl/" + SERVICE1_ID, jsonObject.get("basePath").getAsString());
+    }
+
+    @Test
+    public void givenInstance_whenMetadataHasSwaggerURL_thenCheckParamsByApiDoc() {
+        when(instanceRetrievalService.getInstanceInfo(CoreService.GATEWAY.getServiceId()))
+            .thenReturn(expectedGatewayInstance);
+
+        expectedServiceOneInstance.getMetadata().put("routed-services.api-v1.service-url", "/swaggerId/api/v1");
+        expectedServiceOneInstance.getMetadata().put("apiml.apiInfo.1.gatewayUrl", "api/v1");
+        expectedServiceOneInstance.getMetadata().put("apiml.apiInfo.1.swaggerUrl", "swaggerUrl");
+
+        when(instanceRetrievalService.getInstanceInfo(SERVICE1_ID)).thenReturn(expectedServiceOneInstance);
+
+        String url =  "https://localhost:9090/api/v1/api-doc/swaggerId/";
+        HttpEntity<Object> httpEntity = getObjectHttpEntity();
+        when(restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class))
+            .thenReturn(
+                new ResponseEntity<>("{\"basePath\":\"/api/v1/swaggerId\"}", HttpStatus.OK)
+            );
+
+        ResponseEntity<String> actualResponse = apiDocRetrievalService.retrieveApiDoc(SERVICE1_ID, "v1");
+        assertNotNull(actualResponse);
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+
+        String expectedApiDoc = String.format("{\"basePath\":\"/api/v1/%s\"}", SERVICE1_ID);
+        assertEquals(expectedApiDoc, actualResponse.getBody());
+    }
+
     @Test
     public void testRetrievalOfAPIDoc() {
         HttpEntity<Object> httpEntity = getObjectHttpEntity();
@@ -51,8 +120,7 @@ public class LocalApiDocServiceTest {
         ResponseEntity<String> expectedResponse = new ResponseEntity<>(apiDoc, HttpStatus.OK);
 
         when(instanceRetrievalService.getInstanceInfo(CoreService.GATEWAY.getServiceId()))
-            .thenReturn(
-                getStandardInstance(CoreService.GATEWAY.getServiceId(), InstanceInfo.InstanceStatus.UP));
+            .thenReturn(expectedGatewayInstance);
 
         String url =  "https://localhost:9090/api/v1/api-doc/service1";
         when(
@@ -71,8 +139,7 @@ public class LocalApiDocServiceTest {
         ResponseEntity<String> expectedResponse = new ResponseEntity<>(apiDoc, HttpStatus.NOT_FOUND);
 
         when(instanceRetrievalService.getInstanceInfo(CoreService.GATEWAY.getServiceId()))
-            .thenReturn(
-                getStandardInstance(CoreService.GATEWAY.getServiceId(), InstanceInfo.InstanceStatus.UP));
+            .thenReturn(expectedGatewayInstance);
 
         String url =  "https://localhost:9090/api/v1/api-doc/service1";
         when(
@@ -89,8 +156,7 @@ public class LocalApiDocServiceTest {
         ResponseEntity<String> expectedResponse = new ResponseEntity<>(apiDoc, HttpStatus.INTERNAL_SERVER_ERROR);
 
         when(instanceRetrievalService.getInstanceInfo(CoreService.GATEWAY.getServiceId()))
-            .thenReturn(
-                getStandardInstance(CoreService.GATEWAY.getServiceId(), InstanceInfo.InstanceStatus.UP));
+            .thenReturn(expectedGatewayInstance);
 
         String url =  "https://localhost:9090/api/v1/api-doc/service1";
         when(
@@ -108,8 +174,8 @@ public class LocalApiDocServiceTest {
     }
 
     private InstanceInfo getStandardInstance(String serviceId, InstanceInfo.InstanceStatus status) {
-        return new InstanceInfo(serviceId, null, null, "192.168.0.1", null, new InstanceInfo.PortWrapper(true, 9090),
+        return new InstanceInfo(serviceId, serviceId, null, "192.168.0.1", null, new InstanceInfo.PortWrapper(true, 9090),
                 new InstanceInfo.PortWrapper(true, 9090), "https://localhost:9090/", null, null, null, "localhost", "localhost", 0, null,
-                "localhost", status, null, null, null, null, null, null, null, null, null);
+                "localhost", status, null, null, null, null, new HashMap<>(), null, null, null, null);
     }
 }
