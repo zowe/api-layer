9bca303d766697364870cb62937933da1d27a8f9
Vasyl Melnychuk <vasyl.melnychuk@broadcom.com>
2019-03-05 02:59:00 +0100

    add code checks for gateway-common library
    Signed-off-by: Vasyl Melnychuk <vasyl.melnychuk@broadcom.com>

--- libraries/gateway-common/build.gradle.kts
+++ libraries/gateway-common/build.gradle.kts
@@ -1,5 +1,52 @@
 plugins {
     java
+    checkstyle
+    jacoco
+    id("com.github.spotbugs") version "1.6.10"
+    pmd
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+checkstyle {
+    toolVersion = "8.15"
+    configFile = file("codequality/checkstyle/checkstyle.xml")
+    isIgnoreFailures = true
+}
+
+jacoco {
+    toolVersion = "0.8.3"
+    reportsDir = file("$buildDir/reports/jacoco")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        html.isEnabled = true
+        html.destination = file("$buildDir/reports/jacoco")
+    }
+}
+
+tasks {
+    jacocoTestCoverageVerification {
+        violationRules {
+            rule { limit { minimum = BigDecimal.valueOf(0.0) } }
+        }
+    }
+    check {
+        dependsOn(jacocoTestCoverageVerification)
+    }
+}
+
+spotbugs {
+    isIgnoreFailures = true
+}
+
+
+pmd {
+    isIgnoreFailures = true
 }
 
 repositories {
