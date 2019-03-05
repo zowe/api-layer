2d3fdf5409b88f83e7cbfa744b43b1c7ad3a1f7a
Vasyl Melnychuk <vasyl.melnychuk@broadcom.com>
2019-03-05 02:32:29 +0100

    add security module
    Signed-off-by: Vasyl Melnychuk <vasyl.melnychuk@broadcom.com>

---
+++ libraries/security-library/src/main/java/com/broadcom/apiml/library/security/login/LoginFilter.java
@@ -0,0 +1,90 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package com.broadcom.apiml.library.security.login;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * Filter for process authentication request with username and password in JSON format.
+ */
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(LoginFilter.class);
+    private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
+    private final ObjectMapper mapper;
+
+    public LoginFilter(
+        String authEndpoint,
+        AuthenticationSuccessHandler successHandler,
+        AuthenticationFailureHandler failureHandler,
+        ObjectMapper mapper,
+        AuthenticationManager authenticationManager) {
+        super(authEndpoint);
+        this.successHandler = successHandler;
+        this.failureHandler = failureHandler;
+        this.mapper = mapper;
+        this.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+        throws AuthenticationException {
+        if (!request.getMethod().equals(HttpMethod.POST.name())) {
+            throw new AuthMethodNotSupportedException("Authentication method not supported");
+        }
+
+        LoginRequest loginRequest = null;
+        try {
+            loginRequest = mapper.readValue(request.getInputStream(), LoginRequest.class);
+        } catch (IOException e) {
+            logger.debug("Authentication problem: login object has wrong format");
+            throw new BadCredentialsException("Login object has wrong format");
+        }
+
+        if (StringUtils.isBlank(loginRequest.getUsername()) || StringUtils.isBlank(loginRequest.getPassword())) {
+            throw new BadCredentialsException("Username or password not provided");
+        }
+
+        UsernamePasswordAuthenticationToken authentication
+            = new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword(), Collections.emptyList());
+        return this.getAuthenticationManager().authenticate(authentication);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        successHandler.onAuthenticationSuccess(request, response, authResult);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        SecurityContextHolder.clearContext();
+        failureHandler.onAuthenticationFailure(request, response, failed);
+    }
+}
---
+++ libraries/security-library/src/main/java/com/broadcom/apiml/library/security/token/TokenAuthentication.java
@@ -0,0 +1,73 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package com.broadcom.apiml.library.security.token;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+import java.util.Collections;
+
+/**
+ * This object is added to security context after successful authentication.
+ * Contains username nad valid JWT token.
+ */
+public class TokenAuthentication extends AbstractAuthenticationToken {
+    private final String username;
+    private final String token;
+
+    public TokenAuthentication(String token) {
+        super(Collections.emptyList());
+        this.username = null;
+        this.token = token;
+    }
+
+    public TokenAuthentication(String username, String token) {
+        super(Collections.emptyList());
+        this.username = username;
+        this.token = token;
+    }
+
+    @Override
+    public String getCredentials() {
+        return token;
+    }
+
+    @Override
+    public String getPrincipal() {
+        return username;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof TokenAuthentication)) return false;
+        final TokenAuthentication other = (TokenAuthentication) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$username = this.username;
+        final Object other$username = other.username;
+        if (this$username == null ? other$username != null : !this$username.equals(other$username)) return false;
+        final Object this$token = this.token;
+        final Object other$token = other.token;
+        if (this$token == null ? other$token != null : !this$token.equals(other$token)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof TokenAuthentication;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $username = this.username;
+        result = result * PRIME + ($username == null ? 43 : $username.hashCode());
+        final Object $token = this.token;
+        result = result * PRIME + ($token == null ? 43 : $token.hashCode());
+        return result;
+    }
+}
