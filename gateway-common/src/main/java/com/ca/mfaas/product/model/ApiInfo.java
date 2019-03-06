a4a11c651e30fb851af4ab215d2f53a483ca780c
Andrea Tabone <andrea.tabone@broadcom.com>
2019-03-06 15:29:22 +0100

    Fix homepage link not displayed for static client and added code from ApiRetrevial branch
    Signed-off-by: Andrea Tabone <andrea.tabone@ca.com>

--- gateway-common/src/main/java/com/ca/mfaas/product/model/ApiInfo.java
+++ gateway-common/src/main/java/com/ca/mfaas/product/model/ApiInfo.java
@@ -10,14 +10,71 @@
 package com.ca.mfaas.product.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang.StringEscapeUtils;
 
-/** Represents one API provided by a service */
-@Data public class ApiInfo {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Represents one API provided by a service
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ApiInfo {
     @JsonProperty(required = true)
     private String apiId;
     private String gatewayUrl;
     private String version;
     private String swaggerUrl;
     private String documentationUrl;
+
+    /**
+     * Generate Eureka metadata for ApiInfo configuration
+     *
+     * @param serviceId the identifier of a service which ApiInfo configuration belongs
+     * @return the generated Eureka metadata
+     */
+    public Map<String, String> generateMetadata(String serviceId) {
+        Map<String, String> metadata = new HashMap<>();
+        String encodedGatewayUrl = gatewayUrl.replaceAll("\\W", "-");
+
+        if (gatewayUrl != null) {
+            metadata.put(String.format("apiml.apiInfo.%s.gatewayUrl", encodedGatewayUrl), gatewayUrl);
+        }
+
+        if (version != null) {
+            metadata.put(String.format("apiml.apiInfo.%s.version", encodedGatewayUrl), version);
+        }
+
+        if (swaggerUrl != null) {
+            try {
+                new URL(swaggerUrl);
+            } catch (MalformedURLException e) {
+                throw new InvalidParameterException(
+                    String.format("The Swagger URL \"%s\" for service %s is not valid: %s",
+                        serviceId, swaggerUrl, e.getMessage()));
+            }
+            metadata.put(String.format("apiml.apiInfo.%s.swaggerUrl", encodedGatewayUrl), swaggerUrl);
+        }
+
+        if (documentationUrl != null) {
+            try {
+                new URL(documentationUrl);
+            } catch (MalformedURLException e) {
+                throw new InvalidParameterException(
+                    String.format("The documentation URL \"%s\" for service %s is not valid: %s",
+                        serviceId, documentationUrl, e.getMessage()));
+            }
+            metadata.put(String.format("apiml.apiInfo.%s.documentationUrl", encodedGatewayUrl), documentationUrl);
+        }
+
+        return metadata;
+    }
 }
