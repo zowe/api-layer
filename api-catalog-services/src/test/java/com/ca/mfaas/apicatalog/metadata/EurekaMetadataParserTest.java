addb8a4c3fd14bd02712a5ca2b689af2dafed538
Zikos, Christos <Christos.Zikos@ca.com>
2019-03-11 16:50:43 +0100

    Remove unused import
    Signed-off-by: Zikos, Christos <Christos.Zikos@ca.com>

--- api-catalog-services/src/test/java/com/ca/mfaas/apicatalog/metadata/EurekaMetadataParserTest.java
+++ api-catalog-services/src/test/java/com/ca/mfaas/apicatalog/metadata/EurekaMetadataParserTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class EurekaMetadataParserTest {
 
