ce2b79aa1e495f509718648e9012f324986068b0
Andrea Tabone <andrea.tabone@ca.com>
2019-03-29 18:16:17 +0100

    Unit tests for TansformService
    Signed-off-by: Andrea Tabone <andrea.tabone@ca.com>

--- gateway-common/src/test/java/com/ca/mfaas/product/routing/TransformServiceTest.java
+++ gateway-common/src/test/java/com/ca/mfaas/product/routing/TransformServiceTest.java
@@ -11,25 +11,28 @@
 package com.ca.mfaas.product.routing;
 
 import com.ca.mfaas.product.gateway.GatewayConfigProperties;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TransformServiceTest {
 
+    public static final String UI_PREFIX = "ui";
+    public static final String API_PREFIX = "API";
+    public static final String WS_PREFIX = "ws";
+
     @Test
-    public void transformURL() {
-        //String url = "httrps://localhost:8080/ui";
-        String url = "httfbdffvx";
+    public void shouldTransformURL() {
+        String url = "https://localhost:8080/ui";
         GatewayConfigProperties gateway = GatewayConfigProperties.builder()
             .scheme("https")
-            .hostname("gwhost")
+            .hostname("localhost")
             .build();
-        String prefix = "u";
         String serviceId = "service";
 
         RoutedServices routedServices = new RoutedServices();
-        RoutedService routedService1 = new RoutedService(serviceId, prefix, "/ui");
+        RoutedService routedService1 = new RoutedService(serviceId, UI_PREFIX, "/ui");
         RoutedService routedService2 = new RoutedService(serviceId, "api/v1", "/");
         routedServices.addRoutedService(routedService1);
         routedServices.addRoutedService(routedService2);
@@ -40,8 +43,109 @@ public class TransformServiceTest {
         String expectedUrl = String.format("%s://%s/%s/%s",
             gateway.getScheme(),
             gateway.getHostname(),
-            prefix,
+            UI_PREFIX,
             serviceId);
         assertEquals(expectedUrl, actualUrl);
     }
+
+    @Test
+    public void shouldUseOriginalUrl_IfRouteNotFound() {
+        String url = "https://localhost:8080/u";
+        GatewayConfigProperties gateway = GatewayConfigProperties.builder()
+            .scheme("https")
+            .hostname("localhost")
+            .build();
+        String serviceId = "service";
+
+        RoutedServices routedServices = new RoutedServices();
+        RoutedService routedService1 = new RoutedService(serviceId, UI_PREFIX, "/ui");
+        RoutedService routedService2 = new RoutedService(serviceId, "api/v1", "/");
+        routedServices.addRoutedService(routedService1);
+        routedServices.addRoutedService(routedService2);
+
+        TransformService transformService = new TransformService(gateway);
+        String actualUrl = transformService.transformURL(ServiceType.UI, serviceId, url, routedServices);
+
+        assertEquals(url, actualUrl);
+    }
+
+    @Test
+    public void shouldSelectWsRoute() {
+        String url = "https://localhost:8080/ws";
+        GatewayConfigProperties gateway = GatewayConfigProperties.builder()
+            .scheme("https")
+            .hostname("localhost")
+            .build();
+        String serviceId = "service";
+
+        RoutedServices routedServices = new RoutedServices();
+        RoutedService routedService1 = new RoutedService(serviceId, WS_PREFIX, "/ws");
+        RoutedService routedService2 = new RoutedService(serviceId, "api/v1", "/");
+        routedServices.addRoutedService(routedService1);
+        routedServices.addRoutedService(routedService2);
+
+        TransformService transformService = new TransformService(gateway);
+        String actualUrl = transformService.transformURL(ServiceType.WS, serviceId, url, routedServices);
+
+        String expectedUrl = String.format("%s://%s/%s/%s",
+            gateway.getScheme(),
+            gateway.getHostname(),
+            WS_PREFIX,
+            serviceId);
+        assertEquals(expectedUrl, actualUrl);
+    }
+
+    @Test
+    public void shouldSelectApiRoute() {
+        String url = "https://localhost:8080/api";
+        GatewayConfigProperties gateway = GatewayConfigProperties.builder()
+            .scheme("https")
+            .hostname("localhost")
+            .build();
+        String serviceId = "service";
+
+        RoutedServices routedServices = new RoutedServices();
+        RoutedService routedService1 = new RoutedService(serviceId, API_PREFIX, "/api");
+        RoutedService routedService2 = new RoutedService(serviceId, "api/v1", "/");
+        routedServices.addRoutedService(routedService1);
+        routedServices.addRoutedService(routedService2);
+
+        TransformService transformService = new TransformService(gateway);
+        String actualUrl = transformService.transformURL(ServiceType.API, serviceId, url, routedServices);
+
+        String expectedUrl = String.format("%s://%s/%s/%s",
+            gateway.getScheme(),
+            gateway.getHostname(),
+            API_PREFIX,
+            serviceId);
+        assertEquals(expectedUrl, actualUrl);
+    }
+
+
+    @Ignore
+    public void shouldSelectOriginalUrl_IfPathdIsNotValid() {
+        String url = "https://localhost:8080/wss";
+        GatewayConfigProperties gateway = GatewayConfigProperties.builder()
+            .scheme("https")
+            .hostname("localhost")
+            .build();
+        String serviceId = "service";
+
+        RoutedServices routedServices = new RoutedServices();
+        RoutedService routedService1 = new RoutedService(serviceId, WS_PREFIX, "/ws");
+        RoutedService routedService2 = new RoutedService(serviceId, "api/v1", "/");
+        routedServices.addRoutedService(routedService1);
+        routedServices.addRoutedService(routedService2);
+
+        TransformService transformService = new TransformService(gateway);
+        String actualUrl = transformService.transformURL(ServiceType.WS, serviceId, url, routedServices);
+
+        String expectedUrl = String.format("%s://%s/%s/%s",
+            gateway.getScheme(),
+            gateway.getHostname(),
+            WS_PREFIX,
+            serviceId);
+        assertEquals(expectedUrl, actualUrl);
+    }
+
 }
