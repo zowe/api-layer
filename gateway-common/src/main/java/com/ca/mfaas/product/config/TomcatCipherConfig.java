e0b868d37f702669e085e89a5cd4ec679907523c
Seva Khanin <vsevolod.khanin@broadcom.com>
2019-04-01 12:16:35 +0200

    tls12 ciphers + setUseServerCipherSuitesOrder

---
+++ gateway-common/src/main/java/com/ca/mfaas/product/config/TomcatCipherConfig.java
@@ -0,0 +1,35 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package com.ca.mfaas.product.config;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration of Tomcat for the API Gateway.
+ */
+@Configuration
+public class TomcatCipherConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+        return (factory) -> {
+            (factory)
+                .addConnectorCustomizers((connector) -> {
+                    ((AbstractHttp11Protocol<?>) connector.getProtocolHandler())
+                        .setUseServerCipherSuitesOrder(Boolean.toString(true));
+                });
+        };
+    }
+
+}
