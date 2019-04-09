cb40acc42d917de9faa650bda8395aef3d84cfad
Andrea Tabone <andrea.tabone@broadcom.com>
2019-04-09 17:00:49 +0200

    Add unit test for TokenAuthenticationProvide
    Signed-off-by: Andrea Tabone <andrea.tabone@ca.com>

---
+++ gateway-service/src/test/java/com/ca/mfaas/gateway/security/token/TokenAuthenticationProviderTest.java
@@ -0,0 +1,59 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package com.ca.mfaas.gateway.security.token;
+
+import com.ca.apiml.security.token.TokenAuthentication;
+import com.ca.mfaas.gateway.security.service.AuthenticationService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TokenAuthenticationProviderTest {
+    private AuthenticationService tokenService;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        tokenService = mock(AuthenticationService.class);
+    }
+
+    @Test
+    public void authenticateWithValidToken() {
+        TokenAuthentication tokenAuthentication = new TokenAuthentication("token");
+
+        when(tokenService.validateJwtToken(tokenAuthentication)).thenReturn(tokenAuthentication);
+        TokenAuthenticationProvider authenticationProvider = new TokenAuthenticationProvider(tokenService);
+        Authentication authentication = authenticationProvider.authenticate(tokenAuthentication);
+
+        assertThat(authentication, is(tokenAuthentication));
+    }
+
+
+    @Test
+    public void supportsAuthentication() {
+        TokenAuthenticationProvider authenticationProvider = new TokenAuthenticationProvider(tokenService);
+
+        assertTrue(authenticationProvider.supports(TokenAuthentication.class));
+        assertFalse(authenticationProvider.supports(UsernamePasswordAuthenticationToken.class));
+    }
+}
