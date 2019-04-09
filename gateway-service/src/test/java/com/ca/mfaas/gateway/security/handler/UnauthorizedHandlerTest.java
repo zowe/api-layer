80afa9d72c6c95e38766ba48dde3df77da77a29f
Ilkin Abdullayev <ilkinabdullayev6@gmail.com>
2019-04-09 10:35:25 +0200

    Added unit test

---
+++ gateway-service/src/test/java/com/ca/mfaas/gateway/security/handler/UnauthorizedHandlerTest.java
@@ -0,0 +1,60 @@
+package com.ca.mfaas.gateway.security.handler;
+
+import com.ca.mfaas.error.ErrorService;
+import com.ca.mfaas.error.impl.ErrorServiceImpl;
+import com.ca.mfaas.rest.response.ApiMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+public class UnauthorizedHandlerTest {
+
+
+    @Autowired
+    private ErrorService errorService;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testCommence() throws IOException {
+        UnauthorizedHandler unauthorizedHandler = new UnauthorizedHandler(errorService, objectMapper);
+
+        MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
+        httpServletRequest.setRequestURI("URI");
+
+        MockHttpServletResponse httpServletResponse = new MockHttpServletResponse();
+
+        unauthorizedHandler.commence(httpServletRequest, httpServletResponse, null);
+
+        assertEquals(HttpStatus.UNAUTHORIZED.value(), httpServletResponse.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON_UTF8_VALUE, httpServletResponse.getContentType());
+
+
+        ApiMessage message = errorService.createApiMessage("com.ca.mfaas.security.authenticationRequired", httpServletRequest.getRequestURI());
+        verify(objectMapper).writeValue(httpServletResponse.getWriter(), message);
+    }
+
+    @Configuration
+    static class ContextConfiguration {
+        @Bean
+        public ErrorService errorService() {
+            return new ErrorServiceImpl("/gateway-messages.yml");
+        }
+    }
+}
