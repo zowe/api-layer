8031df56366a7b22009b9a559508d1b055a2ddd7
zhuxun <zhuxun@cn.ibm.com>
2019-04-17 14:00:26 +0800

    support mixed case serviceId
    Signed-off-by: zhuxun <zhuxun@cn.ibm.com>

--- integration-tests/src/test/java/com/ca/mfaas/discoveryservice/staticdef/StaticClientEndpointsTest.java
+++ integration-tests/src/test/java/com/ca/mfaas/discoveryservice/staticdef/StaticClientEndpointsTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertEquals;
 public class StaticClientEndpointsTest {
     private static final String GREET = "/api/v1/discoverableclient/greeting";
     private static final String STATIC_GREET = "/api/v1/staticclient/greeting";
+    private static final String MIXED_CASE_GREET = "/api/v1/StaticClient3/greeting";
 
     @Test
     public void shouldSameResponseAsDynamicService() throws Exception {
@@ -33,5 +34,15 @@ public class StaticClientEndpointsTest {
         // Then
         assertEquals(response.getStatusLine().getStatusCode(), staticResponse.getStatusLine().getStatusCode());
         assertEquals(hello, staticHello);
+    }
+
+    /**
+     * Test service with mixed case service ID
+     * Depends on service StaticClient3 to be defined in staticclient.yml
+     * @throws Exception
+     */
+    @Test
+    public void shouldMixedCaseServiceIdWork() throws Exception {
+        final HttpResponse staticResponse = HttpRequestUtils.getResponse(MIXED_CASE_GREET, SC_OK);
     }
 }
