e4abfc440ef222676b7fe995a71635146425e566
Zikos, Christos <Christos.Zikos@ca.com>
2019-05-27 15:28:32 +0200

    fix typo in readme try path without quotes
    Signed-off-by: Zikos, Christos <Christos.Zikos@ca.com>

--- scripts/apiml_cm.sh
+++ scripts/apiml_cm.sh
@@ -315,7 +315,7 @@ function jwt_key_gen_and_export {
     keytool -genkeypair $V -alias ${JWT_ALIAS} -keyalg RSA -keysize 2048 -keystore ${SERVICE_KEYSTORE}.p12 \
     -dname "${SERVICE_DNAME}" -keypass ${SERVICE_PASSWORD} -storepass ${SERVICE_PASSWORD} -storetype PKCS12 -validity ${SERVICE_VALIDITY}
     keytool -export -alias ${JWT_ALIAS} -keystore ${SERVICE_KEYSTORE}.p12 -storepass ${SERVICE_PASSWORD} -keypass ${SERVICE_PASSWORD} -storetype PKCS12 \
-    -file "keystore/localhost/jwtsecret.cer"
+    -file keystore/localhost/jwtsecret.cer
 }
 
 function export_jwt_key {
