805f936a5e6a9477cbab756f385d7190b04c409b
cZikos <christos.zikos@broadcom.com>
2019-05-29 13:23:48 +0200

    revert testing changes
    Signed-off-by: Zikos, Christos <Christos.Zikos@ca.com>

--- scripts/apiml_cm.test.js
+++ scripts/apiml_cm.test.js
@@ -57,10 +57,7 @@ describe(script, function() {
                 .exist('keystore/localhost/localhost.truststore.p12')
                 .exist('keystore/localhost/localhost.keystore.cer')
                 .exist('keystore/localhost/localhost.keystore.key')
-                // .exist('keystore/localhost/jwtsecret.cer')
-                .exec('pwd')
-                .exec('cd keystore/localhost/')
-                .exec('ls')
+                .exist('keystore/localhost/localhost.keystore.jwtsecret.cer')
                 .end(done);
         });
     });
