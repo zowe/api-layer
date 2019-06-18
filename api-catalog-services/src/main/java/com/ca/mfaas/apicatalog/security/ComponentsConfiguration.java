dfa7614a0caa3be8b417a8689af72069bd398403
janda06 <david.janda@broadcom.com>
2019-06-18 15:34:54 +0200

    cleanup of unused cookie configuration
    Signed-off-by: janda06 <david.janda@broadcom.com>

--- api-catalog-services/src/main/java/com/ca/mfaas/apicatalog/security/ComponentsConfiguration.java
+++ api-catalog-services/src/main/java/com/ca/mfaas/apicatalog/security/ComponentsConfiguration.java
@@ -9,9 +9,6 @@
  */
 package com.ca.mfaas.apicatalog.security;
 
-import com.ca.mfaas.security.token.CookieConfiguration;
-import com.ca.mfaas.security.token.TokenServiceConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,30 +21,4 @@ public class ComponentsConfiguration {
         return new BCryptPasswordEncoder(10);
     }
 
-    @Bean
-    public TokenServiceConfiguration tokenServiceConfiguration() {
-        String secret = "secret";
-        long expirationInSeconds = 60 * 60 * 12L; // 12 hours
-        String issuer = "api-catalog-services";
-
-        return TokenServiceConfiguration.builder()
-            .secret(secret)
-            .expirationInSeconds(expirationInSeconds)
-            .issuer(issuer)
-            .shortTtlUsername("expire")
-            .shortTtlExpiration(1)
-            .build();
-    }
-
-    @Bean
-    public CookieConfiguration cookieConfiguration(@Value("${security.cookie.maxAge:86400}") Integer maxAge,
-                                                   @Value("${security.cookie.secure:true}") Boolean secure) {
-        return CookieConfiguration.builder()
-            .name("apimlAuthenticationToken")
-            .secure(secure)
-            .path("/")
-            .comment("API Catalog security token")
-            .maxAge(maxAge)
-            .build();
-    }
 }
