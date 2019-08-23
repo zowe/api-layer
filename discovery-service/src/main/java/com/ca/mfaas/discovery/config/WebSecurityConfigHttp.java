f6ef85736820e25dec6215870c9a647dbdf2b7a1
janda06 <david.janda@broadcom.com>
2019-08-23 15:28:04 +0200

    Https security with different filterchains Http security with configurable basic integration test
    Signed-off-by: janda06 <david.janda@broadcom.com>

---
+++ discovery-service/src/main/java/com/ca/mfaas/discovery/config/WebSecurityConfigHttp.java
@@ -0,0 +1,62 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package com.ca.mfaas.discovery.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * Main class configuring Spring security for Discovery Service
+ *
+ * This configuration is applied if "https" Spring profile is not active
+ */
+@Configuration
+@EnableWebSecurity
+@Profile("!https")
+public class WebSecurityConfigHttp extends WebSecurityConfigurerAdapter {
+    private static final String DISCOVERY_REALM = "API Mediation Discovery Service realm";
+
+    @Value("${apiml.service.id:#{null}}")
+    private String serviceId;
+
+    @Value("${apiml.discovery.userid:eureka}")
+    private String eurekaUserid;
+
+    @Value("${apiml.discovery.password:password}")
+    private String eurekaPassword;
+
+    @Value("${apiml.security.ssl.verifySslCertificatesOfServices:true}")
+    private boolean verifySslCertificatesOfServices;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(eurekaUserid).password("{noop}" + eurekaPassword).roles("EUREKA");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .headers().httpStrictTransportSecurity().disable().and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().httpBasic().realmName(DISCOVERY_REALM)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/application/info", "/application/health").permitAll()
+            .antMatchers("/**").authenticated();
+    }
+}
