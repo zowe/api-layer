5a38b23b171f0bc5c79026d47ceb4ccd3047fdde
Vsevolod Khanin <vsevolod.khanin@broadcom.com>
2019-09-27 14:08:45 +0200

    Log level settings added to start scripts
    Signed-off-by: Vsevolod Khanin <vsevolod.khanin@broadcom.com>

--- zowe-install/src/main/resources/scripts/api-mediation-start-catalog-template.sh
+++ zowe-install/src/main/resources/scripts/api-mediation-start-catalog-template.sh
@@ -16,7 +16,49 @@ else
   echo "Done."
 fi
 
-LOG_LEVEL=$1
+PARAMS="$@"
+
+# The default log level is WARN
+# Select one of the levels: ERROR | WARN | INFO | DEBUG | TRACE
+LOG_LEVEL=WARN
+
+function usage {
+    echo "Set the log level for API Mediation Layer"
+    echo "usage: api-mediation-start-catalog.sh -level <level>"
+    echo ""
+    echo "  <level> level to be setup:"
+    echo "     - ERROR - setups APIML error level"
+    echo "     - WARN - setups APIML want level"
+    echo "     - INFO - setups APIML info level"
+    echo "     - DEBUG - setups APIML debug level"
+    echo "     - TRACE - setups APIML trace level"
+    echo ""
+    echo "  Called with: ${PARAMS}"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -l | --level )      shift
+                                LOG_LEVEL=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     echo "Unexpected parameter: $1"
+                                usage
+                                exit 1
+    esac
+    shift
+done
+
+case $LOG_LEVEL in
+    ERROR | WARN | INFO | DEBUG | TRACE )
+        LEVEL=$LOG_LEVEL
+        ;;
+    *)
+        usage
+        exit 1
+esac
 
 DIR=`dirname $0`
 
@@ -28,7 +70,7 @@ java -Xms16m -Xmx512m -Dibm.serversocket.recover=true -Dfile.encoding=UTF-8 \
     -Denvironment.eurekaPassword=password \
     -Dapiml.security.auth.zosmfServiceId=zosmf \
     -Dapiml.security.ssl.verifySslCertificatesOfServices=**VERIFY_CERTIFICATES** \
-    -Dspring.profiles.include="$LOG_LEVEL" \
+    -Dspring.profiles.include="$LEVEL" \
     -Dserver.address=0.0.0.0 \
     -Dserver.ssl.enabled=true \
     -Dserver.ssl.keyStore=$DIR/../keystore/localhost/localhost.keystore.p12 \
--- zowe-install/src/main/resources/scripts/api-mediation-start-gateway-template.sh
+++ zowe-install/src/main/resources/scripts/api-mediation-start-gateway-template.sh
@@ -16,7 +16,49 @@ else
   echo "Done."
 fi
 
-LOG_LEVEL=$1
+PARAMS="$@"
+
+# The default log level is WARN
+# Select one of the levels: ERROR | WARN | INFO | DEBUG | TRACE
+LOG_LEVEL=WARN
+
+function usage {
+    echo "Set the log level for API Mediation Layer"
+    echo "usage: api-mediation-start-gateway.sh -level <level>"
+    echo ""
+    echo "  <level> level to be setup:"
+    echo "     - ERROR - setups APIML error level"
+    echo "     - WARN - setups APIML want level"
+    echo "     - INFO - setups APIML info level"
+    echo "     - DEBUG - setups APIML debug level"
+    echo "     - TRACE - setups APIML trace level"
+    echo ""
+    echo "  Called with: ${PARAMS}"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -l | --level )      shift
+                                LOG_LEVEL=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     echo "Unexpected parameter: $1"
+                                usage
+                                exit 1
+    esac
+    shift
+done
+
+case $LOG_LEVEL in
+    ERROR | WARN | INFO | DEBUG | TRACE )
+        LEVEL=$LOG_LEVEL
+        ;;
+    *)
+        usage
+        exit 1
+esac
 
 DIR=$(dirname "$0")
 
@@ -24,7 +66,7 @@ java -Xms32m -Xmx256m -Xquickstart \
     -Dibm.serversocket.recover=true \
     -Dfile.encoding=UTF-8 \
     -Djava.io.tmpdir=/tmp \
-    -Dspring.profiles.include="$LOG_LEVEL" \
+    -Dspring.profiles.include="$LEVEL" \
     -Dapiml.service.hostname=**HOSTNAME** \
     -Dapiml.service.port=**GATEWAY_PORT** \
     -Dapiml.service.discoveryServiceUrls=https://**HOSTNAME**:**DISCOVERY_PORT**/eureka/ \
