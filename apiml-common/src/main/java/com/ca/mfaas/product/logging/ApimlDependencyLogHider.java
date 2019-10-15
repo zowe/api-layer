9dae640fbd2789c4510ad255ebe8fb1d04c92f24
Ilkin Abdullayev <ilkin.abdullayev@broadcom.com>
2019-10-15 09:38:12 +0200

    Messages are hidden
    Signed-off-by: janda06 <david.janda@broadcom.com>

---
+++ apiml-common/src/main/java/com/ca/mfaas/product/logging/ApimlDependencyLogHider.java
@@ -0,0 +1,54 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package com.ca.mfaas.product.logging;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.turbo.TurboFilter;
+import ch.qos.logback.core.spi.FilterReply;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Marker;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class ApimlDependencyLogHider extends TurboFilter {
+
+    private static final List<String> IGNORED_MESSAGE_KEYWORDS = Arrays.asList(
+        "Tomcat initialized",
+        "lease doesn't exist",
+        "Not Found (Renew)",
+        "route 53",
+        "dirty timestamp",
+        "Using the existing instanceInfo instead of the new instanceInfo as the registrant"
+        );
+
+
+    @Override
+    public FilterReply decide(Marker marker, Logger logger, Level level, String format, Object[] params, Throwable t) {
+
+        if (!isLowThanInfoLevel(logger.getEffectiveLevel())
+            && (format != null && isIgnoredMessage(format))) {
+            return FilterReply.DENY;
+        }
+
+        return FilterReply.NEUTRAL;
+    }
+
+    private boolean isLowThanInfoLevel(Level level) {
+        return level.levelInt < Level.INFO.levelInt;
+    }
+
+    private boolean isIgnoredMessage(String format) {
+        return IGNORED_MESSAGE_KEYWORDS.stream()
+            .anyMatch(keyword -> format.contains(keyword));
+    }
+}
