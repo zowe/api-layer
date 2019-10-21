6f74db9652ed5eab6ee6af878317e8333fa4acea
Andrea Tabone <andrea.tabone@broadcom.com>
2019-10-21 12:36:01 +0200

    Update obsolete unit tests with new messages codes
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- api-catalog-ui/frontend/cypress/integration/e2e/detail-page/detail-page.test.js
+++ api-catalog-ui/frontend/cypress/integration/e2e/detail-page/detail-page.test.js
@@ -3,7 +3,7 @@
 /// <reference types="Cypress" />
 
 function login() {
-    cy.visit(`${Cypress.env('baseURL')}ui/v1/apicatalog/#/`);
+    cy.visit(`${Cypress.env('catalogHomePage')}ui/v1/apicatalog/#/`);
     cy.url().should('contain', '/login');
 
     const username = Cypress.env('username');
@@ -23,22 +23,36 @@ describe('>>> Detail page test', () => {
 
         cy.contains('API Mediation Layer API').click();
 
-        cy.url().should('contain', '/tile/apimediationlayer/apicatalog');
+        cy.url().should('contain', ('/tile/apimediationlayer'));
 
         cy.get('#go-back-button').should('exist');
 
         cy.get('.api-description-container').should('exist');
 
+    });
+
+    it('Should display the Gateway information in the detail page', () => {
+
+        login();
+
+        cy.visit(`${Cypress.env('catalogHomePage')}#/tile/apimediationlayer/apicatalog`);
+
+        const baseUrl = Cypress.env('baseUrl');
+
+        cy.get('pre.base-url')
+            .should('exist')
+            .should('contain', `[ Base URL: ${baseUrl.match(/^https?:\/\/([^/?#]+)(?:[/?#]|$)/i)[1]}/api/v1/gateway ]`);
+
         cy.get('.tabs-container')
             .should('exist')
             .should('have.length', 1)
             .within($el => {
-                cy.get('a').should('contain', 'apicatalog');
+                cy.get('a').should('contain', 'gateway');
             });
 
         cy.contains('Service Homepage').should('exist');
 
-        cy.get('pre.version').should('contain', '1.0.0');
+        cy.get('pre.version').should('contain', '1.1.2');
 
         cy.contains('Swagger/OpenAPI JSON Document').should('exist');
 
