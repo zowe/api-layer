06be73b74df3b58dd193bdd03efd32cc89ac0c6d
Ilkin Abdullayev <ilkin.abdullayev@broadcom.com>
2019-10-29 13:51:14 +0100

    Route locator message hided

--- apiml-common/src/main/java/com/ca/mfaas/product/logging/ApimlDependencyLogHider.java
+++ apiml-common/src/main/java/com/ca/mfaas/product/logging/ApimlDependencyLogHider.java
@@ -24,13 +24,15 @@ public class ApimlDependencyLogHider extends TurboFilter {
 
     private static final List<String> IGNORED_MESSAGE_KEYWORDS = Arrays.asList(
         "Tomcat initialized",
+        "Tomcat started on port(s)",
         "lease doesn't exist", "Not Found (Renew)",
         "route 53",
         "eureka.server.peer-node-read-timeout-ms",
         "Found more than one MBeanServer instance",
         "dirty timestamp", "Using the existing instanceInfo instead of the new instanceInfo as the registrant",
         "Network level connection to peer",
-        "Tomcat started on port(s)");
+
+        "No routes found from RouteLocator");
 
     private boolean isFilterActive;
 
