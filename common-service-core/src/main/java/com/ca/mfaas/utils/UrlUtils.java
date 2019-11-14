4a2bdce53f4ab013693ce135a42c24d01ec6572f
pz891284 <pavlin.zahariev@broadcom.com>
2019-11-14 16:19:47 +0100

    UrlUtils and StringUtils UTs. ObjectUtils moved,

--- common-service-core/src/main/java/com/ca/mfaas/utils/UrlUtils.java
+++ common-service-core/src/main/java/com/ca/mfaas/utils/UrlUtils.java
@@ -14,17 +14,18 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.management.*;
-import java.lang.management.ManagementFactory;
 import java.net.*;
-import java.security.InvalidParameterException;
-import java.util.*;
 import java.util.function.Supplier;
 
 
 @UtilityClass
 public class UrlUtils {
     private static final Logger logger = LoggerFactory.getLogger(UrlUtils.class);
+
+    private static Supplier<String> messageSupplier = new Supplier() {
+        public String get() { return "Invalid URL"; }
+    };
+
 
     /**
      * Remove slashes from input string parameter
@@ -50,18 +51,29 @@ public class UrlUtils {
         }
     }
 
+
     /**
      * Checks validity of URL string.
-     * Invalid URL string will trigger throwing a MalformedURLException encapsulating the message of originally thrown MalformedURLException
+     * Invalid URL string will trigger throwing a InvalidParameterException encapsulating the message of originally thrown MalformedURLException
+     *
+     * @param url
+     */
+    public static void validateUrl(String url) throws MalformedURLException {
+        validateUrl(url, messageSupplier);
+    }
+
+    /**
+     * Checks validity of URL string.
+     * Invalid URL string will trigger throwing a InvalidParameterException encapsulating the message of originally thrown MalformedURLException
      *
      * @param url
      * @param exceptionSupplier
      */
-    public static void validateUrl(String url, Supplier<String> exceptionSupplier) {
+    public static void validateUrl(String url, Supplier<String> exceptionSupplier) throws MalformedURLException {
         try {
             new URL(url);
         } catch (MalformedURLException e) {
-            throw new InvalidParameterException(exceptionSupplier.get() + ": " + e.getMessage());
+            throw new MalformedURLException(exceptionSupplier.get() + ": " + e.getMessage());
         }
     }
 
@@ -92,74 +104,4 @@ public class UrlUtils {
         return StringUtils.removeLastOccurrence(uri, "/");
     }
 
-    /**
-     * Experimental - using {@link javax.management.MBeanServer} for finding actual host IP Addresses
-     *
-     * @return
-     */
-    public static List<String> getHostBaseUrls() {
-        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        Set<ObjectName> objs = getHttpConnectorsNames(mbs, "HTTP/1.1", "Http11");
-
-        ArrayList<String> endPoints = new ArrayList<>();
-        if (objs != null) {
-            InetAddress[] addresses = new InetAddress[0];
-            try {
-                InetAddress inetAddress = InetAddress.getLocalHost();
-                addresses = InetAddress.getAllByName(inetAddress.getHostName());
-            } catch (UnknownHostException e) {
-                logger.error("", e);
-            }
-
-            for (ObjectName obj : objs ) {
-                String scheme = "";
-                try {
-                    scheme = mbs.getAttribute(obj, "scheme").toString();
-                } catch (AttributeNotFoundException e) {
-                    logger.error("", e);
-                } catch (InstanceNotFoundException e) {
-                    logger.error("", e);
-                } catch (ReflectionException e) {
-                    logger.error("", e);
-                } catch (MBeanException e) {
-                    logger.error("", e);
-                }
-
-                String port = obj.getKeyProperty("port");
-                for (InetAddress addr : addresses) {
-                    if ((addr instanceof Inet6Address) || addr.isAnyLocalAddress() || addr.isLoopbackAddress() ||
-                        addr.isMulticastAddress()) {
-                        continue;
-                    }
-                    String host = addr.getHostAddress();
-                    String ep = scheme + "://" + host + ":" + port;
-                    endPoints.add(ep);
-                }
-            }
-        }
-
-        return endPoints;
-    }
-
-    /**
-     * Queries #MBeanServer for objects of type Connector using as query values requested HTTP protocol versions
-     *
-     * @param mbs
-     * @param protocol1
-     * @param protocol2
-     * @return
-     */
-    private static Set<ObjectName> getHttpConnectorsNames(MBeanServer mbs, String protocol1, String protocol2) {
-        QueryExp subQuery1 = Query.match(Query.attr("protocol"), Query.value(protocol1));
-        QueryExp subQuery2 = Query.anySubString(Query.attr("protocol"), Query.value(protocol2));
-        QueryExp query = Query.or(subQuery1, subQuery2);
-
-        Set<ObjectName> objs = null;
-        try {
-            objs = mbs.queryNames(new ObjectName("*:type=Connector,*"), query);
-        } catch (MalformedObjectNameException e) {
-            logger.error("", e);
-        }
-        return objs;
-    }
 }
