a67e2e41cf9cdcafd5b1188a028f419a3a1428c1
Petr Plavjanik <plavjanik@gmail.com>
2020-01-16 23:01:23 +0100

    Making ConvertAuthTokenInUriToCookieFilterTest more stable
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- gateway-service/src/test/java/com/ca/mfaas/gateway/filters/post/ConvertAuthTokenInUriToCookieFilterTest.java
+++ gateway-service/src/test/java/com/ca/mfaas/gateway/filters/post/ConvertAuthTokenInUriToCookieFilterTest.java
@@ -11,7 +11,6 @@ package com.ca.mfaas.gateway.filters.post;
 
 import com.ca.apiml.security.common.config.AuthConfigurationProperties;
 import com.netflix.zuul.context.RequestContext;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -26,40 +25,48 @@ import static org.junit.Assert.*;
 public class ConvertAuthTokenInUriToCookieFilterTest {
 
     private final AuthConfigurationProperties authConfigurationProperties = new AuthConfigurationProperties();
-    private final ConvertAuthTokenInUriToCookieFilter filter = new ConvertAuthTokenInUriToCookieFilter(authConfigurationProperties);
+    private final ConvertAuthTokenInUriToCookieFilter filter = new ConvertAuthTokenInUriToCookieFilter(
+            authConfigurationProperties);
 
-    @Before
-    public void setUp() {
+    private RequestContext getMockRequestContext() {
         RequestContext ctx = RequestContext.getCurrentContext();
         ctx.clear();
         ctx.setResponse(new MockHttpServletResponse());
+        return ctx;
+    }
+
+    private void runFilter(RequestContext ctx) {
+        synchronized (ctx) {
+            this.filter.run();
+        }
     }
 
     @Test
     public void doesNotDoAnythingWhenThereIsNoParam() {
-        final RequestContext ctx = RequestContext.getCurrentContext();
-        this.filter.run();
+        RequestContext ctx = getMockRequestContext();
+        runFilter(ctx);
         assertFalse(ctx.getResponse().getHeaderNames().contains("Set-Cookie"));
     }
 
     @Test
     public void doesNotDoAnythingWhenThereIsAnotherParam() {
-        final RequestContext ctx = RequestContext.getCurrentContext();
+        RequestContext ctx = getMockRequestContext();
         Map<String, List<String>> params = new HashMap<>();
         params.put("someParameter", Collections.singletonList("value"));
         ctx.setRequestQueryParams(params);
-        this.filter.run();
+        runFilter(ctx);
         assertFalse(ctx.getResponse().getHeaderNames().contains("Set-Cookie"));
     }
 
     @Test
     public void setsCookieForCorrectParameter() {
-        final RequestContext ctx = RequestContext.getCurrentContext();
+        RequestContext ctx = getMockRequestContext();
         ctx.setRequest(new MockHttpServletRequest("GET", "/api/v1/service"));
         Map<String, List<String>> params = new HashMap<>();
-        params.put(authConfigurationProperties.getCookieProperties().getCookieName(), Collections.singletonList("token"));
+        params.put(authConfigurationProperties.getCookieProperties().getCookieName(),
+                Collections.singletonList("token"));
         ctx.setRequestQueryParams(params);
-        this.filter.run();
+        runFilter(ctx);
         assertTrue(ctx.getResponse().getHeaders("Set-Cookie").toString().contains("apimlAuthenticationToken=token"));
         assertEquals("Location", ctx.getZuulResponseHeaders().get(0).first());
         assertEquals("http://localhost/api/v1/service", ctx.getZuulResponseHeaders().get(0).second());
@@ -67,12 +74,13 @@ public class ConvertAuthTokenInUriToCookieFilterTest {
 
     @Test
     public void setsLocationToDashboardForApiCatalog() {
-        final RequestContext ctx = RequestContext.getCurrentContext();
+        RequestContext ctx = getMockRequestContext();
         ctx.setRequest(new MockHttpServletRequest("GET", "/api/v1/apicatalog/"));
         Map<String, List<String>> params = new HashMap<>();
-        params.put(authConfigurationProperties.getCookieProperties().getCookieName(), Collections.singletonList("token"));
+        params.put(authConfigurationProperties.getCookieProperties().getCookieName(),
+                Collections.singletonList("token"));
         ctx.setRequestQueryParams(params);
-        this.filter.run();
+        runFilter(ctx);
         assertTrue(ctx.getResponse().getHeaders("Set-Cookie").toString().contains("apimlAuthenticationToken=token"));
         assertEquals("Location", ctx.getZuulResponseHeaders().get(0).first());
         assertEquals("http://localhost/api/v1/apicatalog/#/dashboard", ctx.getZuulResponseHeaders().get(0).second());
