ae0115dbe6499ce84ce5b9de91b8d329b310e922
Andrea Tabone <andrea.tabone@broadcom.com>
2020-01-20 14:56:21 +0100

    Package rename for Integration Enabler spring v2 module
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

---
+++ integration-enabler-spring-v2/src/test/java/org/zowe/apiml/enable/swagger/StaticSwaggerFailureTests.java
@@ -0,0 +1,39 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.enable.swagger;
+
+import org.zowe.apiml.enable.api.ApiDocController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+    properties = {
+        "eureka.client.enabled=false",
+        "mfaas.discovery.info.swaggerLocation=missing_swagger.json"
+    },
+    classes = {StaticSwaggerApplication.class}
+)
+public class StaticSwaggerFailureTests {
+
+    @Autowired
+    private ApiDocController apiDocController;
+
+    @Test(expected = IOException.class)
+    public void givenASwaggerLocationThatDoesntExistThenFail() throws IOException {
+        apiDocController.getApiDoc(null);
+    }
+}
