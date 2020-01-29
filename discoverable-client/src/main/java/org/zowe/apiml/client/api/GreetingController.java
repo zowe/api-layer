c5a502b4677272bfcdf0c43838155516d19d30bf
cZikos <christos.zikos@broadcom.com>
2020-01-29 11:20:36 +0100

    minor fixes after merge
    Signed-off-by: cZikos <christos.zikos@broadcom.com>

--- discoverable-client/src/main/java/org/zowe/apiml/client/api/GreetingController.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/api/GreetingController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
-import java.util.Optional;
 
 
 /**
@@ -34,18 +33,11 @@ public class GreetingController {
     /**
      * Gets a greeting for anyone.
      */
-    @GetMapping(value = {"api/v1/{yourName}/greeting", "api/v1/greeting"})
+    @GetMapping(value = "/api/v1/greeting")
     @ApiOperation(value = "Get a greeting", response = Greeting.class,
         tags = {"Other Operations"})
-    public Greeting greeting(@PathVariable(value = "yourName") Optional<String> yourName,
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name,
                              @RequestParam(value = "delayMs", defaultValue = "0", required = false) Integer delayMs) {
-        String name;
-        if (yourName.isPresent()) {
-            name = yourName.get();
-        } else {
-            name = "world";
-        }
-
         if (delayMs > 0) {
             try {
                 Thread.sleep(delayMs);
