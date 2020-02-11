d1a523409127bf8d6b398055f7b0e7c8e1d652a6
Vsevolod Khanin <vsevolod.khanin@broadcom.com>
2020-02-11 14:51:59 +0100

    Changes in the release commands
    Signed-off-by: Vsevolod Khanin <vsevolod.khanin@broadcom.com>

--- scripts/publish_and_release.sh
+++ scripts/publish_and_release.sh
@@ -19,6 +19,7 @@ case $RELEASE_TYPE in
    "MINOR_RELEASE")
    echo "Make MINOR release"
    ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.scope=minor $AUTH
+   ./gradlew publishEnabler '-Penabler=v1' $AUTH
    git archive --format tar.gz -9 --output api-layer.tar.gz HEAD~1
    ;;
    "MAJOR_RELEASE")
