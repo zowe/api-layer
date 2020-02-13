dce27621cb10fb8eac4220c1c00ff4cd8c27b169
Jakub Balhar <jakub.balhar@broadcom.com>
2020-02-13 09:56:04 +0100

    Replace Tests testing framework with tests for the behavior of the application
    The tests that were removed tested behavior of Java Enabler. These tests should
    be and are in the library. Otherwise the tests are integration tests and should
    be in integration tests.
    The new tests verify only the application specific part.
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- helloworld-spring/src/test/java/org/zowe/apiml/hellospring/listener/ServletContextListenerTest.java
+++ helloworld-spring/src/test/java/org/zowe/apiml/hellospring/listener/ServletContextListenerTest.java
@@ -10,21 +10,55 @@
 package org.zowe.apiml.hellospring.listener;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.mock.web.MockServletContext;
 import org.zowe.apiml.eurekaservice.client.ApiMediationClient;
+import org.zowe.apiml.exception.ServiceDefinitionException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
-
+/**
+ * Further tests related to the Eureka Configuration and the Configuration of the service are in
+ * the onboarding-enabler-java
+ *
+ * These unit tests verify that the helloworld properly tries to register itself.
+ */
 public class ServletContextListenerTest {
+    @Test
+    public void testOnContextCreationRegisterWithEureka() throws ServiceDefinitionException {
+        ApiMediationClient mock = Mockito.mock(ApiMediationClient.class);
+        ServletContext context = setValidParametersTo(
+            new MockServletContext()
+        );
+
+        ApiDiscoveryListener registrator = new ApiDiscoveryListener(mock);
+        registrator.contextInitialized(new ServletContextEvent(context));
+
+        // Verify that the mock
+        verify(mock).register(any());
+    }
 
     @Test
-    public void testContextOk() {
-        ServletContext context = new MockServletContext();
+    public void testOnContextDestroyUnregisterWithEureka() {
+        ApiMediationClient mock = Mockito.mock(ApiMediationClient.class);
+        ServletContext context = setValidParametersTo(
+            new MockServletContext()
+        );
+
+        ApiDiscoveryListener registrator = new ApiDiscoveryListener(mock);
+        registrator.contextDestroyed(null);
+
+        // Verify that the mock
+        verify(mock).unregister();
+    }
+
+    private ServletContext setValidParametersTo(ServletContext context) {
         context.setInitParameter("apiml.config.location", "/service-config.yml");
         context.setInitParameter("apiml.config.additional-location", "../config/local/helloworld-additional-config.yml");
         context.setInitParameter("apiml.serviceIpAddress", "127.0.0.2");
@@ -36,57 +70,6 @@ public class ServletContextListenerTest {
         context.setInitParameter("apiml.ssl.trustStore", "../keystore/localhost/localhost.keystore.p12");
         context.setInitParameter("apiml.ssl.trustStorePassword", "password");
 
-        ApiDiscoveryListener contextListener = new ApiDiscoveryListener();
-        contextListener.contextInitialized(new ServletContextEvent(context));
-
-        ApiMediationClient apimlClient = contextListener.getApiMediationClient();
-        assertNotNull(apimlClient);
-        assertNotNull(apimlClient.getEurekaClient());
-        assertNotNull(apimlClient.getEurekaClient().getApplicationInfoManager());
-        assertNotNull(apimlClient.getEurekaClient().getApplicationInfoManager().getInfo());
-        assertNotNull(apimlClient.getEurekaClient().getApplicationInfoManager().getInfo().getMetadata());
-    }
-
-    @Test
-    public void testContextMissingSubstituteParameters() {
-        ServletContext context = new MockServletContext();
-        context.setInitParameter("apiml.config.location", "/service-config.yml");
-        context.setInitParameter("apiml.config.additional-location", "../config/local/helloworld-additional-config.yml");
-        context.setInitParameter("apiml.serviceIpAddress", "127.0.0.2");
-        context.setInitParameter("apiml.discoveryService.port", "10011");
-        context.setInitParameter("apiml.discoveryService.hostname", "localhost");
-        context.setInitParameter("apiml.ssl.keyPassword", "password");
-        context.setInitParameter("apiml.ssl.keyStorePassword", "password");
-        context.setInitParameter("apiml.ssl.trustStore", "password");
-        context.setInitParameter("apiml.ssl.trustStorePassword", "password");
-        context.setInitParameter("apiml.ssl.verifySslCertificatesOfServices", "true");
-
-        ApiDiscoveryListener contextListener = new ApiDiscoveryListener();
-        contextListener.contextInitialized(new ServletContextEvent(context));
-        assertNotNull(contextListener.getApiMediationClient().getEurekaClient());
-    }
-
-    @Test
-    public void testContextDestroyed() {
-        ServletContext context = new MockServletContext();
-        context.setInitParameter("apiml.config.location", "/service-config.yml");
-        context.setInitParameter("apiml.config.additional-location", "../config/local/helloworld-additional-config.yml");
-        context.setInitParameter("apiml.serviceIpAddress", "127.0.0.2");
-        context.setInitParameter("apiml.discoveryService.port", "10011");
-        context.setInitParameter("apiml.discoveryService.hostname", "localhost");
-        context.setInitParameter("apiml.ssl.keyPassword", "password");
-        context.setInitParameter("apiml.ssl.keyStorePassword", "password");
-        context.setInitParameter("apiml.ssl.trustStore", "password");
-        context.setInitParameter("apiml.ssl.trustStorePassword", "password");
-        context.setInitParameter("apiml.ssl.verifySslCertificatesOfServices", "true");
-
-        ApiDiscoveryListener contextListener = new ApiDiscoveryListener();
-        contextListener.contextInitialized(new ServletContextEvent(context));
-        assertNotNull(contextListener.getApiMediationClient());
-
-        contextListener.contextDestroyed(null);
-        assertNull(contextListener.getApiMediationClient());
-
-        contextListener.contextDestroyed(null);
+        return context;
     }
 }
