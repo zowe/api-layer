8a968cb4d45a5496bb2b6a9b1b1bdc0a7f2e88f5
cZikos <christos.zikos@broadcom.com>
2020-03-10 16:20:40 +0100

    Link authorization service health status with api catalog
    Signed-off-by: cZikos <christos.zikos@broadcom.com>

--- api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/health/ApiCatalogHealthIndicator.java
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/health/ApiCatalogHealthIndicator.java
@@ -9,6 +9,8 @@
  */
 package org.zowe.apiml.apicatalog.health;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.zowe.apiml.product.constants.CoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
@@ -16,22 +18,39 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
+import org.zowe.apiml.security.common.config.AuthConfigurationProperties;
+
+import java.util.Map;
 
 /**
  * Api Catalog health information (/application/health)
  */
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ApiCatalogHealthIndicator extends AbstractHealthIndicator {
+    private static String AUTHENTICATION_SERVICE_ID = "apiml.authorizationService.zosmfServiceId";
+    private static String AUTHENTICATION_SERVICE_PROVIDER = "apiml.authorizationService.provider";
 
     private final DiscoveryClient discoveryClient;
+    private final AuthConfigurationProperties authConfigurationProperties;
 
     @Override
     protected void doHealthCheck(Health.Builder builder) {
         String gatewayServiceId = CoreService.GATEWAY.getServiceId();
+        String authServiceId = this.discoveryClient.getInstances(gatewayServiceId).get(0).getMetadata().get(AUTHENTICATION_SERVICE_ID);
+        String authServiceProvider = this.discoveryClient.getInstances(gatewayServiceId).get(0).getMetadata().get(AUTHENTICATION_SERVICE_PROVIDER);
+        boolean authServiceUp = false;
+        try {
+            if (authServiceProvider.equalsIgnoreCase("zosmf")) {
+                authServiceUp = !this.discoveryClient.getInstances(authServiceId).isEmpty();
+            }
+        } catch (AuthenticationServiceException ex) {
+            log.debug("The parameter 'zosmfServiceId' is not configured.", ex);
+        }
 
         boolean gatewayUp = !this.discoveryClient.getInstances(gatewayServiceId).isEmpty();
-        Status healthStatus = gatewayUp ? Status.UP : Status.DOWN;
+        Status healthStatus = (gatewayUp && authServiceUp) ? Status.UP : Status.DOWN;
 
         builder
             .status(healthStatus)
