99f1c9f7547775166a1b17a588c50a7543ccdc04
Elena Kubantseva <elena.kubantseva@broadcom.com>
2020-03-11 12:48:01 +0100

    fixed routing problem
    Signed-off-by: Elena Kubantseva <elena.kubantseva@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/controllers/VersionController.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/controllers/VersionController.java
@@ -25,7 +25,7 @@ import org.zowe.apiml.product.version.VersionService;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/v1/gateway")
+@RequestMapping("/gateway")
 public class VersionController {
 
     private VersionService versionService;
