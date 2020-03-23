165bdca4b027c1d7f925d3510f5a582528702fd1
Pavel Jareš <pavel.jares@broadcom.com>
2020-03-23 14:32:19 +0100

    fix path of api-doc
    Signed-off-by: Pavel Jareš <pavel.jares@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/apidoc/reader/ApiDocController.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/apidoc/reader/ApiDocController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api-doc")
+@RequestMapping("/api/v1/gateway/api-doc")
 @RequiredArgsConstructor
 public class ApiDocController {
 
