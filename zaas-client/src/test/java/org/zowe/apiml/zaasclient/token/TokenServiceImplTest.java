426f8960a7fd849ee4a6763b1ad4d2d4671870b6
krishnachaithanyak <krishnachaithanya.kasibhatla@broadcom.com>
2020-03-26 12:40:52 +0530

    Zaas-Client: Fixed the licencing failure in tests and modified config.properties to use relatve path. Added gradle publish for zaas-client

--- zaas-client/src/test/java/org/zowe/apiml/zaasclient/token/TokenServiceImplTest.java
+++ zaas-client/src/test/java/org/zowe/apiml/zaasclient/token/TokenServiceImplTest.java
@@ -1,5 +1,13 @@
 package org.zowe.apiml.zaasclient.token;
-
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.http.client.methods.CloseableHttpResponse;
