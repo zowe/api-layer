7cfbbd0e83c493580e718f4bc81d87952d8d07ae
Petr Plavjanik <plavjanik@gmail.com>
2020-03-28 20:34:22 +0100

    Use ZosmfServiceV2 only if the authentication endpoint exists
    Signed-off-by: Petr Plavjanik <plavjanik@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfServiceV2.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfServiceV2.java
@@ -16,30 +16,32 @@ import org.zowe.apiml.gateway.security.service.ZosmfService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * This implementation is used for version z/OSMF which support authentication endpoint. This endpoint allows to
- * generate new token, verify using token and also deactivation of token. Token could be LTPA or JWT (depends on
- * z/OSMF's configuration)
+ * This implementation is used for version z/OSMF which can support authentication
+ * endpoint depends on z/OSMF's configuration).
+ * This endpoint allows to generate new token, verify using token and
+ * also deactivation of token. Token could be LTPA or JWT.
  *
  * Bean could be served via {@link ZosmfServiceFacade}
  */
 @Service
+@Order(1)
 public class ZosmfServiceV2 extends AbstractZosmfService {
 
-    public ZosmfServiceV2(
-        AuthConfigurationProperties authConfigurationProperties,
-        DiscoveryClient discovery,
-        @Qualifier("restTemplateWithoutKeystore") RestTemplate restTemplateWithoutKeystore,
-        ObjectMapper securityObjectMapper
-    ) {
+    public ZosmfServiceV2(AuthConfigurationProperties authConfigurationProperties, DiscoveryClient discovery,
+            @Qualifier("restTemplateWithoutKeystore") RestTemplate restTemplateWithoutKeystore,
+            ObjectMapper securityObjectMapper) {
         super(authConfigurationProperties, discovery, restTemplateWithoutKeystore, securityObjectMapper);
     }
 
@@ -52,15 +54,29 @@ public class ZosmfServiceV2 extends AbstractZosmfService {
         headers.add(ZOSMF_CSRF_HEADER, "");
 
         try {
-            final ResponseEntity<String> response = restTemplateWithoutKeystore.exchange(
-                url,
-                HttpMethod.POST,
-                new HttpEntity<>(null, headers),
-                String.class);
+            final ResponseEntity<String> response = restTemplateWithoutKeystore.exchange(url, HttpMethod.POST,
+                    new HttpEntity<>(null, headers), String.class);
             return getAuthenticationResponse(response);
         } catch (RuntimeException re) {
             throw handleExceptionOnCall(url, re);
         }
+    }
+
+    private boolean authenticationEndpointExists() {
+        String url = getURI(getZosmfServiceId()) + ZOSMF_AUTHENTICATE_END_POINT;
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add(ZOSMF_CSRF_HEADER, "");
+
+        try {
+            restTemplateWithoutKeystore.exchange(url, HttpMethod.POST, new HttpEntity<>(null, headers), String.class);
+        } catch (HttpClientErrorException hce) {
+            if (hce.getStatusCode().equals(HttpStatus.UNAUTHORIZED)) {
+                return true;
+            }
+        } catch (RuntimeException re) {
+        }
+        return false;
     }
 
     @Override
@@ -72,13 +88,11 @@ public class ZosmfServiceV2 extends AbstractZosmfService {
         headers.add(HttpHeaders.COOKIE, type.getCookieName() + "=" + token);
 
         try {
-            ResponseEntity<String> re = restTemplateWithoutKeystore.exchange(
-                url ,
-                HttpMethod.POST,
-                new HttpEntity<>(null, headers),
-                String.class);
+            ResponseEntity<String> re = restTemplateWithoutKeystore.exchange(url, HttpMethod.POST,
+                    new HttpEntity<>(null, headers), String.class);
 
-            if (re.getStatusCode().is2xxSuccessful()) return;
+            if (re.getStatusCode().is2xxSuccessful())
+                return;
             if (re.getStatusCodeValue() == 401) {
                 throw new TokenNotValidException("Token is not valid.");
             }
@@ -98,13 +112,11 @@ public class ZosmfServiceV2 extends AbstractZosmfService {
         headers.add(HttpHeaders.COOKIE, type.getCookieName() + "=" + token);
 
         try {
-            ResponseEntity<String> re = restTemplateWithoutKeystore.exchange(
-                url,
-                HttpMethod.DELETE,
-                new HttpEntity<>(null, headers),
-                String.class);
+            ResponseEntity<String> re = restTemplateWithoutKeystore.exchange(url, HttpMethod.DELETE,
+                    new HttpEntity<>(null, headers), String.class);
 
-            if (re.getStatusCode().is2xxSuccessful()) return;
+            if (re.getStatusCode().is2xxSuccessful())
+                return;
             apimlLog.log("org.zowe.apiml.security.serviceUnavailable", url, re.getStatusCodeValue());
             throw new ServiceNotAccessibleException("Could not get an access to z/OSMF service.");
         } catch (RuntimeException re) {
@@ -112,9 +124,10 @@ public class ZosmfServiceV2 extends AbstractZosmfService {
         }
     }
 
+
     @Override
-    public boolean matchesVersion(int version) {
-        return version >= 26;
+    public boolean isSupported(int version) {
+        return (version >= 26) && authenticationEndpointExists();
     }
 
 }
