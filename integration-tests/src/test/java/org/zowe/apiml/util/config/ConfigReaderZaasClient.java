dbffb95e3fe684b99e5eed06e446d39d2340ac11
Berhanu Abebe Dagnaw <berhanu-abebe.dagnaw@broadcom.com>
2020-03-30 22:07:11 +0200

    Compile time error and cosmetic changes

--- integration-tests/src/test/java/org/zowe/apiml/util/config/ConfigReaderZaasClient.java
+++ integration-tests/src/test/java/org/zowe/apiml/util/config/ConfigReaderZaasClient.java
@@ -9,39 +9,32 @@
  */
 package org.zowe.apiml.util.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.zowe.apiml.zaasclient.config.ConfigProperties;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Objects;
-import java.util.Properties;
 
 @Slf4j
 public class ConfigReaderZaasClient {
 
-        public final static int PORT = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getPort();
+    public final static int PORT = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getPort();
 
-        public static ConfigProperties getConfigProperties () throws IOException {
+    public static ConfigProperties getConfigProperties() {
 
-            ConfigProperties configProperties = new ConfigProperties();
+        ConfigProperties configProperties = new ConfigProperties();
 
 
-            configProperties.setApimlHost(ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getHost());
-            configProperties.setApimlPort(ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getPort());
-            configProperties.setApimlBaseUrl("/api/v1/gateway/auth");
-            configProperties.setKeyStorePath(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStore());
-            configProperties.setKeyStorePassword(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStorePassword());
-            configProperties.setKeyStoreType(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStoreType());
-            configProperties.setTrustStorePath(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStore());
-            configProperties.setTrustStorePassword(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStorePassword());
-            configProperties.setTrustStoreType(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStoreType());
+        configProperties.setApimlHost(ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getHost());
+        configProperties.setApimlPort(String.valueOf(ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getPort()));
+        configProperties.setApimlBaseUrl("/api/v1/gateway/auth");
+        configProperties.setKeyStorePath(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStore());
+        configProperties.setKeyStorePassword(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStorePassword());
+        configProperties.setKeyStoreType(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStoreType());
+        configProperties.setTrustStorePath(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStore());
+        configProperties.setTrustStorePassword(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStorePassword());
+        configProperties.setTrustStoreType(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStoreType());
 
-            return configProperties;
-        }
+        return configProperties;
+    }
 }
 
