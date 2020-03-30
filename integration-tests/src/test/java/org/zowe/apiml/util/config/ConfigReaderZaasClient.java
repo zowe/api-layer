8a43519724abde1e0eac36f9111a607d3bb823e9
janamallikarjunaraobattula <janamallikarjunarao@broadcom.com>
2020-03-30 14:41:34 +0530

    Integration testcases for Login

--- integration-tests/src/test/java/org/zowe/apiml/util/config/ConfigReaderZaasClient.java
+++ integration-tests/src/test/java/org/zowe/apiml/util/config/ConfigReaderZaasClient.java
@@ -9,37 +9,30 @@
  */
 package org.zowe.apiml.util.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.zowe.apiml.zaasclient.config.ConfigProperties;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Objects;
-import java.util.Properties;
+
+import static org.zowe.apiml.util.config.ConfigReader.*;
 
 @Slf4j
 public class ConfigReaderZaasClient {
-
-        public final static int PORT = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getPort();
 
         public static ConfigProperties getConfigProperties () throws IOException {
 
             ConfigProperties configProperties = new ConfigProperties();
 
 
-            configProperties.setApimlHost(ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getHost());
-            configProperties.setApimlPort(ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getPort());
+            configProperties.setApimlHost(environmentConfiguration().getGatewayServiceConfiguration().getHost());
+            configProperties.setApimlPort(environmentConfiguration().getGatewayServiceConfiguration().getPort() + "");
             configProperties.setApimlBaseUrl("/api/v1/gateway/auth");
-            configProperties.setKeyStorePath(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStore());
-            configProperties.setKeyStorePassword(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStorePassword());
-            configProperties.setKeyStoreType(ConfigReader.environmentConfiguration().getTlsConfiguration().getKeyStoreType());
-            configProperties.setTrustStorePath(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStore());
-            configProperties.setTrustStorePassword(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStorePassword());
-            configProperties.setTrustStoreType(ConfigReader.environmentConfiguration().getTlsConfiguration().getTrustStoreType());
+            configProperties.setKeyStorePath(environmentConfiguration().getTlsConfiguration().getKeyStore());
+            configProperties.setKeyStorePassword(environmentConfiguration().getTlsConfiguration().getKeyStorePassword());
+            configProperties.setKeyStoreType(environmentConfiguration().getTlsConfiguration().getKeyStoreType());
+            configProperties.setTrustStorePath(environmentConfiguration().getTlsConfiguration().getTrustStore());
+            configProperties.setTrustStorePassword(environmentConfiguration().getTlsConfiguration().getTrustStorePassword());
+            configProperties.setTrustStoreType(environmentConfiguration().getTlsConfiguration().getTrustStoreType());
 
             return configProperties;
         }
--- integration-tests/src/test/java/org/zowe/apiml/zaasclient/LoginZaasClientIntegrationTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/zaasclient/LoginZaasClientIntegrationTest.java
@@ -9,7 +9,7 @@
  */
 package org.zowe.apiml.zaasclient;
 
-//import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64;
 import org.zowe.apiml.util.config.ConfigReaderZaasClient;
 import org.zowe.apiml.zaasclient.config.ConfigProperties;
 import org.zowe.apiml.zaasclient.exception.ZaasClientException;
@@ -21,6 +21,7 @@ import org.junit.Test;
 import org.zowe.apiml.util.config.ConfigReader;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class LoginZaasClientIntegrationTest {
 
@@ -28,6 +29,13 @@ public class LoginZaasClientIntegrationTest {
     private final static String PASSWORD = ConfigReader.environmentConfiguration().getCredentials().getPassword();
     private static final String INVALID_USER = "usr";
     private static final String INVALID_PASS = "usr";
+    private static final String NULL_USER = null;
+    private static final String NULL_PASS = null;
+    private static final String EMPTY_USER = "";
+    private static final String EMPTY_PASS = "";
+    private static final String NULL_AUTH_HEADER = null;
+    private static final String EMPTY_AUTH_HEADER = "";
+
     TokenService tokenService;
 
     @Before
@@ -39,7 +47,7 @@ public class LoginZaasClientIntegrationTest {
 
     //@formatter:off
     @Test
-    public void doLoginWithValidCredentials()  {
+    public void doLoginWithValidCredentials() {
         try {
             String token = tokenService.login(USERNAME, PASSWORD);
             System.out.println("Token obtained: " + token);
@@ -50,7 +58,7 @@ public class LoginZaasClientIntegrationTest {
     }
 
     @Test
-    public void doLoginWithInValidUsername()  {
+    public void doLoginWithInValidPassword() {
         try {
             String token = tokenService.login(INVALID_USER, PASSWORD);
             System.out.println("Token obtained: " + token);
@@ -60,6 +68,185 @@ public class LoginZaasClientIntegrationTest {
         }
     }
 
+    @Test
+    public void doLoginWithInValidUsername() {
+        try {
+            String token = tokenService.login(USERNAME, INVALID_PASS);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithNullUser() {
+        try {
+            String token = tokenService.login(NULL_USER, PASSWORD);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithNullPassword() {
+        try {
+            String token = tokenService.login(USERNAME, NULL_PASS);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithEmptyUser() {
+        try {
+            String token = tokenService.login(EMPTY_USER, PASSWORD);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithEmptyPassword() {
+        try {
+            String token = tokenService.login(USERNAME, EMPTY_PASS);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithAuthHeaderValidCredentials() {
+        try {
+            String auth = USERNAME + ":" + PASSWORD;
+            byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+            String token = tokenService.login(authHeader);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithAuthHeaderValidUsername() {
+        try {
+            String auth = USERNAME + ":" + INVALID_PASS;
+            byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+            String token = tokenService.login(authHeader);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
 
 
+    @Test
+    public void doLoginWithAuthHeaderValidPassWord() {
+        try {
+            String auth = INVALID_USER + ":" + PASSWORD;
+            byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+            String token = tokenService.login(authHeader);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithAuthHeaderNullUserName() {
+        try {
+            String auth = NULL_USER + ":" + PASSWORD;
+            byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+            String token = tokenService.login(authHeader);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+
+    @Test
+    public void doLoginWithAuthHeaderNullPassword() {
+        try {
+            String auth = USERNAME + ":" + NULL_PASS;
+            byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+            String token = tokenService.login(authHeader);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithAuthHeaderEmptyUsername() {
+        try {
+            String auth = EMPTY_USER + ":" + PASSWORD;
+            byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+            String token = tokenService.login(authHeader);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithAuthHeaderEmptyPassword() {
+        try {
+            String auth = USERNAME + ":" + EMPTY_PASS;
+            byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+            String token = tokenService.login(authHeader);
+            System.out.println("Token obtained: " + token);
+            tokenService.query(token);
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithAuthNullHeader() {
+        try {
+            System.out.println("Token obtained: " + tokenService.login(NULL_AUTH_HEADER));
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
+
+    @Test
+    public void doLoginWithAuthEmptyHeader() {
+        try {
+            System.out.println("Token obtained: " + tokenService.login(EMPTY_AUTH_HEADER));
+        } catch (ZaasClientException zce) {
+            System.out.println(zce.getErrorCode() + "\n" + zce.getErrorMessage() + "\n" + zce.getHttpResponseCode());
+        }
+    }
 }
+
+
