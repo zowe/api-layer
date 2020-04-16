da700312a357aafc9173c0502ba245e5463b44d2
Ilkin Abdullayev <ilkinabdullayev6@gmail.com>
2020-04-16 12:41:12 +0200

    US654071: Added auth for catalog endpoints

--- api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/swagger/SwaggerConfiguration.java
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/swagger/SwaggerConfiguration.java
@@ -15,10 +15,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.BasicAuth;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -43,6 +46,13 @@ public class SwaggerConfiguration {
                 PathSelectors.any()
             )
             .build()
+            .securitySchemes(
+                Arrays.asList(
+                    new BasicAuth("LoginBasicAuth"),
+                    new ApiKey("Bearer", "Authorization", "header"),
+                    new ApiKey("CookieAuth", "apimlAuthenticationToken", "header")
+                )
+            )
             .apiInfo(
                 new ApiInfo(
                     apiTitle,
