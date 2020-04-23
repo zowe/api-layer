8653d96812e8d5c14c5b213266a014e7bca5b32f
jandadav <janda.david@gmail.com>
2020-04-23 19:26:50 +0200

    SecurityDecorator exceptionhandling ByPassScheme fix
    Signed-off-by: jandadav <janda.david@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/ByPassScheme.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/ByPassScheme.java
@@ -11,6 +11,7 @@ package org.zowe.apiml.gateway.security.service.schema;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.zuul.context.RequestContext;
+import org.apache.http.HttpRequest;
 import org.springframework.stereotype.Component;
 import org.zowe.apiml.security.common.auth.Authentication;
 import org.zowe.apiml.security.common.auth.AuthenticationScheme;
@@ -38,6 +39,11 @@ public class ByPassScheme implements AbstractAuthenticationScheme {
         @Override
         public void apply(InstanceInfo instanceInfo) {
             RequestContext.getCurrentContext().put(AUTHENTICATION_SCHEME_BY_PASS_KEY, Boolean.TRUE);
+        }
+
+        @Override
+        public void applyToRequest(HttpRequest request) {
+            // do nothing
         }
 
         @Override
