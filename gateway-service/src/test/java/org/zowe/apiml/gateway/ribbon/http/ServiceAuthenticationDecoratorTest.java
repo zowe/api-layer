c56052a4a5b8b2115d70ab54af7c0381b12a3a4a
Andrea Tabone <andrea.tabone@broadcom.com>
2020-04-23 16:40:30 +0200

    Add implementation of process method and add unit tests for Decorator class
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/gateway/ribbon/http/ServiceAuthenticationDecoratorTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/ribbon/http/ServiceAuthenticationDecoratorTest.java
@@ -11,6 +11,7 @@
 package org.zowe.apiml.gateway.ribbon.http;
 
 import com.netflix.appinfo.InstanceInfo;
+import com.netflix.loadbalancer.reactive.ExecutionListener;
 import com.netflix.zuul.context.RequestContext;
 import org.apache.http.HttpRequest;
 import org.apache.http.client.methods.HttpGet;
@@ -21,6 +22,9 @@ import org.zowe.apiml.gateway.security.service.ServiceAuthenticationServiceImpl;
 import org.zowe.apiml.gateway.security.service.schema.AuthenticationCommand;
 import org.zowe.apiml.gateway.security.service.schema.ServiceAuthenticationService;
 import org.zowe.apiml.security.common.auth.Authentication;
+import org.zowe.apiml.security.common.token.TokenAuthentication;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -55,10 +59,12 @@ class ServiceAuthenticationDecoratorTest {
     @Test
     void givenContextWithCorrectKey_whenProcess_thenShouldRetrieveCommand() throws RequestAbortException {
         AuthenticationCommand universalCmd = mock(ServiceAuthenticationServiceImpl.UniversalAuthenticationCommand.class);
-        RequestContext.getCurrentContext().set(AUTHENTICATION_COMMAND_KEY, universalCmd);
-        RequestContext.getCurrentContext().set(LOADBALANCED_INSTANCE_INFO_KEY, info);
+        AuthenticationCommand cmd = getAuthenticationCommand(universalCmd);
+        doReturn(TokenAuthentication.createAuthenticated("user", "jwtToken")).when(authenticationService).validateJwtToken("jwtToken");
+
         decorator.process(request);
         verify(serviceAuthenticationService, atLeastOnce()).getAuthentication(info);
+        verify(cmd, times(1)).applyToRequest(request);
     }
 
     @Test
@@ -69,4 +75,31 @@ class ServiceAuthenticationDecoratorTest {
             "Should fail on RequestContext without InstanceInfo set by LoadBalancer impl.");
     }
 
+    @Test
+    void givenContextWithCorrectKey_whenProcess_thenShouldRetrieve() {
+        AuthenticationCommand universalCmd = mock(ServiceAuthenticationServiceImpl.UniversalAuthenticationCommand.class);
+        AuthenticationCommand cmd = getAuthenticationCommand(universalCmd);
+        TokenAuthentication tokenAuthentication = mock(TokenAuthentication.class);
+        doReturn(tokenAuthentication).when(authenticationService).validateJwtToken("jwtToken");
+        when(authenticationService.validateJwtToken("jwtToken").isAuthenticated()).thenReturn(false);
+
+        assertThrows(ExecutionListener.AbortExecutionException.class, () ->
+            decorator.process(request),
+            "Exception is not BadCredentialsException");
+        verify(cmd, times(0)).applyToRequest(request);
+    }
+
+    private AuthenticationCommand getAuthenticationCommand(AuthenticationCommand universalCmd) {
+
+        RequestContext.getCurrentContext().set(AUTHENTICATION_COMMAND_KEY, universalCmd);
+        RequestContext.getCurrentContext().set(LOADBALANCED_INSTANCE_INFO_KEY, info);
+        doReturn(Optional.of("jwtToken")).when(authenticationService).getJwtTokenFromRequest(any());
+        Authentication authentication = mock(Authentication.class);
+        AuthenticationCommand cmd = mock(AuthenticationCommand.class);
+
+        when(serviceAuthenticationService.getAuthentication(info)).thenReturn(authentication);
+        when(serviceAuthenticationService.getAuthenticationCommand(authentication, "jwtToken")).thenReturn(cmd);
+        doReturn(true).when(cmd).isRequiredValidJwt();
+        return cmd;
+    }
 }
