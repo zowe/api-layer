4a46a0da06f0c426b69dde4f9469c82ec9a7ece0
Andrea Tabone <andrea.tabone@broadcom.com>
2020-04-24 15:35:55 +0200

    Fix more sonar problems and add unit test for RibbonRetryError class
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/ribbon/http/ServiceAuthenticationDecorator.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/ribbon/http/ServiceAuthenticationDecorator.java
@@ -36,7 +36,7 @@ public class ServiceAuthenticationDecorator {
     public void process(HttpRequest request) throws RequestAbortException {
         final RequestContext context = RequestContext.getCurrentContext();
 
-        if (context.get(AUTHENTICATION_COMMAND_KEY) != null && context.get(AUTHENTICATION_COMMAND_KEY) instanceof ServiceAuthenticationServiceImpl.UniversalAuthenticationCommand) {
+        if (context.get(AUTHENTICATION_COMMAND_KEY) instanceof ServiceAuthenticationServiceImpl.UniversalAuthenticationCommand) {
             InstanceInfo info = getInstanceInfoFromContext(context);
             final Authentication authentication = serviceAuthenticationService.getAuthentication(info);
             boolean rejected = false;
@@ -69,7 +69,7 @@ public class ServiceAuthenticationDecorator {
 
     private InstanceInfo getInstanceInfoFromContext(RequestContext context) throws RequestContextNotPreparedException {
         Object o = context.get(LOADBALANCED_INSTANCE_INFO_KEY);
-        if (o == null && ! (o instanceof InstanceInfo)) {
+        if (o == null) {
             throw new RequestContextNotPreparedException("InstanceInfo of loadbalanced instance is not present in RequestContext");
         }
         return (InstanceInfo) o;
