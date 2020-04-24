1342461faaa58d9ad18e223e35b6bc1475806049
achmelo <a.chmelo@gmail.com>
2020-04-24 14:56:22 +0200

    port for VS
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/gatewayservice/AuthenticationOnDeploymentTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/gatewayservice/AuthenticationOnDeploymentTest.java
@@ -57,8 +57,8 @@ public class AuthenticationOnDeploymentTest {
         final String jwt = gatewayToken();
 
         try (
-            final VirtualService service1 = new VirtualService("testService1");
-            final VirtualService service2 = new VirtualService("testService1")
+            final VirtualService service1 = new VirtualService("testService1",5679);
+            final VirtualService service2 = new VirtualService("testService1", 5678)
 
         ) {
             // start first instance - without passTickets
