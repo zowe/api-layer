a86e52253077ce1c97bc85d47be2dac52251a824
jandadav <janda.david@gmail.com>
2020-05-12 16:37:02 +0200

    refresh controller implementation
    Signed-off-by: jandadav <janda.david@gmail.com>

--- api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/controllers/ApiStaticRefreshController.java
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/controllers/ApiStaticRefreshController.java
@@ -10,21 +10,77 @@
 
 package org.zowe.apiml.apicatalog.controllers;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.zowe.apiml.apicatalog.instance.InstanceRetrievalService;
+import org.zowe.apiml.product.constants.CoreService;
+import org.zowe.apiml.util.EurekaUtils;
 
-@AllArgsConstructor
+import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
+
 @RestController
 public class ApiStaticRefreshController {
 
-    @RequestMapping(
-        value = "/discovery/api/v1/staticApi",
-        method = RequestMethod.POST)
-    public String refreshStaticApis() {
-        // TODO find the instances of discovery sercice and redirect to one of them
-        return "redirect:https://localhost:10011/discovery/api/v1/staticApi";
+    private final InstanceRetrievalService instanceRetrievalService;
+    private final RestTemplate restTemplate;
+
+    private static final String REFRESH_ENDPOINT = "/discovery/api/v1/staticApi";
+
+    //TODO when HTTP mode on Eureka is used, this has to be provided on commandline
+    //TODO documentation?
+    @Value("${apiml.discovery.userid:eureka}")
+    private String eurekaUserid;
+
+    @Value("${apiml.discovery.password:password}")
+    private String eurekaPassword;
+
+    public ApiStaticRefreshController(@Qualifier("restTemplateWithKeystore") RestTemplate restTemplate, InstanceRetrievalService instanceRetrievalService) {
+        this.instanceRetrievalService = instanceRetrievalService;
+        this.restTemplate = restTemplate;
+    }
+
+    //TODO this url should change
+    @PostMapping(
+        value = "/discovery/api/v1/staticApi")
+    public String refreshStaticApis(HttpServletResponse response) {
+
+        InstanceInfo discoveryInstance = null;
+        try {
+            discoveryInstance = instanceRetrievalService.getInstanceInfo(CoreService.DISCOVERY.getServiceId());
+
+            if (discoveryInstance != null) {
+                String url = EurekaUtils.getUrl(discoveryInstance) + REFRESH_ENDPOINT;
+                HttpEntity<?> entity = new HttpEntity<>(null, url.startsWith("http://") ? createBasicAuthHeader(eurekaUserid, eurekaPassword) : null);
+                ResponseEntity<String> restResponse = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+                response.setStatus(restResponse.getStatusCode().value());
+                return restResponse.getBody();
+            } else {
+                response.setStatus(HttpStatus.SERVICE_UNAVAILABLE.value());
+                return "Discovery service not available";
+                //TODO logger + message
+            }
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return e.getMessage();
+            //TODO logger + message
+        }
+    }
+
+    private MultiValueMap createBasicAuthHeader(String username, String password) {
+        MultiValueMap headerMap = new HttpHeaders();
+        String auth = username + ":" + password;
+        byte[] encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes()).getBytes();
+        String authHeader = "Basic " + new String(encodedAuth);
+        headerMap.put("Authorization", authHeader);
+
+        return headerMap;
     }
 
 }
