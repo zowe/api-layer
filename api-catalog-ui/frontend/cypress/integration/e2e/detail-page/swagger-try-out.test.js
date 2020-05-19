7d64261ad98439fd11a7dc349a2a981f0f837f09
Jakub Balhar <jakub.balhar@broadcom.com>
2020-05-19 09:52:17 +0200

    Update Swagger Try Out Test
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- api-catalog-ui/frontend/cypress/integration/e2e/detail-page/swagger-try-out.test.js
+++ api-catalog-ui/frontend/cypress/integration/e2e/detail-page/swagger-try-out.test.js
@@ -1,42 +1,47 @@
+function login() {
+    cy.visit(`${Cypress.env('catalogHomePage')}/#/login`);
+
+    const username = Cypress.env('username');
+    const password = Cypress.env('password');
+
+    cy.get('button[type="submit"').as('submitButton');
+
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+
+    cy.get('@submitButton').click();
+}
+
+function goToTile() {
+    cy.contains('API Mediation Layer API').click();
+
+    cy.url().should('contain', ('/tile/apimediationlayer'));
+}
+
 describe('>>> Swagger Test', () => {
-
-    beforeEach(() => {
-        cy.visit(`${Cypress.env('catalogHomePage')}/#/login`);
-
-        const username = Cypress.env('username');
-        const password = Cypress.env('password');
-
-        cy.get('button[type="submit"').as('submitButton');
-
-        cy.get('#username').type(username);
-        cy.get('#password').type(password);
-
-        cy.get('@submitButton').click();
-
-        cy.contains('API Mediation Layer API').click();
-
-        cy.visit(`${Cypress.env('catalogHomePage')}/#/tile/apimediationlayer/apicatalog`);
-    })
-
-
-
     it('Should contain try-out button', () => {
+        login();
+        goToTile();
+
         cy.get('.opblock-summary').eq(0).click();
         cy.get('.try-out').should('exist');
-    })
+    });
 
 
     it('Should protect endpoint', () => {
+        login();
+        goToTile();
+
         cy.get('.authorization__btn').should('exist');
 
-        cy.get('.authorization__btn').eq(0).click()
+        cy.get('.authorization__btn').eq(0).click();
 
         cy.get('input[name=username]').type('non-valid');
         cy.get('input[name=password]').type('non-valid');
 
         cy.contains('Basic authorization').parent().parent().parent().submit();
 
-        cy.get('.close-modal').click()
+        cy.get('.close-modal').click();
 
         cy.get('.opblock-summary').eq(0).click();
 
@@ -47,9 +52,5 @@ describe('>>> Swagger Test', () => {
         cy.get('table.live-responses-table')
             .find('.response-col_status')
             .should('contain', '401')
-
-    
     })
-
-
-})
+});
