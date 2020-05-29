b4857b373c29fd3faf969d1d174ea6bd52e64d13
Petr Plavjanik <petr.plavjanik@broadcom.com>
2020-05-29 14:17:15 +0200

    Export z/OSMF public JWK to PEM certificate
    Signed-off-by: Petr Plavjanik <petr.plavjanik@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/login/zosmf/JwkToPublicKeyConverter.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/login/zosmf/JwkToPublicKeyConverter.java
@@ -9,35 +9,97 @@
  */
 package org.zowe.apiml.gateway.security.login.zosmf;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.text.ParseException;
 import java.util.Base64;
+import java.util.Date;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
 public class JwkToPublicKeyConverter {
 
     /**
-     * Converts the first public key in JWT in JSON to PEM format.
+     * Converts the first public key in JWK in JSON to a certificate PEM format. The
+     * public key is from JWK but the rest is fake.
      */
     public String convertFirstPublicKeyJwkToPem(String jwkJson) {
         try {
-            PublicKey key = JWKSet.parse(jwkJson).toPublicJWKSet().getKeys().get(0).toRSAKey().toPublicKey();
-            String encoded = Base64.getEncoder().encodeToString(key.getEncoded());
-            StringBuilder s = new StringBuilder();
-            s.append("-----BEGIN PUBLIC KEY-----");
-            for (int i = 0; i < encoded.length(); i++) {
-                if (((i % 64) == 0) && (i != (encoded.length() - 1))) {
-                    s.append("\n");
-                }
-                s.append(encoded.charAt(i));
-            }
-            s.append("\n");
-            s.append("-----END PUBLIC KEY-----\n");
-            return s.toString();
-        } catch (ParseException | JOSEException e) {
+            String publicKeyPem = convertFirstPublicKeyJwkToPublicKeyPem(jwkJson);
+            PEMParser pemParser = new PEMParser(new StringReader(publicKeyPem));
+            Object publicKey = pemParser.readObject();
+
+            PrivateKey privateKey = generatePrivateKey();
+
+            ContentSigner signer = new JcaContentSignerBuilder("SHA256with" + privateKey.getAlgorithm())
+                    .build(privateKey);
+            X509CertificateHolder x509CertificateHolder = new X509v3CertificateBuilder(new X500Name("CN=Zowe"),
+                    new BigInteger("0"), new Date(), new Date(), new X500Name("CN=Zowe"),
+                    (SubjectPublicKeyInfo) publicKey).build(signer);
+
+            return certificateHolderToPem(x509CertificateHolder);
+        } catch (ParseException | JOSEException | CertificateException | IOException | OperatorCreationException
+                | NoSuchAlgorithmException e) {
             throw new JwkConversionError(e);
         }
+    }
+
+    private String certificateHolderToPem(X509CertificateHolder x509CertificateHolder)
+            throws CertificateException, IOException {
+        StringWriter sw = new StringWriter();
+        Certificate cert = CertificateFactory.getInstance("X.509")
+                .generateCertificate(new ByteArrayInputStream(x509CertificateHolder.getEncoded()));
+        JcaPEMWriter pemWriter = new JcaPEMWriter(sw);
+        pemWriter.writeObject(cert);
+        pemWriter.flush();
+        return sw.toString();
+    }
+
+    private PrivateKey generatePrivateKey() throws NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048, new SecureRandom(new byte[] { 0 }));
+        KeyPair kp = kpg.generateKeyPair();
+        PrivateKey pvt = kp.getPrivate();
+        return pvt;
+    }
+
+    String convertFirstPublicKeyJwkToPublicKeyPem(String jwkJson) throws JOSEException, ParseException {
+        PublicKey key = JWKSet.parse(jwkJson).toPublicJWKSet().getKeys().get(0).toRSAKey().toPublicKey();
+        String encoded = Base64.getEncoder().encodeToString(key.getEncoded());
+        StringBuilder s = new StringBuilder();
+        s.append("-----BEGIN PUBLIC KEY-----");
+        for (int i = 0; i < encoded.length(); i++) {
+            if (((i % 64) == 0) && (i != (encoded.length() - 1))) {
+                s.append("\n");
+            }
+            s.append(encoded.charAt(i));
+        }
+        s.append("\n");
+        s.append("-----END PUBLIC KEY-----\n");
+        String pem = s.toString();
+        return pem;
     }
 }
--- gateway-service/src/test/java/org/zowe/apiml/gateway/security/login/zosmf/ZosmfJwkToPublicKeyTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/security/login/zosmf/ZosmfJwkToPublicKeyTest.java
@@ -10,7 +10,6 @@
 
 package org.zowe.apiml.gateway.security.login.zosmf;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -29,7 +28,6 @@ public class ZosmfJwkToPublicKeyTest {
     public void zosmfJwkIsConvertedToPublicKey() throws IOException {
         RestTemplate restTemplate = mock(RestTemplate.class);
         String jwk = "{\"keys\":[{\"kty\":\"RSA\",\"e\":\"AQAB\",\"use\":\"sig\",\"kid\":\"ozG_ySMHRsVQFmN1mVBeS-WtCupY1r-K7ewben09IBg\",\"alg\":\"RS256\",\"n\":\"wRdwksGIAR2A4cHsoOsYcGp5AmQl5ZjF5xIPXeyjkaLHmNTMvjixdWso1ecVlVeg_6pIXzMRhmOvmjXjz1PLfI2GD3drmeqsStjISWdDfH_rIQCYc9wYbWIZ3bQ0wFRDaVpZ6iOZ2iNcIevvZQKNw9frJthKSMM52JtsgwrgN--Ub2cKWioU_d52SC2SfDzOdnChqlU7xkqXwKXSUqcGM92A35dJJXkwbZhAHnDy5FST1HqYq27MOLzBkChw1bJQHZtlSqkxcHPxphnnbFKQmwRVUvyC5kfBemX-7Mzp1wDogt5lGvBAf3Eq8rFxaevAke327rM7q2KqO_LDMN2J-Q\"}]}";
-        String expectedPublicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwRdwksGIAR2A4cHsoOsY\ncGp5AmQl5ZjF5xIPXeyjkaLHmNTMvjixdWso1ecVlVeg/6pIXzMRhmOvmjXjz1PL\nfI2GD3drmeqsStjISWdDfH/rIQCYc9wYbWIZ3bQ0wFRDaVpZ6iOZ2iNcIevvZQKN\nw9frJthKSMM52JtsgwrgN++Ub2cKWioU/d52SC2SfDzOdnChqlU7xkqXwKXSUqcG\nM92A35dJJXkwbZhAHnDy5FST1HqYq27MOLzBkChw1bJQHZtlSqkxcHPxphnnbFKQ\nmwRVUvyC5kfBemX+7Mzp1wDogt5lGvBAf3Eq8rFxaevAke327rM7q2KqO/LDMN2J\n+QIDAQAB\n-----END PUBLIC KEY-----";
 
         when(restTemplate.getForObject("https://zosmf:1433/jwt/ibm/api/zOSMFBuilder/jwk", String.class)).thenReturn(jwk);
 
@@ -39,7 +37,7 @@ public class ZosmfJwkToPublicKeyTest {
         try {
             String filename = f.getName();
             assertTrue(zosmfJwkToPublicKey.updateJwtPublicKeyFile("https://zosmf:1433", filename));
-            assertEquals(expectedPublicKey, new String(Files.readAllBytes(Paths.get(filename))).trim());
+            assertTrue(new String(Files.readAllBytes(Paths.get(filename))).contains("-----END CERTIFICATE-----"));
         } finally {
             f.delete();
         }
