eb4942d61e4b9a293380ed16b0573bf95909dc1a
achmelo <37397715+achmelo@users.noreply.github.com>
2020-06-04 14:38:48 +0200

    Add SAF keyring support to ZAAS client  (#686)
    * Add SAF keyring support to ZAAS client and controller to Discoverable client as option to test it
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * for debug purpose only
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * s
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * print error info
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * log RC
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * .
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * tlsv1.2, ssl debug
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * remove logs
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * add unit tests, move zaasclient to service for testing and further extensions
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * test wrong trustStore config, remove public modifiers
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>

---
+++ discoverable-client/src/test/java/org/zowe/apiml/client/api/ZaasClientTestControllerTest.java
@@ -0,0 +1,72 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.client.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.web.servlet.MockMvc;
+import org.zowe.apiml.client.configuration.ApplicationConfiguration;
+import org.zowe.apiml.client.configuration.SpringComponentsConfiguration;
+import org.zowe.apiml.client.service.ZaasClientService;
+import org.zowe.apiml.zaasclient.exception.ZaasClientErrorCodes;
+import org.zowe.apiml.zaasclient.exception.ZaasClientException;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {ZaasClientTestController.class})
+@Import(value = {SpringComponentsConfiguration.class, ApplicationConfiguration.class, AnnotationConfigContextLoader.class})
+public class ZaasClientTestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @MockBean
+    private ZaasClientService zaasClientService;
+
+    @Test
+    public void forwardLoginTest_successfulLogin() throws Exception {
+        LoginRequest loginRequest = new LoginRequest("username", "password");
+        when(zaasClientService.login("username", "password")).thenReturn("token");
+
+        this.mockMvc.perform(
+            post("/api/v1/zaasClient")
+                .content(mapper.writeValueAsString(loginRequest))
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(content().string("token"));
+    }
+
+    @Test
+    public void forwardLoginTest_invalidCredentials() throws Exception {
+        LoginRequest loginRequest = new LoginRequest("incorrectUser", "incorrectPass");
+        when(zaasClientService.login("incorrectUser", "incorrectPass"))
+            .thenThrow(new ZaasClientException(ZaasClientErrorCodes.INVALID_AUTHENTICATION));
+
+        this.mockMvc.perform(
+            post("/api/v1/zaasClient")
+                .content(mapper.writeValueAsString(loginRequest))
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().is(401))
+            .andExpect(content().string("Invalid username or password"));
+    }
+
+}
--- zaas-client/src/test/java/org/zowe/apiml/zaasclient/service/internal/CloseableClientProviderTest.java
+++ zaas-client/src/test/java/org/zowe/apiml/zaasclient/service/internal/CloseableClientProviderTest.java
@@ -25,6 +25,7 @@ import java.util.Properties;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -152,5 +153,23 @@ public class CloseableClientProviderTest {
         });
 
         assertThat(zaasException.getErrorCode().getId(), is("ZWEAS502E"));
+    }
+
+    @Test
+    void givenInvalidTrustStoreType_whenTheClientIsConstructed_thenExceptionIsThrown() {
+        ConfigProperties config = new ConfigProperties();
+        config.setTrustStorePassword("password");
+        config.setTrustStorePath("src/test/resources/localhost.truststore.p12");
+        config.setTrustStoreType("invalidCryptoType");
+        ZaasConfigurationException zaasException = assertThrows(
+            ZaasConfigurationException.class, () -> new HttpsClientProvider(config)
+        );
+        assertThat(zaasException.getErrorCode().getId(), is("ZWEAS502E"));
+    }
+
+    @Test
+    void testReplaceFourSlashes() {
+        String newUrl = HttpsClientProvider.replaceFourSlashes("safkeyring:////userId/keyRing");
+        assertEquals("safkeyring://userId/keyRing", newUrl);
     }
 }
