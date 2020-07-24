f8342f031c9166338ce63871ddbae4a8f48b94d7
Carson Cook <carson.cook@ibm.com>
2020-07-24 13:41:16 -0400

    Add tests for mediation client endpoint in discoverable client
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- discoverable-client/src/test/java/org/zowe/apiml/client/api/ApiMediationClientTestControllerTest.java
+++ discoverable-client/src/test/java/org/zowe/apiml/client/api/ApiMediationClientTestControllerTest.java
@@ -1,4 +1,75 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package org.zowe.apiml.client.api;
 
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.zowe.apiml.client.service.ApiMediationClientService;
+import org.zowe.apiml.exception.ServiceDefinitionException;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {ApiMediationClientTestController.class})
 public class ApiMediationClientTestControllerTest {
+    private static final String MEDIATION_CLIENT_URI = "/api/v1/apiMediationClient";
+
+    private final ExpectedException exceptionRule = ExpectedException.none();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ApiMediationClientService apiMediationClientService;
+
+    @Test
+    public void registrationTest_successful() throws Exception {
+        this.mockMvc.perform(
+            post(MEDIATION_CLIENT_URI))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void registerTest_duplicate() throws ServiceDefinitionException {
+        exceptionRule.expect(ServiceDefinitionException.class);
+        apiMediationClientService.register();
+        apiMediationClientService.register();
+    }
+
+    @Test
+    public void unregisterTest_successful() throws Exception {
+        apiMediationClientService.register();
+        this.mockMvc.perform(
+            delete(MEDIATION_CLIENT_URI))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void isRegisteredTest_notRegistered() throws Exception {
+        this.mockMvc.perform(
+            get(MEDIATION_CLIENT_URI))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.isRegistered", is(false)));
+    }
+
+    @Test
+    public void isRegisteredTestService_notRegistered() throws Exception {
+        assertFalse(apiMediationClientService.isRegistered());
+    }
 }
