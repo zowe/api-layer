9bae9efdf93d3768f362584c69c825492891e93c
Jakub Balhar <jakub.balhar@broadcom.com>
2020-08-10 09:21:56 +0200

    Remove unused import
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/login/saf/ZosAuthenticationProvider.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/login/saf/ZosAuthenticationProvider.java
@@ -17,7 +17,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
