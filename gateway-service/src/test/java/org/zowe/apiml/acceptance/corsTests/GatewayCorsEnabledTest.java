0745329e8df47d638c70e4d18fcf9e85756a36f1
Jakub Balhar <jakub@balhar.net>
2020-08-17 13:19:06 +0200

    Provide simple configuration for CircleCI (#788)
    * Initial version of the Circle CI Config
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Configuration based on the openjdk-8 image
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Add missing space
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Run global command as sudo
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Install pnpm only locally.
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Install FE dependencies as separate step
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Install pnpm as part of dependency installation
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Install directly with sudo
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Return version 4 of pnpm
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Disable tests failing on older Linux host versions
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Disable tests failing on older Linux host versions
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Set CI to false
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Disable only for certain environments
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Echo os info
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * By default run the tests. Ignore on specific environments
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Different aproach to conditional disabling
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Different name for export
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    Co-authored-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/acceptance/corsTests/GatewayCorsEnabledTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/acceptance/corsTests/GatewayCorsEnabledTest.java
@@ -16,6 +16,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.DisabledIf;
 import org.zowe.apiml.acceptance.common.AcceptanceTest;
 import org.zowe.apiml.acceptance.common.AcceptanceTestWithTwoServices;
 
@@ -29,6 +30,10 @@ import static org.mockito.Mockito.*;
 @AcceptanceTest
 @ActiveProfiles("test")
 @DirtiesContext
+@DisabledIf(
+    expression = "${environment.older}",
+    loadContext = true
+)
 class GatewayCorsEnabledTest extends AcceptanceTestWithTwoServices {
     @Test
     // The CORS headers are properly set on the request
