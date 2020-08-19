9916fb1c3cc10a8899541637d8be335861d595c4
Jakub Balhar <jakub.balhar@broadcom.com>
2020-08-19 09:06:35 +0200

    Add tests for zOSMF and Dummy authentication providers
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

---
+++ integration-tests/src/test/java/org/zowe/apiml/gatewayservice/authentication/DummyLogoutTest.java
@@ -0,0 +1,35 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.gatewayservice.authentication;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.zowe.apiml.gatewayservice.SecurityUtils;
+
+import static org.zowe.apiml.gatewayservice.SecurityUtils.getConfiguredSslConfig;
+
+class DummyLogoutTest extends LogoutTest {
+    private static AuthenticationProviders providers = new AuthenticationProviders(SecurityUtils.getGateWayUrl("/authentication"));
+
+    // Change to dummy and run the same test as for the zOSMF
+    @BeforeAll
+    static void switchToTestedProvider() {
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
+
+        providers.switchProvider("dummy");
+    }
+
+    @AfterAll
+    static void switchToDefaultProvider() {
+        providers.switchProvider(null);
+    }
+}
