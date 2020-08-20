541fbf42d939952ff15a8c5e3a8c27b63adeae32
jandadav <janda.david@gmail.com>
2020-08-20 08:49:53 +0200

    two tokens tests
    Signed-off-by: jandadav <janda.david@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/gatewayservice/authentication/SafLogoutTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/gatewayservice/authentication/SafLogoutTest.java
@@ -12,13 +12,17 @@ package org.zowe.apiml.gatewayservice.authentication;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.zowe.apiml.gatewayservice.SecurityUtils;
 import org.zowe.apiml.util.categories.MainframeDependentTests;
 
+import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_NO_CONTENT;
+import static org.hamcrest.core.Is.is;
 import static org.zowe.apiml.gatewayservice.SecurityUtils.getConfiguredSslConfig;
 
 @MainframeDependentTests
-public class SafLogoutTest extends LogoutTest {
+class SafLogoutTest extends LogoutTest {
     private static AuthenticationProviders providers = new AuthenticationProviders(SecurityUtils.getGateWayUrl("/authentication"));
 
     // Change to saf and run the same test as for the zOSMF
@@ -28,6 +32,25 @@ public class SafLogoutTest extends LogoutTest {
         RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
 
         providers.switchProvider("saf");
+    }
+
+    @Test
+    void givenTwoValidTokens_whenLogoutCalledOnFirstOne_thenSecondStillValid() {
+        String jwt1 = generateToken();
+        String jwt2 = generateToken();
+
+        assertIfLogged(jwt1, true);
+        assertIfLogged(jwt2, true);
+
+        given()
+            .cookie(COOKIE_NAME, jwt1)
+            .when()
+            .post(SecurityUtils.getGateWayUrl(LOGOUT_ENDPOINT))
+            .then()
+            .statusCode(is(SC_NO_CONTENT));
+
+        assertIfLogged(jwt1, false);
+        assertIfLogged(jwt2, true);
     }
 
     @AfterAll
