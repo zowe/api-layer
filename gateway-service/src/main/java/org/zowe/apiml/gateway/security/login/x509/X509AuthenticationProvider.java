a0ae1e43ba41e7a647ac0add2153d283c0ac5ded
Jakub Balhar <jakub.balhar@broadcom.com>
2020-09-15 11:28:35 +0200

    X509 Provider supports generation of Passticket for the zOSMF
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/login/x509/X509AuthenticationProvider.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/login/x509/X509AuthenticationProvider.java
@@ -10,10 +10,17 @@
 package org.zowe.apiml.gateway.security.login.x509;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.zowe.apiml.gateway.security.login.Providers;
+import org.zowe.apiml.gateway.security.login.zosmf.ZosmfAuthenticationProvider;
 import org.zowe.apiml.gateway.security.service.AuthenticationService;
+import org.zowe.apiml.passticket.IRRPassTicketGenerationException;
+import org.zowe.apiml.passticket.PassTicketService;
 import org.zowe.apiml.security.common.error.AuthenticationTokenException;
 import org.zowe.apiml.security.common.token.X509AuthenticationToken;
 
@@ -23,21 +30,65 @@ import java.security.cert.X509Certificate;
 @RequiredArgsConstructor
 public class X509AuthenticationProvider implements AuthenticationProvider {
 
+    @Value("${apiml.security.zosmf.applid:IZUDFLT}")
+    private String zosmfApplId;
+    @Value("${apiml.security.x509.enabled:false}")
+    boolean isClientCertEnabled;
+
     private final X509AuthenticationMapper x509AuthenticationMapper;
     private final AuthenticationService authenticationService;
 
+    private final PassTicketService passTicketService;
+    private final ZosmfAuthenticationProvider zosmfAuthenticationProvider;
+    private final Providers providers;
+
+    /**
+     * Performs Authentication of Client certificate
+     * <p>
+     * Maps certificate to mainframe UserId
+     * <p>
+     * If z/OSMF is active, authenticate against it with passticket.
+     * Otherwise, the fact that mapping happened is proof of authentication
+     * If SAF or DUMMY auth providers are selected, they defer the decision to the mapping component.
+     * For list of mapping components, see implementations of {@link X509AuthenticationMapper}
+     *
+     * @param {@link Authentication}
+     * @return {@link Authentication}
+     */
     @Override
     public Authentication authenticate(Authentication authentication) {
         if (authentication instanceof X509AuthenticationToken) {
+            if (!isClientCertEnabled) {
+                return null;
+            }
+
             X509Certificate[] certs = (X509Certificate[]) authentication.getCredentials();
             String username = x509AuthenticationMapper.mapCertificateToMainframeUserId(certs[0]);
             if (username == null) {
                 return null;
             }
-            final String domain = "security-domain";
-            final String jwtToken = authenticationService.createJwtToken(username, domain, null);
-            return authenticationService.createTokenAuthentication(username, jwtToken);
+
+            boolean isZosmfUsedAndAvailable = false;
+            try {
+                isZosmfUsedAndAvailable = providers.isZosfmUsed() && providers.isZosmfAvailable();
+            } catch (AuthenticationServiceException ex) {
+                // Intentionally do nothing. The issue is logged deeper.
+            }
+
+            if (isZosmfUsedAndAvailable) {
+                try {
+                    String passTicket = passTicketService.generate(username, zosmfApplId);
+                    return zosmfAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(username, passTicket));
+                } catch (IRRPassTicketGenerationException e) {
+                    throw new AuthenticationTokenException("Problem with generating PassTicket");
+                }
+            } else {
+                final String domain = "security-domain";
+                final String jwtToken = authenticationService.createJwtToken(username, domain, null);
+                return authenticationService.createTokenAuthentication(username, jwtToken);
+            }
         } else {
+            // This should never happen because of supports method which should fail.
             throw new AuthenticationTokenException("Wrong authentication token. " + authentication.getClass());
         }
     }
