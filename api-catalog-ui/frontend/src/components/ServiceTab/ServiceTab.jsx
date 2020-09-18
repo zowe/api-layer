fc104abcab2e029be074a066032793469a934221
Carson Cook <carson.cook@ibm.com>
2020-09-18 09:17:35 -0400

    Add tooltips
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- api-catalog-ui/frontend/src/components/ServiceTab/ServiceTab.jsx
+++ api-catalog-ui/frontend/src/components/ServiceTab/ServiceTab.jsx
@@ -80,9 +80,29 @@ export default class ServiceTab extends Component {
                                     )}
                                     <br />
                                     <br />
-                                    <Text style={{ fontSize: '13px' }}>Base URL: {selectedService.baseUrl}</Text>
-                                    <Text style={{ fontSize: '13px' }}>API Base Path: {selectedService.basePath}</Text>
-                                    <Text style={{ fontSize: '13px' }}>Service ID: {selectedService.serviceId}</Text>
+                                    <Tooltip
+                                        key={selectedService.baseUrl}
+                                        content="The instance URL for this service"
+                                        placement="bottom"
+                                    >
+                                        <Text style={{ fontSize: '13px' }}>Instance URL: {selectedService.baseUrl}</Text>
+                                    </Tooltip>
+                                    <br/>
+                                    <Tooltip
+                                        key={selectedService.basePath}
+                                        content="The path used by the Gateway to access API endpoints. This can be used to identify a service in client tools like Zowe CLI and Zowe explorer."
+                                        placement="bottom"
+                                    >
+                                        <Text style={{ fontSize: '13px' }}>API Base Path: {selectedService.basePath}</Text>
+                                    </Tooltip>
+                                    <br/>
+                                    <Tooltip
+                                        key={selectedService.serviceId}
+                                        content="The identifier for this service"
+                                        placement="bottom"
+                                    >
+                                        <Text style={{ fontSize: '13px' }}>Service ID: {selectedService.serviceId}</Text>
+                                    </Tooltip>
                                     <Text style={{ marginTop: '15px' }}>{selectedService.description}</Text>
                                 </div>
                             </div>
