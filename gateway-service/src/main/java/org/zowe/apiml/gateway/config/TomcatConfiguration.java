4aef96efa4422350f19f31740c1181af87c9ba47
jandadav <janda.david@gmail.com>
2020-10-13 14:37:01 +0200

    Additional connector
    Signed-off-by: jandadav <janda.david@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/config/TomcatConfiguration.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/config/TomcatConfiguration.java
@@ -9,10 +9,14 @@
  */
 package org.zowe.apiml.gateway.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
 
 /**
  * Configuration of Tomcat
@@ -24,6 +28,25 @@ public class TomcatConfiguration {
         System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
         tomcat.setProtocol(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
         return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+        File keystore = new File("keystore/localhost/localhost.keystore.p12");
+        File truststore = new File("keystore/localhost/localhost.truststore.p12");
+        connector.setScheme("https");
+        connector.setSecure(true);
+        connector.setPort(55756);
+        protocol.setSSLEnabled(true);
+        protocol.setKeystoreFile(keystore.getAbsolutePath());
+        protocol.setKeystorePass("password");
+        protocol.setTruststoreFile(truststore.getAbsolutePath());
+        protocol.setTruststorePass("password");
+        protocol.setKeyAlias("localhost");
+        return connector;
     }
 }
