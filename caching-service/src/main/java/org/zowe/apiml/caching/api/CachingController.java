15527385e3f04803290b19b19a6d0dd7d2bcdb18
Carson Cook <carson.cook@ibm.com>
2020-10-16 15:49:03 -0400

    Setup initial unit tests
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/api/CachingController.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/api/CachingController.java
@@ -13,20 +13,65 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zowe.apiml.caching.model.KeyValue;
 import org.zowe.apiml.caching.service.Storage;
 
 @RestController
 @RequiredArgsConstructor
 public class CachingController {
+    //TODO what is no authorization? How separate from jwt invalid?
+    //TODO how to do authentication?
+    //TODO hash key values to adjust for limit of 250 chars, and only use ascii alphanum
+
     private final Storage storage;
 
     @RequestMapping(value = "/api/v1/cache/{key}", produces = "application/json; charset=utf-8", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<?> getKey(
-        @PathVariable String key
-    ) {
-        String serviceId = "test-service";
+    public ResponseEntity<?> getValue(@PathVariable String key) {
+        //TODO 400 if no key
+        //TODO 404 if key not in cache
+        String serviceId = getServiceId();
         return new ResponseEntity<>(storage.read(serviceId, key), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/api/v1/cache", produces = "application/json; charset=utf-8", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> getAllValues() {
+        String serviceId = getServiceId();
+        return new ResponseEntity<>(storage.readForService(serviceId), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/api/v1/cache", produces = "application/json; charset=utf-8", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> createKey(@RequestBody KeyValue keyValue) {
+        //TODO 400 - invalid json data, no json data
+        //TODO 409 key already exists
+        String serviceId = getServiceId();
+        storage.create(serviceId, keyValue);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/api/v1/cache/{key}", produces = "application/json; charset=utf-8", method = RequestMethod.PUT)
+    @ResponseBody
+    public ResponseEntity<?> update(@RequestBody KeyValue keyValue) {
+        //TODO 400 - no key, no authorization provided
+        //TODO 404 key not in cache
+        String serviceId = getServiceId();
+        storage.update(serviceId, keyValue);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/api/v1/cache/{key}", produces = "application/json; charset=utf-8", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResponseEntity<?> delete(@PathVariable String key) {
+        //TODO 400 - no key
+        //TODO 404 key not in cache
+        String serviceId = getServiceId();
+        storage.delete(serviceId, key);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    private String getServiceId() {
+        return "test-service"; //TODO get from auth
+    }
 }
--- caching-service/src/test/java/org/zowe/apiml/caching/api/CachingControllerTest.java
+++ caching-service/src/test/java/org/zowe/apiml/caching/api/CachingControllerTest.java
@@ -11,16 +11,23 @@ package org.zowe.apiml.caching.api;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.zowe.apiml.caching.model.KeyValue;
 import org.zowe.apiml.caching.service.Storage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CachingControllerTest {
+    private static final String SERVICE_ID = "test-service";
+
     private CachingController underTest;
 
     private Storage mockStorage;
@@ -32,11 +39,56 @@ public class CachingControllerTest {
     }
 
     @Test
-    void givenStorageReturnsValidValue_whenRequired_thenItIsProperlyProvided() {
-        when(mockStorage.read("test-service", "key")).thenReturn(new KeyValue("key", "value"));
+    void givenStorageReturnsValidValue_whenGetByKey_thenItReturnProperValue() {
+        when(mockStorage.read(SERVICE_ID, "key")).thenReturn(new KeyValue("key", "value"));
 
-        ResponseEntity<?> key = underTest.getKey("key");
-        KeyValue result =  (KeyValue) key.getBody();
-        assertThat(result.getValue(), is("value"));
+        ResponseEntity<?> response = underTest.getValue("key");
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+
+        KeyValue body = (KeyValue) response.getBody();
+        assertThat(body.getValue(), is("value"));
+    }
+
+    @Test
+    void givenStorageReturnsValidValues_whenGetByService_thenItReturnsProperValues() {
+        Map<String, KeyValue> values = new HashMap<>();
+        values.put("key", new KeyValue("key2", "value"));
+        when(mockStorage.readForService(SERVICE_ID)).thenReturn(values);
+
+        ResponseEntity<?> response = underTest.getAllValues();
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+
+        Map<String, KeyValue> result = (Map<String, KeyValue>) response.getBody();
+        assertThat(result, is(values));
+    }
+
+    @Test
+    void givenStorage_whenCreateKey_thenResponseOk() {
+        KeyValue keyValue = new KeyValue("key", "value");
+        when(mockStorage.create(SERVICE_ID, keyValue)).thenReturn(keyValue);
+
+        ResponseEntity<?> response = underTest.createKey(keyValue);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody(), is(nullValue()));
+    }
+
+    @Test
+    void givenStorageWithKey_whenUpdateKey_thenResponseNoContent() {
+        //TODO decide payload structure
+        KeyValue keyValue = new KeyValue("key", "value");
+        when(mockStorage.update(SERVICE_ID, keyValue)).thenReturn(keyValue);
+
+        ResponseEntity<?> response = underTest.update(keyValue);
+        assertThat(response.getStatusCode(), is(HttpStatus.NO_CONTENT));
+        assertThat(response.getBody(), is(nullValue()));
+    }
+
+    @Test
+    void givenStorageWithKey_whenDeleteKey_thenResponseNoContent() {
+        when(mockStorage.delete(SERVICE_ID, "key")).thenReturn(new KeyValue("key", "value"));
+
+        ResponseEntity<?> response = underTest.delete("key");
+        assertThat(response.getStatusCode(), is(HttpStatus.NO_CONTENT));
+        assertThat(response.getBody(), is(nullValue()));
     }
 }
