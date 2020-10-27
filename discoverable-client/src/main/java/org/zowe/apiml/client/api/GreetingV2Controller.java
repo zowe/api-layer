d7683a337c9be9f7560a2dd87707f7ed0fc4b601
Carson Cook <carson.cook@ibm.com>
2020-10-27 15:26:02 -0400

    Add greeting v2 api
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- discoverable-client/src/main/java/org/zowe/apiml/client/api/GreetingV2Controller.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/api/GreetingV2Controller.java
@@ -1,12 +1,3 @@
-/*
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Copyright Contributors to the Zowe Project.
- */
 package org.zowe.apiml.client.api;
 
 import io.swagger.annotations.Api;
@@ -18,17 +9,16 @@ import org.zowe.apiml.client.model.Greeting;
 
 import java.util.Date;
 
-
 /**
- * Version 1 of the controller that returns greetings.
+ * Version 2 of the controller that returns greetings.
  */
 @RestController
 @Api(tags = {"Other Operations"})
 @SwaggerDefinition(tags = {
     @Tag(name = "Other Operations", description = "General Operations")})
-@RequestMapping("/api/v1")
-public class GreetingController {
-    private static final String TEMPLATE = "Hello, %s!";
+@RequestMapping("/api/v2")
+public class GreetingV2Controller {
+    private static final String TEMPLATE = "Hi, %s!";
 
     /**
      * Gets a greeting for anyone.
@@ -36,7 +26,7 @@ public class GreetingController {
     @GetMapping(value = "/greeting")
     @ApiOperation(value = "Get a greeting", response = Greeting.class,
         tags = {"Other Operations"})
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name,
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "user") String name,
                              @RequestParam(value = "delayMs", defaultValue = "0", required = false) Integer delayMs) {
         if (delayMs > 0) {
             try {
@@ -51,11 +41,10 @@ public class GreetingController {
     /**
      * Gets a custom greeting.
      */
-    @GetMapping(value = {"/{yourName}/greeting"})
+    @GetMapping(value = {"/greeting/{yourName}"})
     @ApiOperation(value = "Get a greeting", response = Greeting.class,
         tags = {"Other Operations"})
     public Greeting customGreeting(@PathVariable(value = "yourName") String yourName) {
         return new Greeting(new Date(), String.format(TEMPLATE, yourName));
     }
 }
-
