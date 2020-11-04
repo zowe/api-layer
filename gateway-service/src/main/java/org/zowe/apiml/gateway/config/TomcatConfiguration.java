2129c783a76c1b61f8d4a8b4443376a3da3ba394
Jakub Balhar <jakub.balhar@broadcom.com>
2020-11-04 09:22:17 +0100

    Add another configurable SSL port
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/config/TomcatConfiguration.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/config/TomcatConfiguration.java
@@ -9,21 +9,77 @@
  */
 package org.zowe.apiml.gateway.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
 
 /**
  * Configuration of Tomcat
  */
 @Configuration
 public class TomcatConfiguration {
+    @Value("${server.internal.enabled:false}")
+    private boolean enableInternalPort;
+    @Value("${server.internal.ssl.enabled:true}")
+    private boolean enableSslOnInternal;
+    @Value("${server.internal.port:10017}")
+    private int internalPort;
+
+    @Value("${server.internal.ssl.keyStore:keystore/localhost/localhost.keystore.p12}")
+    private String keyStorePath;
+    @Value("${server.internal.ssl.keyStorePassword:password}")
+    private String keyStorePassword;
+    @Value("${server.internal.ssl.keyPassword:password}")
+    private String keyPassword;
+    @Value("${server.internal.ssl.keyAlias:localhost}")
+    private String keyAlias;
+    @Value("${server.internal.ssl.trustStore:keystore/localhost/localhost.truststore.p12}")
+    private String trustStorePath;
+    @Value("${server.internal.ssl.trustStorePassword:password}")
+    private String trustStorePassword;
+
     @Bean
     public ServletWebServerFactory servletContainer() {
         System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
         tomcat.setProtocol(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        if (enableInternalPort) {
+            tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        }
         return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+        connector.setPort(internalPort);
+        if (enableSslOnInternal) {
+            connector.setScheme("https");
+
+            connector.setSecure(true);
+            protocol.setSSLEnabled(true);
+
+            File keyStore = new File(keyStorePath);
+            File trustStore = new File(trustStorePath);
+
+            protocol.setKeystoreFile(keyStore.getAbsolutePath());
+            protocol.setKeystorePass(keyStorePassword);
+            protocol.setTruststoreFile(trustStore.getAbsolutePath());
+            protocol.setTruststorePass(trustStorePassword);
+            protocol.setKeyAlias(keyAlias);
+            protocol.setKeyPass(keyPassword);
+        } else {
+            connector.setScheme("http");
+            connector.setSecure(false);
+        }
+
+        return connector;
     }
 }
