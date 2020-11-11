27ee36ce1a99d86e91572941f58a04a8c861e32d
Carson Cook <carson.cook@ibm.com>
2020-11-11 15:26:51 -0500

    Separate services start into different sh files
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

---
+++ zowe-install/src/main/resources/component-scripts/start-gateway.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+################################################################################
+# This program and the accompanying materials are made available under the terms of the
+# Eclipse Public License v2.0 which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Copyright IBM Corporation 2019
+################################################################################
+
+GATEWAY_CODE=AG
+_BPX_JOBNAME=${ZOWE_PREFIX}${GATEWAY_CODE} java -Xms32m -Xmx256m -Xquickstart \
+    -Dibm.serversocket.recover=true \
+    -Dfile.encoding=UTF-8 \
+    -Djava.io.tmpdir=/tmp \
+    -Dspring.profiles.include=$LOG_LEVEL \
+    -Dapiml.service.hostname=${ZOWE_EXPLORER_HOST} \
+    -Dapiml.service.port=${GATEWAY_PORT} \
+    -Dapiml.service.discoveryServiceUrls=${ZWE_DISCOVERY_SERVICES_LIST} \
+    -Dapiml.service.preferIpAddress=${APIML_PREFER_IP_ADDRESS} \
+    -Dapiml.service.allowEncodedSlashes=${APIML_ALLOW_ENCODED_SLASHES} \
+    -Dapiml.service.corsEnabled=${APIML_CORS_ENABLED} \
+    -Dapiml.cache.storage.location=${WORKSPACE_DIR}/api-mediation/ \
+    -Dapiml.logs.location=${WORKSPACE_DIR}/api-mediation/logs \
+    -Denvironment.ipAddress=${ZOWE_IP_ADDRESS} \
+    -Dapiml.gateway.timeoutMillis=${APIML_GATEWAY_TIMEOUT_MILLIS} \
+    -Dapiml.security.ssl.verifySslCertificatesOfServices=${VERIFY_CERTIFICATES} \
+    -Dapiml.security.auth.zosmfServiceId=zosmf \
+    -Dapiml.security.auth.provider=${APIML_SECURITY_AUTH_PROVIDER} \
+    -Dapiml.zoweManifest=${ZOWE_MANIFEST} \
+    -Dserver.address=0.0.0.0 \
+    -Dserver.ssl.enabled=true \
+    -Dserver.ssl.keyStore=${KEYSTORE} \
+    -Dserver.ssl.keyStoreType=${KEYSTORE_TYPE} \
+    -Dserver.ssl.keyStorePassword=${KEYSTORE_PASSWORD} \
+    -Dserver.ssl.keyAlias=${KEY_ALIAS} \
+    -Dserver.ssl.keyPassword=${KEYSTORE_PASSWORD} \
+    -Dserver.ssl.trustStore=${TRUSTSTORE} \
+    -Dserver.ssl.trustStoreType=${KEYSTORE_TYPE} \
+    -Dserver.ssl.trustStorePassword=${KEYSTORE_PASSWORD} \
+    -Dapiml.security.x509.enabled=${APIML_SECURITY_X509_ENABLED:-false} \
+    -Dapiml.security.x509.externalMapperUrl=http://localhost:${ZOWE_ZSS_SERVER_PORT}/certificate/x509/map \
+    -Dapiml.security.x509.externalMapperUser=ZWESVUSR \
+    -Dapiml.security.zosmf.applid=${APIML_SECURITY_ZOSMF_APPLID} \
+    -Djava.protocol.handler.pkgs=com.ibm.crypto.provider \
+    -cp ${ROOT_DIR}"/components/api-mediation/gateway-service.jar":/usr/include/java_classes/IRRRacf.jar \
+    org.springframework.boot.loader.PropertiesLauncher &
+export GATEWAY_PID=$?
