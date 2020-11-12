e2890caea28f3175cc95fcead79d2c61eaf02021
Carson Cook <carson.cook@ibm.com>
2020-11-12 08:15:44 -0500

    Add setup script
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- zowe-install/src/main/resources/component-scripts/start.sh
+++ zowe-install/src/main/resources/component-scripts/start.sh
@@ -25,37 +25,9 @@
 # - ALLOW_SLASHES - Allows encoded slashes on on URLs through gateway
 # - ZOWE_MANIFEST - The full path to Zowe's manifest.json file
 
-# API Mediation Layer Debug Mode
-export LOG_LEVEL=
+# Script is ran from run-zowe.sh context so use ROOT_DIR to make a full path to each script
 
-if [[ ! -z ${APIML_DEBUG_MODE_ENABLED} && ${APIML_DEBUG_MODE_ENABLED} == true ]]
-then
-  export LOG_LEVEL="debug"
-fi
-
-stop_jobs()
-{
-  kill -15 "$DISCOVERY_PID" "$CATALOG_PID" "$GATEWAY_PID" "$CACHE_PID"
-}
-
-trap 'stop_jobs' INT
-
-# If set append $ZWEAD_EXTERNAL_STATIC_DEF_DIRECTORIES to $STATIC_DEF_CONFIG_DIR
-export APIML_STATIC_DEF=${STATIC_DEF_CONFIG_DIR}
-if [[ ! -z "$ZWEAD_EXTERNAL_STATIC_DEF_DIRECTORIES" ]]
-then
-  export APIML_STATIC_DEF="${APIML_STATIC_DEF};${ZWEAD_EXTERNAL_STATIC_DEF_DIRECTORIES}"
-fi
-
-LIBPATH="$LIBPATH":"/lib"
-LIBPATH="$LIBPATH":"/usr/lib"
-LIBPATH="$LIBPATH":"${JAVA_HOME}"/bin
-LIBPATH="$LIBPATH":"${JAVA_HOME}"/bin/classic
-LIBPATH="$LIBPATH":"${JAVA_HOME}"/bin/j9vm
-LIBPATH="$LIBPATH":"${JAVA_HOME}"/lib/s390/classic
-LIBPATH="$LIBPATH":"${JAVA_HOME}"/lib/s390/default
-LIBPATH="$LIBPATH":"${JAVA_HOME}"/lib/s390/j9vm
-export LIBPATH="$LIBPATH":
+. "${ROOT_DIR}"/components/api-mediation/bin/setup.sh
 
 . "${ROOT_DIR}"/components/api-mediation/bin/start-discovery.sh &
 . "${ROOT_DIR}"/components/api-mediation/bin/start-catalog.sh &
