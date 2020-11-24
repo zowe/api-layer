0b9563ac510c88f6cd733a6e936a9582a7050fd4
Carson Cook <CarsonCook@users.noreply.github.com>
2020-11-24 08:46:31 -0500

    Zaas client enhancements (#935)
    * Move get jwt from request to apiml security common
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add apiml security common dependency to zaas client
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Implement get jwt from request in query
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add unit tests
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Move TokenUtils to common service to avoid login filters
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Improve error responses
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Mock static function for zaas unit tests
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Improve error message for invalid token
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add documentation:
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix code smells
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Remove zaas depedency on apiml-common
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Remove DC secure off flags
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix code smell
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Remove problem dependency
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- zaas-client/src/main/java/org/zowe/apiml/zaasclient/service/internal/TokenService.java
+++ zaas-client/src/main/java/org/zowe/apiml/zaasclient/service/internal/TokenService.java
@@ -13,6 +13,8 @@ import org.zowe.apiml.zaasclient.exception.ZaasClientException;
 import org.zowe.apiml.zaasclient.exception.ZaasConfigurationException;
 import org.zowe.apiml.zaasclient.service.ZaasToken;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Operations related to the tokens. Mainly JWT token.
  */
@@ -20,11 +22,11 @@ interface TokenService {
     /**
      * Tries to login a user given the id and the password of the user. The password is expected in plain text.
      *
-     * @param userId Id of the user as the user is known to the authentication service
+     * @param userId   Id of the user as the user is known to the authentication service
      * @param password Password of the user which is passed through to the authentication service.
      * @return Valid JWT token obtained from the service
      * @throws ZaasClientException If the provided information were incorrect or some other issue with respect to the
-     *   communication with service occurs, this exception with details is thrown.
+     *                             communication with service occurs, this exception with details is thrown.
      */
     String login(String userId, String password) throws ZaasClientException;
 
@@ -34,7 +36,7 @@ interface TokenService {
      * @param authorizationHeader Base 64 encoded information retrievable e.g. from Authorization HTTP header
      * @return Valid JWT token obtained from service
      * @throws ZaasClientException If the provided information were incorrect or some other issue with respect to the
-     *   communication with service occurs, this exception with details is thrown.
+     *                             communication with service occurs, this exception with details is thrown.
      */
     String login(String authorizationHeader) throws ZaasClientException;
 
@@ -43,18 +45,30 @@ interface TokenService {
      *
      * @param token JWT token to get more information about
      * @return Detailed information about the Token based on the information provided by ZAAS integrated in the API
-     *      Mediation Layer
+     * Mediation Layer
      * @throws ZaasClientException If the provided token was expired, invalid or some other issue with respect to
-     *      communication occurs, this exception with details is thrown.
+     *                             communication occurs, this exception with details is thrown.
      */
     ZaasToken query(String token) throws ZaasClientException;
+
+    /**
+     * Return details about the JWT token in the provided HttpServletRequest. If no token exists a ZaasClientException
+     * will be thrown.
+     *
+     * @param request HttpServletRequest representing the request with the token to be queried.
+     * @return ZaasToken instance
+     * @throws ZaasClientException thrown if no token is provided in the request, or if the provided token was expired,
+     *                             invalid or some other issue with respect to communication occurs,
+     *                             this exception with details is thrown.
+     */
+    ZaasToken query(HttpServletRequest request) throws ZaasClientException;
 
     /**
      * Invalidate the provided JWT token in order to perform logout.
      *
      * @param token JWT token to invalidate
-     * @throws ZaasClientException If the provided token was expired, invalid or some other issue with respect to
-     *      communication occurs, this exception with details is thrown.
+     * @throws ZaasClientException        If the provided token was expired, invalid or some other issue with respect to
+     *                                    communication occurs, this exception with details is thrown.
      * @throws ZaasConfigurationException Wrapper for errors in HTTP client and TLS configuration.
      */
     void logout(String token) throws ZaasClientException, ZaasConfigurationException;
