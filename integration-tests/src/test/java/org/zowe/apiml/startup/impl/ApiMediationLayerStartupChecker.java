6c2ef4867222935c8a4724b99f6efb492dd784ef
Jakub Balhar <jakub.balhar@broadcom.com>
2020-11-25 14:12:05 +0100

    Clean the Startup checker and improve readability
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/startup/impl/ApiMediationLayerStartupChecker.java
+++ integration-tests/src/test/java/org/zowe/apiml/startup/impl/ApiMediationLayerStartupChecker.java
@@ -12,6 +12,7 @@ package org.zowe.apiml.startup.impl;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -23,6 +24,8 @@ import org.zowe.apiml.util.http.HttpClientUtils;
 import org.zowe.apiml.util.http.HttpRequestUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -34,9 +37,15 @@ import static org.awaitility.Awaitility.await;
 @Slf4j
 public class ApiMediationLayerStartupChecker {
     private final GatewayServiceConfiguration gatewayConfiguration;
+    private final List<Service> servicesToCheck = new ArrayList<>();
 
     public ApiMediationLayerStartupChecker() {
         gatewayConfiguration = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration();
+
+        servicesToCheck.add(new Service("Gateway", "$.status"));
+        servicesToCheck.add(new Service("Api Catalog", "$.details.gateway.details.apicatalog"));
+        servicesToCheck.add(new Service("Discovery Service", "$.details.gateway.details.discovery"));
+        servicesToCheck.add(new Service("Authentication Service", "$.details.gateway.details.auth"));
     }
 
     public void waitUntilReady() {
@@ -48,7 +57,7 @@ public class ApiMediationLayerStartupChecker {
     }
 
     private boolean isReady() {
-        log.info("Checking of the API Mediation Layer is ready to be used...");
+        log.info("Checking if the API Mediation Layer is ready to be used...");
         int times = 3;
         if (gatewayConfiguration.getInstances() == 2) {
             times = 2;
@@ -58,7 +67,7 @@ public class ApiMediationLayerStartupChecker {
 
     private boolean severalSuccessfulResponses(int times) {
         for (int i = 0; i < times; i++) {
-            if (!successfulResponse()) {
+            if (!areAllServicesUp()) {
                 return false;
             }
         }
@@ -66,45 +75,70 @@ public class ApiMediationLayerStartupChecker {
         return true;
     }
 
-    private boolean successfulResponse() {
+    private DocumentContext getDocumentAsContext() {
         try {
             HttpGet request = HttpRequestUtils.getRequest("/application/health");
             final HttpResponse response = HttpClientUtils.client().execute(request);
             if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
                 log.warn("Unexpected HTTP status code: {}", response.getStatusLine().getStatusCode());
-                return false;
+                return null;
             }
             final String jsonResponse = EntityUtils.toString(response.getEntity());
             log.debug("URI: {}, JsonResponse is {}", request.getURI().toString(), jsonResponse);
 
-            DocumentContext documentContext = JsonPath.parse(jsonResponse);
-
-            boolean isGatewayUp = documentContext.read("$.status").equals("UP");
-            log.debug("Gateway is {}", isGatewayUp ? "UP" : "DOWN");
-
-            boolean isAllInstancesUp = allInstancesUp(documentContext);
-            log.debug("All instances are {}", isAllInstancesUp ? "UP" : "DOWN");
-
-            boolean isTestApplicationUp = testApplicationUp(documentContext);
-            log.debug("Discoverableclient is {}", isTestApplicationUp ? "UP" : "DOWN");
-
-            return isGatewayUp && isAllInstancesUp && isTestApplicationUp;
-        } catch (IOException | PathNotFoundException e) {
-            log.warn("Check failed: {}", e.getMessage());
+            return JsonPath.parse(jsonResponse);
+        } catch (IOException e) {
+            log.warn("Check failed on getting the document: {}", e.getMessage());
+            return null;
         }
-        return false;
     }
 
-    private boolean testApplicationUp(DocumentContext documentContext) {
-        return documentContext.read("$.details.discoveryComposite.details.discoveryClient.details.services").toString()
-            .contains("discoverableclient");
+    private boolean areAllServicesUp() {
+        try {
+            DocumentContext context = getDocumentAsContext();
+            if (context == null) {
+                return false;
+            }
+
+            boolean areAllServicesUp = true;
+            for(Service toCheck: servicesToCheck) {
+                boolean isUp = isServiceUp(context, toCheck.path);
+                logDebug(toCheck.name + " is {}", isUp);
+
+                if(!isUp) {
+                    areAllServicesUp = false;
+                }
+            }
+
+            boolean isTestApplicationUp = context.read("$.details.discoveryComposite.details.discoveryClient.details.services").toString()
+                .contains("discoverableclient");
+            logDebug("Discoverable Client is {}", isTestApplicationUp);
+
+            Integer amountOfActiveGateways = context.read("$.details.gateway.details.gatewayCount");
+            boolean isValidAmountOfGatewaysUp = amountOfActiveGateways != null &&
+                amountOfActiveGateways.equals(gatewayConfiguration.getInstances());
+            log.debug("There is {} gateways", amountOfActiveGateways);
+
+            return areAllServicesUp &&
+                isValidAmountOfGatewaysUp &&
+                isTestApplicationUp;
+        } catch (PathNotFoundException e) {
+            log.warn("Check failed on retrieving the information from document: {}", e.getMessage());
+            return false;
+        }
     }
 
-    private boolean allInstancesUp(DocumentContext documentContext) {
-        return documentContext.read("$.details.gateway.details.apicatalog").equals("UP")
-            && documentContext.read("$.details.gateway.details.discovery").equals("UP")
-            && documentContext.read("$.details.gateway.details.auth").equals("UP")
-            && documentContext.read("$.details.gateway.details.gatewayCount")
-            .equals(gatewayConfiguration.getInstances());
+    private boolean isServiceUp(DocumentContext documentContext, String path) {
+        return documentContext.read(path).equals("UP");
+    }
+
+    private void logDebug(String logMessage, boolean state) {
+        log.debug(logMessage, state ? "UP": "DOWN");
+    }
+
+    @AllArgsConstructor
+    private class Service {
+        String name;
+        String path;
     }
 }
