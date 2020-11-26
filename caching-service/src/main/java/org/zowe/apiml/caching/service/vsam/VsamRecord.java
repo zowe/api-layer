26a6380987dde75fc5c64fa3c423dfd6f8ecbd47
David Janda <janda.david@gmail.com>
2020-11-26 13:59:25 +0100

    Vsam config and helpers (#951)
    * Vsam config and helpers
    
    Signed-off-by: jandadav <janda.david@gmail.com>
    
    * add javadoc
    
    Signed-off-by: jandadav <janda.david@gmail.com>
    
    * more javadoc
    
    Signed-off-by: jandadav <janda.david@gmail.com>
    
    * get rid of VsamUtils
    
    Signed-off-by: jandadav <janda.david@gmail.com>
    
    * reuse getKey method
    
    Signed-off-by: jandadav <janda.david@gmail.com>

---
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/vsam/VsamRecord.java
@@ -0,0 +1,110 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.caching.service.vsam;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.zowe.apiml.caching.config.VsamConfig;
+import org.zowe.apiml.caching.model.KeyValue;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Represents a record data structure in VSAM file.
+ *
+ * The structure is composed of {@link VsamKey} and {@link KeyValue}, serialized to JSON format.
+ * Configuration is driven from {@link VsamConfig}
+ *
+ * Constructors provide ways to create record from raw bytes or from POJOs.
+ * Provides methods to serialize to bytes in platform's encoding.
+ */
+
+public class VsamRecord {
+
+    private final VsamConfig config;
+
+    private String serviceId;
+
+    private VsamKey key;
+    private KeyValue keyValue;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private static final String UNSUPPORTED_ENCODING_MESSAGE = "Unsupported encoding: ";
+
+    public VsamRecord(VsamConfig config, String serviceId, KeyValue kv) {
+        this.config = config;
+        this.serviceId = serviceId;
+        this.keyValue = kv;
+        this.key = new VsamKey(config);
+    }
+
+    public VsamRecord(VsamConfig config, String serviceId, byte[] recordData) throws VsamRecordException {
+        this.config = config;
+        this.serviceId = serviceId;
+        this.key = new VsamKey(config);
+
+        try {
+            String recordString = new String(recordData, config.getEncoding());
+            this.keyValue = mapper.readValue(recordString.substring(config.getKeyLength()).trim(), KeyValue.class);
+        } catch (UnsupportedEncodingException e) {
+            throw new VsamRecordException(UNSUPPORTED_ENCODING_MESSAGE + config.getEncoding(), e);
+        } catch (JsonProcessingException e) {
+            throw new VsamRecordException("Failure deserializing the record value to KeyValue object", e);
+        }
+
+    }
+
+    public byte[] getBytes() throws VsamRecordException {
+        try {
+            byte[] bytes = StringUtils.rightPad(key.getKey(serviceId, keyValue.getKey()) + mapper.writeValueAsString(keyValue), config.getRecordLength())
+                .getBytes(config.getEncoding());
+            if (bytes.length > config.getRecordLength()) {
+                throw new VsamRecordException("Record length exceeds the configured Vsam record length: ");
+            }
+
+            return bytes;
+
+
+        } catch (UnsupportedEncodingException e) {
+            throw new VsamRecordException(UNSUPPORTED_ENCODING_MESSAGE + config.getEncoding(), e);
+        } catch (JsonProcessingException e) {
+            throw new VsamRecordException("Failure serializing KeyValue object to Json: " + config.getEncoding(), e);
+        }
+    }
+
+    public byte[] getKeyBytes() throws VsamRecordException {
+        try {
+            return key.getKeyBytes(serviceId, keyValue);
+        } catch (UnsupportedEncodingException e) {
+            throw new VsamRecordException(UNSUPPORTED_ENCODING_MESSAGE + config.getEncoding(), e);
+        }
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public KeyValue getKeyValue() {
+        return keyValue;
+    }
+
+    @Override
+    public String toString() {
+        return "VsamRecord{" +
+            "config=" + config +
+            ", serviceId='" + serviceId + '\'' +
+            ", key=" + key.getKey(serviceId, keyValue.getKey()) +
+            ", keyValue=" + keyValue +
+            '}';
+    }
+}
---
+++ caching-service/src/test/java/org/zowe/apiml/caching/service/vsam/VsamRecordTest.java
@@ -0,0 +1,122 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.caching.service.vsam;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zowe.apiml.caching.config.VsamConfig;
+import org.zowe.apiml.caching.model.KeyValue;
+import org.zowe.apiml.zfile.ZFileConstants;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+class VsamRecordTest {
+
+    private VsamConfig config;
+
+    private String longValue = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper ipsum fringilla tellus vestibulum, non sodales est faucibus. Praesent gravida, lacus sed eleifend iaculis, sem purus vulputate diam, congue consequat eros lectus quis erat. Integer et nisl justo. Duis sagittis, odio in cursus facilisis, neque mi facilisis odio, eu pulvinar est libero ac risus. In nulla ante, cursus sit amet imperdiet eget, consectetur et tellus. Vivamus bibendum facilisis mauris, sit amet fermentum tortor accumsan eu. Praesent congue mauris elit, non rhoncus orci lobortis vitae. Donec vulputate accumsan ante sit amet aliquam. Nunc faucibus leo id felis rhoncus, sit amet condimentum lectus euismod. Integer ultrices velit non posuere faucibus.\n" +
+        "Integer laoreet sit amet nunc nec fringilla. Donec at nulla risus. Sed vitae tellus vel nibh rhoncus ornare at vel velit. Nullam ornare lacus purus, ut ullamcorper tellus pharetra luctus. Praesent eu facilisis lectus. Suspendisse pellentesque purus at ex mollis, eu porttitor.";
+
+    @BeforeEach
+    void prepareConfig() {
+        this.config = mock(VsamConfig.class);
+        when(config.getKeyLength()).thenReturn(30);
+        when(config.getEncoding()).thenReturn(ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE);
+        when(config.getRecordLength()).thenReturn(512);
+    }
+
+    @Test
+    void bytesRepresentTheRecordStructure() throws UnsupportedEncodingException, VsamRecordException {
+        String serviceId = "Service";
+        KeyValue kv = new KeyValue("key", "value");
+        VsamRecord underTest = new VsamRecord(config, serviceId, kv);
+
+        assertThat(underTest.getBytes().length, is(512));
+        assertThat(new String(underTest.getBytes(), ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE), containsString(String.valueOf(serviceId.hashCode())));
+        assertThat(new String(underTest.getBytes(), ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE), containsString(String.valueOf(kv.getKey().hashCode())));
+    }
+
+    @Test
+    void bytesHoldKeyValueSerializationInJsonFormat() throws UnsupportedEncodingException, VsamRecordException {
+        String serviceId = "Service";
+        KeyValue kv = new KeyValue("key", "value");
+        VsamRecord underTest = new VsamRecord(config, serviceId, kv);
+
+        String expectedSerialization = "{\"key\":\"key\",\"value\":\"value\"}";
+        assertThat(new String(underTest.getBytes(), ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE), containsString(expectedSerialization));
+    }
+
+
+    @Test
+    void keyBytesRepresentTheKey() throws UnsupportedEncodingException, VsamRecordException {
+        String serviceId = "Service";
+        KeyValue kv = new KeyValue("key", "value");
+        VsamRecord underTest = new VsamRecord(config, serviceId, kv);
+
+        assertThat(underTest.getKeyBytes().length, is(30));
+        assertThat(new String(underTest.getBytes(), ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE),
+            startsWith(new String(underTest.getKeyBytes(), ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE)));
+    }
+
+    @Test
+    void recordCanBeCreatedFromBytes() throws VsamRecordException, UnsupportedEncodingException {
+        byte[] recordData = "-646160747:106079             {\"key\":\"daisy\",\"value\":\"flower\"}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             "
+            .getBytes(ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE);
+
+        VsamRecord underTest = new VsamRecord(config, "Service", recordData);
+
+        assertThat(underTest.getServiceId(), is("Service"));
+        assertThat(underTest.getKeyValue().getKey(), is("daisy"));
+        assertThat(underTest.getKeyValue().getValue(), is("flower"));
+    }
+
+    @Test
+    void recordCannotBeCreatedFromIllegalBytes() throws UnsupportedEncodingException {
+        byte[] recordData = "-646160747:106079             {tank}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             "
+            .getBytes(ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE);
+
+        assertThrows(VsamRecordException.class, () -> new VsamRecord(config, "Service", recordData));
+    }
+
+    @Test
+    void recordBytesThatExceedLengthOfRecordCannotBeCreated() {
+        String serviceId = "Service";
+
+        KeyValue kvLongValue = new KeyValue("key", longValue);
+        VsamRecord underTest1 = new VsamRecord(config, serviceId, kvLongValue);
+
+        assertThrows(VsamRecordException.class, () -> underTest1.getBytes());
+
+        KeyValue kvLongKey = new KeyValue(longValue, "value");
+        VsamRecord underTest2 = new VsamRecord(config, serviceId, kvLongKey);
+
+        assertThrows(VsamRecordException.class, () -> underTest2.getBytes());
+
+    }
+
+    @Test
+    void toStringCanBeCalledAfterRecordIsCreated() throws VsamRecordException, UnsupportedEncodingException {
+        byte[] recordData = "-646160747:106079             {\"key\":\"daisy\",\"value\":\"flower\"}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             "
+            .getBytes(ZFileConstants.DEFAULT_EBCDIC_CODE_PAGE);
+        VsamRecord underTestFromBytes = new VsamRecord(config, "Service", recordData);
+        assertDoesNotThrow(() -> underTestFromBytes.toString());
+
+        VsamRecord underTestFromValues = new VsamRecord(config, "Service", new KeyValue("k","v"));
+        assertDoesNotThrow(() -> underTestFromValues.toString());
+    }
+
+}
