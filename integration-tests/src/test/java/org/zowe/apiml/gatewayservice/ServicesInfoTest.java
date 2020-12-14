c82707c4f7fb4935af394e04cc4702540e837dbb
Petr Plavjanik <petr.plavjanik@broadcom.com>
2020-12-14 23:51:21 +0100

    Services info integration tests
    Signed-off-by: Petr Plavjanik <petr.plavjanik@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/gatewayservice/ServicesInfoTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/gatewayservice/ServicesInfoTest.java
@@ -10,8 +10,11 @@
 package org.zowe.apiml.gatewayservice;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
+import static org.zowe.apiml.constants.ApimlConstants.BASIC_AUTHENTICATION_PREFIX;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,30 +26,43 @@ import io.restassured.RestAssured;
 class ServicesInfoTest {
     private final static String USERNAME = ConfigReader.environmentConfiguration().getCredentials().getUser();
     private final static String PASSWORD = ConfigReader.environmentConfiguration().getCredentials().getPassword();
-    private final static String SCHEME = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getScheme();
-    private final static String HOST = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getHost();
+    private final static String SCHEME = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration()
+            .getScheme();
+    private final static String HOST = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration()
+            .getHost();
     private final static int PORT = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration().getPort();
 
     private static final String SERVICES_ENDPOINT = "gateway/api/v1/services";
     private static final String API_CATALOG_SERVICE = "apicatalog";
 
-    private final static String APIML_COOKIE = "apimlAuthenticationToken";
-
-    private final static String BASIC_AUTHENTICATION_PREFIX = "Basic";
+    private String token;
 
     @BeforeEach
     void setUp() {
         RestAssured.useRelaxedHTTPSValidation();
+        token = SecurityUtils.gatewayToken(USERNAME, PASSWORD);
     }
 
-    //@formatter:off
     @Test
-    void accessProtectedEndpointWithoutCredentials() {
-        given()
-        .when()
-            .get(String.format("%s://%s:%d/%s/%s", SCHEME, HOST, PORT, SERVICES_ENDPOINT, API_CATALOG_SERVICE))
-        .then()
-            .statusCode(is(SC_UNAUTHORIZED))
-            .header(HttpHeaders.WWW_AUTHENTICATE, BASIC_AUTHENTICATION_PREFIX);
+    void cannotBeAccessedWithoutAuthentication() {
+        given().when()
+                .get(String.format("%s://%s:%d/%s/%s", SCHEME, HOST, PORT, SERVICES_ENDPOINT, API_CATALOG_SERVICE))
+                .then().statusCode(is(SC_UNAUTHORIZED))
+                .header(HttpHeaders.WWW_AUTHENTICATE, BASIC_AUTHENTICATION_PREFIX);
+    }
+
+    @Test
+    void providesApiCatalogServiceInformation() {
+        given().header("Authorization", "Bearer " + token).when()
+                .get(String.format("%s://%s:%d/%s/%s", SCHEME, HOST, PORT, SERVICES_ENDPOINT, API_CATALOG_SERVICE))
+                .then().statusCode(is(SC_OK)).body("apiml.apiInfo[0].apiId", equalTo("zowe.apiml.apicatalog"))
+                .body("apiml.apiInfo[0].basePath", equalTo("/apicatalog/api/v1"));
+    }
+
+    @Test
+    void providesGatewayServiceInformation() {
+        given().header("Authorization", "Bearer " + token).when()
+                .get(String.format("%s://%s:%d/%s/%s", SCHEME, HOST, PORT, SERVICES_ENDPOINT, "gateway")).then()
+                .statusCode(is(SC_OK)).body("apiml.apiInfo[0].apiId", equalTo("zowe.apiml.gateway"));
     }
 }
