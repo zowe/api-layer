483cc339b7cee5b53ba706235f19341e34d6e4a5
JirkaAichler <jiri.aichler@broadcom.com>
2020-12-14 19:37:12 +0100

    Add tests Add api doc
    Signed-off-by: JirkaAichler <jiri.aichler@broadcom.com>

---
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/services/ServicesInfoControllerTest.java
@@ -0,0 +1,65 @@
+package org.zowe.apiml.gateway.services;
+
+import com.netflix.appinfo.InstanceInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.zowe.apiml.gateway.services.ServicesInfoController.SERVICES_URL;
+
+@ExtendWith(MockitoExtension.class)
+class ServicesInfoControllerTest {
+
+    private static final String SERVICE_ID = "service";
+
+    @Mock
+    private ServicesInfoService servicesInfoService;
+
+    @Test
+    void whenServiceIsUp_thenReturnOK() {
+        when(servicesInfoService.getServiceInfo(SERVICE_ID)).thenReturn(
+                ServiceInfo.builder()
+                        .serviceId(SERVICE_ID)
+                        .status(InstanceInfo.InstanceStatus.UP).
+                        build()
+        );
+
+        //@formatter:off
+        given()
+                .standaloneSetup(new ServicesInfoController(servicesInfoService))
+        .when()
+                .get(SERVICES_URL + "/" + SERVICE_ID)
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("status", is(InstanceInfo.InstanceStatus.UP.toString()))
+                .body("serviceId", is(SERVICE_ID));
+        //@formatter:on
+    }
+
+    @Test
+    void whenServiceDoesNotExist_thenReturnNotFound() {
+        when(servicesInfoService.getServiceInfo(SERVICE_ID)).thenReturn(
+                ServiceInfo.builder()
+                        .serviceId(SERVICE_ID)
+                        .status(InstanceInfo.InstanceStatus.UNKNOWN).
+                        build()
+        );
+
+        //@formatter:off
+        given()
+                .standaloneSetup(new ServicesInfoController(servicesInfoService))
+        .when()
+                .get(SERVICES_URL + "/" + SERVICE_ID)
+       .then()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body("status", is(InstanceInfo.InstanceStatus.UNKNOWN.toString()))
+                .body("serviceId", is(SERVICE_ID));
+        //@formatter:on
+    }
+
+}
