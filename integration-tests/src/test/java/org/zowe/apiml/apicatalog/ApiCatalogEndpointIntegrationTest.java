c5c81f4141eb6b81c65a433156125d89e99c51a1
Jakub Balhar <jakub.balhar@broadcom.com>
2020-12-14 13:44:31 +0100

    Enforce JSON
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/apicatalog/ApiCatalogEndpointIntegrationTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/apicatalog/ApiCatalogEndpointIntegrationTest.java
@@ -225,6 +225,8 @@ public class ApiCatalogEndpointIntegrationTest {
 
         // When
         final String jsonResponse = EntityUtils.toString(response.getEntity());
+
+        System.out.println(jsonResponse);
         JSONArray errors = JsonPath.parse(jsonResponse).read("$.errors");
 
         assertEquals("[]", errors.toString());
@@ -268,6 +270,8 @@ public class ApiCatalogEndpointIntegrationTest {
     private HttpResponse getStaticApiRefreshResponse(String endpoint, int returnCode) throws IOException {
         URI uri = HttpRequestUtils.getUriFromGateway(endpoint);
         HttpPost request = new HttpPost(uri);
+        request.addHeader("Accept", "application/json");
+
         String cookie = HttpSecurityUtils.getCookieForGateway();
         HttpSecurityUtils.addCookie(request, cookie);
 
