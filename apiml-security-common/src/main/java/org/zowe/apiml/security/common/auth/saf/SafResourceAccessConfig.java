e8d8450dc915c1523b1ca4d75b59007598f34fcd
Pavel Jareš <pavel.jares@broadcom.com>
2020-12-18 17:31:49 +0100

    EndpointVerifier
    Signed-off-by: Pavel Jareš <pavel.jares@broadcom.com>

---
+++ apiml-security-common/src/main/java/org/zowe/apiml/security/common/auth/saf/SafResourceAccessConfig.java
@@ -0,0 +1,39 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.security.common.auth.saf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+public class SafResourceAccessConfig {
+
+    @Value("${apiml.security.safEndpoint.enabled:false}")
+    private boolean endpointEnabled;
+
+    @Bean
+    public SafResourceAccessVerifying safResourceAccessVerifying(RestTemplate restTemplate) throws IOException {
+        // TODO: SAF using JZOS - another PR
+
+        if (endpointEnabled) {
+            return new SafResourceAccessEndpoint(restTemplate);
+        }
+
+        // TODO: Dummy - another PR
+        throw new IllegalArgumentException("No implementation is ready");
+    }
+
+}
---
+++ apiml-security-common/src/main/java/org/zowe/apiml/security/common/auth/saf/SafResourceAccessEndpoint.java
@@ -0,0 +1,46 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.security.common.auth.saf;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.client.RestTemplate;
+
+@RequiredArgsConstructor
+public class SafResourceAccessEndpoint implements SafResourceAccessVerifying {
+
+    private static final String PATH_VRIABLE_SUFFIX = "/{userId}/{class}/{entity}/{level}";
+
+    @Value("${apiml.security.safEndpoint.url}")
+    private String endpointUrl;
+
+    private final RestTemplate restTemplate;
+
+    @Override
+    public boolean hasSafResourceAccess(Authentication authentication, String resourceClass, String resourceName, String accessLevel) {
+        Response response = restTemplate.getForObject(endpointUrl + PATH_VRIABLE_SUFFIX, Response.class,
+            authentication.getName(), resourceClass, resourceName, accessLevel);
+        return !response.isError() && response.isAuthorized();
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Response {
+
+        private boolean authorized;
+        private boolean error;
+        private String message;
+
+    }
+
+}
