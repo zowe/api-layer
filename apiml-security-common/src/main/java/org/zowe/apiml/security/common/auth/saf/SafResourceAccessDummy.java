4e33318ce4dd7b1f49760b595047c072567cf63f
Pavel Jareš <pavel.jares@broadcom.com>
2020-12-22 12:32:55 +0100

    fix by code review
    Signed-off-by: Pavel Jareš <pavel.jares@broadcom.com>

--- apiml-security-common/src/main/java/org/zowe/apiml/security/common/auth/saf/SafResourceAccessDummy.java
+++ apiml-security-common/src/main/java/org/zowe/apiml/security/common/auth/saf/SafResourceAccessDummy.java
@@ -12,6 +12,7 @@ package org.zowe.apiml.security.common.auth.saf;
 import lombok.Builder;
 import lombok.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
@@ -19,6 +20,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class represents a dummy implementation of SAF resource checking. Combination of SAF resources and users who
+ * has allowed a resource is defined in a file.
+ * As a default is loaded file `saf.yml` from root folder. It is helpful for tests on local machine. If this file does
+ * not exist it will load file `mock-saf.yml` from classpath. It can be use for unit test. Classpath of test override
+ * the file from applications classpath. It is highly recommended to locate empty file `mock-saf` in each application
+ * using this feature to allow start application without any other action.
+ */
+@Component
 public class SafResourceAccessDummy implements SafResourceAccessVerifying {
 
     private static final String SAF_ACCESS = "safAccess";
@@ -34,11 +44,11 @@ public class SafResourceAccessDummy implements SafResourceAccessVerifying {
                 FileInputStream fis = new FileInputStream(file);
                 BufferedInputStream bis = new BufferedInputStream(fis)
             ) {
-                load(bis);
+                loadDefinition(bis);
             }
         } else {
             try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(DEFAULT_RESOURCE_LOCATION)) {
-                load(inputStream);
+                loadDefinition(inputStream);
             }
         }
     }
@@ -48,7 +58,7 @@ public class SafResourceAccessDummy implements SafResourceAccessVerifying {
     }
 
     public SafResourceAccessDummy(InputStream inputStream) {
-        load(inputStream);
+        loadDefinition(inputStream);
     }
 
     @Override
@@ -63,10 +73,31 @@ public class SafResourceAccessDummy implements SafResourceAccessVerifying {
         return currentLevel.compareTo(AccessLevel.valueOf(accessLevel)) >= 0;
     }
 
-    private void load(InputStream inputStream) {
+    /**
+     * This method loads a YML file with description of allowed SAF resource.
+     *
+     * Structure of file:
+     *
+     * ```
+     * safAccess:
+     *  {CLASS}:
+     *   {RESOURCE}:
+     *    - {User ID}
+     * ```
+     * notes:
+     *  - Classes and resources are mapped into a map, user IDs into list.
+     *  - load method does not support formatting with dot, like {CLASS}.{RESOURCE}, each element has to be separated
+     *  - safAccess is not required to define empty file
+     *  - classes and resources cannot be defined without user ID list
+     *  - method could be called multiple times, definition are added to current (loaded)
+     *  - when a user has multiple definition of same class and resource, just the highest access level is loaded
+     *
+     * @param inputStream stream to be loaded
+     */
+    private void loadDefinition(InputStream inputStream) {
         Yaml yaml = new Yaml();
         Map<String, Object> data = yaml.load(inputStream);
-        load(data);
+        loadDefinition(data);
     }
 
     private void set(ResourceUser resourceUser, AccessLevel accessLevel) {
@@ -95,7 +126,7 @@ public class SafResourceAccessDummy implements SafResourceAccessVerifying {
         return (T) data.get(SAF_ACCESS);
     }
 
-    private void load(Map<String, Object> data) {
+    private void loadDefinition(Map<String, Object> data) {
         Map<String, Map<String, Map<String, List<String>>>> classes = getSafAccess(data);
         if (classes == null) return;
 
