91b852c4223bef86a17f1924e03e74cd3198ddbc
achmelo <37397715+achmelo@users.noreply.github.com>
2021-01-04 08:37:14 +0100

    Rip/gh1009/keep alive connection (#1032)
    * keep alive strategy
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>

---
+++ common-service-core/src/main/java/org/zowe/apiml/security/ApimlKeepAliveStrategy.java
@@ -0,0 +1,43 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.security;
+
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HttpResponse;
+import org.apache.http.annotation.Contract;
+import org.apache.http.annotation.ThreadingBehavior;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+
+@Contract(threading = ThreadingBehavior.IMMUTABLE)
+public class ApimlKeepAliveStrategy implements ConnectionKeepAliveStrategy {
+
+    private static final int KEEPALIVE_TIMOUT_MILLIS = 2000;
+
+    public static final ApimlKeepAliveStrategy INSTANCE = new ApimlKeepAliveStrategy();
+    @Override
+    public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+        HeaderElementIterator it = new BasicHeaderElementIterator
+            (response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+        while (it.hasNext()) {
+            HeaderElement he = it.nextElement();
+            String param = he.getName();
+            String value = he.getValue();
+            if (value != null && param.equalsIgnoreCase
+                ("timeout")) {
+                return Long.parseLong(value) * 1000;
+            }
+        }
+        return KEEPALIVE_TIMOUT_MILLIS;
+    }
+}
