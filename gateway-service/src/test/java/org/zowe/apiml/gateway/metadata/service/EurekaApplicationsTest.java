12b60ab261e12d82b1b7143ad6e6671ff48d31f9
Boris Petkov <boris.petkov@broadcom.com>
2021-01-08 15:38:17 +0100

    Upgrade tests in gateway-service to JUnit 5 (#1069)
    Signed-off-by: Boris Petkov <boris.petkov@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/gateway/metadata/service/EurekaApplicationsTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/metadata/service/EurekaApplicationsTest.java
@@ -22,7 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
