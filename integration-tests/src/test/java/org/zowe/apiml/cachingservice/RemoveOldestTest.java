0f4aad45a4100eff74b0b75c3367be13c474334c
Jakub Balhar <jakub@balhar.net>
2021-01-20 14:49:32 +0100

    Add Integration test verifying the Remove Oldest behavior for the InMemory Storage (#1108)
    * Refactor the Strategies to create space for The oldest one
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Remove duplicate behavior that doesn't belong to the eviction strategy
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Add serviceId and created among the properties of KeyValue
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Add Remove Oldest strategy for the InMemory
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Continue on an item in the same service
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Remove duplication in integration tests
    Introduce remove oldest test, disabled for the moment
    Fix time based abd created based issue in the remove oldest strategy implementation
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Add requests.
    Improve the work on the Remove Oldest tests and setup of the service as part of the tests.
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Working solution for the start of the IT
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Minor refactoring and cleanup
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Clean code smell
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Move to awaitality
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    * Allow further configuration of the CachingService
    
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    Co-authored-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/cachingservice/RemoveOldestTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/cachingservice/RemoveOldestTest.java
@@ -10,56 +10,64 @@
 package org.zowe.apiml.cachingservice;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.zowe.apiml.gatewayservice.SecurityUtils;
 import org.zowe.apiml.util.CachingRequests;
 import org.zowe.apiml.util.categories.NotForMainframeTest;
-import org.zowe.apiml.util.http.HttpRequestUtils;
 
+import java.io.IOException;
 import java.net.URI;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.apache.http.HttpStatus.SC_INSUFFICIENT_STORAGE;
+import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.core.Is.is;
 
-@NotForMainframeTest // Remove later when implemented for VSAM as well.
-class RejectEvictionTest {
-    private static final URI CACHING_PATH = HttpRequestUtils.getUriFromGateway("/cachingservice/api/v1/cache");
-    private final String COOKIE_NAME = "apimlAuthenticationToken";
+@NotForMainframeTest
+class RemoveOldestTest {
+    private final static String COOKIE_NAME = "apimlAuthenticationToken";
     private final String jwtToken = SecurityUtils.gatewayToken();
     private final CachingRequests requests = new CachingRequests();
+    private static final CachingService service = new CachingService();
 
     @BeforeAll
-    static void setup() {
+    static void setup() throws IOException {
         RestAssured.useRelaxedHTTPSValidation();
+        service.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        service.stop();
     }
 
     @Test
-    void givenStorageIsFull_whenAnotherKeyIsInserted_thenItIsRejected() {
-        int amountOfAllowedRecords = 100;
+    void givenStorageIsFull_whenAnotherKeyIsInserted_thenTheOldestIsRemoved() {
+        int amountOfAllowedRecords = 100 + 1;
+        URI removeOldestCachingServiceUri = service.getBaseUrl();
         try {
             KeyValue keyValue;
 
             // The default configuration is to allow 100 records.
             for (int i = 0; i < amountOfAllowedRecords; i++) {
                 keyValue = new KeyValue("key" + i, "testValue");
-                requests.create(keyValue);
+                requests.create(removeOldestCachingServiceUri, keyValue);
             }
 
-            keyValue = new KeyValue("keyThatWontPass", "testValue");
+            keyValue = new KeyValue("keyThatWillReplaceTheKey0", "testValue");
             given()
                 .contentType(JSON)
                 .body(keyValue)
                 .cookie(COOKIE_NAME, jwtToken)
             .when()
-                .post(CACHING_PATH)
+                .get(service.getBaseUrl() + "/key0")
             .then()
-                .statusCode(is(SC_INSUFFICIENT_STORAGE));
+                .statusCode(is(SC_NOT_FOUND));
         } finally {
             for (int i = 0; i < amountOfAllowedRecords; i++) {
-                requests.deleteValueUnderServiceIdWithoutValidation("key" + i, jwtToken);
+                requests.deleteValueUnderServiceIdWithoutValidation(removeOldestCachingServiceUri, "key" + i, jwtToken);
             }
         }
     }
