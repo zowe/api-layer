54d7c3a32c129ed27b689e17d31feee2c954450a
Andrea Tabone <andrea.tabone@broadcom.com>
2021-01-26 14:21:54 +0100

    Add more logs

--- caching-service/src/main/java/org/zowe/apiml/caching/service/vsam/config/RemoveOldestStrategy.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/vsam/config/RemoveOldestStrategy.java
@@ -71,11 +71,12 @@ public class RemoveOldestStrategy implements EvictionStrategy {
 
     private void checkAndRemoveRecord(VsamRecord oldest) {
         if (oldest != null) {
+            log.info("Record to remove {}", oldest);
             log.info("Removing the oldest record {}", oldest.getKeyValue().getKey());
             file = producer.newVsamFile(vsamConfig, VsamConfig.VsamOptions.WRITE);
             Optional<VsamRecord> returned = file.delete(oldest);
             if (returned.isPresent()) {
-                log.info("The oldest record has been removed!");
+                log.info("The oldest record has been successfully removed!");
             }
         }
     }
