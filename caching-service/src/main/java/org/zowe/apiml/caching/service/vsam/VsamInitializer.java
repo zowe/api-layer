f119869f7f837a6003fbbff14a37c7df9c9c11d8
Jakub Balhar <jakub.balhar@broadcom.com>
2021-01-28 13:35:37 +0100

    Refactor VsamFile to allow the Unit tests for the most of the functionality
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/service/vsam/VsamInitializer.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/vsam/VsamInitializer.java
@@ -12,7 +12,11 @@ package org.zowe.apiml.caching.service.vsam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.zowe.apiml.caching.model.KeyValue;
 import org.zowe.apiml.caching.service.vsam.config.VsamConfig;
+import org.zowe.apiml.zfile.ZFile;
+import org.zowe.apiml.zfile.ZFileConstants;
+import org.zowe.apiml.zfile.ZFileException;
 
 /**
  * This performs the warmup of VSAM during startup with retry
@@ -29,5 +33,38 @@ public class VsamInitializer {
         try (VsamFile file = new VsamFile(config, VsamConfig.VsamOptions.WRITE, true)) {
             log.info("Vsam file open successful");
         }
+    }
+
+    /**
+     * This method writes a record to file and deletes it immediately.
+     * Use this method on freshly created empty VSAM to write the fist record
+     * and to verify that records can be written.
+     * <p>
+     * Exceptions are thrown to give chance to the caller to react.
+     *
+     * @throws ZFileException
+     * @throws VsamRecordException
+     */
+    public void warmUpVsamFile(ZFile zFile, VsamConfig vsamConfig) throws ZFileException, VsamRecordException {
+
+        log.info("Warming up the vsam file by writing and deleting a record");
+
+        log.info("VSAM file being used: {}", zFile.getActualFilename());
+
+        VsamRecord record = new VsamRecord(vsamConfig, "delete", new KeyValue("me", "novalue"));
+
+        log.info("Writing Record: {}", record);
+        zFile.write(record.getBytes());
+
+        boolean found = zFile.locate(record.getKeyBytes(), ZFileConstants.LOCATE_KEY_EQ);
+
+        log.info("Test record for deletion found: {}", found);
+        if (found) {
+            byte[] recBuf = new byte[vsamConfig.getRecordLength()];
+            zFile.read(recBuf); //has to be read before update/delete
+            zFile.delrec();
+            log.info("Test record deleted.");
+        }
+
     }
 }
