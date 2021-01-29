4e7ff59d0187b98d14ee05cb9d5baba84065ee40
achmelo <a.chmelo@gmail.com>
2021-01-29 10:37:24 +0100

    include micronaut
    Signed-off-by: achmelo <a.chmelo@gmail.com>

---
+++ onboarding-enabler-micronaut/README.md
@@ -0,0 +1,14 @@
+## Feature http-client documentation
+
+- [Micronaut Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
+### Install Micronaut CLI
+
+with homebrew:
+```
+brew cask install micronaut-projects/tap/micronaut
+```
+download binaries:
+```
+https://micronaut.io/download.html
+```
---
+++ onboarding-enabler-micronaut/src/main/java/org/zowe/apiml/Application.java
@@ -0,0 +1,19 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
