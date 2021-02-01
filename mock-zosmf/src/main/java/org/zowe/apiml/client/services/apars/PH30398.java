ea8205466a85149b7880c8a876411f5b83ecf08c
Carson Cook <carson.cook@ibm.com>
2021-02-01 17:07:34 -0500

    Add 500 response when no auth in ph30398
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- mock-zosmf/src/main/java/org/zowe/apiml/client/services/apars/PH30398.java
+++ mock-zosmf/src/main/java/org/zowe/apiml/client/services/apars/PH30398.java
@@ -9,15 +9,36 @@
  */
 package org.zowe.apiml.client.services.apars;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.zowe.apiml.client.services.versions.Apar;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+@Component
 public class PH30398 implements Apar {
+    @Value("${zosmf.username}")
+    private List<String> usernames;
+    @Value("${zosmf.password}")
+    private List<String> passwords;
+
     @Override
     public Optional<ResponseEntity<?>> apply(Object... parameters) {
-        // TODO: Implement the current changes.
+        String calledService = (String) parameters[0];
+
+        if (calledService.equals("authentication")) {
+            Map<String, String> headers = (Map<String, String>) parameters[4];
+
+            String authorization = headers.get("authorization");
+            if (authorization == null || authorization.isEmpty()) {
+                return Optional.of(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+            }
+        }
+        // TODO implement functionality other than checking for no authentication
         return (Optional<ResponseEntity<?>>) parameters[2];
     }
 }
--- mock-zosmf/src/main/java/org/zowe/apiml/client/services/versions/Apars.java
+++ mock-zosmf/src/main/java/org/zowe/apiml/client/services/versions/Apars.java
@@ -11,7 +11,9 @@ package org.zowe.apiml.client.services.versions;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.zowe.apiml.client.services.apars.*;
+import org.zowe.apiml.client.services.apars.DefaultApar;
+import org.zowe.apiml.client.services.apars.PH12143;
+import org.zowe.apiml.client.services.apars.PH30398;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,17 +25,17 @@ public class Apars {
     private final Map<String, Apar> availableApars = new HashMap<>();
 
     @Autowired
-    public Apars(PH12143 ph12143) {
+    public Apars(PH12143 ph12143, PH30398 ph30398) {
         availableApars.put("PH12143", ph12143);
         availableApars.put("PH17867", new DefaultApar());
         availableApars.put("PH28507", new DefaultApar());
         availableApars.put("PH28532", new DefaultApar());
-        availableApars.put("PH30398", new PH30398());
+        availableApars.put("PH30398", ph30398);
     }
 
     public List<Apar> getApars(String[] names) {
         ArrayList<Apar> result = new ArrayList<>();
-        for(String name: names) {
+        for (String name : names) {
             if (availableApars.containsKey(name)) {
                 result.add(availableApars.get(name));
             }
