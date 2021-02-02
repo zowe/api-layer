ce8aa8db5c6f52128ca25b10f3f1b95a25d11d56
Carson Cook <carson.cook@ibm.com>
2021-02-02 13:32:13 -0500

    Add apar unit tests
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- mock-zosmf/src/test/java/org/zowe/apiml/client/services/apars/PH12143Test.java
+++ mock-zosmf/src/test/java/org/zowe/apiml/client/services/apars/PH12143Test.java
@@ -9,16 +9,144 @@
  */
 package org.zowe.apiml.client.services.apars;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class PH12143Test {
-    @Nested
-    class whenAuthenticating {
+    private static final String SERVICE = "authentication";
+    private static final String USERNAME = "USER";
+    private static final String PASSWORD = "validPassword";
 
+    private PH12143 underTest;
+    private MockHttpServletResponse mockResponse;
+
+    @BeforeEach
+    void setUp() {
+        List<String> usernames = Collections.singletonList(USERNAME);
+        List<String> passwords = Collections.singletonList(PASSWORD);
+
+        underTest = new PH12143(usernames, passwords);
+        mockResponse = new MockHttpServletResponse();
+    }
+
+    @Nested
+    class whenCreating {
+        @Test
+        void givenNoAuthorization_thenReturnUnauthorized() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+            Map<String, String> headers = new HashMap<>();
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+            assertThat(result, is(expected));
+        }
+
+        @Test
+        void givenEmptyAuthorization_thenReturnUnauthorized() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("authorization", "");
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+            assertThat(result, is(expected));
+        }
+
+        @Test
+        void givenNoCredentials_thenReturnUnauthorized() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("authorization", getAuthorizationHeader(null, null));
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+            assertThat(result, is(expected));
+        }
+
+        @Test
+        void givenInvalidUsername_thenReturnUnauthorized() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("authorization", getAuthorizationHeader("baduser", PASSWORD));
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+            assertThat(result, is(expected));
+        }
+
+        @Test
+        void givenInvalidPassword_thenReturnUnauthorized() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("authorization", getAuthorizationHeader(USERNAME, "badpassword"));
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+            assertThat(result, is(expected));
+        }
+
+        @Test
+        void givenValidUserAndPassword_thenReturnJwt() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("authorization", getAuthorizationHeader(USERNAME, PASSWORD));
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+            assertThat(result, is(expected));
+        }
+
+        @Test
+        void givenValidUserAndPassticket_thenReturnJwt() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("authorization", getAuthorizationHeader(USERNAME, "PASS_TICKET"));
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+            assertThat(result, is(expected));
+        }
     }
 
     @Nested
     class whenVerifying {
 
+    }
+
+    @Nested
+    class whenDeleting {
+        @Test
+        void thenReturnNoContent() {
+            Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+
+            Map<String, String> headers = new HashMap<>();
+            Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "delete", Optional.empty(), headers);
+
+            assertThat(result, is(expected));
+        }
+    }
+
+    @Test
+    void givenServiceNotHandled_whenApplyApar_thenReturnOriginalResult(){
+        Optional<ResponseEntity<?>> previousResult = Optional.of(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+
+        Map<String, String> headers = new HashMap<>();
+        Optional<ResponseEntity<?>> result = underTest.apply("badservice", "", previousResult, headers);
+
+        assertThat(result, is(previousResult));
+    }
+
+    private String getAuthorizationHeader(String user, String password) {
+        return "Basic " + Base64.encodeBase64String((user + ":" + password).getBytes());
     }
 }
---
+++ mock-zosmf/src/test/java/org/zowe/apiml/client/services/apars/PH30398Test.java
@@ -0,0 +1,61 @@
+package org.zowe.apiml.client.services.apars;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PH30398Test {
+    private static final String SERVICE = "authentication";
+    private static final String USERNAME = "USER";
+    private static final String PASSWORD = "validPassword";
+
+    private PH30398 underTest;
+    private MockHttpServletResponse mockResponse;
+
+    @BeforeEach
+    void setUp() {
+        List<String> usernames = Collections.singletonList(USERNAME);
+        List<String> passwords = Collections.singletonList(PASSWORD);
+
+        underTest = new PH30398(usernames, passwords);
+        mockResponse = new MockHttpServletResponse();
+    }
+
+    @Test
+    void givenNoAuthorization_thenReturnInternalServerError() {
+        Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+
+        Map<String, String> headers = new HashMap<>();
+        Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    void givenEmptyAuthorization_thenReturnInteralServerError() {
+        Optional<ResponseEntity<?>> expected = Optional.of(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("authorization", "");
+        Optional<ResponseEntity<?>> result = underTest.apply(SERVICE, "create", Optional.empty(), mockResponse, headers);
+
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    void givenServiceNotHandled_whenApplyApar_thenReturnOriginalResult(){
+        Optional<ResponseEntity<?>> previousResult = Optional.of(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+
+        Map<String, String> headers = new HashMap<>();
+        Optional<ResponseEntity<?>> result = underTest.apply("badservice", "", previousResult, headers);
+
+        assertThat(result, is(previousResult));
+    }
+}
