53728a1a3841921f2e510f42e14f51c4d919a8fe
Andrea Tabone <andrea.tabone@broadcom.com>
2021-02-09 14:55:49 +0100

    Add nodejs enabler publishing as part of the apiml release
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- scripts/publish_and_release.sh
+++ scripts/publish_and_release.sh
@@ -4,6 +4,7 @@ set -ex
 
 ./gradle/bootstrap/bootstrap_gradlew.sh
 AUTH="-Pzowe.deploy.username=$USERNAME -Pzowe.deploy.password=$PASSWORD -Partifactory_user=$USERNAME -Partifactory_password=$PASSWORD"
+DIST_REGISTRY="https://registry.npmjs.org/"
 
 case $RELEASE_TYPE in
    "SNAPSHOT_RELEASE")
@@ -30,6 +31,12 @@ case $RELEASE_TYPE in
    echo "Make specific release"
    ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=$RELEASE_VERSION -Prelease.newVersion=$NEW_VERSION $AUTH
    git archive --format tar.gz -9 --output api-layer.tar.gz "v$RELEASE_VERSION"
+   cd onboarding-enabler-nodejs
+   echo \"//registry.npmjs.org/:_authToken=$TOKEN\" > ~/.npmrc
+   echo \"registry=$DIST_REGISTRY\" >> ~/.npmrc
+   npm version $RELEASE_VERSION
+   npm publish @zowe/apiml-onboarding-enabler-nodejs --access public
+
 esac
 
 echo "End of publish and release"
