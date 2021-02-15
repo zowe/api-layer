f6b7cc3bd273c416a90342b1aba3484ad6fc6a24
achmelo <a.chmelo@gmail.com>
2021-02-15 18:14:31 +0100

    2 retries, 1,5s backoff
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfService.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfService.java
@@ -134,7 +134,7 @@ public class ZosmfService extends AbstractZosmfService {
     }
 
 
-    @Retryable(value = {TokenNotValidException.class}, maxAttempts = 1, backoff = @Backoff(value = 2000))
+    @Retryable(value = {TokenNotValidException.class}, maxAttempts = 2, backoff = @Backoff(value = 1500))
     public AuthenticationResponse authenticate(Authentication authentication) {
         AuthenticationResponse authenticationResponse;
         if (loginEndpointExists()) {
