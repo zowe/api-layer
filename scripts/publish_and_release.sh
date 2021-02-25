2309c5f8740c76b7f9820486feaf54f8c6e836d5
achmelo <37397715+achmelo@users.noreply.github.com>
2021-02-25 16:31:30 +0100

    chore: temporally remove broken release types, issue will be created for fix (#1198)
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- scripts/publish_and_release.sh
+++ scripts/publish_and_release.sh
@@ -7,26 +7,6 @@ AUTH="-Pzowe.deploy.username=$USERNAME -Pzowe.deploy.password=$PASSWORD -Partifa
 DIST_REGISTRY="https://registry.npmjs.org/"
 
 case $RELEASE_TYPE in
-   "SNAPSHOT_RELEASE")
-   echo "Make SNAPSHOT release"
-   ./gradlew publishAllVersions $AUTH
-   git archive --format tar.gz -9 --output api-layer.tar.gz HEAD~1
-   ;;
-   "PATCH_RELEASE")
-   echo "Make PATCH release"
-   ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.scope=patch $AUTH
-   git archive --format tar.gz -9 --output api-layer.tar.gz HEAD~1
-   ;;
-   "MINOR_RELEASE")
-   echo "Make MINOR release"
-   ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.scope=minor $AUTH
-   git archive --format tar.gz -9 --output api-layer.tar.gz HEAD~1
-   ;;
-   "MAJOR_RELEASE")
-   echo "Make MAJOR release"
-   ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.scope=major $AUTH
-   git archive --format tar.gz -9 --output api-layer.tar.gz HEAD~1
-   ;;
    "SPECIFIC_RELEASE")
    echo "Make specific release"
    cd onboarding-enabler-nodejs
