a4f37cb8d9d142f06df1a784c9b11588d6bb38aa
Boris Petkov <boris.petkov@broadcom.com>
2021-03-04 20:28:08 +0100

    APIService flowing builder
    Signed-off-by: Boris Petkov <boris.petkov@broadcom.com>

--- api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/controllers/api/ApiCatalogControllerTests.java
+++ api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/controllers/api/ApiCatalogControllerTests.java
@@ -197,10 +197,28 @@ class ApiCatalogControllerTests {
     private List<APIContainer> createContainers() {
         Set<APIService> services = new HashSet<>();
 
-        APIService service = new APIService("service1", "service-1", "service-1", false, "url", "home", "base", false, Collections.emptyMap());
+        APIService service =  new APIService.Builder("service1")
+            .title("service-1")
+            .description("service-1")
+            .secured(true)
+            .baseUrl("url")
+            .homePageUrl("home")
+            .basePath("base")
+            .sso(false)
+            .apiId(Collections.emptyMap())
+            .build();
         services.add(service);
 
-        service = new APIService("service2", "service-2", "service-2", true, "url", "home", "base", false, Collections.emptyMap());
+        service =  new APIService.Builder("service2")
+            .title("service-2")
+            .description("service-2")
+            .secured(true)
+            .baseUrl("url")
+            .homePageUrl("home")
+            .basePath("base")
+            .sso(false)
+            .apiId(Collections.emptyMap())
+            .build();
         services.add(service);
 
         APIContainer container = new APIContainer("api-one", "API One", "This is API One", services);
