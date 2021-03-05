2cd56377588d33df8b47118025a3ec597c7352be
Jakub Balhar <jakub@balhar.net>
2021-03-05 16:12:31 +0100

    chore: Enable acceptance tests again (#1223)
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>
    
    Co-authored-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/acceptance/TimeoutPerServiceTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/acceptance/TimeoutPerServiceTest.java
@@ -12,7 +12,6 @@ package org.zowe.apiml.acceptance;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.zowe.apiml.acceptance.common.AcceptanceTest;
@@ -28,7 +27,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @AcceptanceTest
-@Disabled
 public class TimeoutPerServiceTest extends AcceptanceTestWithTwoServices {
     private int SECOND = 1000;
 
