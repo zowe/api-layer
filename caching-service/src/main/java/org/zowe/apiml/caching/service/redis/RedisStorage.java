647a75cd5befcc11d73cffd4037c184ed2a6a7a8
Carson Cook <carson.cook@ibm.com>
2021-03-05 14:47:49 -0500

    PoC connecting to local docker redis
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/service/redis/RedisStorage.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/redis/RedisStorage.java
@@ -9,6 +9,10 @@
  */
 package org.zowe.apiml.caching.service.redis;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import lombok.extern.slf4j.Slf4j;
 import org.zowe.apiml.caching.model.KeyValue;
 import org.zowe.apiml.caching.service.Storage;
@@ -19,9 +23,14 @@ import java.util.Map;
 @Slf4j
 public class RedisStorage implements Storage {
     private RedisConfig config;
+    private RedisAsyncCommands<String, String> redis;
 
-    public RedisStorage(RedisConfig config) {
+    public RedisStorage(RedisConfig config, RedisURI redisUri) {
         this.config = config;
+
+        RedisClient redisClient = RedisClient.create(redisUri);
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        redis = connection.async();
     }
 
     @Override
--- caching-service/src/main/java/org/zowe/apiml/caching/service/redis/config/RedisConfiguration.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/redis/config/RedisConfiguration.java
@@ -9,14 +9,15 @@
  */
 package org.zowe.apiml.caching.service.redis.config;
 
+import io.lettuce.core.RedisURI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zowe.apiml.caching.service.Storage;
 import org.zowe.apiml.caching.service.redis.RedisStorage;
-import org.zowe.apiml.message.core.MessageService;
-import org.zowe.apiml.message.log.ApimlLogger;
+
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,6 +27,11 @@ public class RedisConfiguration {
     @ConditionalOnProperty(name = "caching.storage.mode", havingValue = "redis")
     @Bean
     public Storage redis() {
-        return new RedisStorage(redisConfig);
+        // TODO make this configuration
+        // TODO how do we determine and handle if multiple instances are being used
+        // TODO handle authenticating to redis
+        System.out.println("Carson here");
+        RedisURI redisURI = new RedisURI("127.0.0.1", 6379, Duration.ofSeconds(60));
+        return new RedisStorage(redisConfig, redisURI);
     }
 }
