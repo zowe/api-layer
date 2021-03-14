9b8eaa1d36172a96655ca07f7785694aa1bf8a0b
jandadav <janda.david@gmail.com>
2021-03-14 09:19:06 +0100

    refactor
    Signed-off-by: jandadav <janda.david@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/util/service/CachingService.java
+++ integration-tests/src/test/java/org/zowe/apiml/util/service/CachingService.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import static org.awaitility.Awaitility.await;
 
 @Slf4j
+@Deprecated
 @RequiredArgsConstructor
 public class CachingService {
     private final String id;
