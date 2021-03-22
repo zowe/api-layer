b1041c3e2b017a71f4c7cbc683bba404949c87bc
achmelo <a.chmelo@gmail.com>
2021-03-22 08:48:37 +0100

    move instance env variables to configuration
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/util/service/RunningService.java
+++ integration-tests/src/test/java/org/zowe/apiml/util/service/RunningService.java
@@ -12,6 +12,7 @@ package org.zowe.apiml.util.service;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Duration;
 import org.zowe.apiml.util.DiscoveryRequests;
+import org.zowe.apiml.util.config.ConfigReader;
 
 import java.io.*;
 import java.net.URISyntaxException;
@@ -30,6 +31,7 @@ public class RunningService {
     private final String jarFile;
     private final String id;
     private String subprocessPid;
+    private Map<String,String> instanceEnv = ConfigReader.environmentConfiguration().getInstanceEnv();
 
     private final Map<String, String> parametersBefore;
     private final Map<String, String> parametersAfter;
@@ -66,42 +68,12 @@ public class RunningService {
 
         ProcessBuilder builder1 = new ProcessBuilder(bashScript);
         Map<String, String> envVariables = builder1.environment();
+        envVariables.putAll(instanceEnv);
         envVariables.put("LAUNCH_COMPONENT", jarFile);
-        envVariables.put("CMMN_LB", "build/libs/api-layer-lite-lib-all.jar");
-        envVariables.put("ZOWE_EXPLORER_HOST", "localhost");
-        envVariables.put("ZOWE_PREFIX", "ZWE");
-        envVariables.put("CATALOG_PORT", "10014");
-        envVariables.put("DISCOVERY_PORT", "10011");
-        envVariables.put("GATEWAY_PORT", "10010");
-        envVariables.put("APIML_ALLOW_ENCODED_SLASHES", "true");
-        envVariables.put("APIML_PREFER_IP_ADDRESS", "false");
-        envVariables.put("APIML_GATEWAY_TIMEOUT_MILLIS", "10000");
-        envVariables.put("APIML_SECURITY_X509_ENABLED", "true");
-        envVariables.put("APIML_SECURITY_AUTH_PROVIDER", "zosmf");
-        envVariables.put("ZOWE_IP_ADDRESS", "127.0.0.1");
-        envVariables.put("VERIFY_CERTIFICATES", "true");
-        envVariables.put("KEYSTORE", "keystore/localhost/localhost.keystore.p12");
-        envVariables.put("KEYSTORE_TYPE", "PKCS12");
-        envVariables.put("KEYSTORE_PASSWORD", "password");
-        envVariables.put("KEY_ALIAS", "localhost");
-        envVariables.put("TRUSTSTORE", "keystore/localhost/localhost.truststore.p12");
-        envVariables.put("ZWE_DISCOVERY_SERVICES_LIST", "https://localhost:10011/eureka/");
-        envVariables.put("WORKSPACE_DIR", "./workspace");
-        envVariables.put("APIML_MAX_CONNECTIONS_PER_ROUTE", "10");
-        envVariables.put("APIML_MAX_TOTAL_CONNECTIONS", "100");
-        envVariables.put("APIML_CORS_ENABLED", "true");
-        envVariables.put("APIML_SECURITY_ZOSMF_JWT_AUTOCONFIGURATION_MODE", "JWT");
-        envVariables.put("STATIC_DEF_CONFIG_DIR", "config/local/api-defs");
-        envVariables.put("APIML_GATEWAY_INTERNAL_ENABLED", "true");
-        envVariables.put("APIML_GATEWAY_INTERNAL_PORT", "10017");
-        envVariables.put("APIML_GATEWAY_INTERNAL_SSL_KEY_ALIAS", "localhost-multi");
-        envVariables.put("APIML_GATEWAY_INTERNAL_SSL_KEYSTORE", "keystore/localhost/localhost-multi.keystore.p12");
-        envVariables.put("APIML_DIAG_MODE_ENABLED", "diag");
-        envVariables.put("APIML_GATEWAY_CATALOG_ID","apicatalog");
+
         builder1.directory(new File("../"));
         ExecutorService executorService = Executors.newFixedThreadPool(1);
         executorService.submit(() -> executeCommand(builder1));
-//        executeCommand(builder1);
     }
 
     private void executeCommand(ProcessBuilder pb) {
@@ -112,14 +84,12 @@ public class RunningService {
             BufferedReader br = new BufferedReader(
                 new InputStreamReader(inputStream));
             String line;
-            int i = 0;
             while ((line = br.readLine()) != null) {
                 log.info(line);
                 if (line.startsWith("pid")) {
                     this.subprocessPid = line.substring(line.indexOf("=") + 1);
                     log.info("found " + this.subprocessPid);
                 }
-                i++;
             }
         } catch (IOException e) {
             log.error(e.getMessage());
