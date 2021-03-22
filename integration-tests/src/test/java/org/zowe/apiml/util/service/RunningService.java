a38eebb1a247ebd6f9e1affe9a4f86afd0db0e18
achmelo <37397715+achmelo@users.noreply.github.com>
2021-03-22 10:01:40 +0100

    chore: run apiml components with start script (#1281)
    * run apiml components with start script
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * print logs
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * diag mode catalog
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * eureka params
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * localhost IP address
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * GW url
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * apicatalog id
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * revert
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * revert gw url
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * print start script
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * debug
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * quotes
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * brackets
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * move instance env variables to configuration
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * remove debug messages
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * parametrize zosmf id
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/util/service/RunningService.java
+++ integration-tests/src/test/java/org/zowe/apiml/util/service/RunningService.java
@@ -12,12 +12,14 @@ package org.zowe.apiml.util.service;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Duration;
 import org.zowe.apiml.util.DiscoveryRequests;
+import org.zowe.apiml.util.config.ConfigReader;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.awaitility.Awaitility.await;
 
@@ -28,6 +30,8 @@ public class RunningService {
 
     private final String jarFile;
     private final String id;
+    private String subprocessPid;
+    private Map<String,String> instanceEnv = ConfigReader.environmentConfiguration().getInstanceEnv();
 
     private final Map<String, String> parametersBefore;
     private final Map<String, String> parametersAfter;
@@ -59,6 +63,39 @@ public class RunningService {
         newCachingProcess = builder1.inheritIO().start();
     }
 
+    public void startWithScript(String bashScript) {
+        log.info("Starting new Service with JAR file {} and ID {}", jarFile, id);
+
+        ProcessBuilder builder1 = new ProcessBuilder(bashScript);
+        Map<String, String> envVariables = builder1.environment();
+        envVariables.putAll(instanceEnv);
+        envVariables.put("LAUNCH_COMPONENT", jarFile);
+
+        builder1.directory(new File("../"));
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(() -> executeCommand(builder1));
+    }
+
+    private void executeCommand(ProcessBuilder pb) {
+        try {
+            Process terminalCommandProcess = pb.start();
+
+            InputStream inputStream = terminalCommandProcess.getInputStream();
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(inputStream));
+            String line;
+            while ((line = br.readLine()) != null) {
+                log.info(line);
+                if (line.startsWith("pid")) {
+                    this.subprocessPid = line.substring(line.indexOf("=") + 1);
+                    log.info("found " + this.subprocessPid);
+                }
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     public void waitUntilReady() {
         await()
             .atMost(Duration.TWO_MINUTES)
@@ -81,8 +118,17 @@ public class RunningService {
     }
 
     public void stop() {
+        if (subprocessPid != null) {
+            ProcessBuilder pb = new ProcessBuilder("kill", "-9", subprocessPid);
+            try {
+                pb.inheritIO().start();
+                return;
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
         if (newCachingProcess != null) {
-            log.info("Stopping new Service with ID {} on the port {}", id);
+            log.info("Stopping new Service with ID {}", id);
             newCachingProcess.destroy();
         }
     }
