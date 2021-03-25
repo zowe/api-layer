6520b1fc2717389a58edd56510ead0222df2b595
jandadav <janda.david@gmail.com>
2021-03-25 09:16:23 +0100

    Revert "run just one IT"
    This reverts commit 7657f602
    
    Signed-off-by: jandadav <janda.david@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/cachingservice/CachingApiIntegrationTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/cachingservice/CachingApiIntegrationTest.java
@@ -13,7 +13,8 @@ package org.zowe.apiml.cachingservice;
 import io.restassured.RestAssured;
 import io.restassured.config.RestAssuredConfig;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.zowe.apiml.util.TestWithStartedInstances;
 import org.zowe.apiml.util.categories.TestsNotMeantForZowe;
 import org.zowe.apiml.util.config.SslContext;
@@ -31,7 +32,6 @@ import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 
-@Tag("OnlyTestToRun")
 @TestsNotMeantForZowe
 class CachingApiIntegrationTest implements TestWithStartedInstances {
 
