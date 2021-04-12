015d985b90bd2103aea4ee601240b51664de6fc5
Boris Petkov <boris.petkov@broadcom.com>
2021-04-12 18:42:44 +0200

    Fix major issues from Sonar in Catalog UI
    Signed-off-by: Boris Petkov <boris.petkov@broadcom.com>

--- api-catalog-ui/frontend/src/reducers/error-reducer.jsx
+++ api-catalog-ui/frontend/src/reducers/error-reducer.jsx
@@ -1,6 +1,6 @@
 import { SEND_ERROR, CLEAR_ALL_ERRORS } from '../constants/error-constants';
 
-const errorReducer = (state = { errors: [] }, action) => {
+const errorReducer = (state = { errors: [] }, action = {}) => {
     switch (action.type) {
         case SEND_ERROR:
             return { ...state, errors: [...state.errors, action.payload] };
