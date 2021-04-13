04e9500eb743c0ea940fd9a8b4b05fec5afdac03
achmelo <a.chmelo@gmail.com>
2021-04-13 15:49:39 +0200

    code smell
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- gateway-service/src/test/java/org/zowe/apiml/gateway/security/login/zosmf/SaveZosmfPublicKeyConsoleApplicationTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/security/login/zosmf/SaveZosmfPublicKeyConsoleApplicationTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class SaveZosmfPublicKeyConsoleApplicationTest {
+class SaveZosmfPublicKeyConsoleApplicationTest {
 
     @Test
     void whenProvidedCorrectInputs_thenJWTSecretIsCreated() throws Exception {
