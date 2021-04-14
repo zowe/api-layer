69730e130e2f442d76ab3be6e8f7c2e30c8bb459
Boris Petkov <boris.petkov@broadcom.com>
2021-04-14 13:16:41 +0200

    chore: remove outstanding security hotspots reported by sonar (#1362)
    Signed-off-by: Boris Petkov <boris.petkov@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/config/SecurityConfiguration.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/config/SecurityConfiguration.java
@@ -101,7 +101,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .cors().and()
-            .csrf().disable()
+            .csrf().disable()   // NOSONAR
             .headers()
             .httpStrictTransportSecurity().disable()
             .frameOptions().disable()
