aa5c28e89ca2d2f4f58609a9f8b3f043786b9718
achmelo <a.chmelo@gmail.com>
2021-04-22 15:33:02 +0200

    get connector for request
    Signed-off-by: achmelo <a.chmelo@gmail.com>

---
+++ apiml-common/src/main/java/org/zowe/apiml/product/tomcat/ApimlTomcatCustomizer.java
@@ -0,0 +1,94 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.product.tomcat;
+
+import org.apache.coyote.AbstractProtocol;
+import org.apache.coyote.ProtocolHandler;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.net.AbstractEndpoint;
+import org.apache.tomcat.util.net.SocketEvent;
+import org.apache.tomcat.util.net.SocketWrapperBase;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+@Component
+public class ApimlTomcatCustomizer<S,U> implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> {
+            Http11NioProtocol protocolHandler = (Http11NioProtocol)connector.getProtocolHandler();
+            try {
+
+                Field handlerField = AbstractProtocol.class.getDeclaredField("handler");
+                handlerField.setAccessible(true);
+
+                AbstractEndpoint.Handler<S> handler = (AbstractEndpoint.Handler<S>)handlerField.get(protocolHandler);
+                handler = new ApimlAttlHandler<S>(handler);
+                Method method = AbstractProtocol.class.getDeclaredMethod("getEndpoint");
+                method.setAccessible(true);
+                AbstractEndpoint<S,U> abstractEndpoint = (AbstractEndpoint<S, U>)method.invoke(protocolHandler);
+                abstractEndpoint.setHandler(handler);
+                handlerField.set(protocolHandler,handler);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private static class ApimlAttlHandler<S> implements AbstractEndpoint.Handler<S>{
+
+        private final AbstractEndpoint.Handler<S> handler;
+
+        public ApimlAttlHandler(AbstractEndpoint.Handler<S> handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public SocketState process(SocketWrapperBase<S> socket, SocketEvent status) {
+
+            return handler.process(socket,status);
+        }
+
+        @Override
+        public Object getGlobal() {
+
+            return handler.getGlobal();
+        }
+
+        @Override
+        public Set<S> getOpenSockets() {
+            return handler.getOpenSockets();
+        }
+
+        @Override
+        public void release(SocketWrapperBase<S> socketWrapper) {
+            handler.release(socketWrapper);
+        }
+
+        @Override
+        public void pause() {
+            handler.pause();
+        }
+
+        @Override
+        public void recycle() {
+            handler.recycle();
+        }
+    }
+
+
+}
