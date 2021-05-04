99d14e0b21aa3ce3f0e64d11df24b6b76ed7111b
Jakub Balhar <jakub@balhar.net>
2021-05-04 14:03:56 +0200

    Update the action to identify security related PRs
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

--- scripts/security_sensitive/index.js
+++ scripts/security_sensitive/index.js
@@ -1,4 +1,39 @@
+import {Octokit} from "octokit";
+
 // Get the directory with files
-const workDirectory = process.argv[2];
-// Test data
+const owner = process.argv[2];
+const repository = process.argv[3];  //${{ github.repository }} // Would it contain owner or is it separated? But I know these values could be set
+const prNumber = process.argv[4]; // ${{ github.event.pull_request.number }}
+const githubToken = process.argv[5];
+
+console.log(prNumber);
+
+(async function () {
+
+    const octokit = new Octokit({auth: githubToken});
+
+    const { data } = await octokit.rest.pulls.listFiles({
+        owner: owner,
+        repo: repository,
+        pull_number: prNumber
+    });
+
+    let isRisky = false;
+
+    // What do we consider security risky?
+    data.forEach(file => {
+        if(file.filename.indexOf("security") !== -1) {
+            isRisky = true;
+        }
+    });
+
+    if(isRisky) {
+        await octokit.rest.issues.addLabels({
+            owner,
+            repo: repository,
+            issue_number: prNumber,
+            labels: ['Sensitive']
+        });
+    }
+})()
 
