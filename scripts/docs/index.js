b05081f2badef276b9d05e76129f1561dd37f340
Jakub Balhar <jakub@balhar.net>
2021-05-05 16:12:20 +0200

    Update the way to verify the difference on files
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

--- scripts/docs/index.js
+++ scripts/docs/index.js
@@ -1,4 +1,5 @@
 import {Octokit} from "octokit";
+import {readFile} from "fs/promises";
 
 // Get the directory with files
 const owner = process.argv[2];
@@ -11,12 +12,17 @@ const prNumber = process.argv[5];
     const octokit = new Octokit({auth: githubToken});
 
     try {
-        await octokit.rest.issues.addLabels({
-            owner,
-            repo: repository,
-            issue_number: prNumber,
-            labels: ['docs']
-        });
+        const file1 = await readFile("../../docs/docgen/ErrorMessagesDocumentation.md");
+        const file2 = await readFile("../../../docs-site/docs/troubleshoot/troubleshoot-apiml-error-codes.md");
+
+        if(file1.toString() != file2.toString()) {
+            await octokit.rest.issues.addLabels({
+                owner,
+                repo: repository,
+                issue_number: prNumber,
+                labels: ['docs']
+            });
+        }
     } catch (e) {
         console.log(e);
     }
