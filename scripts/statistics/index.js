d5a5a5e03408359dba0309774e30e3c57f62efe3
Jakub Balhar <jakub@balhar.net>
2021-05-06 17:21:03 +0200

    Add baseline information about the PRs
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

--- scripts/statistics/index.js
+++ scripts/statistics/index.js
@@ -8,14 +8,6 @@ const owner = process.argv[2];
 const repository = process.argv[3];  //${{ github.repository }} // Would it contain owner or is it separated? But I know these values could be set
 const githubToken = process.argv[4];
 
-// Issues related
-// Breakdown between issues opened by the squad vs. opened externally
-// The time how long we resolve external issues.
-
-// PR related
-// Size of the PR
-// Time the PR is opened
-
 // CI related
 // How long does our pipelines run?
 
@@ -24,10 +16,9 @@ const githubToken = process.argv[4];
     const octokit = new Octokit({auth: githubToken});
 
     try {
-        gatherCiStats(octokit, owner, repository);
-        gatherPrStats(octokit, owner, repository);
-        gatherIssueStats(octokit, owner, repository);
-        // This works, wew can limit or not limit runs as we see fit.
+        await gatherCiStats(octokit, owner, repository);
+        await gatherPrStats(octokit, owner, repository);
+        // await gatherIssueStats(octokit, owner, repository);
     } catch (e) {
         console.log(e);
     }
--- scripts/statistics/pullRequests.js
+++ scripts/statistics/pullRequests.js
@@ -1,5 +1,51 @@
-async function gatherPrStats(octokit, owner, repository) {
+// PR related
+// Size of the PR
+// Time the PR is opened
 
+async function gatherPrStats(octokit, owner, repo) {
+    let pullRequests = await loadPage(octokit, owner, repo, 0);
+
+    let page = 1;
+    let time = 0;
+    let amountOfPrs = 0;
+    while(pullRequests.length > 0) {
+        pullRequests
+            .forEach(pullRequest => {
+                const createdAt = new Date(pullRequest.created_at);
+                const closedAt = new Date(pullRequest.closed_at);
+
+                const timeToClose = closedAt - createdAt;
+                time += timeToClose;
+                amountOfPrs++;
+            });
+
+        pullRequests = await loadPage(octokit, owner, repo, page);
+        page++;
+    }
+
+    const averageTime = time / amountOfPrs;
+    const HOUR = 1000 * 60 * 60
+    const DAY = 24 * HOUR;
+    const days = Math.floor(averageTime / (DAY));
+    const hours = Math.floor((averageTime - (days * DAY)) / HOUR);
+
+    console.log({
+        averageTime: `Days: ${days}, Hours: ${hours}`
+    })
 }
+
+async function loadPage (octokit, owner, repo, page) {
+    const result = await octokit.rest.pulls.list({
+        owner,
+        repo,
+        per_page: 100,
+        state: 'closed',
+        page: page
+    });
+
+    return result.data;
+}
+
+
 
 exports.gatherPrStats = gatherPrStats;
