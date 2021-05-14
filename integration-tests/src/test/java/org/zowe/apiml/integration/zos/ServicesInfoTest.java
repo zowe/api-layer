eaf14467bfd9c5e757cd7e5de560c2f4724c5f2f
Jakub Balhar <jakub@balhar.net>
2021-05-14 13:14:30 +0200

    Refactor tests around the Service info
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

--- integration-tests/src/test/java/org/zowe/apiml/integration/zos/ServicesInfoTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/zos/ServicesInfoTest.java
@@ -13,8 +13,11 @@ import io.restassured.RestAssured;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.http.HttpHeaders;
 import org.zowe.apiml.util.SecurityUtils;
@@ -23,8 +26,11 @@ import org.zowe.apiml.util.categories.GeneralAuthenticationTest;
 import org.zowe.apiml.util.config.ConfigReader;
 import org.zowe.apiml.util.http.HttpRequestUtils;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -33,7 +39,14 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.core.Is.is;
 import static org.zowe.apiml.constants.ApimlConstants.BASIC_AUTHENTICATION_PREFIX;
 import static org.zowe.apiml.util.SecurityUtils.GATEWAY_TOKEN_COOKIE_NAME;
+import static org.zowe.apiml.util.http.HttpRequestUtils.getUriFromGateway;
 
+/**
+ * Verify it's possible to retrieve information about services onboarded to the gateway if the user requesting the
+ * information has valid authorization in the ESM.
+ *
+ * The integration is about verifying the integration with ESM.
+ */
 @GeneralAuthenticationTest
 class ServicesInfoTest implements TestWithStartedInstances {
 
@@ -48,8 +61,10 @@ class ServicesInfoTest implements TestWithStartedInstances {
 
     private static final String SERVICES_ENDPOINT = "gateway/api/v1/services";
     private static final String SERVICES_ENDPOINT_NOT_VERSIONED = "gateway/services";
+
     private static final String API_CATALOG_SERVICE_ID = "apicatalog";
     private static final String API_CATALOG_SERVICE_API_ID = "zowe.apiml.apicatalog";
+    private static final String API_CATALOG_PATH = "/apicatalog/api/v1";
 
     private static String token;
 
@@ -57,105 +72,121 @@ class ServicesInfoTest implements TestWithStartedInstances {
     void setUp() {
         RestAssured.useRelaxedHTTPSValidation();
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
         token = SecurityUtils.gatewayToken(USERNAME, PASSWORD);
     }
 
-    @ParameterizedTest(name = "cannotBeAccessedWithoutAuthentication({0})")
-    @ValueSource(strings = {
-        SERVICES_ENDPOINT,
-        SERVICES_ENDPOINT_NOT_VERSIONED,
-        SERVICES_ENDPOINT + "/" + API_CATALOG_SERVICE_ID,
-        SERVICES_ENDPOINT_NOT_VERSIONED + "/" + API_CATALOG_SERVICE_ID
-    })
-    void cannotBeAccessedWithoutAuthentication(String endpoint) {
-        //@formatter:off
-        when()
-            .get(HttpRequestUtils.getUriFromGateway(endpoint))
-        .then()
-            .statusCode(is(SC_UNAUTHORIZED))
-            .header(HttpHeaders.WWW_AUTHENTICATE, BASIC_AUTHENTICATION_PREFIX);
-        //@formatter:on
+    @Nested
+    class WhenGettingInformationAboutServices {
+        @Nested
+        class ReturnUnauthorized {
+            @ParameterizedTest(name = "givenNoAuthentication {index} {0} ")
+            @ValueSource(strings = {
+                SERVICES_ENDPOINT,
+                SERVICES_ENDPOINT_NOT_VERSIONED,
+                SERVICES_ENDPOINT + "/" + API_CATALOG_SERVICE_ID,
+                SERVICES_ENDPOINT_NOT_VERSIONED + "/" + API_CATALOG_SERVICE_ID
+            })
+            void givenNoAuthentication(String endpoint) {
+                //@formatter:off
+                when()
+                    .get(getUriFromGateway(endpoint))
+                .then()
+                    .statusCode(is(SC_UNAUTHORIZED));
+                //@formatter:on
+            }
+        }
+
+        @Nested
+        class ReturnAllServices {
+            @Test
+            @SuppressWarnings({"squid:S2699", "Assets are after then()"})
+            void givenValidToken() {
+                //@formatter:off
+                given()
+                    .cookie(GATEWAY_TOKEN_COOKIE_NAME, token)
+                .when()
+                    .get(getUriFromGateway(SERVICES_ENDPOINT))
+                .then()
+                    .statusCode(is(SC_OK))
+                    .header(VERSION_HEADER, CURRENT_VERSION)
+                    .body("serviceId", hasItems("gateway", "discovery", API_CATALOG_SERVICE_ID));
+                //@formatter:on
+            }
+        }
+
+        @Nested
+        class ReturnForbidden {
+            @Test
+            @SuppressWarnings({"squid:S2699", "Assets are after then()"})
+            void givenInvalidCredentials() {
+                String expectedMessage = "The user is not authorized to the target resource:";
+
+                //@formatter:off
+                given()
+                    .auth().basic(UNAUTHORIZED_USERNAME, UNAUTHORIZED_PASSWORD)
+                .when()
+                    .get(getUriFromGateway(SERVICES_ENDPOINT))
+                .then()
+                    .statusCode(is(SC_FORBIDDEN))
+                    .body("messages.find { it.messageNumber == 'ZWEAT403E' }.messageContent", startsWith(expectedMessage));
+                //@formatter:on
+            }
+        }
     }
 
-    @Test
-    @SuppressWarnings({"squid:S2699", "Assets are after then()"})
-    void providesAllServicesInformation() {
-        //@formatter:off
-        given()
-            .cookie(GATEWAY_TOKEN_COOKIE_NAME, token)
-        .when()
-            .get(HttpRequestUtils.getUriFromGateway(SERVICES_ENDPOINT))
-        .then()
-            .statusCode(is(SC_OK))
-            .header(VERSION_HEADER, CURRENT_VERSION)
-            .body("serviceId", hasItems("gateway", "discovery", API_CATALOG_SERVICE_ID));
-        //@formatter:on
+    protected static Stream<Arguments> serviceSpecificUrls() {
+        return Stream.of(
+            Arguments.of(getUriFromGateway(SERVICES_ENDPOINT + "/" + API_CATALOG_SERVICE_ID), API_CATALOG_SERVICE_ID, API_CATALOG_SERVICE_API_ID, API_CATALOG_PATH),
+            Arguments.of(getUriFromGateway(SERVICES_ENDPOINT + "/gateway"), "gateway", "zowe.apiml.gateway", "/gateway/api/v1")
+        );
     }
 
-    @Test
-    @SuppressWarnings({"squid:S2699", "Assets are after then()"})
-    void providesServicesInformationByApiId() {
-        List<NameValuePair> arguments = new ArrayList<>();
-        arguments.add(new BasicNameValuePair("apiId", API_CATALOG_SERVICE_API_ID));
+    @Nested
+    class WhenGettingDetailsAboutSpecificService {
+        @Nested
+        class ReturnDetailedInformationInObject {
+            @ParameterizedTest(name = "givenValidTokenWithAuthorizedUserAndValidServiceId {index} {0} {1} {2} {3}")
+            @MethodSource("org.zowe.apiml.integration.zos.ServicesInfoTest#serviceSpecificUrls")
+            @SuppressWarnings({"squid:S2699", "Assets are after then()"})
+            void givenValidTokenWithAuthorizedUserAndValidServiceId(URI uri, String serviceId, String serviceApiId, String servicePath) {
+                //@formatter:off
+                given()
+                    .cookie(GATEWAY_TOKEN_COOKIE_NAME, token)
+                .when()
+                    .get(uri)
+                .then()
+                    .statusCode(is(SC_OK))
+                    .header(VERSION_HEADER, CURRENT_VERSION)
 
-        //@formatter:off
-        given()
-            .cookie(GATEWAY_TOKEN_COOKIE_NAME, token)
-        .when()
-            .get(HttpRequestUtils.getUriFromGateway(SERVICES_ENDPOINT, arguments))
-        .then()
-            .statusCode(is(SC_OK))
-            .header(VERSION_HEADER, CURRENT_VERSION)
-            .body("size()", is(1))
-            .body("serviceId", hasItem(API_CATALOG_SERVICE_ID));
-        //@formatter:on
+                    .body("serviceId", is(serviceId))
+                    .body("apiml.apiInfo[0].apiId", is(serviceApiId))
+                    .body("apiml.apiInfo[0].basePath", is(servicePath));
+
+                //@formatter:on
+            }
+        }
+
+        @Nested
+        class ReturnDetailedInformationInArray {
+            @Test
+            @SuppressWarnings({"squid:S2699", "Assets are after then()"})
+            void givenValidTokenWithAuthorizedUserAndValidServiceId() {
+                //@formatter:off
+                given()
+                    .cookie(GATEWAY_TOKEN_COOKIE_NAME, token)
+                .when()
+                    .get(getUriFromGateway(SERVICES_ENDPOINT, Collections.singletonList(new BasicNameValuePair("apiId", API_CATALOG_SERVICE_API_ID))))
+                .then()
+                    .statusCode(is(SC_OK))
+                    .header(VERSION_HEADER, CURRENT_VERSION)
+
+                    .body("serviceId", hasItem(API_CATALOG_SERVICE_ID))
+                    .body("apiml.apiInfo[0].apiId", hasItem(API_CATALOG_SERVICE_API_ID))
+                    .body("apiml.apiInfo[0].basePath", hasItem(API_CATALOG_PATH));
+
+                //@formatter:on
+            }
+        }
     }
-
-    @Test
-    @SuppressWarnings({"squid:S2699", "Assets are after then()"})
-    void providesApiCatalogServiceInformation() {
-        //@formatter:off
-        given()
-            .cookie(GATEWAY_TOKEN_COOKIE_NAME, token)
-        .when()
-            .get(HttpRequestUtils.getUriFromGateway(SERVICES_ENDPOINT + "/" + API_CATALOG_SERVICE_ID))
-        .then()
-            .statusCode(is(SC_OK))
-            .header(VERSION_HEADER, CURRENT_VERSION)
-            .body("apiml.apiInfo[0].apiId", equalTo(API_CATALOG_SERVICE_API_ID))
-            .body("apiml.apiInfo[0].basePath", equalTo("/apicatalog/api/v1"));
-        //@formatter:on
-    }
-
-    @Test
-    @SuppressWarnings({"squid:S2699", "Assets are after then()"})
-    void providesGatewayServiceInformation() {
-        //@formatter:off
-        given()
-            .cookie(GATEWAY_TOKEN_COOKIE_NAME, token)
-        .when()
-            .get(HttpRequestUtils.getUriFromGateway(SERVICES_ENDPOINT + "/gateway"))
-        .then()
-            .statusCode(is(SC_OK))
-            .header(VERSION_HEADER, CURRENT_VERSION)
-            .body("apiml.apiInfo[0].apiId", equalTo("zowe.apiml.gateway"));
-        //@formatter:on
-    }
-
-    @Test
-    @SuppressWarnings({"squid:S2699", "Assets are after then()"})
-    void whenUnauthorized_thenReturn403() {
-        String expectedMessage = "The user is not authorized to the target resource:";
-
-        //@formatter:off
-        given()
-            .auth().basic(UNAUTHORIZED_USERNAME, UNAUTHORIZED_PASSWORD)
-        .when()
-            .get(HttpRequestUtils.getUriFromGateway(SERVICES_ENDPOINT))
-        .then()
-            .statusCode(is(SC_FORBIDDEN))
-            .body("messages.find { it.messageNumber == 'ZWEAT403E' }.messageContent", startsWith(expectedMessage));
-        //@formatter:on
-    }
-
 }
