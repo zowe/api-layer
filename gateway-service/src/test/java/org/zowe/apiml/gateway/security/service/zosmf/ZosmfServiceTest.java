ce83881dbb5f7cd49654b538fe562f5e9edadf9d
Andrea Tabone <andrea.tabone@broadcom.com>
2021-05-20 14:56:17 +0200

    Remove unused import
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfServiceTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfServiceTest.java
@@ -38,7 +38,6 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
