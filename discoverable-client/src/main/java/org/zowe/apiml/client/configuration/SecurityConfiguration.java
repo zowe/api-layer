05c9db8605b17e341c92a0db2534c5e87ce1fec7
achmelo <a.chmelo@gmail.com>
2021-05-24 18:53:42 +0200

    ignore auth header for /api/**
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
@@ -12,6 +12,7 @@ package org.zowe.apiml.client.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,6 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+
             .authorizeRequests()
             .antMatchers("/ws/**").authenticated()
             .antMatchers("/**").permitAll()
@@ -33,5 +35,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
             .withUser("user")
             .password("{noop}pass").roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+            .antMatchers( "/api/**");
     }
 }
