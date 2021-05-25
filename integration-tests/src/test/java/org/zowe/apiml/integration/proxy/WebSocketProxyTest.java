112da99c291d5beb288af33af97944f4682914ef
achmelo <37397715+achmelo@users.noreply.github.com>
2021-05-25 09:35:21 +0200

    feat: websocket authentication support (#1482)
    * unauthorized response message, websocket tests
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * ignore auth header for /api/**
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * format
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/proxy/WebSocketProxyTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/proxy/WebSocketProxyTest.java
@@ -10,6 +10,8 @@
 package org.zowe.apiml.integration.proxy;
 
 import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -27,6 +29,7 @@ import org.zowe.apiml.util.http.HttpClientUtils;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -39,9 +42,26 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class WebSocketProxyTest implements TestWithStartedInstances {
     private final GatewayServiceConfiguration serviceConfiguration = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration();
 
-    private final static int WAIT_TIMEOUT_MS = 10000;
-    private final static String UPPERCASE_URL = "/ws/v1/discoverableclient/uppercase";
-    private final static String HEADER_URL = "/ws/v1/discoverableclient/header";
+    private static final int WAIT_TIMEOUT_MS = 10000;
+    private static final String UPPERCASE_URL = "/ws/v1/discoverableclient/uppercase";
+    private static final String HEADER_URL = "/ws/v1/discoverableclient/header";
+
+    private static final WebSocketHttpHeaders VALID_AUTH_HEADERS = new WebSocketHttpHeaders();
+    private static final WebSocketHttpHeaders INVALID_AUTH_HEADERS = new WebSocketHttpHeaders();
+    private static final String validToken = "apimlAuthenticationToken=tokenValue";
+
+
+    @BeforeAll
+    static void setup() {
+        String plainCred = "user:pass";
+        String base64cred = Base64.getEncoder().encodeToString(plainCred.getBytes());
+        VALID_AUTH_HEADERS.add("Authorization", "Basic " + base64cred);
+
+        String invalidPlainCred = "user:invalidPass";
+        String invalidBase64cred = Base64.getEncoder().encodeToString(invalidPlainCred.getBytes());
+        INVALID_AUTH_HEADERS.add("Authorization", "Basic " + invalidBase64cred);
+
+    }
 
     private TextWebSocketHandler appendResponseHandler(StringBuilder target, int countToNotify) {
         final AtomicInteger counter = new AtomicInteger(countToNotify);
@@ -89,86 +109,127 @@ class WebSocketProxyTest implements TestWithStartedInstances {
         return appendingWebSocketSession(url, null, response, countToNotify);
     }
 
-    @Test
-    void shouldRouteWebSocketSession() throws Exception {
-        final StringBuilder response = new StringBuilder();
-        WebSocketSession session = appendingWebSocketSession(discoverableClientGatewayUrl(UPPERCASE_URL), response, 1);
 
-        session.sendMessage(new TextMessage("hello world!"));
-        synchronized (response) {
-            response.wait(WAIT_TIMEOUT_MS);
+    @Nested
+    class WhenRoutingSession {
+        @Nested
+        class Authentication {
+            @Nested
+            class WhenValid {
+                @Nested
+                class ReturnSuccess {
+                    @Test
+                    void message() throws Exception {
+                        final StringBuilder response = new StringBuilder();
+
+                        WebSocketSession session = appendingWebSocketSession(discoverableClientGatewayUrl(UPPERCASE_URL), VALID_AUTH_HEADERS, response, 1);
+
+                        session.sendMessage(new TextMessage("hello world!"));
+                        synchronized (response) {
+                            response.wait(WAIT_TIMEOUT_MS);
+                        }
+
+                        assertEquals("HELLO WORLD!", response.toString());
+                        session.close();
+                    }
+
+                    @Test
+                    void headers() throws Exception {
+                        final StringBuilder response = new StringBuilder();
+                        VALID_AUTH_HEADERS.add("X-Test", "value");
+                        VALID_AUTH_HEADERS.add("Cookie", validToken);
+                        WebSocketSession session = appendingWebSocketSession(discoverableClientGatewayUrl(HEADER_URL), VALID_AUTH_HEADERS, response, 1);
+
+                        session.sendMessage(new TextMessage("gimme those headers"));
+                        synchronized (response) {
+                            response.wait(WAIT_TIMEOUT_MS);
+                        }
+
+                        assertTrue(response.toString().contains("x-test:\"value\""));
+                        assertTrue(response.toString().contains(validToken));
+                        session.sendMessage(new TextMessage("bye"));
+                        session.close();
+                    }
+                }
+
+                @Nested
+                class ReturnError {
+                    @Test
+                    void whenPathIsNotCorrect() throws Exception {
+                        final StringBuilder response = new StringBuilder();
+                        appendingWebSocketSession(discoverableClientGatewayUrl(UPPERCASE_URL + "bad"), VALID_AUTH_HEADERS, response, 1);
+
+                        synchronized (response) {
+                            response.wait(WAIT_TIMEOUT_MS);
+                        }
+
+                        System.out.println("Response: " + response.toString());
+                        assertEquals(0, response.toString().indexOf("CloseStatus[code=1003,"));
+                    }
+
+                    @Test
+                    void whenServiceIsNotCorrect() throws Exception {
+                        final StringBuilder response = new StringBuilder();
+                        appendingWebSocketSession(discoverableClientGatewayUrl("/ws/v1/wrong-service/uppercase"), VALID_AUTH_HEADERS, response, 1);
+
+                        synchronized (response) {
+                            response.wait(WAIT_TIMEOUT_MS);
+                        }
+
+                        assertEquals("CloseStatus[code=1003, reason=Requested service wrong-service is not known by the gateway]",
+                            response.toString());
+                    }
+
+                    @Test
+                    void whenUrlFormatIsNotCorrect() throws Exception {
+                        final StringBuilder response = new StringBuilder();
+                        appendingWebSocketSession(discoverableClientGatewayUrl("/ws/wrong"), response, 1);
+
+                        synchronized (response) {
+                            response.wait(WAIT_TIMEOUT_MS);
+                        }
+
+                        assertEquals("CloseStatus[code=1003, reason=Invalid URL format]", response.toString());
+                    }
+                }
+            }
+
+            @Nested
+            class WhenInvalid {
+                @Test
+                void returnError() throws Exception {
+                    final StringBuilder response = new StringBuilder();
+
+                    WebSocketSession session = appendingWebSocketSession(discoverableClientGatewayUrl(UPPERCASE_URL), INVALID_AUTH_HEADERS, response, 1);
+
+                    session.sendMessage(new TextMessage("hello world!"));
+                    synchronized (response) {
+                        response.wait(WAIT_TIMEOUT_MS);
+                    }
+
+                    assertEquals("CloseStatus[code=1003, reason=Invalid login credentials]", response.toString());
+                    session.close();
+                }
+            }
         }
 
-        assertEquals("HELLO WORLD!", response.toString());
-        session.close();
     }
 
-    @Test
-    void shouldRouteHeaders() throws Exception {
-        final StringBuilder response = new StringBuilder();
-        WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
-        headers.add("X-Test", "value");
-        WebSocketSession session = appendingWebSocketSession(discoverableClientGatewayUrl(HEADER_URL), headers, response, 1);
+    @Nested
+    class WhenClosingSession {
+        @Test
+        void getCorrectResponse() throws Exception {
+            final StringBuilder response = new StringBuilder();
+            WebSocketSession session = appendingWebSocketSession(discoverableClientGatewayUrl(UPPERCASE_URL), VALID_AUTH_HEADERS, response, 2);
 
-        session.sendMessage(new TextMessage("gimme those headers"));
-        synchronized (response) {
-            response.wait(WAIT_TIMEOUT_MS);
+            session.sendMessage(new TextMessage("bye"));
+            synchronized (response) {
+                response.wait(WAIT_TIMEOUT_MS);
+            }
+
+            assertEquals("BYECloseStatus[code=1000, reason=null]", response.toString());
         }
-
-        assertTrue(response.toString().contains("x-test:\"value\""));
-        session.sendMessage(new TextMessage("bye"));
-        session.close();
     }
 
-    @Test
-    void shouldCloseSessionAfterClientServerCloses() throws Exception {
-        final StringBuilder response = new StringBuilder();
-        WebSocketSession session = appendingWebSocketSession(discoverableClientGatewayUrl(UPPERCASE_URL), response, 2);
-
-        session.sendMessage(new TextMessage("bye"));
-        synchronized (response) {
-            response.wait(WAIT_TIMEOUT_MS);
-        }
-
-        assertEquals("BYECloseStatus[code=1000, reason=null]", response.toString());
-    }
-
-    @Test
-    void shouldFailIfPathIsNotCorrect() throws Exception {
-        final StringBuilder response = new StringBuilder();
-        appendingWebSocketSession(discoverableClientGatewayUrl(UPPERCASE_URL + "bad"), response, 1);
-
-        synchronized (response) {
-            response.wait(WAIT_TIMEOUT_MS);
-        }
-
-        System.out.println("Response: " + response.toString());
-        assertEquals(0, response.toString().indexOf("CloseStatus[code=1003,"));
-    }
-
-    @Test
-    void shouldFailIfServiceIsNotCorrect() throws Exception {
-        final StringBuilder response = new StringBuilder();
-        appendingWebSocketSession(discoverableClientGatewayUrl("/ws/v1/wrong-service/uppercase"), response, 1);
-
-        synchronized (response) {
-            response.wait(WAIT_TIMEOUT_MS);
-        }
-
-        assertEquals("CloseStatus[code=1003, reason=Requested service wrong-service is not known by the gateway]",
-            response.toString());
-    }
-
-    @Test
-    void shouldFailIfUrlFormatIsNotCorrect() throws Exception {
-        final StringBuilder response = new StringBuilder();
-        appendingWebSocketSession(discoverableClientGatewayUrl("/ws/wrong"), response, 1);
-
-        synchronized (response) {
-            response.wait(WAIT_TIMEOUT_MS);
-        }
-
-        assertEquals("CloseStatus[code=1003, reason=Invalid URL format]", response.toString());
-    }
 
 }
