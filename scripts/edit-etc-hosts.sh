d45caa75aefbe542574f30b6958cb39a821e5bfb
Andrea Tabone <andrea.tabone@broadcom.com>
2021-05-26 11:50:03 +0200

    Refactor to make the remaining services start at the same time with the others
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- scripts/edit-etc-hosts.sh
+++ scripts/edit-etc-hosts.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 # run:
-# ./manage-etc-hosts.sh add 10.20.1.2 test.com
-# ./manage-etc-hosts.sh remove 10.20.1.2 test.com
+# ./edit-etc-hosts.sh add 10.20.1.2 test.com
 
 # PATH TO YOUR HOSTS FILE
 ETC_HOSTS=/etc/hosts
-
 
 add() {
     IP=$1
@@ -17,14 +15,14 @@ add() {
         then
             echo "$line_content already exists : $(grep "$HOSTNAME" $ETC_HOSTS)"
         else
-            echo "Adding $line_content to your $ETC_HOSTS";
-            sh -c -e "echo '$line_content' >> /etc/hosts";
+            echo "Adding $line_content to your $ETC_HOSTS"
+            echo "$line_content" >> /etc/hosts
 
             if [ -n "$(grep -P "$HOSTNAME" $ETC_HOSTS)" ]
                 then
-                    echo "$line_content was added succesfully";
+                    echo "$line_content was added succesfully"
                 else
-                    echo "Failed to Add $line_content, Try again!";
+                    echo "Failed to Add $line_content, Try again!"
             fi
     fi
 }
