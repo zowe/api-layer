7a1a7aaf9579a0fbe5f76b3d43124f43cd389c58
Andrea Tabone <andrea.tabone@broadcom.com>
2021-05-26 11:22:30 +0200

    fix
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- scripts/edit-etc-hosts.sh
+++ scripts/edit-etc-hosts.sh
@@ -8,34 +8,19 @@
 ETC_HOSTS=/etc/hosts
 
 
-function remove() {
-    # IP to add/remove.
-    IP=$1
-    # Hostname to add/remove.
-    HOSTNAME=$2
-    HOSTS_LINE="$IP[[:space:]]$HOSTNAME"
-    if [ -n "$(grep -P $HOSTS_LINE $ETC_HOSTS)" ]
-    then
-        echo "$HOSTS_LINE Found in your $ETC_HOSTS, Removing now...";
-        sed -i".bak" "/$HOSTS_LINE/d" $ETC_HOSTS
-    else
-        echo "$HOSTS_LINE was not found in your $ETC_HOSTS";
-    fi
-}
-
-function add() {
+add() {
     IP=$1
     HOSTNAME=$2
     HOSTS_LINE="$IP[[:space:]]$HOSTNAME"
     line_content=$( printf "%s\t%s\n" "$IP" "$HOSTNAME" )
-    if [ -n "$(grep -P $HOSTS_LINE $ETC_HOSTS)" ]
+    if [ -n "$(grep -P "$HOSTS_LINE" $ETC_HOSTS)" ]
         then
-            echo "$line_content already exists : $(grep $HOSTNAME $ETC_HOSTS)"
+            echo "$line_content already exists : $(grep "$HOSTNAME" $ETC_HOSTS)"
         else
             echo "Adding $line_content to your $ETC_HOSTS";
-            -- sh -c -e "echo '$line_content' >> /etc/hosts";
+            sh -c -e "echo '$line_content' >> /etc/hosts";
 
-            if [ -n "$(grep -P $HOSTNAME $ETC_HOSTS)" ]
+            if [ -n "$(grep -P "$HOSTNAME" $ETC_HOSTS)" ]
                 then
                     echo "$line_content was added succesfully";
                 else
