7d1d477026720428e2b962413a98c8630b4cc001
Carson Cook <carson.cook@ibm.com>
2021-05-26 08:54:12 -0400

    Add cypress e2e tests
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

---
+++ metrics-service-ui/frontend/cypress/integration/e2e/login.test.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+
+describe('>>> Login page tests', () => {
+    describe('>>> Login ok page test', () => {
+        it('succesfully loads login page', () => {
+            cy.visit(`${Cypress.env('metricsHomePage')}`);
+        });
+
+        it('should not display header', () => {
+            cy.get('.header').should('not.exist');
+        });
+
+        it('should log in user and check session cookie', () => {
+            const username = Cypress.env('username');
+            const password = Cypress.env('password');
+
+            cy.get('button[type="submit"').as('submitButton').should('exist');
+
+            cy.get('#username').type(username);
+            cy.get('#password').type(password);
+
+            cy.get('@submitButton').should('not.be.disabled');
+            cy.get('@submitButton').click();
+
+            cy.url().should('contain', '/dashboard');
+            cy.get('.header').should('exist');
+
+            cy.getCookie('apimlAuthenticationToken').should('exist');
+        });
+
+        it('should logout and delete session cookie', () => {
+            cy.get('button[data-testid="logout"]').click();
+            cy.contains('Metrics Service');
+
+            cy.getCookie('apimlAuthenticationToken').should('not.exist');
+        });
+    });
+
+    describe('>>> Login bad test', () => {
+        it('succesfully loads login page', () => {
+            cy.visit(`${Cypress.env('metricsHomePage')}/`);
+        });
+
+        it('should not display header', () => {
+            cy.get('.header').should('not.exist');
+        });
+
+        it('should fail with wrong credentials', () => {
+            const user = { username: 'bad', password: 'bad' };
+
+            cy.get('#username').type(user.username);
+            cy.get('#password').type(user.password);
+            cy.get('button[type="submit"').as('submitButton');
+            cy.get('@submitButton').click();
+
+            cy.url().should('contain', '/login');
+
+            cy.get('.error-message-content')
+                .should('exist')
+                .should('contain', '(ZWEAS120E) Invalid username or password');
+        });
+    });
+});
