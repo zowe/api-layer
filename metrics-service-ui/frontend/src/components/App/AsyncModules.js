54357a842a79648d9127d41fb1149279b4558727
Carson Cook <carson.cook@ibm.com>
2021-05-27 17:06:34 -0400

    Cleanup
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- metrics-service-ui/frontend/src/components/App/AsyncModules.js
+++ metrics-service-ui/frontend/src/components/App/AsyncModules.js
@@ -1,6 +1,5 @@
 import Loadable from 'react-loadable';
-// eslint-disable-next-line no-unused-vars
-import React, { lazy } from 'react';
+import { lazy } from 'react';
 
 export const AsyncAppContainer = Loadable({
     loader: () => import('./AppContainer'),
