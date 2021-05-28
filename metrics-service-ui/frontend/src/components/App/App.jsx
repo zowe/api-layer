c06b87feaf4e89f9c2cf2e486730f1909428e3ae
Carson Cook <carson.cook@ibm.com>
2021-05-28 09:54:15 -0400

    Add license to metrics ui dev files
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- metrics-service-ui/frontend/src/components/App/App.jsx
+++ metrics-service-ui/frontend/src/components/App/App.jsx
@@ -1,3 +1,13 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
 import React, { Component, Suspense } from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
