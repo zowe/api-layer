1dac9fb9e0cf49f218e9982653f40acdac594cfb
Carson Cook <carson.cook@ibm.com>
2021-05-28 14:03:20 -0400

    log test
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- metrics-service-ui/frontend/src/setupTests.js
+++ metrics-service-ui/frontend/src/setupTests.js
@@ -15,3 +15,6 @@ import 'jest-enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 enzyme.configure({ adapter: new Adapter() });
+
+// eslint-disable-next-line no-console
+console.log('IN SETUP TESTS');
