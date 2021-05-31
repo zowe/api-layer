fb9a5efd8902d4290b174900ab8fc125990e6de4
Carson Cook <CarsonCook@users.noreply.github.com>
2021-05-31 01:44:54 -0400

    chore: Add license header to jsx/js files (#1499)
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- api-catalog-ui/frontend/src/actions/error-actions.jsx
+++ api-catalog-ui/frontend/src/actions/error-actions.jsx
@@ -1,3 +1,13 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
 import * as log from 'loglevel';
 import uuidv4 from 'uuid/v4';
 import { SEND_ERROR, CLEAR_ALL_ERRORS } from '../constants/error-constants';
