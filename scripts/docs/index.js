25514a838e702d8c164fac50591c06fc93c64198
Jakub Balhar <jakub@balhar.net>
2021-06-15 08:21:16 +0200

    chore: Run the script for error messages once a week (#1534)
    * Run once a week
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Use has and don't depend on the PR
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Just run weekly
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Proper hash in hex encoding.
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

--- scripts/docs/index.js
+++ scripts/docs/index.js
@@ -1,12 +1,16 @@
 import {Octokit} from "octokit";
 import {readFile} from "fs/promises";
 import {execSync} from "child_process";
+const {
+    randomBytes,
+} = await import('crypto');
 
 // Get the directory with files
 const owner = process.argv[2];
 const repository = process.argv[3];
 const githubToken = process.argv[4];
-const prNumber = process.argv[5];
+// replace with short hash
+const branchHash = randomBytes(32).toString('hex');
 
 (async function () {
 
@@ -17,51 +21,30 @@ const prNumber = process.argv[5];
         const file2 = await readFile("../../docs/docgen/original-codes.md");
 
         if (file1.toString() != file2.toString()) {
-            await octokit.rest.issues.addLabels({
-                owner,
-                repo: repository,
-                issue_number: prNumber,
-                labels: ['docs']
+            const branch = `apiml/pr${branchHash}/changed_errors`;
+            let gitNewCode = `git branch apiml/pr${branchHash}/changed_errors && git checkout apiml/pr${branchHash}/changed_errors && cp ../docs/docgen/ErrorMessagesDocumentation.md docs/troubleshoot/troubleshoot-apiml-error-codes.md && git add docs/troubleshoot/troubleshoot-apiml-error-codes.md && git commit --signoff -m "Update error codes" && git push origin apiml/pr${branchHash}/changed_errors`;
+
+            execSync(gitNewCode, {
+                cwd: '../../docs-site'
             });
 
-            const branch = `apiml/pr${prNumber}/changed_errors`;
-            const pulls = await octokit.rest.pulls.list({
+            // Create the PR from this
+            const {data} = await octokit.rest.pulls.create({
                 owner,
                 repo: 'docs-site',
-                head: `zowe:${branch}`
+                title: 'Automatic update for the Error messages in API-Layer PR',
+                head: branch,
+                base: 'docs-staging',
+                body: 'Updated API ML error messages'
             });
 
-            let gitExistingCode = `git checkout apiml/pr${prNumber}/changed_errors && git pull origin apiml/pr${prNumber}/changed_errors && cp ../docs/docgen/ErrorMessagesDocumentation.md docs/troubleshoot/troubleshoot-apiml-error-codes.md && git add docs/troubleshoot/troubleshoot-apiml-error-codes.md && git commit --signoff -m "Update error codes" && git push origin apiml/pr${prNumber}/changed_errors`;
-            let gitNewCode = `git branch apiml/pr${prNumber}/changed_errors && git checkout apiml/pr${prNumber}/changed_errors && cp ../docs/docgen/ErrorMessagesDocumentation.md docs/troubleshoot/troubleshoot-apiml-error-codes.md && git add docs/troubleshoot/troubleshoot-apiml-error-codes.md && git commit --signoff -m "Update error codes" && git push origin apiml/pr${prNumber}/changed_errors`;
-
-            const isExisting = pulls.data.length > 0;
-            if(isExisting) {
-                execSync(gitExistingCode, {
-                    cwd: '../../docs-site'
-                });
-            } else {
-                execSync(gitNewCode, {
-                    cwd: '../../docs-site'
-                });
-
-                // Create the PR from this
-                const {data} = await octokit.rest.pulls.create({
-                    owner,
-                    repo: 'docs-site',
-                    title: 'Automatic update for the Error messages in API-Layer PR',
-                    head: branch,
-                    base: 'docs-staging',
-                    body: 'Updated API ML error messages'
-                });
-
-                // To comment on the current PR I need the url so data.url
-                await octokit.rest.issues.createComment({
-                    owner,
-                    repo: repository,
-                    issue_number: prNumber,
-                    body: `The changes to the error code documentation are available in this PR: ${data.html_url}`,
-                });
-            }
+            // To comment on the current PR I need the url so data.url
+            await octokit.rest.issues.createComment({
+                owner,
+                repo: repository,
+                issue_number: branchHash,
+                body: `The changes to the error code documentation are available in this PR: ${data.html_url}`,
+            });
 
             console.log("Created a PR and updated the changes");
         }
