9fe22bd7c1f8d7fbb793873a1d7864e357337aa1
Andrea Tabone <andrea.tabone@broadcom.com>
2021-06-21 12:28:02 +0200

    attempt
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/ha/ChaoticTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/ha/ChaoticTest.java
@@ -10,7 +10,12 @@
 
 package org.zowe.apiml.integration.ha;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -24,6 +29,9 @@ import java.util.Collections;
 
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.zowe.apiml.util.SecurityUtils.getConfiguredSslConfig;
@@ -43,6 +51,7 @@ public class ChaoticTest {
     private String password;
     private String discoverableClientPort;
     private String discoverableClientHost;
+    private int instances;
 
     @BeforeEach
     void setUp() {
@@ -50,12 +59,36 @@ public class ChaoticTest {
         username = ConfigReader.environmentConfiguration().getCredentials().getUser();
         password = ConfigReader.environmentConfiguration().getCredentials().getPassword();
         RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
+        instances = gatewayServiceConfiguration.getInstances();
     }
 
     @Nested
     class GivenHASetUp {
         @Nested
         class whenOneGatewayIsNotAvailable {
+
+            // attempt
+
+            @Test
+            void gatewayInstancesAreUp() throws IOException {
+                assumeTrue(instances > 1);
+                RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
+                assertThat(gatewayServiceConfiguration.getInternalPorts(), is(not(nullValue())), is(not("")));
+
+                checkInstancesAreUp();
+            }
+            private void checkInstancesAreUp() throws IOException {
+                String[] hosts = gatewayServiceConfiguration.getHost().split(",");
+                int port = gatewayServiceConfiguration.getPort();
+                for (String host : hosts) {
+                    HttpResponse response = HttpRequestUtils.getResponse("/application/health", HttpStatus.SC_OK, port, host);
+                    DocumentContext context = JsonPath.parse(EntityUtils.toString(response.getEntity()));
+                    Integer amountOfActiveGateways = context.read("$.components.gateway.details.gatewayCount");
+                    assertThat(amountOfActiveGateways, is(instances));
+                }
+
+            }
+            //
             @Test
             void routeToTheAliveGatewayInstance() throws IOException {
                 final int instances = gatewayServiceConfiguration.getInstances();
