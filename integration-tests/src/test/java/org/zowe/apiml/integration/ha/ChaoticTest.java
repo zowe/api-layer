c4a0814b0e5a96b7e3d074b8bdef10fe5006cf15
Jakub Balhar <jakub@balhar.net>
2021-06-25 13:39:11 +0200

    Extract request related behavior to request classes
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

--- integration-tests/src/test/java/org/zowe/apiml/integration/ha/ChaoticTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/ha/ChaoticTest.java
@@ -14,20 +14,11 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.zowe.apiml.util.DiscoveryRequests;
 import org.zowe.apiml.util.categories.ChaoticHATest;
-import org.zowe.apiml.util.config.ConfigReader;
-import org.zowe.apiml.util.config.DiscoveryServiceConfiguration;
-import org.zowe.apiml.util.config.EnvironmentConfiguration;
-import org.zowe.apiml.util.config.GatewayServiceConfiguration;
-import org.zowe.apiml.util.http.HttpRequestUtils;
+import org.zowe.apiml.util.requests.JsonResponse;
+import org.zowe.apiml.util.requests.ha.HADiscoveryRequests;
+import org.zowe.apiml.util.requests.ha.HAGatewayRequests;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Collections;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -39,32 +30,13 @@ import static org.zowe.apiml.util.SecurityUtils.getConfiguredSslConfig;
  */
 @ChaoticHATest
 public class ChaoticTest {
-
-    private GatewayServiceConfiguration gatewayServiceConfiguration;
-    private DiscoveryServiceConfiguration discoveryServiceConfiguration;
+    private final HADiscoveryRequests haDiscoveryRequests = new HADiscoveryRequests();
+    private final HAGatewayRequests haGatewayRequests = new HAGatewayRequests();
 
     private final String DISCOVERABLE_GREET = "/api/v1/discoverableclient/greeting";
-    private final String SHUTDOWN = "/application/shutdown";
-    private int gatewayInstances;
-    private int discoveryInstances;
-    private DiscoveryRequests discoveryRequests;
-    private String username;
-    private String password;
-    private String[] discoveryHosts;
-    private String[] gatewayHosts;
 
     @BeforeEach
     void setUp() {
-        EnvironmentConfiguration environmentConfiguration = ConfigReader.environmentConfiguration();
-        gatewayServiceConfiguration = environmentConfiguration.getGatewayServiceConfiguration();
-        discoveryServiceConfiguration = environmentConfiguration.getDiscoveryServiceConfiguration();
-        username = environmentConfiguration.getCredentials().getUser();
-        password = environmentConfiguration.getCredentials().getPassword();
-        discoveryHosts = discoveryServiceConfiguration.getHost().split(",");
-        gatewayHosts = gatewayServiceConfiguration.getHost().split(",");
-        discoveryRequests = new DiscoveryRequests(discoveryHosts[1]);
-        gatewayInstances = gatewayServiceConfiguration.getInstances();
-        discoveryInstances = discoveryServiceConfiguration.getInstances();
         RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
     }
 
@@ -73,47 +45,25 @@ public class ChaoticTest {
         @Nested
         class whenOneDiscoveryServiceIsNotAvailable {
             @Test
-            void serviceStillRegisteredToOtherDiscovery() throws URISyntaxException {
-                assumeTrue(gatewayInstances > 1 && discoveryInstances > 1);
-                shutDownDiscoveryInstance(discoveryHosts[0]);
-                assertThat(discoveryRequests.isApplicationRegistered("DISCOVERABLECLIENT"), is(true));
-            }
+            void serviceStillRegisteredToOtherDiscovery() {
+                assumeTrue(haDiscoveryRequests.existing() > 1);
 
-            void shutDownDiscoveryInstance(String host) throws URISyntaxException {
-                //@formatter:off
-                given()
-                    .contentType(JSON)
-                    .auth().basic(username, password)
-                    .when()
-                    .post(HttpRequestUtils.getUriFromDiscovery(SHUTDOWN, host))
-                    .then()
-                    .statusCode(is(SC_OK))
-                    .extract().body().asString();
-                //@formatter:on
+                haDiscoveryRequests.shutdown(0);
+
+                assertThat(haDiscoveryRequests.isApplicationRegistered(1, "DISCOVERABLECLIENT"), is(true));
             }
         }
 
         @Nested
         class whenOneGatewayIsNotAvailable {
             @Test
-            void routeToInstanceThroughAliveGateway() throws IOException {
-                assumeTrue(gatewayInstances > 1 && discoveryInstances > 1);
-                shutDownGatewayInstance(gatewayHosts[0]);
-                int port = gatewayServiceConfiguration.getPort();
-                HttpRequestUtils.getResponse(DISCOVERABLE_GREET, SC_OK, port, gatewayHosts[1]);
-            }
+            void routeToInstanceThroughAliveGateway() {
+                assumeTrue(haGatewayRequests.existing() > 1 && haDiscoveryRequests.existing() > 1);
 
-            void shutDownGatewayInstance(String host) {
-                //@formatter:off
-                given()
-                    .contentType(JSON)
-                    .auth().basic(username, password)
-                    .when()
-                    .post(HttpRequestUtils.getUriFromGateway(SHUTDOWN, gatewayServiceConfiguration.getPort(), host, Collections.emptyList()))
-                    .then()
-                    .statusCode(is(SC_OK))
-                    .extract().body().asString();
-                //@formatter:on
+                haGatewayRequests.shutdown(0);
+
+                JsonResponse response = haGatewayRequests.route(1, DISCOVERABLE_GREET);
+                assertThat(response.getStatus(), is(SC_OK));
             }
         }
     }
--- integration-tests/src/test/java/org/zowe/apiml/integration/ha/EurekaReplicationTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/ha/EurekaReplicationTest.java
@@ -9,88 +9,39 @@
  */
 package org.zowe.apiml.integration.ha;
 
-import io.restassured.RestAssured;
-import io.restassured.path.xml.XmlPath;
-import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.zowe.apiml.util.TestWithStartedInstances;
 import org.zowe.apiml.util.categories.HATest;
-import org.zowe.apiml.util.config.ConfigReader;
-import org.zowe.apiml.util.config.DiscoveryServiceConfiguration;
-import org.zowe.apiml.util.http.HttpRequestUtils;
+import org.zowe.apiml.util.requests.ha.HADiscoveryRequests;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
-import static org.zowe.apiml.util.SecurityUtils.getConfiguredSslConfig;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 /**
  * Verify that eureka is aware of other replicas if they are online.
  */
 @HATest
 class EurekaReplicationTest implements TestWithStartedInstances {
-    private DiscoveryServiceConfiguration discoveryServiceConfiguration;
-    private String username;
-    private String password;
-    private String[] hosts;
-
-    @BeforeEach
-    void setUp() {
-        discoveryServiceConfiguration = ConfigReader.environmentConfiguration().getDiscoveryServiceConfiguration();
-        username = ConfigReader.environmentConfiguration().getCredentials().getUser();
-        password = ConfigReader.environmentConfiguration().getCredentials().getPassword();
-        hosts = discoveryServiceConfiguration.getHost().split(",");
-    }
+    private HADiscoveryRequests haDiscoveryRequests = new HADiscoveryRequests();
 
     /**
-     * Based on configuration it tests either for one or for more instances.
+     * It tests for registered instances.
      */
     @Nested
     class GivenMultipleEurekaInstances {
         @Nested
         class WhenLookingForEurekas {
             @Test
-            void eurekaReplicasAreVisible() throws Exception {
-                final int instances = discoveryServiceConfiguration.getInstances();
-                //@formatter:off
-                RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
-                String xml =
-                    given()
-                        .auth().basic(username, password)
-                    .when()
-                        .get(HttpRequestUtils.getUriFromDiscovery("/eureka/status", hosts[0]))
-                    .then()
-                        .statusCode(is(HttpStatus.SC_OK))
-                        .extract().body().asString();
-                //@formatter:on
+            void eurekaReplicasAreVisible() {
+                assumeTrue(haDiscoveryRequests.existing() > 1);
 
-                xml = xml.replaceAll("com.netflix.eureka.util.StatusInfo", "StatusInfo");
-
-                String availableReplicas = XmlPath.from(xml).getString("StatusInfo.applicationStats.available-replicas");
-                String registeredReplicas = XmlPath.from(xml).getString("StatusInfo.applicationStats.registered-replicas");
-                String unavailableReplicas = XmlPath.from(xml).getString("StatusInfo.applicationStats.unavailable-replicas");
-                List<String> servicesList = Arrays.asList(registeredReplicas.split(","));
-                if (instances == 1) {
-                    assertThat(registeredReplicas, is(""));
-                    assertThat(availableReplicas, is(""));
-                    assertThat(unavailableReplicas, is(""));
-                } else {
-                    if (availableReplicas.charAt(availableReplicas.length() - 1) == ',') {
-                        availableReplicas = availableReplicas.substring(0, availableReplicas.length() - 1);
-                    }
-                    assertThat(registeredReplicas, is(not("")));
-                    assertThat(availableReplicas, is(not("")));
-                    assertThat(unavailableReplicas, is(""));
-
-                    assertThat(registeredReplicas, is(availableReplicas));
-                    assertThat(servicesList, hasSize(instances - 1));
+                List<Integer> instances = haDiscoveryRequests.getAmountOfRegisteredInstancesForService("DISCOVERY");
+                for(Integer registeredToInstance: instances) {
+                    assertThat(registeredToInstance, is(haDiscoveryRequests.existing()));
                 }
             }
         }
