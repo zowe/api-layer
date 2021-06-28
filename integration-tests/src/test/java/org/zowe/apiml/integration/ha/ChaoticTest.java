a5a14dc78a6c0eb03f3084fa6822092ef371a74d
Jakub Balhar <jakub@balhar.net>
2021-06-28 11:25:38 +0200

    refactor: Refactor HA tests (#1572)
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    Co-authored-by: Andrea Tabone <andrea.tabone@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/ha/ChaoticTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/ha/ChaoticTest.java
@@ -10,22 +10,35 @@
 
 package org.zowe.apiml.integration.ha;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.zowe.apiml.util.categories.ChaoticHATest;
 import org.zowe.apiml.util.requests.Apps;
+import org.zowe.apiml.util.requests.Endpoints;
+import org.zowe.apiml.util.requests.JsonResponse;
 import org.zowe.apiml.util.requests.ha.HADiscoveryRequests;
+import org.zowe.apiml.util.requests.ha.HAGatewayRequests;
 
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.zowe.apiml.util.SecurityUtils.getConfiguredSslConfig;
 
 /**
- * Verify behaviour of the Discovery Service under chaotic testing
+ * Verify behaviour of the application under chaotic testing
  */
 @ChaoticHATest
-public class DiscoveryChaoticTest {
+public class ChaoticTest {
     private final HADiscoveryRequests haDiscoveryRequests = new HADiscoveryRequests();
+    private final HAGatewayRequests haGatewayRequests = new HAGatewayRequests();
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
+    }
 
     @Nested
     class GivenHASetUp {
@@ -36,7 +49,21 @@ public class DiscoveryChaoticTest {
                 assumeTrue(haDiscoveryRequests.existing() > 1);
 
                 haDiscoveryRequests.shutdown(0);
+
                 assertThat(haDiscoveryRequests.isApplicationRegistered(1, Apps.DISCOVERABLE_CLIENT), is(true));
+            }
+        }
+
+        @Nested
+        class whenOneGatewayIsNotAvailable {
+            @Test
+            void routeToInstanceThroughAliveGateway() {
+                assumeTrue(haGatewayRequests.existing() > 1 && haDiscoveryRequests.existing() > 1);
+
+                haGatewayRequests.shutdown(0);
+
+                JsonResponse response = haGatewayRequests.route(1, Endpoints.DISCOVERABLE_GREET);
+                assertThat(response.getStatus(), is(SC_OK));
             }
         }
     }
--- integration-tests/src/test/java/org/zowe/apiml/integration/ha/GatewayMultipleInstancesTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/ha/GatewayMultipleInstancesTest.java
@@ -10,32 +10,18 @@
 
 package org.zowe.apiml.integration.ha;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-
-import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.zowe.apiml.util.DiscoveryRequests;
 import org.zowe.apiml.util.categories.HATest;
-import org.zowe.apiml.util.config.ConfigReader;
-import org.zowe.apiml.util.config.DiscoveryServiceConfiguration;
-import org.zowe.apiml.util.config.EnvironmentConfiguration;
-import org.zowe.apiml.util.config.GatewayServiceConfiguration;
-import org.zowe.apiml.util.http.HttpRequestUtils;
-
-import java.io.IOException;
-import java.util.Optional;
+import org.zowe.apiml.util.requests.Apps;
+import org.zowe.apiml.util.requests.ha.HADiscoveryRequests;
+import org.zowe.apiml.util.requests.ha.HAGatewayRequests;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
-import static org.junit.jupiter.api.Assumptions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.zowe.apiml.util.SecurityUtils.getConfiguredSslConfig;
 
 /**
@@ -43,24 +29,12 @@ import static org.zowe.apiml.util.SecurityUtils.getConfiguredSslConfig;
  */
 @HATest
 public class GatewayMultipleInstancesTest {
-    private GatewayServiceConfiguration gatewayServiceConfiguration;
-    private DiscoveryServiceConfiguration discoveryServiceConfiguration;
-    private DiscoveryRequests discoveryRequests;
-    private final String HEALTH_ENDPOINT = "/application/health";
-    private int gatewayInstances;
-    private int discoveryInstances;
-    private String[] hosts;
+    private final HAGatewayRequests haGatewayRequests = new HAGatewayRequests();
+    private final HADiscoveryRequests haDiscoveryRequests = new HADiscoveryRequests();
 
     @BeforeEach
     void setUp() {
-        EnvironmentConfiguration environmentConfiguration = ConfigReader.environmentConfiguration();
-        gatewayServiceConfiguration = environmentConfiguration.getGatewayServiceConfiguration();
-        discoveryServiceConfiguration = environmentConfiguration.getDiscoveryServiceConfiguration();
-        gatewayInstances = gatewayServiceConfiguration.getInstances();
-        discoveryInstances = discoveryServiceConfiguration.getInstances();
-        hosts = discoveryServiceConfiguration.getHost().split(",");
-        discoveryRequests = new DiscoveryRequests(hosts[0]);
-
+        RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
     }
 
     @Nested
@@ -68,37 +42,17 @@ public class GatewayMultipleInstancesTest {
         @Nested
         class WhenSendingRequest {
             @Test
-            void gatewayInstancesAreUp() throws IOException {
-                assumeTrue(gatewayInstances > 1 && discoveryInstances > 1);
-                RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
-                assertThat(gatewayServiceConfiguration.getInternalPorts(), is(not(nullValue())), is(not("")));
+            void gatewayInstancesAreUp() {
+                assumeTrue(haGatewayRequests.existing() > 1);
 
-                checkInstancesAreUp();
+                assertThat(haGatewayRequests.up(), is(true));
             }
 
             @Test
             void gatewayInstancesAreRegistered() {
-                assumeTrue(gatewayInstances > 1 && discoveryInstances > 1);
+                assumeTrue(haGatewayRequests.existing() > 1 && haDiscoveryRequests.existing() > 1);
 
-                String[] internalPorts = gatewayServiceConfiguration.getInternalPorts().split(",");
-                String[] hosts = gatewayServiceConfiguration.getHost().split(",");
-
-                String instanceId = hosts[0] + ":" + "gateway" + ":" + internalPorts[0];
-                assertThat(discoveryRequests.isApplicationRegistered("GATEWAY", Optional.of(instanceId)), is(true));
-                instanceId = hosts[1] + ":" + "gateway" + ":" + internalPorts[1];
-                assertThat(discoveryRequests.isApplicationRegistered("GATEWAY", Optional.of(instanceId)), is(true));
-            }
-
-            private void checkInstancesAreUp() throws IOException {
-                String[] hosts = gatewayServiceConfiguration.getHost().split(",");
-                int port = gatewayServiceConfiguration.getPort();
-                for (String host : hosts) {
-                    HttpResponse response = HttpRequestUtils.getResponse(HEALTH_ENDPOINT, HttpStatus.SC_OK, port, host);
-                    DocumentContext context = JsonPath.parse(EntityUtils.toString(response.getEntity()));
-                    Integer amountOfActiveGateways = context.read("$.components.gateway.details.gatewayCount");
-                    assertThat(amountOfActiveGateways, is(gatewayInstances));
-                }
-
+                assertThat(haDiscoveryRequests.getAmountOfRegisteredInstancesForService(0, Apps.GATEWAY), is(2));
             }
         }
     }
