060d69e9f1afc15079508481139a2b1fe37be940
ViktorKorladinov <v.korladinov@gmail.com>
2021-07-07 11:30:49 +0200

    Created WizardDialog component Signed-off-by: ViktorKorladinov <v.korladinov@gmail.com>

---
+++ api-catalog-ui/frontend/src/components/Wizard/WizardDialog.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Wizard.css';
+
+export default class WizardDialog extends Component {
+    render() {
+        return <div className="dialog">New Component</div>;
+    }
+}
