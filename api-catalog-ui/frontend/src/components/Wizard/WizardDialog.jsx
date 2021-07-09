042e552623eb28936f41bc310ef887a33861fde3
DianaKorladinova <chanchunche@gmail.com>
2021-07-09 12:58:15 +0200

    Laid out the foundation for the input forms. Signed-off-by: DianaKorladinova <chanchunche@gmail.com>

--- api-catalog-ui/frontend/src/components/Wizard/WizardDialog.jsx
+++ api-catalog-ui/frontend/src/components/Wizard/WizardDialog.jsx
@@ -9,8 +9,22 @@
  */
 
 import React, { Component } from 'react';
-import { Dialog, DialogBody, DialogHeader, DialogTitle, DialogFooter, DialogActions, Button, Text } from 'mineral-ui';
+import TextInput from 'mineral-ui/TextInput';
+import ButtonGroup from 'mineral-ui/ButtonGroup';
+import {
+    Dialog,
+    DialogBody,
+    DialogHeader,
+    DialogTitle,
+    DialogFooter,
+    DialogActions,
+    Button,
+    Text,
+    Dropdown,
+} from 'mineral-ui';
+import { IconArrowDropDown } from 'mineral-ui-icons';
 import './wizard.css';
+import { data } from '../../constants/wizard-constants';
 
 export default class WizardDialog extends Component {
     closeWizard = () => {
@@ -27,6 +41,19 @@ export default class WizardDialog extends Component {
                     </DialogHeader>
                     <DialogBody>
                         <Text>This wizard will guide you through creating a correct YAML for your application.</Text>
+                        <ButtonGroup aria-label="Optional compositions">
+                            <Button>Previous</Button>
+                            <Button>Next</Button>
+                            <Dropdown data={data}>
+                                <Button iconEnd={<IconArrowDropDown />}>Categories</Button>
+                            </Dropdown>
+                        </ButtonGroup>
+                        <div className="wizardForm">
+                            <TextInput size="large" placeholder="serviceId" />
+                            <TextInput size="large" placeholder="title" />
+                            <TextInput size="large" placeholder="description" />
+                            <TextInput size="large" placeholder="baseURL" />
+                        </div>
                     </DialogBody>
                     <DialogFooter className="dialog-footer">
                         <DialogActions>
