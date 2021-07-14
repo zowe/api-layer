5b5abf1f92792702d365a18ba1ae2beded4acd1c
jandadav <janda.david@gmail.com>
2021-07-14 14:08:44 +0200

    Caching service client skeleton
    Signed-off-by: jandadav <janda.david@gmail.com>

---
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/cache/CachingServiceClient.java
@@ -0,0 +1,101 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.gateway.cache;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+public class CachingServiceClient {
+
+    private final RestTemplate restTemplate;
+
+    public void create(KeyValue kv) throws CachingServiceClientException {
+        try {
+            ResponseEntity<String> response = restTemplate.exchange("https://localhost:10010/cachingservice/api/v1/cache", HttpMethod.POST, new HttpEntity<KeyValue>(kv, new HttpHeaders()), String.class);
+        } catch (RestClientException e) {
+            throw new CachingServiceClientException("Unable to create keyValue: " + kv.toString() + ", caused by: " + e.getMessage(), e);
+        }
+    }
+
+    public KeyValue read(String key) throws CachingServiceClientException {
+        try {
+            ResponseEntity<KeyValue> response = response = restTemplate.exchange("https://localhost:10010/cachingservice/api/v1/cache" + "/" + key, HttpMethod.GET, new HttpEntity<KeyValue>(null, new HttpHeaders()), KeyValue.class);
+            if (response != null && response.hasBody()) {
+                return response.getBody();
+            } else {
+                throw new CachingServiceClientException("Unable to read key: " + key + ", caused by response from caching service is null or has no body");
+            }
+        } catch (RestClientException e) {
+            throw new CachingServiceClientException("Unable to read key: " + key + ", caused by: " + e.getMessage(), e);
+        }
+    }
+
+    public ResponseEntity<String> update(KeyValue kv) {
+        ResponseEntity<String> response = restTemplate.exchange("https://localhost:10010/cachingservice/api/v1/cache", HttpMethod.PUT, new HttpEntity<KeyValue>(kv, new HttpHeaders()), String.class);
+        return response;
+    }
+
+    public ResponseEntity<String> delete(String key) {
+        ResponseEntity<String> response = restTemplate.exchange("https://localhost:10010/cachingservice/api/v1/cache" + "/" + key, HttpMethod.DELETE, new HttpEntity<KeyValue>(null, new HttpHeaders()), String.class);
+        return response;
+    }
+
+    /**
+     * Data POJO that represents entry in caching service
+     */
+    @RequiredArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @Data
+    static class KeyValue {
+        private final String key;
+        private final String value;
+        private String serviceId;
+        private final String created;
+
+        public KeyValue(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.serviceId = "";
+            this.created = currentTime();
+        }
+
+        private static String currentTime() {
+            return String.valueOf(new Date().getTime());
+        }
+
+        @JsonCreator
+        public KeyValue() {
+            key = "";
+            value = "";
+            serviceId = "";
+            created = currentTime();
+        }
+    }
+
+    public class CachingServiceClientException extends Exception {
+
+        public CachingServiceClientException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public CachingServiceClientException(String message) {
+            super(message);
+        }
+    }
+}
---
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/cache/CachingServiceClientTest.java
@@ -0,0 +1,116 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.gateway.cache;
+
+import org.junit.jupiter.api.*;
+import org.springframework.http.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+// TODO refactor this separately, out of Gateway?
+class CachingServiceClientTest {
+    CachingServiceClient underTest;
+    RestTemplate restTemplate = mock(RestTemplate.class);
+    String urlBase = "https://localhost:10010/cachingservice/api/v1/cache";
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CachingServiceClient(restTemplate);
+    }
+
+    @Nested
+    class givenCreateOperation {
+
+        @Test
+        void createWithoutProblem() {
+            CachingServiceClient.KeyValue kv = new CachingServiceClient.KeyValue("Britney", "Spears");
+            assertDoesNotThrow(() -> underTest.create(kv));
+            verify(restTemplate).exchange(eq(urlBase), eq(HttpMethod.POST), eq(new HttpEntity<>(kv, new HttpHeaders())), eq(String.class));
+        }
+
+        @Test
+        void createWithExceptionFromRestTemplateThrowsDefined() {
+            doThrow(new RestClientException("oops")).when(restTemplate).exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class));
+            assertThrows(CachingServiceClient.CachingServiceClientException.class,() -> underTest.create(new CachingServiceClient.KeyValue("Britney", "Spears")));
+        }
+    }
+
+    @Nested
+    class givenUpdateOperation {
+        @Test
+        void updateWithoutProblem() {
+            CachingServiceClient.KeyValue kv = new CachingServiceClient.KeyValue("Britney", "Speeeeers");
+            assertDoesNotThrow(() -> underTest.update(kv));
+            verify(restTemplate).exchange(eq(urlBase), eq(HttpMethod.PUT), eq(new HttpEntity<>(kv, new HttpHeaders())), eq(String.class));
+        }
+
+        @Test
+        void updateWithExceptionFromRestTemplateThrowsDefined() {
+            doThrow(new RestClientException("oops")).when(restTemplate).exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class));
+            assertThrows(CachingServiceClient.CachingServiceClientException.class,() -> underTest.create(new CachingServiceClient.KeyValue("Britney", "Spears")));
+        }
+    }
+
+    @Nested
+    class givenReadOperation {
+
+        private String keyToRead = "reee";
+
+        @Test
+        void readWithNullResponseOrNullBody() {
+            assertThrows(CachingServiceClient.CachingServiceClientException.class, () -> underTest.read(keyToRead));
+            verify(restTemplate).exchange(eq(urlBase + "/" + keyToRead), eq(HttpMethod.GET), any(HttpEntity.class), eq(CachingServiceClient.KeyValue.class));
+            ResponseEntity<CachingServiceClient.KeyValue> responseEntity = mock(ResponseEntity.class);
+            doReturn(false).when(responseEntity).hasBody();
+            doReturn(responseEntity).when(restTemplate).exchange(eq(urlBase + "/" + keyToRead), eq(HttpMethod.GET), any(HttpEntity.class), eq(CachingServiceClient.KeyValue.class));
+            assertThrows(CachingServiceClient.CachingServiceClientException.class, () -> underTest.read(keyToRead));
+        }
+
+        @Test
+        void readWithoutProblem() throws CachingServiceClient.CachingServiceClientException {
+            ResponseEntity<CachingServiceClient.KeyValue> responseEntity = mock(ResponseEntity.class);
+            doReturn(true).when(responseEntity).hasBody();
+            doReturn(new CachingServiceClient.KeyValue(keyToRead, "Wonder")).when(responseEntity).getBody();
+            doReturn(responseEntity).when(restTemplate).exchange(eq(urlBase + "/" + keyToRead), eq(HttpMethod.GET), any(HttpEntity.class), eq(CachingServiceClient.KeyValue.class));
+            assertThat(underTest.read(keyToRead).getValue(), is("Wonder"));
+        }
+
+        @Test
+        void readWithExceptonFromRestTemplateThrowsDefined() {
+            doThrow(new RestClientException("oops")).when(restTemplate).exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class));
+            assertThrows(CachingServiceClient.CachingServiceClientException.class, () -> underTest.read(keyToRead));
+        }
+    }
+
+    @Nested
+    class givenDeleteOperation {
+        private String keyToDelete = "reee";
+        @Test
+        void deleteWithoutProblem() {
+            assertDoesNotThrow(() -> underTest.delete(keyToDelete));
+            verify(restTemplate).exchange(eq(urlBase + "/" + keyToDelete), eq(HttpMethod.DELETE), any(HttpEntity.class), eq(String.class));
+        }
+
+        @Test
+        void updateWithExceptionFromRestTemplateThrowsDefined() {
+            doThrow(new RestClientException("oops")).when(restTemplate).exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class));
+            assertThrows(CachingServiceClient.CachingServiceClientException.class,() -> underTest.create(new CachingServiceClient.KeyValue("Britney", "Spears")));
+        }
+    }
+
+
+}
