65927bda0bbdb5d149fb0668cb768a653d9f3536
Kumar Saurabh Raj <kumarsaurabhraj.sr@gmail.com>
2021-07-16 20:07:50 +0530

    Embeded Hystrix Dashboard
    Signed-off-by: Kumar Saurabh Raj <kumarsaurabhraj.sr@gmail.com>

--- metrics-service-ui/frontend/src/components/Dashboard/Dashboard.jsx
+++ metrics-service-ui/frontend/src/components/Dashboard/Dashboard.jsx
@@ -9,11 +9,21 @@
  */
 
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import React, { useEffect } from 'react';
 
 export default function Dashboard() {
+    useEffect(() => {
+        setTimeout(() => {
+            window.addStreams('https://localhost:10012/discoverableclient/application/hystrix.stream');
+        }, 0);
+    }, []);
     return (
-        <Typography id="name" variant="h2" component="h1" gutterBottom align="center">
-            Metrics Service
-        </Typography>
+        <React.Fragment>
+            <Typography id="name" variant="h2" component="h1" gutterBottom align="center">
+                Metrics Service
+            </Typography>
+            <Container maxWidth="false" id="content" className="dependencies" />
+        </React.Fragment>
     );
 }
