0a787fcbf9318bde0e597491bc0c1d154b92f43a
DianaKorladinova <chanchunche@gmail.com>
2021-07-19 10:10:19 +0200

    Changed 'Onboard New API' button to a dropdown which displays the different enablers.
    Signed-off-by: DianaKorladinova <chanchunche@gmail.com>

---
+++ api-catalog-ui/frontend/src/components/Wizard/wizard-config.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/prefer-default-export
+// import { wizardToggleDisplay } from '../../actions/wizard-actions';
+
+export const enablerData = [
+    {
+        text: 'Plain Java Enabler',
+        onClick: '',
+    },
+    {
+        text: 'Spring Enabler',
+        onClick: '',
+    },
+    {
+        text: 'Micronaut Enabler',
+    },
+    {
+        text: 'Node JS Enabler',
+    },
+    {
+        text: 'Static Onboarding',
+    },
+    {
+        text: 'Direct Call to Eureka',
+    },
+];
+
+export default enablerData;
