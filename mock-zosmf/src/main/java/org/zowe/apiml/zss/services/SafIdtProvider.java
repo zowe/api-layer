17d691e72093529d883e09cb1c5e6279485275a9
Jakub Balhar <jakub@balhar.net>
2021-08-17 13:25:36 +0200

    Add mock implementation for the REST endpoint.
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

---
+++ mock-zosmf/src/main/java/org/zowe/apiml/zss/services/SafIdtProvider.java
@@ -0,0 +1,43 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.zss.services;
+
+import org.springframework.stereotype.Service;
+import org.zowe.apiml.zss.model.Authentication;
+import org.zowe.apiml.zss.model.Token;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class SafIdtProvider {
+    private Map<String, String> providedTokens;
+
+    public Optional<Token> authenticate(
+        Authentication authentication
+    ) {
+        String token = authentication.getUsername() + ";" + UUID.randomUUID();
+        providedTokens.put(authentication.getUsername(), token);
+
+        Token result = new Token();
+        result.setJwt(token);
+        return Optional.of(result);
+    }
+
+    public boolean verify(
+        Token token
+    ) {
+        String safToken = token.getJwt();
+        String username = safToken.split(";")[0];
+
+        return providedTokens.containsKey(username) && providedTokens.get(username).equals("sasfToken");
+    }
+}
