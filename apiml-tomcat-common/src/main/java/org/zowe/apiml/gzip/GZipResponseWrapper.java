ee9f3b77c6ad674dea89395c6214c2787d4f8f5f
achmelo <37397715+achmelo@users.noreply.github.com>
2021-08-25 14:41:35 +0200

    feat: compress response if service wants to (#1728)
    * initial commit, compress response if service wants to
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * add unit tests
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * empty gzip body should be 20 bytes
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * test flush buffers
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * test no instances available and close buffers
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * compress per service acceptance test
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * compress per service acceptance test
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * resolve conflicts
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    
    * provide description for filter class
    
    Signed-off-by: achmelo <a.chmelo@gmail.com>
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

---
+++ apiml-tomcat-common/src/main/java/org/zowe/apiml/gzip/GZipResponseWrapper.java
@@ -0,0 +1,120 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.gzip;
+
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.zip.GZIPOutputStream;
+
+public class GZipResponseWrapper extends HttpServletResponseWrapper {
+
+    private GZipServletOutputStream gzipOutputStream;
+    private PrintWriter printWriter = null;
+    private boolean disableFlushBuffer = false;
+
+    /**
+     * Constructs a response adaptor wrapping the given response.
+     *
+     * @param response The response to be wrapped
+     * @throws IllegalArgumentException if the response is null
+     */
+    public GZipResponseWrapper(HttpServletResponse response, GZIPOutputStream stream) {
+        super(response);
+        gzipOutputStream = new GZipServletOutputStream(stream);
+    }
+
+    public void close() throws IOException {
+        if (this.printWriter != null) {
+            this.printWriter.close();
+        }
+
+        if (this.gzipOutputStream != null) {
+            this.gzipOutputStream.close();
+        }
+    }
+
+    /**
+     * Flush OutputStream or PrintWriter
+     *
+     * @throws IOException
+     */
+    @Override
+    public void flushBuffer() throws IOException {
+        flush();
+
+        // doing this might leads to response already committed exception
+        // when the PageInfo has not yet built but the buffer already flushed
+        // Happens in Weblogic when a servlet forward to a JSP page and the forward
+        // method trigger a flush before it forwarded to the JSP
+        // disableFlushBuffer for that purpose is 'true' by default
+        if (!disableFlushBuffer) {
+            super.flushBuffer();
+        }
+    }
+
+    /**
+     * Flushes all the streams for this response.
+     */
+    public void flush() throws IOException {
+        if (printWriter != null) {
+            printWriter.flush();
+        }
+
+        if (gzipOutputStream != null) {
+            gzipOutputStream.flush();
+        }
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() {
+        if (this.printWriter != null) {
+            throw new IllegalStateException(
+                "PrintWriter obtained already - cannot get OutputStream");
+        }
+
+        return this.gzipOutputStream;
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        if (this.printWriter == null) {
+            this.gzipOutputStream = new GZipServletOutputStream(
+                getResponse().getOutputStream());
+
+            this.printWriter = new PrintWriter(new OutputStreamWriter(
+                this.gzipOutputStream, getResponse().getCharacterEncoding()), true);
+        }
+
+        return this.printWriter;
+    }
+
+
+    @Override
+    public void setContentLength(int length) {
+        //ignore, since content length of zipped content
+        //does not match content length of unzipped content.
+    }
+
+
+    /**
+     * Set if the wrapped reponse's buffer flushing should be disabled.
+     *
+     * @param disableFlushBuffer true if the wrapped reponse's buffer flushing should be disabled
+     */
+    public void setDisableFlushBuffer(boolean disableFlushBuffer) {
+        this.disableFlushBuffer = disableFlushBuffer;
+    }
+
+}
