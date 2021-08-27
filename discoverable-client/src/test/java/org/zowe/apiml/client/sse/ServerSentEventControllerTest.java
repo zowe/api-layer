f65e6693f9f7d0f5414b17364a02a2aee578e7bf
Carson Cook <carson.cook@ibm.com>
2021-08-27 12:23:39 -0400

    Add SSE route to DC for testing purposes
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

---
+++ discoverable-client/src/test/java/org/zowe/apiml/client/sse/ServerSentEventControllerTest.java
@@ -0,0 +1,33 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.client.sse;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = {ServerSentEventController.class})
+class ServerSentEventControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void whenCallEventEndpoint_thenGetServerSentEvents() throws Exception {
+        this.mockMvc.perform(get("/sse/v1/events"))
+            .andExpect(status().isOk());
+    }
+}
