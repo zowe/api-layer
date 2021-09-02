2bc7e90830ef7586fc85b73b4a4f702a1e540583
jandadav <janda.david@gmail.com>
2021-09-02 10:03:55 +0200

    add functional test
    Signed-off-by: jandadav <janda.david@gmail.com>

---
+++ discovery-service/src/test/java/org/zowe/apiml/discovery/functional/DiscoveryFunctionalTest.java
@@ -0,0 +1,32 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.discovery.functional;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public abstract class DiscoveryFunctionalTest {
+
+    @LocalServerPort
+    protected int port;
+
+    @Value("${apiml.service.hostname:localhost}")
+    protected String hostname;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+}
---
+++ discovery-service/src/test/java/org/zowe/apiml/discovery/functional/HttpSecuredEndpointTest.java
@@ -0,0 +1,41 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.discovery.functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.given;
+
+@ActiveProfiles("http")
+class HttpSecuredEndpointTest extends DiscoveryFunctionalTest {
+
+    @Value("${apiml.discovery.userid:eureka}")
+    private String eurekaUserid;
+
+    @Value("${apiml.discovery.password:password}")
+    private String eurekaPassword;
+
+    @Test
+    void UiIsSecuredWithConfiguredBasicAuth() {
+        given()
+            .get(String.format("http://%s:%d/", hostname, port))
+            .then()
+            .statusCode(HttpStatus.UNAUTHORIZED.value());
+
+        given().auth().basic(eurekaUserid, eurekaPassword)
+            .get(String.format("http://%s:%d/", hostname, port))
+            .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+}
