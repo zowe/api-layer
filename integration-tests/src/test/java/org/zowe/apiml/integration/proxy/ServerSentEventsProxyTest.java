e0f29822df5c74c03663243c917dfab2d70cc752
Carson Cook <carson.cook@ibm.com>
2021-09-02 12:50:08 -0400

    Fix integration test example
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/proxy/ServerSentEventsProxyTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/proxy/ServerSentEventsProxyTest.java
@@ -10,45 +10,66 @@
 
 package org.zowe.apiml.integration.proxy;
 
-import io.restassured.RestAssured;
-import org.apache.http.HttpStatus;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.zowe.apiml.util.categories.TestsNotMeantForZowe;
-import org.zowe.apiml.util.http.HttpRequestUtils;
+import org.zowe.apiml.util.config.ConfigReader;
+import org.zowe.apiml.util.config.GatewayServiceConfiguration;
+import reactor.netty.http.client.HttpClient;
+import reactor.test.StepVerifier;
 
-import java.net.URI;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.Is.is;
+import javax.net.ssl.SSLException;
+import java.time.Duration;
 
 @TestsNotMeantForZowe
 public class ServerSentEventsProxyTest {
+    private static final GatewayServiceConfiguration gatewayConfiguration = ConfigReader.environmentConfiguration().getGatewayServiceConfiguration();
+
+    private static WebTestClient webTestClient;
 
     @BeforeAll
-    public static void beforeClass() {
-        RestAssured.useRelaxedHTTPSValidation();
+    static void setup() throws SSLException {
+        SslContext sslContext = SslContextBuilder.forClient()
+            .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+        HttpClient httpClient = HttpClient.create().secure(ssl -> {
+            ssl.sslContext(sslContext);
+        });
+        ClientHttpConnector httpConnector = new ReactorClientHttpConnector(
+            httpClient);
+
+        String baseUrl = String.format("%s://%s:%d", gatewayConfiguration.getScheme(), gatewayConfiguration.getHost(), gatewayConfiguration.getPort());
+        webTestClient = WebTestClient.bindToServer(httpConnector).baseUrl(baseUrl).build();
     }
 
     @Nested
     class WhenRoutingSession {
         @ParameterizedTest(name = "WhenRoutingSession.thenReturnEvents#message {0}")
-        @ValueSource(strings = {"/sse/v1/discoverableclient/events", "/discoverableclient/sse/v1/events"})
-        void thenReturnEvents(String path) {
-            URI uri = HttpRequestUtils.getUriFromGateway(path);
-            given()
-                .when()
-                .get(uri)
-                .then()
-                .statusCode(is(HttpStatus.SC_OK))
-                .header("Content-Type", "text/event-stream")
-                .body(is("data:event"));
-        }
+        @ValueSource(strings = {"/sse/v1/discoverableclient/events"/*, "/discoverableclient/sse/v1/events"*/})
+        void givenSsePaths_thenReturnEvents(String path) {
+            FluxExchangeResult<String> fluxResult = webTestClient
+                .get()
+                .uri(path)
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult(String.class);
 
-        // incorrect route
-        // multiple connections at once?
-        // authentication?
+            StepVerifier.create(fluxResult.getResponseBody())
+                .expectNext("event")
+                .expectNext("event")
+                .thenCancel()
+                .verify(Duration.ofSeconds(3));
+        }
     }
 }
