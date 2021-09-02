2976db5a27ec885d6c771e474a03ecece2deea53
Andrea Tabone <39694626+taban03@users.noreply.github.com>
2021-09-02 15:04:00 +0200

    feat: Static Definition creation endpoints in API Catalog (#1735)
    * WIP
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add controller and error handler
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add override controller
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Refactoring and debug log
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add license and fix checkstyle
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add functionality to retrieve static location from discovery env endpoint
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add unit test
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add tests
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix code smell
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix bug
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add more test
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Raise coverage
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Address code review comments
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Address code review comments pt.2
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Change the logic and add shared volumes mount
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add integration tests
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix location
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add logs
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Set environment variable in docker containers
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * more debugging
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * additional debug
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix test
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Improve check
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix indentation
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Change the way to get static location
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix IT
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix static def path
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Try the docker URL
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Another take on volumes
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Fix assertion message
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Revert changes
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Get the first directory defined
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix code smells
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Revert staticApiservice
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Increase code coverage
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * revert docker-compose.yml files for local setup
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    Co-authored-by: Jakub Balhar <jakub@balhar.net>

---
+++ api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/staticapi/StaticDefinitionGeneratorTest.java
@@ -0,0 +1,106 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.apicatalog.staticapi;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.zowe.apiml.security.common.token.TokenAuthentication;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class StaticDefinitionGeneratorTest {
+
+    @InjectMocks
+    private StaticDefinitionGenerator staticDefinitionGenerator;
+
+    @Nested
+    class WhenStaticDefinitionGenerationResponse {
+
+        @BeforeEach
+        void setUp() {
+            ReflectionTestUtils.setField(staticDefinitionGenerator,"staticApiDefinitionsDirectories","config/local/api-defs");
+        }
+
+        @Test
+        void givenRequestWithInvalidServiceId_thenThrow400() throws IOException {
+            TokenAuthentication authentication = new TokenAuthentication("token");
+            authentication.setAuthenticated(true);
+            SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+            StaticAPIResponse actualResponse = staticDefinitionGenerator.generateFile("services: \\n  ", "");
+            StaticAPIResponse expectedResponse = new StaticAPIResponse(400, "The service ID format is not valid.");
+            assertEquals(expectedResponse, actualResponse);
+        }
+
+        @Test
+        void givenValidRequest_thenThrowExceptionWithCorrectPath() {
+            TokenAuthentication authentication = new TokenAuthentication("token");
+            authentication.setAuthenticated(true);
+            SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+            Exception exception = assertThrows(IOException.class, () ->
+                staticDefinitionGenerator.generateFile("services: \\n serviceId: service\\n ", "service"));
+            assertEquals("./config/local/api-defs/service.yml (No such file or directory)", exception.getMessage());
+        }
+
+        @Test
+        void givenHttpValidRequest_thenThrowExceptionWithCorrectPath() {
+            TokenAuthentication authentication = new TokenAuthentication("token");
+            authentication.setAuthenticated(true);
+            SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+            Exception exception = assertThrows(IOException.class, () ->
+                staticDefinitionGenerator.generateFile("services: \\n serviceId: service\\n ", "service"));
+            assertEquals("./config/local/api-defs/service.yml (No such file or directory)", exception.getMessage());
+        }
+
+    }
+
+
+    @Nested
+    class WhenStaticDefinitionOverrideResponse {
+
+        @BeforeEach
+        void setUp() {
+            ReflectionTestUtils.setField(staticDefinitionGenerator,"staticApiDefinitionsDirectories","config/local/api-defs");
+        }
+
+        @Test
+        void givenInvalidRequest_thenThrowException() throws IOException {
+            TokenAuthentication authentication = new TokenAuthentication("token");
+            authentication.setAuthenticated(true);
+            SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+            StaticAPIResponse actualResponse = staticDefinitionGenerator.overrideFile("services: \\n  ", "");
+            StaticAPIResponse expectedResponse = new StaticAPIResponse(400, "The service ID format is not valid.");
+            assertEquals(expectedResponse, actualResponse);
+        }
+
+        @Test
+        void givenInvalidRequest_thenThrowExceptionWithCorrectPath() {
+            TokenAuthentication authentication = new TokenAuthentication("token");
+            authentication.setAuthenticated(true);
+            SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+            Exception exception = assertThrows(IOException.class, () ->
+                staticDefinitionGenerator.overrideFile("services: \\n serviceId: service\\n ", "service"));
+            assertEquals("./config/local/api-defs/service.yml (No such file or directory)", exception.getMessage());
+        }
+    }
+
+}
