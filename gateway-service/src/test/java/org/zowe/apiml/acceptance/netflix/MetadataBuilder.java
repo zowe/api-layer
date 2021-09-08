cc612e5b4bed522fa67eab26874c1120c62da9ec
Jakub Balhar <jakub@balhar.net>
2021-09-08 14:40:02 +0200

    feat: Provide compression for specific paths only.  (#1755)
    * Limit to Accept-Encoding containing gzip
    Limit to specific routes.
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Test new and old paths alike.
    Test noncompressed paths
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Refactor the Metadata preparation.
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Add Acceptance Tests for the Compression per path
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Use the provided fulent API
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Correctly handle relative paths
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Show debug information from the Gradle.
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Try just building
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Info instead of debug
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Get the info about tests
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Remove the cached packages.
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Limit the amount of time taken
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Provide scan after the build
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Verify the build of gateway service
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Try removing the functional tests
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Return one functional test
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Return the tests
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Limit the tests to the default configuration
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Return the previous setup
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>
    
    * Test response isn't compressed when client doesn't accept gzip encoding
    
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

---
+++ gateway-service/src/test/java/org/zowe/apiml/acceptance/netflix/MetadataBuilder.java
@@ -0,0 +1,84 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.acceptance.netflix;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MetadataBuilder {
+    private Map<String, String> metadata;
+
+    public MetadataBuilder() {
+        metadata = new HashMap<>();
+
+        metadata.put("apiml.lb.cacheRecordExpirationTimeInHours", "8");
+        metadata.put("apiml.routes.gateway-url", "/");
+    }
+
+    public MetadataBuilder withRibbonConfig() {
+        metadata.put("apiml.connectTimeout", "5000");
+        metadata.put("apiml.readTimeout", "5000");
+        metadata.put("apiml.connectionManagerTimeout", "5000");
+        metadata.put("apiml.okToRetryOnAllOperations", "true");
+
+        return this;
+    }
+
+    public MetadataBuilder withLoadBalancerStrategy(String strategy) {
+        metadata.put("apiml.lb.type", strategy);
+
+        return this;
+    }
+
+    public MetadataBuilder withCorsEnabled(boolean corsEnabled) {
+        metadata.put("apiml.corsEnabled", String.valueOf(corsEnabled));
+
+        return this;
+    }
+
+    public MetadataBuilder withSafIdt() {
+        metadata.put("apiml.authentication.scheme", "safIdt");
+
+        return this;
+    }
+
+    public MetadataBuilder withCompression(boolean compressionEnabled) {
+        metadata.put("apiml.response.compress", String.valueOf(compressionEnabled));
+
+        return this;
+    }
+
+    public MetadataBuilder withCompressionPath(String compressionPath) {
+        metadata.put("apiml.response.compressRoutes", compressionPath);
+
+        return this;
+    }
+
+    public Map<String, String> build() {
+        return metadata;
+    }
+
+    public static MetadataBuilder defaultInstance() {
+        MetadataBuilder builder = new MetadataBuilder();
+        builder.withCorsEnabled(false);
+        builder.withLoadBalancerStrategy("headerRequest");
+        builder.withCompression(false);
+        return builder;
+    }
+
+    public static MetadataBuilder customInstance() {
+        MetadataBuilder builder = new MetadataBuilder();
+        builder.withRibbonConfig();
+        builder.withCorsEnabled(true);
+        builder.withLoadBalancerStrategy("headerRequest");
+        builder.withCompression(true);
+        return builder;
+    }
+}
