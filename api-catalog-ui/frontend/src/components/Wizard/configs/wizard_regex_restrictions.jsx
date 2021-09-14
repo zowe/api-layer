a6d72880e53472eb1c0ff03bcaa042dcb9560546
DianaKorladinova <chanchunche@gmail.com>
2021-09-14 11:47:42 +0200

    Regex version fix.
    Signed-off-by: DianaKorladinova <chanchunche@gmail.com>

--- api-catalog-ui/frontend/src/components/Wizard/configs/wizard_regex_restrictions.jsx
+++ api-catalog-ui/frontend/src/components/Wizard/configs/wizard_regex_restrictions.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const wizRegex = {
     gatewayUrl: { value: '^(/[a-z]+\\/v\\d+)$', tooltip: 'Format: /api/vX, Example: /api/v1' },
-    version: { value: '^(\\d+)\\.(\\d+)\\.(\\d+)', tooltip: 'Semantic versioning expected, example: 1.0.7' },
+    version: { value: '^(\\d+)\\.(\\d+)\\.(\\d+)$', tooltip: 'Semantic versioning expected, example: 1.0.7' },
     validRelativeUrl: {
         value: '^(?!www\\.|(?:http|ftp)s?://|[A-Za-z]:\\\\|//).*',
         tooltip: 'The relative URL has to be valid, example: /application/info',
