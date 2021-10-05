c2bb8b1ffb62ec559c3e6de03bcaf96c097f43cc
achmelo <a.chmelo@gmail.com>
2021-10-05 10:32:25 +0200

    revert scheduling
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- discovery-service/src/main/java/org/zowe/apiml/discovery/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/org/zowe/apiml/discovery/DiscoveryServiceApplication.java
@@ -9,6 +9,10 @@
  */
 package org.zowe.apiml.discovery;
 
+import org.zowe.apiml.product.logging.annotations.EnableApimlLogger;
+import org.zowe.apiml.product.monitoring.LatencyUtilsConfigInitializer;
+import org.zowe.apiml.product.service.ServiceStartupEventHandler;
+import org.zowe.apiml.product.version.BuildInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -16,11 +20,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.HystrixAutoConfiguration;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.zowe.apiml.product.logging.annotations.EnableApimlLogger;
-import org.zowe.apiml.product.monitoring.LatencyUtilsConfigInitializer;
-import org.zowe.apiml.product.service.ServiceStartupEventHandler;
-import org.zowe.apiml.product.version.BuildInfo;
 
 import javax.annotation.Nonnull;
 
@@ -32,7 +31,6 @@ import javax.annotation.Nonnull;
     "org.zowe.apiml.product.web"
 })
 @EnableApimlLogger
-@EnableScheduling
 public class DiscoveryServiceApplication implements ApplicationListener<ApplicationReadyEvent> {
 
     public static void main(String[] args) {
