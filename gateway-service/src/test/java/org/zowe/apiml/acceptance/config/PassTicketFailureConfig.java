9f09e598f9ea6cf3b9a75f121adb024ccde28c68
Andrea Tabone <andrea.tabone@broadcom.com>
2021-10-11 11:49:38 +0200

    Add license
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/acceptance/config/PassTicketFailureConfig.java
+++ gateway-service/src/test/java/org/zowe/apiml/acceptance/config/PassTicketFailureConfig.java
@@ -1,3 +1,12 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package org.zowe.apiml.acceptance.config;
 
 import com.netflix.zuul.ZuulFilter;
@@ -6,6 +15,9 @@ import org.zowe.apiml.gateway.security.service.PassTicketException;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
+/**
+ * Configuration that creates a Zuul filter which throws a PassTicketException to test the error handler.
+ */
 public class PassTicketFailureConfig {
 
     @Bean
