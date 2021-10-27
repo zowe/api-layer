8964039038ba3582b158f1aa59de56111d48ed25
Carson Cook <carson.cook@ibm.com>
2021-10-27 09:06:52 -0400

    Add change comment to monitor.js
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- metrics-service-ui/frontend/public/hystrix-dashboard/monitor/monitor.js
+++ metrics-service-ui/frontend/public/hystrix-dashboard/monitor/monitor.js
@@ -4,7 +4,7 @@ This file is a modified version of monitor.html.
 The changes are largely simply taking the monitor.html script elements and providing them in this Javascript file.
 */
 
-// removed adding streams on a timeout in window.load
+// CHANGE: removed adding streams on a timeout in window.load
 
 //Read a page's GET URL variables and return them as an associative array.
 // from: http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
