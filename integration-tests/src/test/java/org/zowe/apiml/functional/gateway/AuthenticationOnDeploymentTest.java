0c0cc2a0316c535ddb424d2192b53cb02b103435
Jakub Balhar <jakub.balhar@broadcom.net>
2021-11-12 11:09:55 +0100

    Return unrelated test
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.net>

--- integration-tests/src/test/java/org/zowe/apiml/functional/gateway/AuthenticationOnDeploymentTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/functional/gateway/AuthenticationOnDeploymentTest.java
@@ -15,6 +15,8 @@ import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.zowe.apiml.auth.Authentication;
+import org.zowe.apiml.auth.AuthenticationScheme;
 import org.zowe.apiml.util.TestWithStartedInstances;
 import org.zowe.apiml.util.categories.NotForMainframeTest;
 import org.zowe.apiml.util.categories.TestsNotMeantForZowe;
@@ -33,6 +35,8 @@ import static org.apache.http.HttpStatus.SC_NO_CONTENT;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.zowe.apiml.util.SecurityUtils.*;
 
 /**
@@ -58,6 +62,91 @@ class AuthenticationOnDeploymentTest implements TestWithStartedInstances {
         RestAssured.config = RestAssured.config().sslConfig(getConfiguredSslConfig());
         verifier = RequestVerifier.getInstance();
         verifier.clear();
+    }
+
+    @Test
+    @Disabled("The test is flaky and often fails at random on different environments.")
+    void testMultipleAuthenticationSchemes() throws Exception {
+        final String jwt = gatewayToken();
+
+        List<Integer> ports = RandomPorts.generateUniquePorts(2);
+
+        try (
+            final VirtualService service1 = new VirtualService("testService", ports.get(0));
+            final VirtualService service2 = new VirtualService("testService", ports.get(1));
+        ) {
+            // start first instance - without passTickets
+            service1
+                .addVerifyServlet()
+                .start()
+                .waitForGatewayRegistration(1, TIMEOUT);
+
+
+            // on each gateway make a call to service
+            service1.getGatewayVerifyUrls().forEach(x ->
+                given()
+                    .cookie(GATEWAY_TOKEN_COOKIE_NAME, jwt)
+                    .when().get(x + "/test")
+                    .then().statusCode(is(SC_OK))
+            );
+
+            // verify if each gateway sent request to service
+            service1.getGatewayVerifyUrls().forEach(gw ->
+                verifier.existAndClean(service1, x -> x.getHeader(HttpHeaders.AUTHORIZATION) == null && x.getRequestURI().equals("/verify/test"))
+            );
+
+            // start second service (with passTicket authorization)
+            service2
+                .addVerifyServlet()
+                .setAuthentication(new Authentication(AuthenticationScheme.HTTP_BASIC_PASSTICKET, "ZOWEAPPL"))
+                .start()
+                .waitForGatewayRegistration(2, TIMEOUT);
+
+            // on each gateway make calls (count same as instances) to service
+            service1.getGatewayVerifyUrls().forEach(x -> given()
+                .cookie(GATEWAY_TOKEN_COOKIE_NAME, jwt)
+                .when().get(x + "/test")
+                .then().statusCode(is(SC_OK)));
+            service2.getGatewayVerifyUrls().forEach(x -> {
+                given()
+                    .cookie(GATEWAY_TOKEN_COOKIE_NAME, jwt)
+                    .when().get(x + "/test")
+                    .then().statusCode(is(SC_OK));
+            });
+
+            // verify if each gateway sent request to service (one with and one without passTicket)
+            service1.getGatewayVerifyUrls().forEach(gw -> {
+                    verifier.existAndClean(service1, x -> x.getHeader(HttpHeaders.AUTHORIZATION) == null && x.getRequestURI().equals("/verify/test"));
+                    verifier.existAndClean(service2, x -> {
+                        assertNotNull(x.getHeader(HttpHeaders.AUTHORIZATION));
+                        assertEquals("/verify/test", x.getRequestURI());
+                        return true;
+                    });
+                }
+            );
+
+            // stop first service without authentication
+            service1
+                .unregister()
+                .waitForGatewayUnregistering(2, TIMEOUT)
+                .stop();
+
+            // check second service, all called second one with passTicket, same url like service1 (removed)
+            service1.getGatewayVerifyUrls().forEach(x -> {
+                given()
+                    .cookie(GATEWAY_TOKEN_COOKIE_NAME, jwt)
+                    .when().get(x + "/test")
+                    .then().statusCode(is(SC_OK));
+            });
+            service1.getGatewayVerifyUrls().forEach(gw -> {
+                verifier.existAndClean(service2, x -> {
+                    assertNotNull(x.getHeader(HttpHeaders.AUTHORIZATION));
+                    assertEquals("/verify/test", x.getRequestURI());
+                    return true;
+                });
+            });
+        }
+
     }
 
     @Test
