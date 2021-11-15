c32aae0eba177d116eb21daeacc7fce968050ac7
Carson Cook <carson.cook@ibm.com>
2021-11-15 14:51:12 -0500

    Remove hystrix dashboard from ac
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/ApiCatalogApplication.java
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/ApiCatalogApplication.java
@@ -13,8 +13,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.HystrixAutoConfiguration;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -36,7 +34,6 @@ import org.zowe.apiml.product.version.BuildInfo;
 @EnableAsync
 @EnableApimlLogger
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 public class ApiCatalogApplication {
 
     public static void main(String[] args) {
