1209d88e19af7731e9b7ae4dc91e27923cc6782e
Carson Cook <carson.cook@ibm.com>
2021-11-18 14:19:58 -0500

    Remove unused vars
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- metrics-service-ui/frontend/src/components/Dashboard/Dashboard.jsx
+++ metrics-service-ui/frontend/src/components/Dashboard/Dashboard.jsx
@@ -12,29 +12,12 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
 import axios from 'axios';
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-}));
-
 export default function Dashboard() {
-    const classes = useStyles();
-
     const [availableClusters, setAvailableClusters] = useState([]);
     const [currentCluster, setCurrentCluster] = useState(null);
     const [haveGottenClusters, setHaveGottenClusters] = useState(false);
