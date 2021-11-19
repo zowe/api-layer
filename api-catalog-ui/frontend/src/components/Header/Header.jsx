9bd4a6212c8c60b266529c61367abd47603add9a
at670475 <andrea.tabone@broadcom.com>
2021-11-19 13:17:50 +0100

    WIP - Header migration
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- api-catalog-ui/frontend/src/components/Header/Header.jsx
+++ api-catalog-ui/frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import { Button, Link, Text, Tooltip } from 'mineral-ui';
-import { IconPowerSettingsNew } from 'mineral-ui-icons';
+import { Button, Link, Typography, Tooltip } from '@material-ui/core';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import productImage from '../../assets/images/api-catalog-logo.png';
 import './Header.css';
 
@@ -11,8 +11,8 @@ export default class Header extends Component {
     };
 
     render() {
-        const iconLogout = <IconPowerSettingsNew color="white" />;
-        const dashboard = '/ui/v1/apicatalog/#/dashboard';
+        const iconLogout = <PowerSettingsNewIcon style={{ color: 'white' }} />;
+        const dashboard = 'ui/v1/apicatalog/#/dashboard';
         return (
             <div className="header">
                 <div className="product-name">
@@ -22,22 +22,16 @@ export default class Header extends Component {
                         </div>
                     </Link>
                     <Link href={dashboard}>
-                        <Text element="h3" color="#ffffff">
-                            API Catalog
-                        </Text>
+                        <Typography variant="h7">API Catalog</Typography>
                     </Link>
                 </div>
                 <div className="right-icons">
                     <div className="logout-container">
-                        <Tooltip content="Logout">
-                            <Button
-                                iconStart={iconLogout}
-                                data-testid="logout"
-                                primary
-                                circular
-                                onClick={this.handleLogout}
-                            />
-                        </Tooltip>
+                        <Button data-testid="logout" primary circular onClick={this.handleLogout}>
+                            <Tooltip circular title="Logout">
+                                {iconLogout}
+                            </Tooltip>
+                        </Button>
                     </div>
                 </div>
             </div>
