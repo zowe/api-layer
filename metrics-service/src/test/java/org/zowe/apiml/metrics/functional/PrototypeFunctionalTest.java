969b386ce0768abdaa7d5779844f9406a65bbc2a
Jakub Balhar <jakub.balhar@broadcom.net>
2021-11-24 14:00:20 +0100

    Remove unneeded public modifier
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.net>

--- metrics-service/src/test/java/org/zowe/apiml/metrics/functional/PrototypeFunctionalTest.java
+++ metrics-service/src/test/java/org/zowe/apiml/metrics/functional/PrototypeFunctionalTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class PrototypeFunctionalTest extends MetricsFunctionalTest {
+class PrototypeFunctionalTest extends MetricsFunctionalTest {
     @Test
     void testContextStarts() {
         assertTrue(true);
