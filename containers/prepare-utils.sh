b9cd047ecbcbf87737fbe05d73f67bd371a8b294
Carson Cook <carson.cook@ibm.com>
2021-11-25 15:20:33 -0500

    WIP debug logging
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- containers/prepare-utils.sh
+++ containers/prepare-utils.sh
@@ -84,7 +84,9 @@ function preparePackage {
     cd "${BASE_DIR}/${WORK_DIR}/${subdirectory}"
     unzip "${REPO_ROOT_DIR}/${service_package}/build/distributions/${service_package}.zip"
     chmod +x bin/*
+    echo "Carson debug"
     echo "$(ls -la)"
+    echo "$(ls -la bin)"
     echo "$(pwd)"
 }
 
