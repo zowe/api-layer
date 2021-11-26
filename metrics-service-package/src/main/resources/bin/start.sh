4a7360efe20b200dadf79a662f8fdbdd21539811
Carson Cook <CarsonCook@users.noreply.github.com>
2021-11-26 00:57:58 -0500

    Chore: Publish Metrics Service Zowe conformant containers (#1925)
    * Fix ms port var name
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Use ms lite lib
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add common lib to loader path
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add proper default values
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add ms container files
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add missing default value
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add metrics service to container workflows
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix metrics start script for container
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add wait to metrics start script
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * WIP debug
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * WIP debug logging
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Set ms packaging to use lite jar
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Clean up debugging logging
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add metrics service to containers readme
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- metrics-service-package/src/main/resources/bin/start.sh
+++ metrics-service-package/src/main/resources/bin/start.sh
@@ -28,7 +28,20 @@
 if [[ ! -z ${APIML_METRICS_ENABLED} && ${APIML_METRICS_ENABLED} == true ]]
 then
 
-JAR_FILE="${LAUNCH_COMPONENT}/metrics-service.jar"
+if [[ -z "${LAUNCH_COMPONENT}" ]]
+then
+  # component should be started from component home directory
+  LAUNCH_COMPONENT=$(pwd)/bin
+fi
+
+JAR_FILE="${LAUNCH_COMPONENT}/metrics-service-lite.jar"
+# script assumes it's in the metrics component directory and common_lib needs to be relative path
+if [[ -z ${CMMN_LB} ]]
+then
+    COMMON_LIB="../apiml-common-lib/bin/api-layer-lite-lib-all.jar"
+else
+    COMMON_LIB=${CMMN_LB}
+fi
 
 # API Mediation Layer Debug Mode
 export LOG_LEVEL=
@@ -42,6 +55,9 @@ if [ `uname` = "OS/390" ]
 then
     QUICK_START=-Xquickstart
 fi
+
+EXPLORER_HOST=${ZOWE_EXPLORER_HOST:-localhost}
+
 LIBPATH="$LIBPATH":"/lib"
 LIBPATH="$LIBPATH":"/usr/lib"
 LIBPATH="$LIBPATH":"${JAVA_HOME}"/bin
@@ -59,28 +75,31 @@ _BPX_JOBNAME=${ZOWE_PREFIX}${METRICS_CODE} java -Xms16m -Xmx512m \
   -Dfile.encoding=UTF-8 \
   -Djava.io.tmpdir=/tmp \
   -Dspring.profiles.include=$LOG_LEVEL \
-  -Dapiml.service.port=${ZWE_METRICS_SERVICE_PORT} \
-  -Dapiml.service.hostname=${ZOWE_EXPLORER_HOST} \
-  -Dapiml.service.discoveryServiceUrls=${ZWE_DISCOVERY_SERVICES_LIST} \
-  -Dapiml.service.ipAddress=${ZOWE_IP_ADDRESS} \
-  -Dapiml.service.customMetadata.apiml.gatewayPort=${GATEWAY_PORT} \
+  -Dapiml.service.port=${METRICS_PORT:-7551} \
+  -Dapiml.service.hostname=${EXPLORER_HOST} \
+  -Dapiml.service.discoveryServiceUrls=${ZWE_DISCOVERY_SERVICES_LIST:-"https://${EXPLORER_HOST}:${DISCOVERY_PORT:-7553}/eureka/"} \
+  -Dapiml.service.ipAddress=${ZOWE_IP_ADDRESS:-127.0.0.1} \
+  -Dapiml.service.customMetadata.apiml.gatewayPort=${GATEWAY_PORT:-7554} \
   -Dapiml.service.ssl.verifySslCertificatesOfServices=${VERIFY_CERTIFICATES:-false} \
   -Dapiml.service.ssl.nonStrictVerifySslCertificatesOfServices=${NONSTRICT_VERIFY_CERTIFICATES:-false} \
-  -Dapiml.service.preferIpAddress=${APIML_PREFER_IP_ADDRESS} \
+  -Dapiml.service.preferIpAddress=${APIML_PREFER_IP_ADDRESS:-false} \
   -Dserver.address=0.0.0.0 \
-  -Dserver.ssl.enabled=true \
+  -Dserver.ssl.enabled=${APIML_SSL_ENABLED:-true} \
   -Dserver.ssl.keyStore="${KEYSTORE}" \
-  -Dserver.ssl.keyStoreType="${KEYSTORE_TYPE}" \
+  -Dserver.ssl.keyStoreType="${KEYSTORE_TYPE-PKCS12}" \
   -Dserver.ssl.keyStorePassword="${KEYSTORE_PASSWORD}" \
   -Dserver.ssl.keyAlias="${KEY_ALIAS}" \
   -Dserver.ssl.keyPassword="${KEYSTORE_PASSWORD}" \
   -Dserver.ssl.trustStore="${TRUSTSTORE}" \
-  -Dserver.ssl.trustStoreType="${KEYSTORE_TYPE}" \
+  -Dserver.ssl.trustStoreType="${KEYSTORE_TYPE-PKCS12}" \
   -Dserver.ssl.trustStorePassword="${KEYSTORE_PASSWORD}" \
   -Djava.protocol.handler.pkgs=com.ibm.crypto.provider \
+  -Dloader.path=${COMMON_LIB} \
   -Djava.library.path=${LIBPATH} \
   -jar ${JAR_FILE} &
 pid=$!
 echo "pid=${pid}"
+
+wait %1
 
 fi
