1b8844cb3f82d2983be5e2147bb8fa5d7a409771
Carson Cook <CarsonCook@users.noreply.github.com>
2021-11-30 02:27:37 -0500

    fix: Swagger server url transform (#1934)
    * Fix transformation of swagger servers urls
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix bug in tests with undefined swagger version
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add test for swagger server url transform
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- api-catalog-ui/frontend/src/components/Swagger/Swagger.test.jsx
+++ api-catalog-ui/frontend/src/components/Swagger/Swagger.test.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 import { shallow } from 'enzyme';
 import SwaggerUI from './Swagger';
+import { act } from "react-dom/test-utils";
+import { render } from 'react-dom';
 
 describe('>>> Swagger component tests', () => {
     it('should not render swagger if apiDoc is null', () => {
@@ -16,11 +18,37 @@ describe('>>> Swagger component tests', () => {
         };
         const wrapper = shallow(
             <div>
-                <SwaggerUI service={service} />
+                <SwaggerUI selectedService={service} />
             </div>
         );
         const swaggerDiv = wrapper.find('#swaggerContainer');
 
         expect(swaggerDiv.length).toEqual(0);
+    });
+
+    it('should transform swagger server url', async () => {
+        const endpoint = '/enabler/api/v1';
+        const service = {
+            serviceId: 'testservice',
+            title: 'Spring Boot Enabler Service',
+            description: 'Dummy Service for enabling others',
+            status: 'UP',
+            secured: false,
+            homePageUrl: 'http://localhost:10013/enabler/',
+            basePath: '/enabler/api/v1',
+            apiDoc: JSON.stringify({
+                openapi: "3.0.0",
+                servers: [{url: 'https://bad.com' + endpoint}],
+            }),
+            apiId: {
+                default: 'enabler'
+            }
+        };
+
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+
+        await act(async () => render(<SwaggerUI selectedService={service}/>, container));
+        expect(container.textContent).toContain('Servershttp://localhost' + endpoint);
     });
 });
