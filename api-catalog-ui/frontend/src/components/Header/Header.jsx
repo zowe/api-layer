537fa34b0163023ca8fa97b0e93b329c53198df3
Andrea Tabone <39694626+taban03@users.noreply.github.com>
2021-12-02 11:31:50 +0100

    feat: Migrate header and footer to Material UI (#1943)
    * POC - Fully migrate Login component to Material
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * WIP - Header migration
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix icon
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Migrate footer
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * fix unit test
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix tests
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix error
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix link
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- api-catalog-ui/frontend/src/components/Header/Header.jsx
+++ api-catalog-ui/frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import { Button, Link, Text, Tooltip } from 'mineral-ui';
-import { IconPowerSettingsNew } from 'mineral-ui-icons';
+import { IconButton, Link, Typography, Tooltip } from '@material-ui/core';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import productImage from '../../assets/images/api-catalog-logo.png';
 import './Header.css';
 
@@ -11,32 +11,26 @@ export default class Header extends Component {
     };
 
     render() {
-        const iconLogout = <IconPowerSettingsNew color="white" />;
-        const dashboard = '/ui/v1/apicatalog/#/dashboard';
+        const iconLogout = <PowerSettingsNewIcon id="logoutIcon" style={{ color: 'white' }} />;
+        const dashboard = 'ui/v1/apicatalog/#/dashboard';
         return (
             <div className="header">
                 <div className="product-name">
-                    <Link href={dashboard}>
+                    <Link data-testid="link" href={dashboard}>
                         <div className="app-icon-container">
                             <img id="logo" alt="API Catalog Product Name" src={productImage} />
                         </div>
                     </Link>
                     <Link href={dashboard}>
-                        <Text element="h3" color="#ffffff">
-                            API Catalog
-                        </Text>
+                        <Typography variant="subtitle2">API Catalog</Typography>
                     </Link>
                 </div>
                 <div className="right-icons">
                     <div className="logout-container">
-                        <Tooltip content="Logout">
-                            <Button
-                                iconStart={iconLogout}
-                                data-testid="logout"
-                                primary
-                                circular
-                                onClick={this.handleLogout}
-                            />
+                        <Tooltip title="Logout">
+                            <IconButton data-testid="logout" onClick={this.handleLogout}>
+                                {iconLogout}
+                            </IconButton>
                         </Tooltip>
                     </div>
                 </div>
