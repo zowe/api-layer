0048241c63dcb760ca12769382dbd7bdaa1076cb
achmelo <a.chmelo@gmail.com>
2021-12-09 15:55:32 +0100

    implement infinispan storage similar to inmemory
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/service/infinispan/storage/InfinispanStorage.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/infinispan/storage/InfinispanStorage.java
@@ -9,7 +9,6 @@
  */
 package org.zowe.apiml.caching.service.infinispan.storage;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
@@ -24,52 +23,69 @@ import java.util.Map;
 @Slf4j
 public class InfinispanStorage implements Storage {
 
-    private final DefaultCacheManager defaultCacheManager;
-    private Cache<Object,Object> cache;
+
+    private final Cache<String, Map<String, KeyValue>> cache;
 
     public InfinispanStorage(DefaultCacheManager defaultCacheManager) {
-        this.defaultCacheManager = defaultCacheManager;
         cache = defaultCacheManager.getCache("myCache");
     }
 
     @Override
     public KeyValue create(String serviceId, KeyValue toCreate) {
         log.info("Writing record: {}|{}|{}", serviceId, toCreate.getKey(), toCreate.getValue());
-        cache.computeIfAbsent(serviceId, k -> new HashMap<>());
-        Map<String, KeyValue> serviceEntries = (Map<String, KeyValue>) cache.get(serviceId);
-        if(serviceEntries.containsKey(toCreate.getKey())){
+
+        Map<String, KeyValue> storage = cache.computeIfAbsent(serviceId, k -> new HashMap<>());
+
+        if (storage.containsKey(toCreate.getKey())) {
             throw new StorageException(Messages.DUPLICATE_KEY.getKey(), Messages.DUPLICATE_KEY.getStatus(), toCreate.getKey());
         }
-
-        Object o = cache.putIfAbsent(toCreate.getKey(), toCreate);
-        return toCreate;
+        KeyValue entry = storage.put(toCreate.getKey(), toCreate);
+        cache.put(serviceId, storage);
+        return entry;
     }
 
     @Override
     public KeyValue read(String serviceId, String key) {
-        return (KeyValue) defaultCacheManager.getCache("myCache").get(key);
+        Map<String, KeyValue> serviceCache = cache.get(serviceId);
+        if (serviceCache != null && serviceCache.containsKey(key)) {
+            return serviceCache.get(key);
+        } else {
+            throw new StorageException(Messages.KEY_NOT_IN_CACHE.getKey(), Messages.KEY_NOT_IN_CACHE.getStatus(), key, serviceId);
+        }
     }
 
     @Override
     public KeyValue update(String serviceId, KeyValue toUpdate) {
-        defaultCacheManager.getCache("myCache").put(toUpdate.getKey(), toUpdate);
-        return
+        Map<String, KeyValue> serviceCache = cache.get(serviceId);
+        if (serviceCache == null || !serviceCache.containsKey(toUpdate.getKey())) {
+            throw new StorageException(Messages.KEY_NOT_IN_CACHE.getKey(), Messages.KEY_NOT_IN_CACHE.getStatus(), toUpdate.getKey(), serviceId);
+        }
+        serviceCache.put(toUpdate.getKey(), toUpdate);
+        cache.put(serviceId, serviceCache);
+        return toUpdate;
 
-            null;
     }
 
     @Override
     public KeyValue delete(String serviceId, String toDelete) {
-        return (KeyValue) defaultCacheManager.getCache("myCache").remove(toDelete);
+        Map<String, KeyValue> serviceCache = cache.get(serviceId);
+        KeyValue entry;
+        if (serviceCache.containsKey(toDelete)) {
+            entry = serviceCache.remove(toDelete);
+            cache.put(serviceId, serviceCache);
+            return entry;
+        } else {
+            throw new StorageException(Messages.KEY_NOT_IN_CACHE.getKey(), Messages.KEY_NOT_IN_CACHE.getStatus(), toDelete, serviceId);
+        }
     }
 
     @Override
     public Map<String, KeyValue> readForService(String serviceId) {
-        return null;
+        return cache.get(serviceId);
     }
 
     @Override
     public void deleteForService(String serviceId) {
-
+        cache.remove(serviceId);
     }
 }
