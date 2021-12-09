83a67f0bc2e0eac604b2fef078a9bdd67bee59c6
achmelo <a.chmelo@gmail.com>
2021-12-09 10:46:27 +0100

    add entry
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/service/infinispan/storage/InfinispanStorage.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/infinispan/storage/InfinispanStorage.java
@@ -10,22 +10,38 @@
 package org.zowe.apiml.caching.service.infinispan.storage;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
 import org.zowe.apiml.caching.model.KeyValue;
+import org.zowe.apiml.caching.service.Messages;
 import org.zowe.apiml.caching.service.Storage;
+import org.zowe.apiml.caching.service.StorageException;
 
+import java.util.HashMap;
 import java.util.Map;
 
-
-@RequiredArgsConstructor
+@Slf4j
 public class InfinispanStorage implements Storage {
 
     private final DefaultCacheManager defaultCacheManager;
+    private Cache<Object,Object> cache;
+
+    public InfinispanStorage(DefaultCacheManager defaultCacheManager) {
+        this.defaultCacheManager = defaultCacheManager;
+        cache = defaultCacheManager.getCache("myCache");
+    }
 
     @Override
     public KeyValue create(String serviceId, KeyValue toCreate) {
+        log.info("Writing record: {}|{}|{}", serviceId, toCreate.getKey(), toCreate.getValue());
+        cache.computeIfAbsent(serviceId, k -> new HashMap<>());
+        Map<String, KeyValue> serviceEntries = (Map<String, KeyValue>) cache.get(serviceId);
+        if(serviceEntries.containsKey(toCreate.getKey())){
+            throw new StorageException(Messages.DUPLICATE_KEY.getKey(), Messages.DUPLICATE_KEY.getStatus(), toCreate.getKey());
+        }
 
-        Object o = defaultCacheManager.getCache("myCache").putIfAbsent(toCreate.getKey(), toCreate);
+        Object o = cache.putIfAbsent(toCreate.getKey(), toCreate);
         return toCreate;
     }
 
