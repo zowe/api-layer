36004b4dc9dad1ccd6eb7154f586b5b7153afe4b
achmelo <a.chmelo@gmail.com>
2021-12-16 10:43:28 +0100

    fix styles and smells
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- caching-service/src/test/java/org/zowe/apiml/caching/service/infinispan/storage/InfinispanStorageTest.java
+++ caching-service/src/test/java/org/zowe/apiml/caching/service/infinispan/storage/InfinispanStorageTest.java
@@ -25,12 +25,13 @@ import static org.mockito.Mockito.*;
 class InfinispanStorageTest {
 
     public static final KeyValue TO_CREATE = new KeyValue("key1", "val1");
+    public static final KeyValue TO_UPDATE = new KeyValue("key1", "val2");
     Cache<String, Map<String, KeyValue>> cache;
     InfinispanStorage storage;
     String serviceId1 = "service1";
 
     @BeforeEach
-    void setup(){
+    void setup() {
         cache = mock(Cache.class);
         storage = new InfinispanStorage(cache);
     }
@@ -39,15 +40,14 @@ class InfinispanStorageTest {
     class WhenEntryDoesntExist {
 
         Map<String, KeyValue> serviceStore;
+
         @BeforeEach
-        void createEmptyStore(){
+        void createEmptyStore() {
             serviceStore = new HashMap<>();
         }
 
         @Test
         void whenCreate_thenCacheIsUpdated() {
-
-
             when(cache.computeIfAbsent(any(), any())).thenReturn(serviceStore);
             storage.create(serviceId1, TO_CREATE);
             verify(cache, times(1)).put(serviceId1, serviceStore);
@@ -55,29 +55,28 @@ class InfinispanStorageTest {
 
         @Test
         void whenRead_thenExceptionIsThrown() {
-
+            String key = TO_CREATE.getKey();
             when(cache.get(serviceId1)).thenReturn(serviceStore);
-            assertThrows(StorageException.class,()->storage.read(serviceId1,TO_CREATE.getKey()));
+            assertThrows(StorageException.class, () -> storage.read(serviceId1, key));
         }
+
         @Test
         void whenDelete_thenExceptionIsThrown() {
 
-
+            String key = TO_CREATE.getKey();
             when(cache.get(serviceId1)).thenReturn(serviceStore);
-            assertThrows(StorageException.class, ()->storage.delete(serviceId1,TO_CREATE.getKey()));
-            verify(cache,times(0)).put(serviceId1,serviceStore);
+            assertThrows(StorageException.class, () -> storage.delete(serviceId1, key));
+            verify(cache, times(0)).put(serviceId1, serviceStore);
         }
 
         @Test
         void whenUpdate_thenCacheIsUpdated() {
 
             when(cache.get(serviceId1)).thenReturn(serviceStore);
-            assertThrows(StorageException.class, ()->storage.update(serviceId1, new KeyValue("key1", "val2")));
+            assertThrows(StorageException.class, () -> storage.update(serviceId1, TO_UPDATE));
             verify(cache, times(0)).put(serviceId1, serviceStore);
         }
     }
-
-
 
 
     @Nested
@@ -85,7 +84,7 @@ class InfinispanStorageTest {
         Map<String, KeyValue> serviceStore;
 
         @BeforeEach
-        void createStoreWithEntry(){
+        void createStoreWithEntry() {
             serviceStore = new HashMap<>();
             serviceStore.put(TO_CREATE.getKey(), TO_CREATE);
         }
@@ -93,36 +92,36 @@ class InfinispanStorageTest {
         @Test
         void whenCreate_thenExceptionIsThrown() {
             when(cache.computeIfAbsent(any(), any())).thenReturn(serviceStore);
-            assertThrows(StorageException.class,()->storage.create(serviceId1, TO_CREATE));
+            assertThrows(StorageException.class, () -> storage.create(serviceId1, TO_CREATE));
         }
+
         @Test
         void whenRead_thenEntryIsReturned() {
             when(cache.get(serviceId1)).thenReturn(serviceStore);
-            KeyValue result = storage.read(serviceId1,TO_CREATE.getKey());
-            assertEquals(TO_CREATE.getValue(),result.getValue());
+            KeyValue result = storage.read(serviceId1, TO_CREATE.getKey());
+            assertEquals(TO_CREATE.getValue(), result.getValue());
         }
 
         @Test
         void whenUpdate_thenCacheIsUpdated() {
 
             when(cache.get(serviceId1)).thenReturn(serviceStore);
-            storage.update(serviceId1, new KeyValue("key1", "val2"));
+            storage.update(serviceId1, TO_UPDATE);
             verify(cache, times(1)).put(serviceId1, serviceStore);
-            assertEquals("val2",serviceStore.get(TO_CREATE.getKey()).getValue());
+            assertEquals("val2", serviceStore.get(TO_CREATE.getKey()).getValue());
         }
 
         @Test
         void whenDelete_thenCacheIsUpdated() {
 
             when(cache.get(serviceId1)).thenReturn(serviceStore);
-            KeyValue result = storage.delete(serviceId1,TO_CREATE.getKey());
-            verify(cache,times(1)).put(serviceId1,serviceStore);
-            assertEquals(TO_CREATE.getValue(),result.getValue());
+            KeyValue result = storage.delete(serviceId1, TO_CREATE.getKey());
+            verify(cache, times(1)).put(serviceId1, serviceStore);
+            assertEquals(TO_CREATE.getValue(), result.getValue());
             assertNull(serviceStore.get(TO_CREATE.getKey()));
         }
 
     }
-
 
 
 }
