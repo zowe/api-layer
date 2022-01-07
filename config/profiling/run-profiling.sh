8d744ef25fecadc25ea97aa24d89d0848fdf0c3d
Carson Cook <carson.cook@ibm.com>
2022-01-07 10:47:41 -0500

    Add options to profiling script
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- config/profiling/run-profiling.sh
+++ config/profiling/run-profiling.sh
@@ -6,11 +6,24 @@
 # Allow overriding directory name
 
 load="high"
+threads_flag=
 threads=50
+dataset="mock_csv.csv"
 dirName="output-${load}"
 host=
 port=
 
+while getopts "l:d:h:p:t:o:" flag; do
+    case $flag in
+        l) load=$OPTARG ;;
+        d) dataset=$OPTARG ;;
+        h) host=$OPTARG ;;
+        p) port=$OPTARG ;;
+        t) threads_flag=$OPTARG ;;
+        o) dirName=./$OPTARG ;; # use current directory to reduce change of accidentally deleted dir
+    esac
+done
+
 rm -rf $dirName # jmeter fails if output directory already exists
 
-jmeter -Jhost=$host -Jport=$port -Jthreads=$threads -Jdataset=mock_csv.csv -Jjmeter.reportgenerator.overall_granularity=1000 -n -t caching-profiling-parametrized.jmx -l $dirName/result -e -o $dirName/test-results -j $dirName/result.log
+jmeter -Jhost=$host -Jport=$port -Jthreads=$threads -Jdataset=$dataset -Jjmeter.reportgenerator.overall_granularity=1000 -n -t caching-profiling-parametrized.jmx -l $dirName/result -e -o $dirName/test-results -j $dirName/result.log
