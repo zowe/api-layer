176348b0e5e0349ef640eb800ddadffbf5f209af
wanger26 <jwanger@uwo.ca>
2022-02-15 18:06:13 -0500

    Added inital POC for data-source plugin for metrics service connecting to turbine gateway cluster
    Signed-off-by: wanger26 <jwanger@uwo.ca>

---
+++ metrics-service-ui/data-source-plugin/src/datasource.ts
@@ -0,0 +1,61 @@
+import defaults from 'lodash/defaults';
+import { Observable, merge } from 'rxjs';
+
+import {
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceApi,
+  DataSourceInstanceSettings,
+  FieldType,
+  CircularDataFrame,
+  LoadingState,
+} from '@grafana/data';
+
+import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
+
+export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+    super(instanceSettings);
+  }
+
+  query(options: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
+    const streams = options.targets.map((target) => {
+      const query = defaults(target, defaultQuery);
+
+      return new Observable<DataQueryResponse>((subscriber) => {
+        const frame = new CircularDataFrame({
+          append: 'tail',
+          capacity: 1000,
+        });
+
+        frame.refId = query.refId;
+        frame.addField({ name: 'time', type: FieldType.time });
+        frame.addField({ name: 'value', type: FieldType.number });
+
+        const eventSource = new EventSource(
+          'https://localhost:10010/metrics-service/sse/v1/turbine.stream?cluster=GATEWAY',
+          { withCredentials: true }
+        );
+        eventSource.addEventListener('message', (event) => {
+          // "event.data" is a string
+          const data = JSON.parse(event.data);
+          frame.add({ time: Date.now(), value: data['timestamp'] });
+          subscriber.next({
+            data: [frame],
+            key: query.refId,
+            state: LoadingState.Streaming,
+          });
+        });
+
+        return () => {
+          eventSource.close();
+        };
+      });
+    });
+
+    return merge(...streams);
+  }
+  testDatasource(): Promise<any> {
+    return Promise.resolve(undefined);
+  }
+}
