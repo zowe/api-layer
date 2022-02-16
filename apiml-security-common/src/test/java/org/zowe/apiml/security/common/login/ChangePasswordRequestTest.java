51e8bd3816b1a9c2931f3cff6d1ec8aa8631bebe
Andrea Tabone <39694626+taban03@users.noreply.github.com>
2022-02-16 09:46:49 +0100

    feat: Add support to change password via zOSMF (#2095)
    * Add change password support to z/OSMF provider
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add unit tests + refactoring
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix checkstyle
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Fix change password request body
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Authenticate with new pw in case of change password success
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add forgotten files to git
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Remove unused constructor
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Remove code smells
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * Add unit tests
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

---
+++ apiml-security-common/src/test/java/org/zowe/apiml/security/common/login/ChangePasswordRequestTest.java
@@ -0,0 +1,34 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.security.common.login;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChangePasswordRequestTest {
+    public static final String NEW_PASS = "newPass";
+    public static final String PASS = "pass";
+    public static final String USERNAME = "user";
+
+    @Nested
+    class WhenPassingLoginRequestObject {
+        LoginRequest loginRequest = new LoginRequest(USERNAME, PASS, NEW_PASS);
+        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest(loginRequest);
+
+        @Test
+        void thenMapConstructor() {
+            assertEquals(loginRequest.getUsername(), changePasswordRequest.getUsername());
+            assertEquals(loginRequest.getPassword(), changePasswordRequest.getPassword());
+            assertEquals(loginRequest.getNewPassword(), changePasswordRequest.getNewPassword());
+        }
+    }
+}
