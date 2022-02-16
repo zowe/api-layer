3fd4c9f1bd6f7f161f37c717cd551bc5603c8843
achmelo <a.chmelo@gmail.com>
2022-02-16 17:37:50 +0100

    blue color on hover, white arrow
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- api-catalog-ui/frontend/src/components/Header/Header.jsx
+++ api-catalog-ui/frontend/src/components/Header/Header.jsx
@@ -7,91 +7,92 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
-import { Component } from 'react';
-import { Button, Link, Typography, Menu, MenuItem } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, Link, Typography, Menu, MenuItem, makeStyles } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import productImage from '../../assets/images/api-catalog-logo.png';
 import './Header.css';
 
-export default class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpen: false,
-            anchorEl: null,
-        };
-        this.handleLogout = this.handleLogout.bind(this);
-        this.openMenu = this.openMenu.bind(this);
-        this.handleLogout = this.handleLogout.bind(this);
-        this.closeMenu = this.closeMenu.bind(this);
-    }
-    handleLogout = () => {
-        const { logout } = this.props;
+const useStyles = makeStyles({
+    root: {
+        '&:hover': {
+            backgroundColor: 'rgba(16,155,255,0.94)',
+        },
+    },
+});
+const Header = (props) => {
+    const [open, setOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const { logout } = props;
+    const classes = useStyles();
+    const handleLogout = () => {
         logout();
     };
 
-    closeMenu() {
-        this.setState({ isOpen: false });
-    }
+    const closeMenu = () => {
+        setOpen(false);
+    };
 
-    openMenu(event) {
-        this.setState({ isOpen: true, anchorEl: event.target });
-    }
+    const openMenu = (event) => {
+        setOpen(true);
+        setAnchorEl(event.target);
+    };
 
-    render() {
-        const iconProfile = <PersonIcon id="profileIcon" style={{ color: 'white' }} />;
-        const dashboard = 'ui/v1/apicatalog/#/dashboard';
-        const username = localStorage.getItem('username');
-        return (
-            <div className="header">
-                <div className="product-name">
-                    <Link data-testid="link" href={dashboard}>
-                        <div className="app-icon-container">
-                            <img id="logo" alt="API Catalog Product Name" src={productImage} />
-                        </div>
-                    </Link>
-                    <Link href={dashboard}>
-                        <Typography variant="subtitle2">API Catalog</Typography>
-                    </Link>
-                </div>
-                <div className="right-icons">
-                    <div className="logout-container">
-                        <Button
-                            aria-controls={this.state.isOpen ? 'basic-menu' : undefined}
-                            aria-expanded={this.state.isOpen ? 'true' : undefined}
-                            aria-haspopup="true"
-                            aria-label="more"
-                            onClick={this.openMenu}
-                            endIcon={<KeyboardArrowDownIcon />}
-                        >
-                            {iconProfile}
-                        </Button>
-                        <Menu
-                            keepMounted
-                            open={this.state.isOpen}
-                            onClose={this.closeMenu}
-                            anchorEl={this.state.anchorEl}
-                            getContentAnchorEl={null}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'center',
-                            }}
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'center',
-                            }}
-                        >
-                            <div id="profile-menu">
-                                Logged in as <strong>{username}</strong>
-                                <MenuItem id="logout-button" data-testid="logout" onClick={this.handleLogout}>
-                                    Log out
-                                </MenuItem>
-                            </div>
-                        </Menu>
+    const s = <PersonIcon id="profileIcon" style={{ color: 'white' }} />;
+    const dashboard = 'ui/v1/apicatalog/#/dashboard';
+    const username = localStorage.getItem('username');
+    return (
+        <div className="header">
+            <div className="product-name">
+                <Link data-testid="link" href={dashboard}>
+                    <div className="app-icon-container">
+                        <img id="logo" alt="API Catalog Product Name" src={productImage} />
                     </div>
+                </Link>
+                <Link href={dashboard}>
+                    <Typography variant="subtitle2">API Catalog</Typography>
+                </Link>
+            </div>
+            <div className="right-icons">
+                <div className="logout-container">
+                    <Button
+                        className={classes.root}
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-expanded={open ? 'true' : undefined}
+                        aria-haspopup="true"
+                        aria-label="more"
+                        onClick={openMenu}
+                        endIcon={<KeyboardArrowDownIcon id="down-arrow" />}
+                    >
+                        {s}
+                    </Button>
+                    <Menu
+                        keepMounted
+                        open={open}
+                        onClose={closeMenu}
+                        anchorEl={anchorEl}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'center',
+                        }}
+                    >
+                        <div id="profile-menu">
+                            Signed in as <strong>{username}</strong>
+                            <MenuItem id="logout-button" data-testid="logout" onClick={handleLogout}>
+                                Sign out
+                            </MenuItem>
+                        </div>
+                    </Menu>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default Header;
