1407e2e95c38b1824493af80301fb556ec88587c
achmelo <37397715+achmelo@users.noreply.github.com>
2022-03-04 09:45:17 +0100

    fix: configure southbound timeout with APIML_GATEWAY_TIMEOUT_MILLIS (#2154)
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/functional/gateway/GatewayTimeoutTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/functional/gateway/GatewayTimeoutTest.java
@@ -9,9 +9,11 @@
  */
 package org.zowe.apiml.functional.gateway;
 
+import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.apache.http.message.BasicNameValuePair;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -33,13 +35,19 @@ class GatewayTimeoutTest implements TestWithStartedInstances {
     private static final int SECOND = 1000;
     private static final int DEFAULT_TIMEOUT = 30 * SECOND;
 
+    @BeforeEach
+    void setUp() {
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
     @Nested
     class GivenRequestTakesTooLong {
         @Nested
         class WhenCallingThroughGateway {
             @Test
             void returnGatewayTimeout() {
-                assertTimeout(Duration.ofMillis(DEFAULT_TIMEOUT + (5 * SECOND)), () -> {
+                assertTimeout(Duration.ofMillis(DEFAULT_TIMEOUT * 6 + (2 * SECOND)), () -> {
                     given()
                     .when()
                         .get(getUriFromGateway(DISCOVERABLE_GREET,
@@ -47,7 +55,7 @@ class GatewayTimeoutTest implements TestWithStartedInstances {
                                 new BasicNameValuePair("delayMs", String.valueOf(DEFAULT_TIMEOUT + SECOND)))
                             )
                         )
-                    .then()
+                        .then()
                         .statusCode(HttpStatus.SC_GATEWAY_TIMEOUT);
                 });
             }
