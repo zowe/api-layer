2ce65dbcde8ca23ec48dcd3648fada442e49fdcb
wanger26 <jwanger@uwo.ca>
2022-03-14 18:38:35 -0400

    Paramtized urls and refactored code to address the comments in the PR
    Signed-off-by: wanger26 <jwanger@uwo.ca>

--- metrics-service-ui/data-source-plugin/src/QueryEditor.tsx
+++ metrics-service-ui/data-source-plugin/src/QueryEditor.tsx
@@ -1,71 +1,69 @@
 import defaults from 'lodash/defaults';
 
-import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms } from '@grafana/ui';
-import { QueryEditorProps, SelectableValue } from '@grafana/data';
-import { DataSource } from './datasource';
-import { defaultQuery, MyDataSourceOptions, MyQuery } from './types';
+import React, {ChangeEvent, PureComponent} from 'react';
+import {LegacyForms} from '@grafana/ui';
+import {QueryEditorProps, SelectableValue} from '@grafana/data';
+import {DataSource} from './datasource';
+import {defaultQuery, MyDataSourceOptions, MyQuery} from './types';
 
-const { FormField } = LegacyForms;
-const availableClustersEndpoint = 'https://localhost:10010/metrics-service/api/v1/clusters';
+const {FormField} = LegacyForms;
+const config = require('../config/default.json');
+const availableClustersEndpoint = config.availableClustersEndpoint;
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
+type State = { availableClusters: JSX.Element[] };
 
-export class QueryEditor extends PureComponent<Props> {
-  onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query } = this.props;
-    onChange({ ...query, metricName: event.target.value });
-  };
+export class QueryEditor extends PureComponent<Props, State> {
+    onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const {onChange, query} = this.props;
+        onChange({...query, metricName: event.target.value});
+    };
 
-  onClusterChange = (event: SelectableValue<HTMLInputElement>) => {
-    const { onChange, query } = this.props;
-    onChange({ ...query, clusterName: event.target.value });
-  };
+    onClusterChange = (event: SelectableValue<HTMLInputElement>) => {
+        const {onChange, query} = this.props;
+        onChange({...query, clusterName: event.target.value});
+    };
 
-  async componentDidMount() {
-    fetch(availableClustersEndpoint)
-      .then((resp) => resp.json())
-      .then((clusters) => {
-        let items: JSX.Element[] = [];
+    async componentDidMount() {
+        fetch(availableClustersEndpoint)
+            .then((resp) => resp.json())
+            .then((clusters) => {
+                let availableClusters: JSX.Element[] = [];
 
-        // Add empty starting item to dropdown menu
-        items.push(<option key="" value="" />);
+                // Add empty starting item to dropdown menu
+                availableClusters.push(<option key="" value=""/>);
 
-        // Add clusters from rest call to dropdown menu
-        clusters.forEach((cluster: { name: string; link: string }) => {
-          items.push(
-            <option key={cluster.name} value={cluster.name}>
-              {cluster.name}
-            </option>
-          );
-        });
+                // Add clusters from rest call to dropdown menu
+                clusters.forEach((cluster: { name: string; link: string }) => {
+                    availableClusters.push(
+                        <option key={cluster.name} value={cluster.name}>
+                            {cluster.name}
+                        </option>
+                    );
+                });
 
-        this.setState({ items });
-      });
-  }
-
-  render() {
-    const query = defaults(this.props.query, defaultQuery);
-
-    let availableClusters = '';
-
-    if (this.state != null) {
-      // @ts-ignore
-      availableClusters = this.state.items;
+                this.setState({availableClusters});
+            });
     }
 
-    return (
-      <div className="gf-form">
-        <select onChange={this.onClusterChange} value={query.clusterName || ''}>
-          {availableClusters}
-        </select>
-        <FormField
-          labelWidth={8}
-          value={query.metricName || ''}
-          onChange={this.onQueryTextChange}
-          label="Metric Name"
-        />
-      </div>
-    );
-  }
+    render() {
+        const query = defaults(this.props.query, defaultQuery);
+
+        let availableClusters: JSX.Element[] = [];
+        let clusterName = query.clusterName || '';
+        let metricName = query.metricName || '';
+
+        if (this.state != null) {
+            availableClusters = this.state.availableClusters;
+        }
+
+        return (
+            <div className="gf-form">
+                <select onChange={this.onClusterChange} value={clusterName}>
+                    {availableClusters}
+                </select>
+                <FormField labelWidth={8} value={metricName} onChange={this.onQueryTextChange} label="Metric Name"/>
+            </div>
+        );
+    }
 }
