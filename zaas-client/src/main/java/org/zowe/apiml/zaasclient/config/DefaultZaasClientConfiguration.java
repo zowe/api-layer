60e1c679966d6bb59627d58d471f81ef03317404
Amanda D'Errico <amanda.derrico@ibm.com>
2022-03-25 16:28:42 -0400

    zaas client supports v2 compatibility with updated path
    Signed-off-by: Amanda D'Errico <amanda.derrico@ibm.com>

--- zaas-client/src/main/java/org/zowe/apiml/zaasclient/config/DefaultZaasClientConfiguration.java
+++ zaas-client/src/main/java/org/zowe/apiml/zaasclient/config/DefaultZaasClientConfiguration.java
@@ -52,7 +52,6 @@ public class DefaultZaasClientConfiguration {
         ConfigProperties configProperties = new ConfigProperties();
         configProperties.setApimlHost(host);
         configProperties.setApimlPort(port);
-        configProperties.setApimlBaseUrl(baseUrl);
         configProperties.setKeyStorePath(keyStorePath);
         configProperties.setKeyStorePassword(keyStorePassword);
         configProperties.setKeyStoreType(keyStoreType);
@@ -60,6 +59,15 @@ public class DefaultZaasClientConfiguration {
         configProperties.setTrustStorePassword(trustStorePassword);
         configProperties.setTrustStoreType(trustStoreType);
         configProperties.setNonStrictVerifySslCertificatesOfServices(nonStrictVerifySslCertificatesOfServices);
+
+        String updatedBaseUrl;
+        if (!baseUrl.startsWith("/gateway")) {
+            // rearrange path to match new base url format - {service id}/{type}/{api version} - both paths supported until March 2023
+            updatedBaseUrl = "/gateway" + baseUrl.substring(0, baseUrl.indexOf("/gateway")) + "/auth";
+        } else {
+            updatedBaseUrl = baseUrl;
+        }
+        configProperties.setApimlBaseUrl(updatedBaseUrl);
         return configProperties;
     }
 
