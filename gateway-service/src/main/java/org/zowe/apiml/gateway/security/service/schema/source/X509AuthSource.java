53cbf7f2c07880523806b268caf3cff4810b9acd
Yelyzaveta Chebanova <yelyzaveta.chebanova@broadcom.com>
2022-04-04 09:23:30 +0200

    feat: Enhance x509 authentication scheme to support client certificates (part 2)
    -remove unnecessary usage of Serializable
    
    Signed-off-by: Yelyzaveta Chebanova <yelyzaveta.chebanova@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/source/X509AuthSource.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/source/X509AuthSource.java
@@ -9,7 +9,6 @@
  */
 package org.zowe.apiml.gateway.security.service.schema.source;
 
-import java.io.Serializable;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 import lombok.EqualsAndHashCode;
@@ -43,7 +42,7 @@ public class X509AuthSource implements AuthSource {
     @RequiredArgsConstructor
     @Getter
     @EqualsAndHashCode
-    public static class Parsed implements AuthSource.Parsed, X509Parsed, Serializable {
+    public static class Parsed implements AuthSource.Parsed, X509Parsed {
         private static final long serialVersionUID = -2984464419130383590L;
         private final String userId;
         private final Date creation;
