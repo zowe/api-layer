a692a2a445b37b764046d241b44d808c00763424
Yelyzaveta Chebanova <yelyzaveta.chebanova@broadcom.com>
2022-04-05 16:35:13 +0200

    feat: Enhance x509 authentication scheme to support client certificates (part 3)
    - add IT for X509Scheme
    
    Signed-off-by: Yelyzaveta Chebanova <yelyzaveta.chebanova@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/authentication/schemes/X509SchemeTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/authentication/schemes/X509SchemeTest.java
@@ -81,5 +81,27 @@ class X509SchemeTest implements TestWithStartedInstances {
                 .body("dn", is(""))
                 .body("cn", is("")).statusCode(200);
         }
+
+        @Test
+        void givenServerCertificateInRequest() {
+            given()
+                .config(SslContext.apimlRootCert)
+                .when()
+                .get(X509SchemeTest.URL)
+                .then()
+                .body("X-Zowe-Auth-Failure", is("ZWEAG164E Error occurred while validating X509 certificate. X509 certificate is missing the client certificate extended usage definition")).statusCode(200);
+        }
+
+        @Test
+        void givenNoCertificate_thenEmptyBodyIsReturned() {
+            given()
+                .when()
+                .get(X509SchemeTest.URL)
+                .then()
+                .body("publicKey", is(""))
+                .body("dn", is(""))
+                .body("cn", is(""))
+                .body("X-Zowe-Auth-Failure", is("ZWEAG160E No authentication provided in the request")).statusCode(200);
+        }
     }
 }
