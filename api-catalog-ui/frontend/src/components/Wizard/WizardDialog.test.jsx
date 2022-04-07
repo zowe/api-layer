ce2d8fe5da9b2a6fb64483fb716b567315a6ca87
Andrea Tabone <andrea.tabone@broadcom.com>
2022-04-07 15:01:20 +0200

    Add unit test
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- api-catalog-ui/frontend/src/components/Wizard/WizardDialog.test.jsx
+++ api-catalog-ui/frontend/src/components/Wizard/WizardDialog.test.jsx
@@ -253,4 +253,23 @@ describe('>>> WizardDialog tests', () => {
         expect(screen.getByText('Choose File')).toBeInTheDocument();
         expect(screen.getByText('Or fill the fields:')).toBeInTheDocument();
     });
+    it('should fill inputs', () => {
+        const convertedCategoryData =  Object.keys(categoryData);
+
+        const validateInput = jest.fn();
+        const updateWizardData = jest.fn();
+        const wrapper = enzyme.shallow(
+            <WizardDialog
+                wizardToggleDisplay={jest.fn()}
+                updateWizardData={updateWizardData}
+                inputData={convertedCategoryData}
+                navsObj={{ Basics: { 'Basic info': [['key']], silent: true } }}
+                wizardIsOpen
+                validateInput={validateInput}
+            />
+        );
+        const instance = wrapper.instance();
+        instance.fillInputs(categoryData[0]);
+        expect(updateWizardData).toHaveBeenCalled();
+    });
 });
