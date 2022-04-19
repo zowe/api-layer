86272ad089093b554a30ec15459a8a8442bf5279
Huda Mukhtar <hudamukhtar1@gmail.com>
2022-04-18 23:27:35 -0400

    completed embedded https metrics dashboard Signed-off-by: Huda Mukhtar <hudamukhtar1@gmail.com>

--- metrics-service-ui/frontend/src/components/Iframe/IFRAME.jsx
+++ metrics-service-ui/frontend/src/components/Iframe/IFRAME.jsx
@@ -17,27 +17,75 @@ export function IFRAME() {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            padding: '2rem',
         }}>
             <div       dangerouslySetInnerHTML={{
-                 __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649169576404&to=1649169876405&panelId=10' width='450' height='200' />",
+                 __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649169576404&to=1649169876405&panelId=10' width='450' height='300' />",
+            }} />
+
+
+            <div style={{
+                display: 'grid',
+            }}>
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649178335450&to=1649178635450&panelId=12' width='250' height='100' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649178527343&to=1649178827343&panelId=13' width='250' height='100' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815030049&to=1649815330049&panelId=16' width='250' height='100' />",
+                }} />
+            </div>
+
+            <div style={{
+                display: 'grid',
+            }}>
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815715083&to=1649816015083&panelId=14'  width='250' height='100' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815748843&to=1649816048843&panelId=15'  width='250' height='100' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815777647&to=1649816077647&panelId=17' width='250' height='100' />",
+                }} />
+            </div>
+
+            <div       dangerouslySetInnerHTML={{
+                __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815800739&to=1649816100739&panelId=24'  width='450' height='300' />",
             }} />
             <div       dangerouslySetInnerHTML={{
-                __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649178335450&to=1649178635450&panelId=12' width='250' height='100' />",
+                __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815829688&to=1649816129688&panelId=19'  width='450' height='300' />",
             }} />
-            <div       dangerouslySetInnerHTML={{
-                __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649178445869&to=1649178745869&panelId=14' width='250' height='100' />",
-            }} />
-            <div       dangerouslySetInnerHTML={{
-                __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649178527343&to=1649178827343&panelId=13' width='450' height='200' />",
-            }} />
+
         </div>
             <div style={{
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
+                padding: '2rem',
             }}>
                 <div       dangerouslySetInnerHTML={{
-                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649179375601&to=1649179675601&panelId=24' width='450' height='200' />",
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815865502&to=1649816165502&panelId=22'  width='250' height='200' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815895850&to=1649816195850&panelId=21'  width='250' height='200' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815919231&to=1649816219231&panelId=23'  width='250' height='200' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815945641&to=1649816245641&panelId=25'  width='250' height='200' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815966434&to=1649816266434&panelId=26'  width='250' height='200' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649815988200&to=1649816288200&panelId=27'  width='250' height='200' />",
+                }} />
+                <div       dangerouslySetInnerHTML={{
+                    __html: "<iframe src='http://localhost:8080/d-solo/JyDukHP7k/apiml-metrics-service-http?orgId=1&from=1649816014874&to=1649816314874&panelId=28'  width='250' height='200' />",
                 }} />
             </div>
 
