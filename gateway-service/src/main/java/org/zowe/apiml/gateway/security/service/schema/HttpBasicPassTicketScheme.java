279f9aabe7ffaf0fba3af2dbf513a22d8c8a6bfe
Amanda D'Errico <amanda.derrico@ibm.com>
2022-04-19 19:34:13 -0400

    updated HttpBasicPassTicketSchemeTest to reflect new PassTicketCommand containing error messages
    Signed-off-by: Amanda D'Errico <amanda.derrico@ibm.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/HttpBasicPassTicketScheme.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/HttpBasicPassTicketScheme.java
@@ -11,8 +11,11 @@ package org.zowe.apiml.gateway.security.service.schema;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.zuul.context.RequestContext;
+
+import java.security.cert.CertificateEncodingException;
 import java.util.Optional;
 import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.apache.http.Header;
 import org.apache.http.HttpHeaders;
@@ -22,12 +25,17 @@ import org.springframework.stereotype.Component;
 import org.zowe.apiml.gateway.security.service.PassTicketException;
 import org.zowe.apiml.gateway.security.service.schema.source.AuthSource;
 import org.zowe.apiml.gateway.security.service.schema.source.AuthSourceService;
+import org.zowe.apiml.gateway.security.service.schema.source.UserNotMappedException;
 import org.zowe.apiml.gateway.security.service.schema.source.X509AuthSource;
+import org.zowe.apiml.message.core.MessageService;
 import org.zowe.apiml.passticket.IRRPassTicketGenerationException;
 import org.zowe.apiml.passticket.PassTicketService;
 import org.zowe.apiml.auth.Authentication;
 import org.zowe.apiml.auth.AuthenticationScheme;
 import org.zowe.apiml.security.common.config.AuthConfigurationProperties;
+import org.zowe.apiml.security.common.error.InvalidCertificateException;
+import org.zowe.apiml.security.common.token.TokenExpireException;
+import org.zowe.apiml.security.common.token.TokenNotValidException;
 import org.zowe.apiml.util.CookieUtil;
 
 import java.nio.charset.StandardCharsets;
@@ -44,17 +52,20 @@ public class HttpBasicPassTicketScheme implements IAuthenticationScheme {
     private final AuthSourceService authSourceService;
     private final AuthConfigurationProperties authConfigurationProperties;
     private final String cookieName;
+    private final MessageService messageService;
     public static final String AUTH_FAIL_HEADER = "X-Zowe-Auth-Failure";
 
     public HttpBasicPassTicketScheme(
         PassTicketService passTicketService,
         AuthSourceService authSourceService,
-        AuthConfigurationProperties authConfigurationProperties
+        AuthConfigurationProperties authConfigurationProperties,
+        MessageService messageService
     ) {
         this.passTicketService = passTicketService;
         this.authSourceService = authSourceService;
         this.authConfigurationProperties = authConfigurationProperties;
         cookieName = authConfigurationProperties.getCookieProperties().getCookieName();
+        this.messageService = messageService;
     }
 
     @Override
@@ -69,27 +80,50 @@ public class HttpBasicPassTicketScheme implements IAuthenticationScheme {
         if (context.containsKey(AUTH_FAIL_HEADER)) {
             String errorHeaderValue = context.get(AUTH_FAIL_HEADER).toString();
             // this command should expire immediately after creation because it is build based on missing/incorrect authentication
-            return new PassTicketCommand(System.currentTimeMillis(), errorHeaderValue);
+            return new PassTicketCommand(cookieName, System.currentTimeMillis(), errorHeaderValue);
         }
 
         final long before = System.currentTimeMillis();
 
-        final AuthSource.Parsed parsedAuthSource = authSourceService.parse(authSource);
+        AuthSource.Parsed parsedAuthSource = null;
+        String error = null;
+        try {
+            parsedAuthSource = authSourceService.parse(authSource);
+        } catch (InvalidCertificateException e) {
+            error = e.getMessage();
+        } catch (TokenNotValidException e) {
+            error = this.messageService.createMessage("org.zowe.apiml.gateway.security.invalidToken").mapToLogMessage();
+        } catch (TokenExpireException e) {
+            error = this.messageService.createMessage("org.zowe.apiml.gateway.security.expiredToken").mapToLogMessage();
+        }
 
-        if (authSource == null || parsedAuthSource == null) {
-            return AuthenticationCommand.EMPTY;
+        if (authSource == null || authSource.getRawSource() == null) { // auth source null
+            error = this.messageService.createMessage("org.zowe.apiml.gateway.security.schema.missingAuthentication").mapToLogMessage();
+        }
+        else if (parsedAuthSource == null) { // invalid authSource - can be due to
+            error = this.messageService.createMessage("org.zowe.apiml.gateway.security.scheme.x509ParsingError", "Cannot parse provided authentication source").mapToLogMessage();
+        }
+        else if (parsedAuthSource.getUserId() == null) {
+            error = this.messageService.createMessage("org.zowe.apiml.gateway.security.schema.x509.mappingFailed").mapToLogMessage();
+        }
+
+        if (error != null) { // catching all exceptions relating to parsing all AuthSourceService types and auth source null errors, can apply command to set X-Zowe-Auth-Failure header
+            return new PassTicketCommand(cookieName, null, error);
         }
 
         final String applId = authentication.getApplid();
         final String userId = parsedAuthSource.getUserId();
-        String passTicket; // should be able to generate passticket for a user authenticated with cient certificate instead of with userid with token
+        String passTicket = null; // should be able to generate passticket for a user authenticated with cient certificate instead of with userid with token
         try {
             passTicket = passTicketService.generate(userId, applId); //edit generate to have user authenticated with clietn certificate, should not just get the userid
         } catch (IRRPassTicketGenerationException e) { // change to PassTicketCOmmand with auth fail header
-            throw new PassTicketException(
-                String.format("Could not generate PassTicket for user ID %s and APPLID %s", userId, applId), e
-            );
+            error = String.format("Could not generate PassTicket for user ID %s and APPLID %s", userId, applId);
         }
+
+        if (error != null) { // catching all exceptions relating to parsing all AuthSourceService types and auth source null errors, can apply command to set X-Zowe-Auth-Failure header
+            return new PassTicketCommand(cookieName, null, error);
+        }
+
         final String encoded = Base64.getEncoder()
             .encodeToString((userId + ":" + passTicket).getBytes(StandardCharsets.UTF_8));
         final String value = "Basic " + encoded;
@@ -97,7 +131,7 @@ public class HttpBasicPassTicketScheme implements IAuthenticationScheme {
         final long expiredAt = Math.min(before + authConfigurationProperties.getPassTicket().getTimeout() * 1000,
             parsedAuthSource.getExpiration().getTime());
 
-        return new PassTicketCommand(value, cookieName, expiredAt, null);
+        return new PassTicketCommand(value, cookieName, expiredAt, error);
     }
 
     @Override
@@ -107,7 +141,8 @@ public class HttpBasicPassTicketScheme implements IAuthenticationScheme {
 
     @Value
     @EqualsAndHashCode(callSuper = false)
-    public class PassTicketCommand extends AuthenticationCommand {
+    @RequiredArgsConstructor
+    public static class PassTicketCommand extends AuthenticationCommand {
 
         private static final long serialVersionUID = 3941300386857998443L;
 
@@ -115,50 +150,57 @@ public class HttpBasicPassTicketScheme implements IAuthenticationScheme {
 
         String authorizationValue;
         String cookieName;
-        long expireAt;
+        Long expireAt;
         String errorValue;
 
-        public PassTicketCommand(String authorizationValue, String cookieName, long expireAt, String errorValue) {
-            this.authorizationValue = authorizationValue;
+        public PassTicketCommand(String cookieName, Long expireAt, String errorValue) {
+            this.authorizationValue = null;
             this.cookieName = cookieName;
             this.expireAt = expireAt;
             this.errorValue = errorValue;
         }
 
-        public PassTicketCommand(long expireAt, String errorValue) {
-            this.authorizationValue = null;
-            this.cookieName = null;
-            this.expireAt = expireAt;
-            this.errorValue = errorValue;
-        }
-
+        // EDIT HERE
 
         @Override
         public void apply(InstanceInfo instanceInfo) {
             final RequestContext context = RequestContext.getCurrentContext();
-            context.addZuulRequestHeader(HttpHeaders.AUTHORIZATION, authorizationValue);
-            context.addZuulRequestHeader(COOKIE_HEADER,
-                CookieUtil.removeCookie(
-                    context.getZuulRequestHeaders().get(COOKIE_HEADER),
-                    cookieName
-                )
-            );
-        }
-
-        @Override
-        public void applyToRequest(HttpRequest request) {
-            request.setHeader(
-                new BasicHeader(HttpHeaders.AUTHORIZATION, authorizationValue)
-            );
-            Header header = request.getFirstHeader(COOKIE_HEADER);
-            if (header != null) {
-                request.setHeader(COOKIE_HEADER,
+            if (authorizationValue != null) {
+                context.addZuulRequestHeader(HttpHeaders.AUTHORIZATION, authorizationValue);
+                context.addZuulRequestHeader(COOKIE_HEADER,
                     CookieUtil.removeCookie(
-                        header.getValue(),
+                        context.getZuulRequestHeaders().get(COOKIE_HEADER),
                         cookieName
                     )
                 );
             }
+            else {
+                JwtCommand.setErrorHeader(context, errorValue);
+            }
+        }
+
+        @Override
+        public void applyToRequest(HttpRequest request) {
+
+            if (authorizationValue != null) {
+                request.setHeader(
+                    new BasicHeader(HttpHeaders.AUTHORIZATION, authorizationValue)
+                );
+                Header header = request.getFirstHeader(COOKIE_HEADER);
+                if (header != null) {
+                    request.setHeader(COOKIE_HEADER,
+                        CookieUtil.removeCookie(
+                            header.getValue(),
+                            cookieName
+                        )
+                    );
+                }
+
+            }
+            else {
+                request.addHeader(AUTH_FAIL_HEADER, errorValue);
+            }
+
         }
 
         @Override
