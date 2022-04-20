7014712b108852128f9c7205e38fd75b6f0e9d0a
wanger26 <jwanger@uwo.ca>
2022-04-20 12:20:45 -0400

    Fixed license build issues for metrics service
    Signed-off-by: wanger26 <jwanger@uwo.ca>

--- metrics-service/src/test/java/org/zowe/apiml/metrics/services/ZebraMetricsServiceTest.java
+++ metrics-service/src/test/java/org/zowe/apiml/metrics/services/ZebraMetricsServiceTest.java
@@ -1,3 +1,12 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package org.zowe.apiml.metrics.services;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -27,7 +36,7 @@ public class ZebraMetricsServiceTest {
     }
 
     @Test
-    public void TC01_validLparAndReport_getRmfData_returnMetricsWithTransformedTimestamps() {
+    public void validLparAndReport_getRmfData_returnMetricsWithTransformedTimestamps() {
 
         // MOCK INPUT
         String lpar = "RPRT";
