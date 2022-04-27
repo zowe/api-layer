a49c7c075eed4049a42e06f07e042cc491b8f9c0
Carson Cook <CarsonCook@users.noreply.github.com>
2022-04-27 14:08:27 -0400

    feat: Create endpoint to retrieve default API doc for service (#2327)
    * Add unit tests for api doc controller
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add default api doc endpoint
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Add integration test for default api doc endpoint
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Return 404 when api doc not found
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix integration test for default apidoc
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix typo
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix imports
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix apidoc path for auth tests
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix unit test
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix api catalog auth integration tests
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/controllers/api/CatalogApiDocController.java
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/controllers/api/CatalogApiDocController.java
@@ -10,7 +10,6 @@
 package org.zowe.apiml.apicatalog.controllers.api;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.zowe.apiml.apicatalog.services.status.APIServiceStatusService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -19,7 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.zowe.apiml.apicatalog.services.status.model.ApiDocNotFoundException;
+import org.zowe.apiml.apicatalog.services.status.APIServiceStatusService;
 
 /**
  * Main API for handling requests from the API Catalog UI, routed through the gateway
@@ -45,8 +44,8 @@ public class CatalogApiDocController {
     /**
      * Retrieve the api-doc info for this service
      *
-     * @param serviceId  the eureka id
-     * @param apiId the version of the api
+     * @param serviceId the eureka id
+     * @param apiId     the version of the api
      * @return api-doc info (as JSON)
      */
     @GetMapping(value = "/{serviceId}/{apiId}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -70,11 +69,34 @@ public class CatalogApiDocController {
         @PathVariable(value = "serviceId") String serviceId,
         @ApiParam(name = "apiId", value = "The API ID and version, separated by a space, of the API documentation", required = true, example = "zowe.apiml.apicatalog v1.0.0")
         @PathVariable(value = "apiId") String apiId) {
-        try {
-            return this.apiServiceStatusService.getServiceCachedApiDocInfo(serviceId, apiId);
-        } catch (ApiDocNotFoundException e) {
-            return this.apiServiceStatusService.getServiceCachedApiDocInfo(serviceId, null);
-        }
+        return this.apiServiceStatusService.getServiceCachedApiDocInfo(serviceId, apiId);
+    }
+
+    /**
+     * Retrieve the api-doc info for this service's default API
+     *
+     * @param serviceId the eureka id
+     * @return api-doc info (as JSON)
+     */
+    @GetMapping(value = "/{serviceId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Retrieves the API documentation for the default service version",
+        notes = "Returns the API documentation for a specific service {serviceId} and its default version.",
+        authorizations = {
+            @Authorization("LoginBasicAuth"), @Authorization("CookieAuth")
+        },
+        response = String.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 401, message = "Unauthorized"),
+        @ApiResponse(code = 403, message = "Forbidden"),
+        @ApiResponse(code = 404, message = "URI not found"),
+        @ApiResponse(code = 500, message = "An unexpected condition occurred"),
+    })
+    @HystrixCommand
+    public ResponseEntity<String> getDefaultApiDocInfo(
+        @ApiParam(name = "serviceId", value = "The unique identifier of the registered service", required = true, example = "apicatalog")
+        @PathVariable(value = "serviceId") String serviceId) {
+        return this.apiServiceStatusService.getServiceCachedDefaultApiDocInfo(serviceId);
     }
 
     @GetMapping(value = "/{serviceId}/{apiId1}/{apiId2}", produces = MediaType.TEXT_HTML_VALUE)
