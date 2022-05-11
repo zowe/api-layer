0a3f1011b36a92e5624e12e09a7d0047f2f43cca
Amanda D'Errico <amanda.derrico@ibm.com>
2022-05-11 11:50:15 -0400

    python enabler eureka client imported
    Signed-off-by: Amanda D'Errico <amanda.derrico@ibm.com>

--- onboarding-enabler-python/test.py
+++ onboarding-enabler-python/test.py
@@ -1,2 +1,13 @@
 import py_eureka_client.eureka_client as eureka_client
 
+
+def name():
+    print("hello world")
+
+def registerService():
+    # //eureka_client.init()
+
+
+if __name__ == "__main__":
+    name()
+
