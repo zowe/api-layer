77d191ec1c253e877376aa8982172170ab6e3b51
Carson Cook <carson.cook@ibm.com>
2022-05-17 11:29:10 -0400

    Add Zuul config for error controllers
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/error/controllers/InternalServerErrorController.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/error/controllers/InternalServerErrorController.java
@@ -7,11 +7,10 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
-package org.zowe.apiml.gateway.error;
+package org.zowe.apiml.gateway.error.controllers;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -19,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.zowe.apiml.gateway.error.ErrorUtils;
 import org.zowe.apiml.gateway.error.check.*;
 import org.zowe.apiml.message.api.ApiMessageView;
 import org.zowe.apiml.message.core.Message;
@@ -34,7 +34,7 @@ import java.util.List;
 @Controller
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Primary
-public class InternalServerErrorController implements ErrorController {
+public class InternalServerErrorController extends AbstractGatewayErrorController {
     public static final String ERROR_ENDPOINT = "/internal_error";
 
     private final MessageService messageService;
