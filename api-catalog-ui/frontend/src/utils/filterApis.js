be2cfed671a66c3878acce2a281c227beda359d4
Andrea Tabone <andrea.tabone@broadcom.com>
2022-05-30 14:30:05 +0200

    refactoring and test
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- api-catalog-ui/frontend/src/utils/filterApis.js
+++ api-catalog-ui/frontend/src/utils/filterApis.js
@@ -7,6 +7,44 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
+
+/**
+ * Extend the filter by allowing to search not only by tags, but also by description and summary.
+ * The filter is also case-insensitive.
+ * @param phrase the search input
+ * @param taggedOps the API doc
+ * @param system the system
+ * @returns {*} the filtered API operation
+ */
+export function extendFilter(phrase, taggedOps, system) {
+    // eslint-disable-next-line no-param-reassign
+    phrase = phrase.toLowerCase();
+    const normalTaggedOps = JSON.parse(JSON.stringify(taggedOps));
+    Object.keys(normalTaggedOps).forEach((tagObj) => {
+        const { operations } = normalTaggedOps[tagObj];
+        let i = operations.length;
+        // eslint-disable-next-line no-plusplus
+        while (i--) {
+            const { operation } = operations[i];
+            if (
+                operations[i].path.toLowerCase().indexOf(phrase) === -1 &&
+                operation.summary !== undefined &&
+                operation.description !== undefined &&
+                operation.summary.toLowerCase().indexOf(phrase) === -1 &&
+                operation.description.toLowerCase().indexOf(phrase) === -1
+            ) {
+                operations.splice(i, 1);
+            }
+        }
+        if (operations.length === 0) {
+            delete normalTaggedOps[tagObj];
+        } else {
+            normalTaggedOps[tagObj].operations = operations;
+        }
+    });
+    return system.Im.fromJS(normalTaggedOps);
+}
+
 /**
  * Custom Plugin which extends the SwaggerUI filter functionality to filter APIs by tag, summary and description
  */
@@ -14,34 +52,7 @@
 export const AdvancedFilterPlugin = function (system) {
     return {
         fn: {
-            opsFilter: (taggedOps, phrase) => {
-                // eslint-disable-next-line no-param-reassign
-                phrase = phrase.toLowerCase();
-                const normalTaggedOps = JSON.parse(JSON.stringify(taggedOps));
-                Object.keys(normalTaggedOps).forEach((tagObj) => {
-                    const { operations } = normalTaggedOps[tagObj];
-                    let i = operations.length;
-                    // eslint-disable-next-line no-plusplus
-                    while (i--) {
-                        const { operation } = operations[i];
-                        if (
-                            operations[i].path.toLowerCase().indexOf(phrase) === -1 &&
-                            operation.summary !== undefined &&
-                            operation.description !== undefined &&
-                            operation.summary.toLowerCase().indexOf(phrase) === -1 &&
-                            operation.description.toLowerCase().indexOf(phrase) === -1
-                        ) {
-                            operations.splice(i, 1);
-                        }
-                    }
-                    if (operations.length === 0) {
-                        delete normalTaggedOps[tagObj];
-                    } else {
-                        normalTaggedOps[tagObj].operations = operations;
-                    }
-                });
-                return system.Im.fromJS(normalTaggedOps);
-            },
+            opsFilter: (taggedOps, phrase) => extendFilter(phrase, taggedOps, system),
         },
     };
 };
