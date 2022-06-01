178f2c86d74447218232450ec45829ddf0240977
achmelo <a.chmelo@gmail.com>
2022-06-01 10:23:33 +0200

    commit correct class
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/staticapi/StaticAPIServiceTest.java
+++ api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/staticapi/StaticAPIServiceTest.java
@@ -9,19 +9,27 @@
  */
 package org.zowe.apiml.apicatalog.staticapi;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpStatus;
 import org.zowe.apiml.apicatalog.discovery.DiscoveryConfigProperties;
 
-import java.util.Base64;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,8 +39,8 @@ class StaticAPIServiceTest {
 
     private static final String DISCOVERY_LOCATION = "https://localhost:60004/eureka/";
     private static final String DISCOVERY_LOCATION_2 = "https://localhost:60005/eureka/";
+    private static final String DISCOVERY_LOCATION_3 = "https://localhost:60006/eureka/";
     private static final String DISCOVERY_URL = "https://localhost:60004/";
-    private static final String DISCOVERY_URL_2 = "https://localhost:60005/";
 
     private static final String DISCOVERY_LOCATION_HTTP = "http://localhost:60004/eureka/";
     private static final String DISCOVERY_URL_HTTP = "http://localhost:60004/";
@@ -41,75 +49,114 @@ class StaticAPIServiceTest {
     private StaticAPIService staticAPIService;
 
     @Mock
-    private RestTemplate restTemplate;
+    private CloseableHttpClient httpClient;
+    @Mock
+    private CloseableHttpResponse okResponse;
+    @Mock
+    private CloseableHttpResponse notFoundResponse;
+    @Mock
+    private StatusLine okStatusLine;
+    @Mock
+    private StatusLine notFoundStatusLine;
+    @Mock
+    private HttpEntity entity;
+
     @Mock
     private DiscoveryConfigProperties discoveryConfigProperties;
 
+    private final String[] discoveryLocations = {DISCOVERY_LOCATION, DISCOVERY_LOCATION_2};
+    private static final String BODY = "This is body";
     @Nested
-    class WhenRefreshEndpointPresentsResponse {
+    class WhenRefreshEndpointPresentsResponseTest {
 
-        @Test
-        void givenRefreshAPIWithSecureDiscoveryService_thenReturnApiResponseCodeWithBody() {
-            when(discoveryConfigProperties.getLocations()).thenReturn(new String[]{DISCOVERY_LOCATION});
+        @Nested
+        class GivenSingleUrlTest {
 
-            mockRestTemplateExchange(DISCOVERY_URL, new ResponseEntity<>("This is body", HttpStatus.OK));
+            @BeforeEach
+            void setup()throws IOException{
 
-            StaticAPIResponse actualResponse = staticAPIService.refresh();
-            StaticAPIResponse expectedResponse = new StaticAPIResponse(200, "This is body");
-            assertEquals(expectedResponse, actualResponse);
-        }
+                when(okResponse.getStatusLine()).thenReturn(okStatusLine);
+                when(okStatusLine.getStatusCode()).thenReturn(HttpStatus.OK.value());
 
-        @Test
-        void givenRefreshAPIWithUnSecureDiscoveryService_thenReturnApiResponseCodeWithBody() {
-            when(discoveryConfigProperties.getLocations()).thenReturn(new String[]{DISCOVERY_LOCATION_HTTP});
+                when(okResponse.getEntity()).thenReturn(entity);
+                when(entity.getContent()).thenReturn(new ByteArrayInputStream(BODY.getBytes()));
+            }
 
-            mockRestTemplateExchange(DISCOVERY_URL_HTTP, new ResponseEntity<>("This is body", HttpStatus.OK));
+            @Test
+            void givenRefreshAPIWithSecureDiscoveryService_thenReturnApiResponseCodeWithBody() throws IOException {
 
-            StaticAPIResponse actualResponse = staticAPIService.refresh();
-            StaticAPIResponse expectedResponse = new StaticAPIResponse(200, "This is body");
-            assertEquals(expectedResponse, actualResponse);
+                when(discoveryConfigProperties.getLocations()).thenReturn(new String[]{DISCOVERY_LOCATION});
+                mockRestTemplateExchange(DISCOVERY_URL);
+
+                StaticAPIResponse actualResponse = staticAPIService.refresh();
+                StaticAPIResponse expectedResponse = new StaticAPIResponse(200, BODY);
+                assertEquals(expectedResponse, actualResponse);
+            }
+
+            @Test
+            void givenRefreshAPIWithUnSecureDiscoveryService_thenReturnApiResponseCodeWithBody() throws IOException {
+                when(discoveryConfigProperties.getLocations()).thenReturn(new String[]{DISCOVERY_LOCATION_HTTP});
+
+                mockRestTemplateExchange(DISCOVERY_URL_HTTP);
+                StaticAPIResponse actualResponse = staticAPIService.refresh();
+                StaticAPIResponse expectedResponse = new StaticAPIResponse(200, BODY);
+                assertEquals(expectedResponse, actualResponse);
+            }
         }
 
         @Nested
-        class GivenTwoDiscoveryUrls {
-            private final String[] discoveryLocations = {DISCOVERY_LOCATION, DISCOVERY_LOCATION_2};
+        class GivenTwoDiscoveryUrlsTest {
+            @Nested
+            class WhenOneSucceedsTest{
+                @BeforeEach
+                void setup()throws IOException{
 
-            @Test
-            void whenFirstFails_thenReturnResponseFromSecond() {
-                when(discoveryConfigProperties.getLocations()).thenReturn(discoveryLocations);
+                    when(okResponse.getStatusLine()).thenReturn(okStatusLine);
+                    when(okStatusLine.getStatusCode()).thenReturn(HttpStatus.OK.value());
 
-                mockRestTemplateExchange(DISCOVERY_URL, new ResponseEntity<>(HttpStatus.NOT_FOUND));
-                mockRestTemplateExchange(DISCOVERY_URL_2, new ResponseEntity<>("body", HttpStatus.OK));
+                    when(okResponse.getEntity()).thenReturn(entity);
+                    when(entity.getContent()).thenReturn(new ByteArrayInputStream(BODY.getBytes()));
+                }
+                @Test
+                void whenFirstSucceeds_thenReturnResponseFromFirst() throws IOException{
+                    when(discoveryConfigProperties.getLocations()).thenReturn(discoveryLocations);
+                    mockRestTemplateExchange(DISCOVERY_LOCATION);
+                    StaticAPIResponse actualResponse = staticAPIService.refresh();
+                    StaticAPIResponse expectedResponse = new StaticAPIResponse(200, BODY);
+                    assertEquals(expectedResponse, actualResponse);
+                }
 
-                StaticAPIResponse actualResponse = staticAPIService.refresh();
-                StaticAPIResponse expectedResponse = new StaticAPIResponse(200, "body");
-                assertEquals(expectedResponse, actualResponse);
+                @Test
+                void whenFirstFails_thenReturnResponseFromSecond() throws IOException{
+                    when(discoveryConfigProperties.getLocations()).thenReturn(discoveryLocations);
+                    when(notFoundResponse.getStatusLine()).thenReturn(notFoundStatusLine);
+                    when(notFoundStatusLine.getStatusCode()).thenReturn(HttpStatus.NOT_FOUND.value());
+                    mockRestTemplateExchange(DISCOVERY_LOCATION_2);
+                    StaticAPIResponse actualResponse = staticAPIService.refresh();
+                    StaticAPIResponse expectedResponse = new StaticAPIResponse(200, BODY);
+                    assertEquals(expectedResponse, actualResponse);
+                }
             }
+            @Nested
+            class WhenBothFailsTest{
+                @Test
+                void whenBothFail_thenReturnResponseFromSecond() throws IOException{
+                    when(discoveryConfigProperties.getLocations()).thenReturn(discoveryLocations);
+                    when(notFoundResponse.getStatusLine()).thenReturn(notFoundStatusLine);
+                    when(notFoundStatusLine.getStatusCode()).thenReturn(HttpStatus.NOT_FOUND.value());
+                    when(notFoundResponse.getEntity()).thenReturn(entity);
+                    when(entity.getContent()).thenReturn(new ByteArrayInputStream(BODY.getBytes()));
+                    mockRestTemplateExchange(DISCOVERY_LOCATION_3);
 
-            @Test
-            void whenFirstSucceeds_thenReturnResponseFromFirst() {
-                when(discoveryConfigProperties.getLocations()).thenReturn(discoveryLocations);
-
-                mockRestTemplateExchange(DISCOVERY_URL, new ResponseEntity<>("body", HttpStatus.OK));
-
-                StaticAPIResponse actualResponse = staticAPIService.refresh();
-                StaticAPIResponse expectedResponse = new StaticAPIResponse(200, "body");
-                assertEquals(expectedResponse, actualResponse);
-            }
-
-            @Test
-            void whenBothFail_thenReturnResponseFromSecond() {
-                when(discoveryConfigProperties.getLocations()).thenReturn(discoveryLocations);
-
-                mockRestTemplateExchange(DISCOVERY_URL, new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
-                mockRestTemplateExchange(DISCOVERY_URL_2, new ResponseEntity<>("body", HttpStatus.NOT_FOUND));
-
-                StaticAPIResponse actualResponse = staticAPIService.refresh();
-                StaticAPIResponse expectedResponse = new StaticAPIResponse(404, "body");
-                assertEquals(expectedResponse, actualResponse);
+                    StaticAPIResponse actualResponse = staticAPIService.refresh();
+                    StaticAPIResponse expectedResponse = new StaticAPIResponse(404, BODY);
+                    assertEquals(expectedResponse, actualResponse);
+                }
             }
         }
     }
+
+
 
     @Test
     void givenNoDiscoveryLocations_whenAttemptRefresh_thenReturn500() {
@@ -120,22 +167,18 @@ class StaticAPIServiceTest {
         assertEquals(expectedResponse, actualResponse);
     }
 
-    private void mockRestTemplateExchange(String discoveryUrl, ResponseEntity expectedResponse) {
-        when(restTemplate.exchange(
-            discoveryUrl + REFRESH_ENDPOINT,
-            HttpMethod.POST, getHttpEntity(discoveryUrl), String.class
-        )).thenReturn(expectedResponse);
-    }
+    private void mockRestTemplateExchange(String discoveryUrl) throws IOException {
+        HttpPost post = new HttpPost(discoveryUrl.replace("/eureka", "") + REFRESH_ENDPOINT);
 
-    private HttpEntity<?> getHttpEntity(String discoveryServiceUrl) {
-        boolean isHttp = discoveryServiceUrl.startsWith("http://");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Accept", "application/json");
-        if (isHttp) {
-            String basicToken = "Basic " + Base64.getEncoder().encodeToString("null:null".getBytes());
-            httpHeaders.add("Authorization", basicToken);
-        }
-
-        return new HttpEntity<>(null, httpHeaders);
+        when(httpClient.execute(any())).thenAnswer((invocation)-> {
+           HttpPost httpRequest = (HttpPost) invocation.getArguments()[0];
+           URI uri = httpRequest.getURI();
+           int i = uri.compareTo(post.getURI());
+           if(i == 0){
+               return okResponse;
+           } else {
+               return notFoundResponse;
+           }
+       });
     }
 }
