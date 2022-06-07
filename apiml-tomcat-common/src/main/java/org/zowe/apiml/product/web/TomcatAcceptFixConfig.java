35b493a596e4eeeac129e4451b1b73d7d13277b3
Pavel Jareš <pavel.jares@broadcom.com>
2022-06-07 12:55:53 +0200

    fix
    Signed-off-by: Pavel Jareš <pavel.jares@broadcom.com>

---
+++ apiml-tomcat-common/src/main/java/org/zowe/apiml/product/web/TomcatAcceptFixConfig.java
@@ -0,0 +1,261 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.product.web;
+
+import lombok.experimental.Delegate;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.LifecycleState;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.AbstractProtocol;
+import org.apache.coyote.ProtocolHandler;
+import org.apache.tomcat.util.net.AbstractEndpoint;
+import org.apache.tomcat.util.net.NioEndpoint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.SocketAddress;
+import java.nio.channels.*;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * This class extends embedded Tomcat to handle a case with restarting TCP/IP stack. Tomcat itself is not able
+ * recovery from this situation. It tries accepting new request, and it fails until the service is restarted.
+ *
+ * This handler detects this issue and try to rebind the port. It can also wait till the TCP/IP stack is available again.
+ *
+ * This bean is related only to z/OS.
+ */
+@Slf4j
+@Configuration
+public class TomcatAcceptFixConfig {
+
+    @Value("${server.tomcat.retryRebindTimeoutSecs:10}")
+    int retryRebindTimeoutSecs;
+
+    private static final Field endpointField;
+    private static final Field nioSocketField;
+
+    /**
+     * To mitigate parallel treatment of socket rebinding
+     */
+    private static final AtomicBoolean running = new AtomicBoolean(true);
+
+    static {
+        try {
+            endpointField = AbstractProtocol.class.getDeclaredField("endpoint");
+            nioSocketField = NioEndpoint.class.getDeclaredField("serverSock");
+        } catch (NoSuchFieldException e) {
+            throw new IllegalStateException("Unknown structure of protocols", e);
+        }
+        endpointField.setAccessible(true); // NOSONAR
+        nioSocketField.setAccessible(true); // NOSONAR
+    }
+
+    /**
+     * Update Protocol class of Tomcat. For supported protocols (Nio) it will replace endpoint implementation
+     * with the wrapper class providing the fix - handling of restart of TCP/IP stack.
+     *
+     * @param abstractProtocol instance to update
+     * @param rebindHandler call-back (to next update after TCP/IP restart)
+     */
+    private void update(AbstractProtocol<?> abstractProtocol, Runnable rebindHandler) {
+        try {
+            AbstractEndpoint<?, ?> abstractEndpoint = (AbstractEndpoint<Object, Object>) endpointField.get(abstractProtocol);
+
+            if (abstractEndpoint instanceof NioEndpoint) {
+                ServerSocketChannel serverSocketChannel = (ServerSocketChannel) nioSocketField.get(abstractEndpoint);
+                serverSocketChannel = new FixedServerSocketChannel(serverSocketChannel, abstractEndpoint, rebindHandler);
+                nioSocketField.set(abstractEndpoint, serverSocketChannel); // NOSONAR
+            } else {
+                log.warn("Unsupported protocol: {}", abstractEndpoint.getClass().getName());
+            }
+        } catch (Exception e) {
+            log.warn("Cannot update connector to handle errors on socket accepting", e);
+        }
+    }
+
+    /**
+     * Update protocol inside the protocol. This method is on the highest level to allow updating each required part.
+     *
+     * @param connector connector to update
+     *
+     * Note: If protocol of connector is not supported no change happened.
+     */
+    private void update(Connector connector) {
+        ProtocolHandler protocolHandler = connector.getProtocolHandler();
+        if (protocolHandler instanceof AbstractProtocol) {
+            update((AbstractProtocol<?>) protocolHandler, () -> update(connector));
+        } else {
+            log.warn("Unsupported protocol handler: {}", protocolHandler.getClass().getName());
+        }
+    }
+
+    /**
+     * @return customizer class for Embedded Tomcat to extend connectors. It will restart server socket in the case
+     * of TCP/IP stack restart
+     */
+    @Bean
+    public TomcatConnectorCustomizer tomcatAcceptorFix() {
+        return connector -> connector.addLifecycleListener(event -> {
+            if (event.getLifecycle().getState() == LifecycleState.STARTED) {
+                update(connector);
+            }
+        });
+    }
+
+    /**
+     * Detection of service stopping. It will unblock threads to successful shutdown.
+     */
+    @PreDestroy
+    public void stopping() {
+        running.set(false);
+    }
+
+    /**
+     * Socket implemtation wrapper to handle rebinding on TCP Stack restart
+     */
+    private class FixedServerSocketChannel extends ServerSocketChannel {
+
+        /**
+         * Wrapper server socket inside
+         */
+        @Delegate(excludes = Overridden.class)
+        private final ServerSocketChannel socket;
+
+        /**
+         * The endpoint instance used by Server socket
+         */
+        private final AbstractEndpoint<?, ?> abstractEndpoint;
+
+        /**
+         * Define the generation of binding, each rebinding the value is increased
+         */
+        private final AtomicInteger state = new AtomicInteger();
+
+        /**
+         * Handler to call after successful rebind of server socket
+         */
+        private final Runnable rebindHandler;
+
+        private FixedServerSocketChannel(ServerSocketChannel socket, AbstractEndpoint<?, ?> abstractEndpoint, Runnable rebindHandler) {
+            super(socket.provider());
+            this.socket = socket;
+            this.abstractEndpoint = abstractEndpoint;
+            this.rebindHandler = rebindHandler;
+        }
+
+        @Override
+        protected void implCloseSelectableChannel() {
+            // the real implementation is in the delegate
+        }
+
+        @Override
+        protected void implConfigureBlocking(boolean block) {
+            // the real implementation is in the delegate
+        }
+
+        /**
+         * Method tries to bind port. In case it is not available it waits until TCP/IP stack is restarted
+         *
+         * @throws IOException bind is not possible at the moment for an unspecific error
+         * @throws InterruptedException if thread was interrupted. It cannot wait to next attempt of binding.
+         */
+        private void bindWithWait() throws IOException, InterruptedException {
+            while (true) {
+                try {
+                    abstractEndpoint.bind();
+                    break;
+                } catch (Throwable t) {
+                    log.debug("Cannot rebind socket", t);
+                    if (!running.get()) {
+                        throw new IOException("Application is stopping during the attempt to rebind the socket", t);
+                    }
+                    // delay between attempt to rebinding to avoid overloading
+                    Thread.sleep(retryRebindTimeoutSecs);
+                }
+            }
+        }
+
+        /**
+         * Rebind the server socket. The action could be done just by one thread. Other treats are waiting to be finish
+         * by first one.
+         *
+         * @param stateBefore id of socket binding
+         * @throws IOException unexpected exception during rebinding (socket cannot be closed or the connector cannot
+         * be re-updated)
+         */
+        private synchronized void rebind(int stateBefore) throws IOException {
+            if (state.compareAndSet(stateBefore, stateBefore + 1)) {
+                try {
+                    // socket must be closed before new binding
+                    socket.close();
+
+                    // till TCP/IP stack is running again try to bind the port
+                    bindWithWait();
+
+                    // in case of successfull update the connector instance, it is not handled by this wrapper anymore
+                    rebindHandler.run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } catch (Exception e) {
+                    throw new IOException("Cannot rebind the port", e);
+                }
+            }
+        }
+
+        public SocketChannel accept() throws IOException {
+            // obtain current state of rebinding to detection parallel actions
+            final int stateBefore = state.get();
+            try {
+                return socket.accept();
+            } catch (IOException ioe) {
+                if (ioe.getMessage().contains("EDC5122I")) {
+                    // the fix solve just one issue about stopped TCP/IP stack
+                    log.debug("The TCP/IP stack was probably restarted. The socket of Tomcat will rebind.");
+                    rebind(stateBefore);
+                    return socket.accept();
+                } else {
+                    throw ioe;
+                }
+            }
+        }
+
+    }
+
+    /**
+     * The list of final methods, which cannot be delegated. See {@link FixedServerSocketChannel#socket}
+     */
+    private interface Overridden {
+
+        SocketChannel accept() throws IOException;
+        int validOps();
+        ServerSocketChannel bind(SocketAddress local) throws IOException;
+        SelectorProvider provider();
+        boolean isRegistered();
+        SelectionKey keyFor(Selector sel);
+        SelectionKey register(Selector sel, int ops, Object att);
+        void implCloseChannel() throws IOException;
+        boolean isBlocking();
+        Object blockingLock();
+        SelectableChannel configureBlocking(boolean block) throws IOException;
+        SelectionKey register(Selector sel, int ops) throws ClosedChannelException;
+        void close() throws IOException;
+        boolean isOpen();
+
+    }
+
+}
---
+++ apiml-tomcat-common/src/test/java/org/zowe/apiml/product/web/TomcatAcceptFixConfigTest.java
@@ -0,0 +1,275 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package org.zowe.apiml.product.web;
+
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleState;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.AbstractProtocol;
+import org.apache.coyote.ProtocolHandler;
+import org.apache.coyote.ajp.AjpNio2Protocol;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.net.AbstractEndpoint;
+import org.apache.tomcat.util.net.NioEndpoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.ServerSocket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class TomcatAcceptFixConfigTest {
+
+    private TomcatAcceptFixConfig tomcatAcceptFixConfig;
+    private SocketChannel socketChannel;
+    private TestServerSocketChannel serverSocket;
+    private TestEndpoint testEndpoint;
+    private Connector connector;
+
+    private void fireEventStarted(Connector connector, LifecycleState lifecycleState) {
+        Lifecycle lifecycle = mock(Lifecycle.class);
+        doReturn(lifecycleState).when(lifecycle).getState();
+        LifecycleEvent lifecycleEvent = new LifecycleEvent(lifecycle, "type", "data");
+        Stream.of(connector.findLifecycleListeners()).forEach(l -> l.lifecycleEvent(lifecycleEvent));
+    }
+
+    private void customizeConnector() {
+        tomcatAcceptFixConfig = new TomcatAcceptFixConfig();
+        tomcatAcceptFixConfig.retryRebindTimeoutSecs = 0;
+        TomcatConnectorCustomizer tomcatConnectorCustomizer = tomcatAcceptFixConfig.tomcatAcceptorFix();
+        tomcatConnectorCustomizer.customize(connector);
+        fireEventStarted(connector, LifecycleState.STARTED);
+    }
+
+    private Connector createCustomizer(ProtocolHandler protocolHandler) {
+        TomcatAcceptFixConfig tomcatAcceptFixConfig = new TomcatAcceptFixConfig();
+        TomcatConnectorCustomizer tomcatConnectorCustomizer = tomcatAcceptFixConfig.tomcatAcceptorFix();
+        Connector connector = new Connector(protocolHandler);
+        tomcatConnectorCustomizer.customize(connector);
+        return connector;
+    }
+
+    @BeforeEach
+    void setUp() {
+        SelectorProvider provider = mock(SelectorProvider.class);
+        socketChannel = mock(SocketChannel.class);
+        serverSocket = spy(new TestServerSocketChannel(provider));
+        testEndpoint = spy(new TestEndpoint(serverSocket));
+        TestProtocol testProtocol = spy(new TestProtocol(testEndpoint));
+        connector = new Connector(testProtocol);
+
+        customizeConnector();
+    }
+
+    @Test
+    void givenCustomizedConnector_whenTcpipIsReady_thenReturnAccepted() throws Exception {
+        doReturn(socketChannel).when(serverSocket).accept();
+        assertSame(socketChannel, testEndpoint.serverSocketAccept());
+    }
+
+    @Test
+    void givenCustomizedConnector_whenTcpipIsRestarted_thenRebind() throws Exception {
+        AtomicInteger counter = new AtomicInteger(0);
+        doAnswer(invocation -> {
+            if (counter.getAndIncrement() == 0) {
+                throw new IOException("EDC5122I");
+            } else {
+                return socketChannel;
+            }
+        }).when(serverSocket).accept();
+
+        assertSame(socketChannel, testEndpoint.serverSocketAccept());
+        verify(serverSocket, times(1)).implCloseSelectableChannel();
+        verify(testEndpoint, times(1)).bind();
+    }
+
+    @Test
+    void givenCustomizedConnector_whenTcpipIsRestarted_thenWaitForTcpIpAndRebind() throws Exception {
+        AtomicInteger counterAccept = new AtomicInteger(0);
+        AtomicInteger counterBind = new AtomicInteger(0);
+        doAnswer(invocation -> {
+            if (counterAccept.getAndIncrement() == 0) {
+                throw new IOException("EDC5122I TCP/IP stack was restarted");
+            } else {
+                return socketChannel;
+            }
+        }).when(serverSocket).accept();
+        doAnswer(invocation -> {
+            if (counterBind.getAndIncrement() < 2) {
+                throw new IOException();
+            } else {
+                return 0;
+            }
+        }).when(testEndpoint).bind();
+
+        assertSame(socketChannel, testEndpoint.serverSocketAccept());
+        verify(serverSocket, times(1)).implCloseSelectableChannel();
+        verify(testEndpoint, times(3)).bind();
+    }
+
+    @Test
+    void givenCustomizedConnector_whenSocketCannotBeClosed_thenThrowException() throws Exception {
+        doThrow(new IOException("EDC5122I Socket accept failed")).when(serverSocket).accept();
+        doThrow(new IOException("EDC5122I Resource temporarily unavailable.")).when(serverSocket).close();
+
+        assertThrows(IOException.class, testEndpoint::serverSocketAccept);
+    }
+
+    @Test
+    void givenCustomizedConnector_whenAcceptFails_thenThrowException() throws IOException {
+        IOException ioe = new IOException("Unexpected error");
+        doThrow(ioe).when(serverSocket).accept();
+        IOException ioe2 = assertThrows(IOException.class, testEndpoint::serverSocketAccept);
+        assertSame(ioe, ioe2);
+        verify(serverSocket, never()).implCloseSelectableChannel();
+        verify(testEndpoint, never()).bind();
+    }
+
+    @Test
+    void givenNonNioProtocol_whenCustomize_thenNotCustomized() {
+        AjpNio2Protocol protocol = new AjpNio2Protocol();
+        Connector connector = createCustomizer(protocol);
+        fireEventStarted(connector, LifecycleState.STARTED);
+        assertSame(protocol, connector.getProtocolHandler());
+    }
+
+    @Test
+    void givenNonAbstractProtocol_whenCustomize_thenNotCustomized() {
+        ProtocolHandler protocol = mock(ProtocolHandler.class);
+        Connector connector = createCustomizer(protocol);
+        fireEventStarted(connector, LifecycleState.STARTED);
+        assertSame(protocol, connector.getProtocolHandler());
+    }
+
+    @Test
+    void givenSupportedProtocol_whenNonStartedEvent_thenNotCustomized() {
+        ProtocolHandler protocol = mock(ProtocolHandler.class);
+        Connector connector = createCustomizer(protocol);
+        fireEventStarted(connector, LifecycleState.STARTING);
+        assertSame(protocol, connector.getProtocolHandler());
+    }
+
+    @Test
+    void givenRestartedTcpipStack_whenServiceShutingDown_thenStop() throws IOException {
+        doThrow(new IOException("EDC5122I Socket accept failed")).when(serverSocket).accept();
+        doThrow(new IOException("EDC5122I Resource temporarily unavailable.")).when(testEndpoint).bind();
+        tomcatAcceptFixConfig.stopping();
+        assertThrows(IOException.class, testEndpoint::serverSocketAccept);
+    }
+
+    private static class TestEndpoint extends NioEndpoint {
+
+        public TestEndpoint(ServerSocketChannel serverSocket) {
+            setServerSocket(serverSocket);
+        }
+
+        public void setServerSocket(ServerSocketChannel serverSocket) {
+            try {
+                Field field = NioEndpoint.class.getDeclaredField("serverSock");
+                field.setAccessible(true);
+                field.set(this, serverSocket);
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
+        }
+
+        @Override
+        public void bind() throws IOException { // Mockito throw IOException in the test
+        }
+
+        @Override
+        public SocketChannel serverSocketAccept() throws Exception {
+            return ((ServerSocketChannel) getServerSocket()).accept();
+        }
+
+    }
+
+    private static class TestProtocol extends Http11NioProtocol {
+
+        public <S, U> TestProtocol(AbstractEndpoint<S, U> endpoint) {
+            try {
+                Field endpointField = AbstractProtocol.class.getDeclaredField("endpoint");
+                endpointField.setAccessible(true);
+                endpointField.set(this, endpoint);
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
+        }
+
+    }
+
+    private static class TestServerSocketChannel extends ServerSocketChannel {
+
+        public TestServerSocketChannel(SelectorProvider provider) {
+            super(provider);
+        }
+
+        @Override
+        public void implCloseSelectableChannel() throws IOException { // Mockito throw IOException in the test
+
+        }
+
+        @Override
+        protected void implConfigureBlocking(boolean block) {
+
+        }
+
+        @Override
+        public ServerSocketChannel bind(SocketAddress local, int backlog) {
+            return null;
+        }
+
+        @Override
+        public <T> ServerSocketChannel setOption(SocketOption<T> name, T value) {
+            return null;
+        }
+
+        @Override
+        public <T> T getOption(SocketOption<T> name) {
+            return null;
+        }
+
+        @Override
+        public Set<SocketOption<?>> supportedOptions() {
+            return null;
+        }
+
+        @Override
+        public ServerSocket socket() {
+            return null;
+        }
+
+        @Override
+        public SocketChannel accept() throws IOException { // Mockito throw IOException in the test
+            return null;
+        }
+
+        @Override
+        public SocketAddress getLocalAddress() {
+            return null;
+        }
+
+    }
+
+}
