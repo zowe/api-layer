4c8eb801c0547ab6aa6101713d6a511f441c8b04
Andrea Tabone <andrea.tabone@broadcom.com>
2022-06-09 17:23:48 +0200

    Remove configuration for the transaction manager as not used
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/service/infinispan/config/InfinispanConfig.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/service/infinispan/config/InfinispanConfig.java
@@ -19,9 +19,6 @@ import org.infinispan.lock.EmbeddedClusteredLockManagerFactory;
 import org.infinispan.lock.api.ClusteredLock;
 import org.infinispan.lock.api.ClusteredLockManager;
 import org.infinispan.manager.DefaultCacheManager;
-import org.infinispan.transaction.LockingMode;
-import org.infinispan.transaction.TransactionMode;
-import org.infinispan.transaction.lookup.GenericTransactionManagerLookup;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -84,11 +81,7 @@ public class InfinispanConfig {
         builder.persistence().passivation(true)
             .addSoftIndexFileStore()
             .shared(false)
-            .dataLocation(dataLocation).indexLocation(indexLocation)
-            .transaction().transactionMode(TransactionMode.TRANSACTIONAL)
-            .lockingMode(LockingMode.PESSIMISTIC)
-            .useSynchronization(true)
-            .transactionManagerLookup(new GenericTransactionManagerLookup());
+            .dataLocation(dataLocation).indexLocation(indexLocation);
         cacheManager.administration()
             .withFlags(CacheContainerAdmin.AdminFlag.VOLATILE)
             .getOrCreateCache("zoweCache", builder.build());
