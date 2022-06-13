65cff1eabb70e47f0f8044bc82f859b887dc60d5
Carson Cook <CarsonCook@users.noreply.github.com>
2022-06-13 13:12:57 -0400

    chore: Migrate DC to springdoc (#2430)
    * Set up versions.gradle
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Set config for dc
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Set swagger configuration
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Migrate to springdoc for api docs
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix integration tests
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix e2e tests
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Remove springfox config prop
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix api version
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>
    
    * Fix e2e tests
    
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- discoverable-client/src/main/java/org/zowe/apiml/client/api/GreetingV2Controller.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/api/GreetingV2Controller.java
@@ -9,10 +9,8 @@
  */
 package org.zowe.apiml.client.api;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import org.zowe.apiml.client.model.Greeting;
 
@@ -22,9 +20,7 @@ import java.util.Date;
  * Version 2 of the controller that returns greetings.
  */
 @RestController
-@Api(tags = {"Other Operations"})
-@SwaggerDefinition(tags = {
-    @Tag(name = "Other Operations", description = "General Operations")})
+@Tag(name = "Other Operations", description = "General Operations")
 @RequestMapping("/api/v2")
 public class GreetingV2Controller {
     private static final String TEMPLATE = "Hi, %s!";
@@ -33,8 +29,7 @@ public class GreetingV2Controller {
      * Gets a greeting for anyone.
      */
     @GetMapping(value = "/greeting")
-    @ApiOperation(value = "Get a greeting", response = Greeting.class,
-        tags = {"Other Operations"})
+    @Operation(summary = "Get a greeting", tags = {"Other Operations"})
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "user") String name,
                              @RequestParam(value = "delayMs", defaultValue = "0", required = false) Integer delayMs) {
         if (delayMs > 0) {
@@ -51,8 +46,7 @@ public class GreetingV2Controller {
      * Gets a custom greeting.
      */
     @GetMapping(value = {"/greeting/{yourName}"})
-    @ApiOperation(value = "Get a greeting", response = Greeting.class,
-        tags = {"Other Operations"})
+    @Operation(summary = "Get a greeting", tags = {"Other Operations"})
     public Greeting customGreeting(@PathVariable(value = "yourName") String yourName) {
         return new Greeting(new Date(), String.format(TEMPLATE, yourName));
     }
--- discoverable-client/src/main/java/org/zowe/apiml/client/model/RedirectLocation.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/model/RedirectLocation.java
@@ -9,11 +9,10 @@
  */
 package org.zowe.apiml.client.model;
 
-import org.zowe.apiml.client.model.state.New;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.zowe.apiml.client.model.state.New;
 
 import javax.validation.constraints.NotNull;
 
@@ -21,12 +20,12 @@ import javax.validation.constraints.NotNull;
  * This model is used for integration test of PageRedirectionFilter.java
  * It wraps Location url
  */
-@ApiModel
+@Schema
 @Data
 public class RedirectLocation {
 
     @NotNull(groups = New.class, message = "Location should not be null")
-    @ApiModelProperty(value = "Redirect location", example = "https://hostA:8080/some/path")
+    @Schema(description = "Redirect location", example = "https://hostA:8080/some/path")
     private String location;
 
     public RedirectLocation(@JsonProperty("location") String location) {
