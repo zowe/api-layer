00594ef31eec93725c2ecba83e8ae015bb467169
Amanda D'Errico <amanda.derrico@ibm.com>
2022-06-15 15:04:17 -0400

    fix: conventional-changelog testing maxBuffer on execSync, shouldn't affect
    Signed-off-by: Amanda D'Errico <amanda.derrico@ibm.com>

--- scripts/release_docs/index.js
+++ scripts/release_docs/index.js
@@ -18,7 +18,7 @@ const branchToMerge = process.argv[6];
     console.log("execSync for changes...manually putting 1")
     try {
         let convChange = `conventional-changelog -r`;
-        const changesTest = execSync(convChange, {cwd: '../../'});
+        const changesTest = execSync(convChange, {maxBuffer: 107374182000});
         console.log("printing out changes from execSync: \n" + changesTest)
         const changesTestToString = changesTest.toString();
         console.log("printing out changes toString: \n" + changesTestToString)
