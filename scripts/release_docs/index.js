ddd1a2e3b678e9c17c1f1b3bca118c59efa9d58a
Amanda D'Errico <amanda.derrico@ibm.com>
2022-06-16 11:07:37 -0400

    fetching before checking out to get commits
    Signed-off-by: Amanda D'Errico <amanda.derrico@ibm.com>

--- scripts/release_docs/index.js
+++ scripts/release_docs/index.js
@@ -43,7 +43,7 @@ const branchToMerge = process.argv[6];
 
 
         console.log("git checkout...")
-        let convChangeCheckout = `git checkout origin/apiml/GH2405/fix_gh_workflow && git --no-pager log -n 10`;
+        let convChangeCheckout = `git fetch --unshallow origin apiml/GH2405/fix_gh_workflow && git checkout origin/apiml/GH2405/fix_gh_workflow && git --no-pager log -n 10`;
         const convChangeCheckoutExecSync = execSync(convChangeCheckout, {maxBuffer: 107374182000});
         console.log("printing out checkout changes from execSync: \n" + convChangeCheckoutExecSync)
     } catch (error) {
