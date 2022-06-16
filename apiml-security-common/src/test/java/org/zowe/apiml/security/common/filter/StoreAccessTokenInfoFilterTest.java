f63e63162e41a56e4de35aa101c1eec7698c4254
Andrea Tabone <andrea.tabone@broadcom.com>
2022-06-16 18:54:03 +0200

    Add tests
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- apiml-security-common/src/test/java/org/zowe/apiml/security/common/filter/StoreAccessTokenInfoFilterTest.java
+++ apiml-security-common/src/test/java/org/zowe/apiml/security/common/filter/StoreAccessTokenInfoFilterTest.java
@@ -9,11 +9,15 @@
  */
 package org.zowe.apiml.security.common.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.zowe.apiml.message.core.MessageService;
+import org.zowe.apiml.message.yaml.YamlMessageService;
 import org.zowe.apiml.security.common.error.ResourceAccessExceptionHandler;
 
 import javax.servlet.FilterChain;
@@ -21,17 +25,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 class StoreAccessTokenInfoFilterTest {
-    StoreAccessTokenInfoFilter underTest;
+    private StoreAccessTokenInfoFilter underTest;
     private MockHttpServletRequest request;
     private HttpServletResponse response;
     private FilterChain chain;
     private static final String VALID_JSON = "{\"validity\": 90, \"scopes\": [\"service\"]}";
-    private final ResourceAccessExceptionHandler resourceAccessExceptionHandler = mock(ResourceAccessExceptionHandler.class);
+    private static final String INVALID_JSON = "{ \"notValid\", \"scopes\": [\"service\"]}";
+    private static final String INVALID_JSON2 = "{\"valissdity\": 90, \"scopes\": [\"service\"]}";
+    private final MessageService messageService = new YamlMessageService("/security-service-messages.yml");
+    private final ResourceAccessExceptionHandler resourceAccessExceptionHandler = new ResourceAccessExceptionHandler(messageService, new ObjectMapper());
 
     @BeforeEach
     public void setUp() {
@@ -54,6 +63,28 @@ class StoreAccessTokenInfoFilterTest {
             assertNotNull(expirationTime);
             assertEquals("90", expirationTime);
             verify(chain, times(1)).doFilter(request, response);
+        }
+    }
+
+    @Nested
+    class GivenRequestWithNotValidBody {
+        @BeforeEach
+        public void setUp() {
+            response = new MockHttpServletResponse();
+        }
+
+        @Test
+        void whenInvalidValue_thenThrowException() throws ServletException {
+            request.setContent(INVALID_JSON.getBytes());
+            underTest.doFilterInternal(request, response, chain);
+            assertThat(response.getStatus(), is(400));
+        }
+
+        @Test
+        void whenInvalidKey_thenThrowException() throws ServletException {
+            request.setContent(INVALID_JSON2.getBytes());
+            underTest.doFilterInternal(request, response, chain);
+            assertThat(response.getStatus(), is(400));
         }
     }
 }
