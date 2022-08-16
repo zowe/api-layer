109b3d116cf28c077325fec188211f5d68216f1a
achmelo <a.chmelo@gmail.com>
2022-08-16 13:40:52 +0200

    remove import
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
