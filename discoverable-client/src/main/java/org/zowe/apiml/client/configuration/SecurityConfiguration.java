f22d12e305ddf863068bb4b70983f257f4a048bf
achmelo <a.chmelo@gmail.com>
2022-08-16 17:51:48 +0200

    integration with okta
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
@@ -10,10 +10,13 @@
 
 package org.zowe.apiml.client.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -24,12 +27,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
+    @ConditionalOnProperty(value = "okta.oauth2.issuer")
+    public SecurityFilterChain filterChainOath(HttpSecurity http) throws Exception {
+        return http.csrf().disable() // NOSONAR
+            .authorizeRequests()
+            .antMatchers("/ws/**").authenticated()
+            .antMatchers("/**").permitAll()
+            .antMatchers("/whoami").authenticated().and().oauth2ResourceServer().jwt().and()
+
+            .and().build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = SecurityFilterChain.class)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable() // NOSONAR
             .authorizeRequests()
             .antMatchers("/ws/**").authenticated()
             .antMatchers("/**").permitAll()
-            .and().httpBasic()
             .and().build();
     }
 
@@ -43,5 +58,9 @@ public class SecurityConfiguration {
         return new InMemoryUserDetailsManager(user);
     }
 
-
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+            web.ignoring().antMatchers("/api/**");
+    }
 }
