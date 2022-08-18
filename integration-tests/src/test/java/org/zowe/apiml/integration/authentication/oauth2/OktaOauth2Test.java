4c52f018c6d5c62e3873873150efaac5d9db6823
achmelo <a.chmelo@gmail.com>
2022-08-18 11:23:32 +0200

    separate oauth tests
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/authentication/oauth2/OktaOauth2Test.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/authentication/oauth2/OktaOauth2Test.java
@@ -13,6 +13,7 @@ package org.zowe.apiml.integration.authentication.oauth2;
 import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.zowe.apiml.util.config.ConfigReader;
 import org.zowe.apiml.util.config.DiscoverableClientConfiguration;
@@ -27,6 +28,7 @@ import static io.restassured.RestAssured.given;
 public class OktaOauth2Test {
 
     @Test
+    @Tag("OktaOauth2Test")
     void givenValidAccessToken_thenAllowAccessToResource() {
         DiscoverableClientConfiguration dcConfig = ConfigReader.environmentConfiguration().getDiscoverableClientConfiguration();
         String username = System.getProperty("okta.client.id");
@@ -40,7 +42,6 @@ public class OktaOauth2Test {
         headers.put("content-type", "application/x-www-form-urlencoded");
         headers.put("accpets", "application/json");
         RestAssured.useRelaxedHTTPSValidation();
-        log.error("Calling https://dev-95727686.okta.com/oauth2/default/v1/token?grant_type=client_credentials&scope=customScope");
         Object accessToken = given().port(443).log().all().headers(headers).when().post("https://dev-95727686.okta.com:443/oauth2/default/v1/token?grant_type=client_credentials&scope=customScope")
             .then().statusCode(200).extract().body().path("access_token");
         if (accessToken instanceof String) {
