c55a9813079479ea7d075ec9dca72a9a02757ea2
achmelo <a.chmelo@gmail.com>
2022-08-18 09:58:36 +0200

    security filter chain
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/configuration/SecurityConfiguration.java
@@ -10,8 +10,6 @@
 
 package org.zowe.apiml.client.configuration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,24 +25,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    @ConditionalOnProperty(value = "okta.oauth2.issuer")
     public SecurityFilterChain filterChainOath(HttpSecurity http) throws Exception {
-        return http.csrf().disable() // NOSONAR
+        return http.csrf().disable().requestMatchers().antMatchers("/whoami").and() // NOSONAR
             .authorizeRequests()
-            .antMatchers("/ws/**").authenticated()
-            .antMatchers("/**").permitAll()
-            .antMatchers("/whoami").authenticated().and().oauth2ResourceServer().jwt().and()
-
+            .anyRequest().authenticated()
+            .and().oauth2ResourceServer().jwt().and()
             .and().build();
     }
 
     @Bean
-    @ConditionalOnMissingBean(value = SecurityFilterChain.class)
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable() // NOSONAR
+    public SecurityFilterChain filterChainWebSocket(HttpSecurity http) throws Exception {
+        return http.csrf().disable().requestMatchers().antMatchers("/ws/**").and() // NOSONAR
             .authorizeRequests()
-            .antMatchers("/ws/**").authenticated()
-            .antMatchers("/**").permitAll()
+            .anyRequest().authenticated()
             .and().httpBasic()
             .and().build();
     }
