a86d9ca8067c4258b14e4ee1d00a73357d9f1668
achmelo <a.chmelo@gmail.com>
2022-09-23 17:07:04 +0200

    disable hostname validation, route to correct host
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/service/ProxyRouteLocator.java
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/service/ProxyRouteLocator.java
@@ -14,8 +14,11 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.expression.Expression;
 import org.zowe.apiml.product.routing.RoutedService;
 
+import java.net.URI;
+import java.util.LinkedHashMap;
 import java.util.Locale;
 
 public class ProxyRouteLocator extends RouteLocator {
@@ -26,11 +29,24 @@ public class ProxyRouteLocator extends RouteLocator {
     }
 
     protected void setProperties(RouteDefinition routeDefinition, ServiceInstance instance, RoutedService service) {
+
         PredicateDefinition predicate = new PredicateDefinition();
         predicate.setName("Header");
         predicate.addArg("header", "X-Request-Id");
-        predicate.addArg("regexp", instance.getServiceId().toLowerCase(Locale.ROOT));
+        predicate.addArg("regexp", (instance.getServiceId() + instance.getHost()).toLowerCase(Locale.ROOT));
+
         routeDefinition.getPredicates().add(predicate);
 
+    }
+
+    protected RouteDefinition buildRouteDefinition(Expression urlExpr, ServiceInstance serviceInstance, String routeId) {
+        String serviceId = serviceInstance.getInstanceId();
+        RouteDefinition routeDefinition = new RouteDefinition();
+        routeDefinition.setId(getRouteIdPrefix() + serviceId + routeId);
+        String uri = String.format("%s://%s:%d", serviceInstance.getScheme(), serviceInstance.getHost(), serviceInstance.getPort());
+        routeDefinition.setUri(URI.create(uri));
+        // add instance metadata
+        routeDefinition.setMetadata(new LinkedHashMap<>(serviceInstance.getMetadata()));
+        return routeDefinition;
     }
 }
