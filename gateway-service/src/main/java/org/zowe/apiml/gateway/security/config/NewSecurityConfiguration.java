97db735a066ac63998ade2696615bb23df32ef57
Andrea Tabone <andrea.tabone@broadcom.com>
2022-10-10 17:34:51 +0200

    Enable token auth via http header
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/config/NewSecurityConfiguration.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/config/NewSecurityConfiguration.java
@@ -57,6 +57,7 @@ import org.zowe.apiml.security.common.config.SimpleUserDetailService;
 import org.zowe.apiml.security.common.content.BasicContentFilter;
 import org.zowe.apiml.security.common.content.BearerContentFilter;
 import org.zowe.apiml.security.common.content.CookieContentFilter;
+import org.zowe.apiml.security.common.content.HttpHeaderContentFilter;
 import org.zowe.apiml.security.common.filter.CategorizeCertsFilter;
 import org.zowe.apiml.security.common.filter.StoreAccessTokenInfoFilter;
 import org.zowe.apiml.security.common.handler.FailedAuthenticationHandler;
@@ -514,7 +515,8 @@ public class NewSecurityConfiguration {
                     http
                         .addFilterBefore(basicFilter(authenticationManager), org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter.class)
                         .addFilterBefore(cookieFilter(authenticationManager), org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter.class)
-                        .addFilterBefore(bearerContentFilter(authenticationManager), org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter.class);
+                        .addFilterBefore(bearerContentFilter(authenticationManager), org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter.class)
+                        .addFilterBefore(httpHeaderContentFilter(authenticationManager), org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter.class);
                 }
 
                 /**
@@ -549,6 +551,16 @@ public class NewSecurityConfiguration {
                         handlerInitializer.getAuthenticationFailureHandler(),
                         handlerInitializer.getResourceAccessExceptionHandler(),
                         protectedEndpoints);
+                }
+                /**
+                 * Secures content with a token in HTTP header
+                 */
+                private HttpHeaderContentFilter httpHeaderContentFilter(AuthenticationManager authenticationManager) {
+                    return new HttpHeaderContentFilter(
+                        authenticationManager,
+                        handlerInitializer.getAuthenticationFailureHandler(),
+                        handlerInitializer.getResourceAccessExceptionHandler()
+                    );
                 }
             }
 
