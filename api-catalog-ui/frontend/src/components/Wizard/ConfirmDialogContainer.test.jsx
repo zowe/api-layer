8bbe9b282ba63d852487d21ca251d6c4f6ae12b1
Andrea Tabone <andrea.tabone@broadcom.com>
2022-12-09 12:10:23 +0100

    Add tests for containers pt.4
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

---
+++ api-catalog-ui/frontend/src/components/Wizard/ConfirmDialogContainer.test.jsx
@@ -0,0 +1,40 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import ConfirmDialogContainer from './ConfirmDialogContainer';
+
+const mockStore = configureStore();
+
+describe('ConfirmDialog Container', () => {
+    let store;
+    let container;
+    beforeEach(() => {
+        store = mockStore({
+            wizardReducer: {
+                wizardIsOpen: true,
+                yamlObject: {},
+                serviceId: 'service',
+                confirmDialog: false,
+            },
+        });
+        container = render(
+            <Provider store={store}>
+                <ConfirmDialogContainer versions={['v1', 'v2']} />
+            </Provider>
+        );
+    });
+
+    it('should render the container', () => {
+        expect(container).toMatchSnapshot();
+    });
+});
