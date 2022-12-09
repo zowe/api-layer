1dfca581b1cf8d497dd752aa3a3e4930a5ebc68c
Andrea Tabone <andrea.tabone@broadcom.com>
2022-12-09 10:38:04 +0100

    Add tests for containers pt.3
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

---
+++ api-catalog-ui/frontend/src/components/Login/LoginContainer.test.jsx
@@ -0,0 +1,45 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { Router } from 'react-router-dom';
+import LoginContainer from './LoginContainer';
+
+const mockStore = configureStore();
+
+describe('Login Container', () => {
+    let store;
+    let container;
+    beforeEach(() => {
+        store = mockStore({
+            authenticationReducer: {},
+        });
+        const history = {
+            location: {
+                pathname: {},
+            },
+            push: jest.fn(),
+            listen: jest.fn(),
+        };
+        container = render(
+            <Router history={history}>
+                <Provider store={store}>
+                    <LoginContainer />
+                </Provider>
+            </Router>
+        );
+    });
+
+    it('should render the container', () => {
+        expect(container).toMatchSnapshot();
+    });
+});
