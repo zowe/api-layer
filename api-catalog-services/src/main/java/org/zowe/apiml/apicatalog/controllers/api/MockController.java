c913c9f22aa63511fbb23e0f3b6a907892e27b9a
Pavel Jareš <pavel.jares@broadcom.com>
2023-01-17 17:53:50 +0100

    basic implementation
    Signed-off-by: Pavel Jareš <pavel.jares@broadcom.com>

---
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/controllers/api/MockController.java
@@ -0,0 +1,36 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.apicatalog.controllers.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
+@RequestMapping("/mock")
+@Tag(name = "API Catalog")
+@ConditionalOnProperty(value = "apiml.catalog.standalone.enabled", havingValue = "true")
+public class MockController {
+
+    @RequestMapping("/**")
+    public void mockEndpoint(HttpServletResponse httpServletResponse) throws IOException {
+        try (PrintWriter pw = httpServletResponse.getWriter()) {
+            pw.print("{}");
+        }
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
