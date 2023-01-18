6175bb46fdf24472e220f4bbe33c3ee3cfd2aead
Pablo Hernán Carle <pablo.carle@broadcom.com>
2023-01-18 13:33:22 +0100

    add log message when standalone mode
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/standalone/StandaloneSecurityConfig.java
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/standalone/StandaloneSecurityConfig.java
@@ -1,15 +1,17 @@
 /*
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Copyright Contributors to the Zowe Project.
- */
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*/
 
 package org.zowe.apiml.apicatalog.standalone;
 
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -18,19 +20,28 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.zowe.apiml.product.constants.CoreService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @ConditionalOnProperty(value = "apiml.catalog.standalone.enabled", havingValue = "true")
+@Slf4j
 public class StandaloneSecurityConfig {
+
+    @PostConstruct
+    void init() {
+        log.warn(CoreService.API_CATALOG.getServiceId() + " is running in standalone mode. Authentication is disabled. Do not use in production.");
+    }
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain permitAll(HttpSecurity http) throws Exception {
         return http
-            .authorizeRequests()
-            .anyRequest().permitAll()
-            .and()
-            .build();
+        .authorizeRequests()
+        .anyRequest().permitAll()
+        .and()
+        .build();
     }
 
 }
