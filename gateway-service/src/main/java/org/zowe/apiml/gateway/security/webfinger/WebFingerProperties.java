4e74404b9ee2fc11bbdadc95321460dd3de6a994
achmelo <a.chmelo@gmail.com>
2023-01-19 15:06:55 +0100

    remove imports
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/webfinger/WebFingerProperties.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/webfinger/WebFingerProperties.java
@@ -11,8 +11,6 @@
 package org.zowe.apiml.gateway.security.webfinger;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
