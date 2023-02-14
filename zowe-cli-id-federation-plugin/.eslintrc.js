d84091accd836cb4f7f68e80fea30ad35bf869be
Elena Kubantseva <elena.kubantseva@broadcom.com>
2023-02-14 16:18:38 +0100

    Addressed comments: update versions, add package-lock.json
    Signed-off-by: Elena Kubantseva <elena.kubantseva@broadcom.com>

--- zowe-cli-id-federation-plugin/.eslintrc.js
+++ zowe-cli-id-federation-plugin/.eslintrc.js
@@ -63,7 +63,6 @@ module.exports = {
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/semi": "warn",
-        "license-header/header": ["error", "./LICENSE_HEADER"],
         "unused-imports/no-unused-imports": "warn",
         "unused-imports/no-unused-vars": ["warn", {
             "args": "none"
