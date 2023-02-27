9ed8fdcdad3992ffbdd29cc37186dfa9fd69df59
Andrea Tabone <andrea.tabone@broadcom.com>
2023-02-27 10:38:04 +0100

    Fix test execution path
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- zowe-cli-id-federation-plugin/__tests__/__integration__/cli/map/__scripts__/map_error_handler.sh
+++ zowe-cli-id-federation-plugin/__tests__/__integration__/cli/map/__scripts__/map_error_handler.sh
@@ -3,4 +3,5 @@ set -e # fail the script if we get a non zero exit code
 
 CSV=$1
 
+cd ../../../../
 zowe idf map "${CSV}"
--- zowe-cli-id-federation-plugin/__tests__/__integration__/cli/map/__scripts__/map_old_profiles.sh
+++ zowe-cli-id-federation-plugin/__tests__/__integration__/cli/map/__scripts__/map_old_profiles.sh
@@ -4,6 +4,7 @@ set -e # fail the script if we get a non zero exit code
 CSV=$1
 
 # create old school profiles
+cd ../../../../
 zowe profiles create id-federation my_idf --esm ACF2 --system TST1 --registry ldap://zowe.org > /dev/null
 
 zowe idf map "${CSV}"
