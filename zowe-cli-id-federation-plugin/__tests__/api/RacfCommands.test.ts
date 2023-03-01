9a90bdfe2986734389875366e13501bf938237bf
Andrea Tabone <andrea.tabone@broadcom.com>
2023-03-01 17:45:43 +0100

    fix eslint
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- zowe-cli-id-federation-plugin/__tests__/api/RacfCommands.test.ts
+++ zowe-cli-id-federation-plugin/__tests__/api/RacfCommands.test.ts
@@ -16,9 +16,10 @@ describe("Racf Commands", () => {
         const csvParser = new CsvParser('__tests__/__resources__/csv/users.csv');
         const identities = csvParser.getIdentities();
 
-       const racfCommands = new RacfCommands("ldap://host:1234", identities);
-       const commands = racfCommands.getCommands();
-       expect(commands.toString()).toBe("RACMAP ID(mf_jir) MAP USERDIDFILTER(NAME('dist_jirka')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Jirka'),RACMAP ID(mf_lena) MAP USERDIDFILTER(NAME('dist_lena')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Lena'),RACMAP ID(mf_pab) MAP USERDIDFILTER(NAME('dist_pablo')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Pablo'),RACMAP ID(mf_name) MAP USERDIDFILTER(NAME('Dist naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame ok')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Name'),SETROPTS RACLIST(IDIDMAP) REFRESH\n");
-       expect(commands).toMatchSnapshot();
+        const racfCommands = new RacfCommands("ldap://host:1234", identities);
+        const commands = racfCommands.getCommands();
+        // eslint-disable-next-line max-len
+        expect(commands.toString()).toBe("RACMAP ID(mf_jir) MAP USERDIDFILTER(NAME('dist_jirka')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Jirka'),RACMAP ID(mf_lena) MAP USERDIDFILTER(NAME('dist_lena')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Lena'),RACMAP ID(mf_pab) MAP USERDIDFILTER(NAME('dist_pablo')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Pablo'),RACMAP ID(mf_name) MAP USERDIDFILTER(NAME('Dist naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame ok')) REGISTRY(NAME('ldap://host:1234')) WITHLABEL('Name'),SETROPTS RACLIST(IDIDMAP) REFRESH\n");
+        expect(commands).toMatchSnapshot();
     });
 });
