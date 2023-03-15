4cf6f88cd52e0d9e2b98a41114cbe8bf111c688b
Petr Weinfurt <petr.weinfurt@broadcom.com>
2023-03-15 18:23:31 +0100

    Use Commands class for all ESMs
    Signed-off-by: Petr Weinfurt <petr.weinfurt@broadcom.com>

--- zowe-cli-id-federation-plugin/src/api/Commands.ts
+++ zowe-cli-id-federation-plugin/src/api/Commands.ts
@@ -10,8 +10,11 @@
 
 import {ImperativeError, TextUtils} from "@zowe/imperative";
 import { warn } from "console";
+import * as fs from "fs";
 import {IIdentity} from "./CsvParser";
 import {hasValidLength} from "./ValidateUtil";
+import {Constants} from "./Constants";
+import {IHandlerResponseApi} from "@zowe/imperative/lib/cmd/src/doc/response/api/handler/IHandlerResponseApi";
 
 export class Commands {
 
@@ -22,43 +25,51 @@ export class Commands {
     constructor(
         private registry: string,
         private identities: IIdentity[],
-        private commandTemplate: string,
-        private refreshCommand: string
+        private esm: string,
+        private response: IHandlerResponseApi
     ) {
     }
 
     getCommands(): string[] {
+        const commandTemplate = fs.readFileSync(`src/api/templates/${this.esm.toLowerCase()}.jcl`).toString();
+        const refreshCommand = fs.readFileSync(`src/api/templates/${this.esm.toLowerCase()}_refresh.jcl`).toString();
+
         const commands = this.identities
-            .map(identity => this.getCommand(identity))
+            .map(identity => this.getCommand(identity, commandTemplate))
             .filter(command => command);
 
         if (!commands.some(Boolean)) {
+            this.response.data.setExitCode(Constants.fatalCode);
             throw new ImperativeError({msg: "Error when trying to create the identity mapping."});
         }
-        commands.push(this.refreshCommand);
+        commands.push("");
+        commands.push(refreshCommand);
         return commands;
     }
 
-    private getCommand(identity: IIdentity): string {
+    private getCommand(identity: IIdentity, commandTemplate: string): string {
         if(!hasValidLength(identity.mainframeId, this.maxLengthMainframeId)) {
             warn(`The mainframe user ID '${identity.mainframeId}' has exceeded maximum length of ${this.maxLengthMainframeId} characters. ` +
            `Identity mapping for the user '${identity.userName}' has not been created.`);
+            this.response.data.setExitCode(Constants.warnCode);
             return '';
         }
 
         if(!hasValidLength(identity.distributedId, this.maxLengthDistributedId)) {
             warn(`The distributed user ID '${identity.distributedId}' has exceeded maximum length of ${this.maxLengthDistributedId} characters. ` +
                 `Identity mapping for the user '${identity.userName}' has not been created.`);
+            this.response.data.setExitCode(Constants.warnCode);
             return '';
         }
 
         if(!hasValidLength(identity.userName, this.maxLengthLabel)) {
             warn(`The user name '${identity.userName}' has exceeded maximum length of ${this.maxLengthLabel} characters. ` +
                 `Identity mapping for the user '${identity.userName}' has not been created.`);
+            this.response.data.setExitCode(Constants.warnCode);
             return '';
         }
 
-        return TextUtils.renderWithMustache(this.commandTemplate, {
+        return TextUtils.renderWithMustache(commandTemplate, {
             mainframe_id: identity.mainframeId.trim(),
             distributed_id: identity.distributedId.trim(),
             registry: this.registry,
