ec383f873c13cf3990d028ee0fc9db9a8a7d69d1
sj895092 <shobhackm9@gmail.com>
2023-04-12 23:44:53 +0200

    chore: creating a separate module for npm release
    Signed-off-by: sj895092 <shobhackm9@gmail.com>

--- zowe-plugin-deploy-components/scripts/npm_login.sh
+++
@@ -1,5 +0,0 @@
-set -e
-registry=$1
-echo "//$(echo ${registry} | sed s/'http[s]\?:\/\/'//):_authToken=${NPM_TOKEN}" >> ~/.npmrc
-echo "registry=${registry}" >> ~/.npmrc
-npm whoami --registry=${registry}
