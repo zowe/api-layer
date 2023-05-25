d29b6c7a4ede2490ada97a77b4f968618c5ff6da
achmelo <a.chmelo@gmail.com>
2023-05-25 12:53:13 +0200

    revert mock service changes
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- mock-services/src/main/java/org/zowe/apiml/client/api/AuthenticationController.java
+++ mock-services/src/main/java/org/zowe/apiml/client/api/AuthenticationController.java
@@ -11,7 +11,6 @@
 package org.zowe.apiml.client.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.zowe.apiml.client.model.LoginBody;
@@ -31,9 +30,6 @@ public class AuthenticationController {
     @DeleteMapping(value = "/zosmf/services/authenticate", produces = "application/json; charset=utf-8")
     public ResponseEntity<?> logout(HttpServletResponse response,
                                     @RequestHeader Map<String, String> headers) {
-        if(headers.get("cookie") == null) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
         return authentication.process(AUTHENTICATION_SERVICE, "delete", response, headers);
     }
 
