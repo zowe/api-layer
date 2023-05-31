76eaecaf26e78ef3760be76d5e3af5d437c075f9
Pablo Hernán Carle <pablo.carle@broadcom.com>
2023-05-31 16:50:25 +0200

    2 of 4 version-compare e2e pass
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- api-catalog-ui/frontend/cypress/integration/e2e/detail-page/service-version-compare.test.js
+++ api-catalog-ui/frontend/cypress/integration/e2e/detail-page/service-version-compare.test.js
@@ -7,10 +7,12 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
+/// <reference types="Cypress" />
+
 const PATH_TO_VERSION_SELECTORS =
-    '.detail-page > div.content-description-container > div > div.serviceTab > div.api-diff-container > div.api-diff-form > div:nth-child(2) > div > div';
+    '.detail-page > div.content-description-container > div > div.tabs-swagger > div.serviceTab > div.api-diff-container > div.api-diff-form > div:nth-child(2) > div > div';
 const PATH_TO_VERSION_SELECTORS2 =
-    '.detail-page > div.content-description-container > div > div.serviceTab > div.api-diff-container > div.api-diff-form > div:nth-child(4) > div > div';
+    '.detail-page > div.content-description-container > div > div.tabs-swagger > div.serviceTab > div.api-diff-container > div.api-diff-form > div:nth-child(4) > div > div';
 const PATH_TO_VERSION_SELECTOR_ITEMS =
     '#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul> li';
 const PATH_TO_VERSION_SELECTOR_ITEMS2 =
@@ -30,48 +32,47 @@ describe('>>> Service version compare Test', () => {
 
         cy.get('@submitButton').click();
 
-        cy.contains('API Mediation Layer API').click();
+        cy.contains('Service Spring Onboarding Enabler sample application API').click(); // discoverable client
 
-        cy.visit(`${Cypress.env('catalogHomePage')}/#/tile/cademoapps/discoverableclient`);
+        cy.visit(`${Cypress.env('catalogHomePage')}/#/service/discoverableclient`);
     });
 
     it('Should show compare tab', () => {
-        cy.get('.tabs-container').should('exist');
-        cy.get('.tabs-container').should('have.length', 2);
-        cy.get('.nav-tab').should('exist');
-        cy.get('.nav-tab').should('have.length', 5);
+        // Location of the compare has changed, it's no longer a specific tab
+        cy.get('.tabs-container').should('not.exist');
+        cy.get('.nav-tabs').should('exist');
+        cy.get('.nav-tabs').should('have.length', 13);
         cy.get('.version-text').should('exist');
-        cy.get('.version-text').should('have.length', 4);
         cy.get('.version-text').should('contain.text', 'Compare');
     });
 
     it('Should switch to compare tab when clicked', () => {
         cy.get('.api-diff-container').should('not.exist');
         cy.get(
-            '#root > div > div.content > div.detail-page > div.content-description-container > div > div.serviceTab > div.tabs-container > span:nth-child(4) > p'
+            '#root > div > div.content > div.detail-page > div.content-description-container > div > div.tabs-swagger > div.serviceTab > div:nth-child(2) > button > span > p'
         ).should('contain.text', 'Compare');
-        cy.get('.nav-tab:nth-child(4)').click();
+        cy.get('#compare-button').click();
         cy.get('.api-diff-container').should('exist');
 
         cy.get('.api-diff-form').should('exist');
-        cy.get('.api-diff-form > div:nth-child(2) > label').eq(0).should('exist');
-        cy.get('.api-diff-form > p:nth-child(1)').eq(0).should('contain.text', 'Compare');
-        cy.get('.api-diff-container > div.api-diff-form > p:nth-child(3)').eq(0).should('exist');
-        cy.get('.api-diff-container > div.api-diff-form > p:nth-child(3)').eq(0).should('contain.text', 'with');
-        cy.get('.api-diff-form > div:nth-child(2) > label').eq(0).should('exist');
-        cy.get('.api-diff-container > div.api-diff-form > div:nth-child(2) > label')
-            .eq(0)
-            .should('contain.text', 'Version');
-        cy.get('.api-diff-container > div.api-diff-form > div:nth-child(4) > label').eq(0).should('exist');
-        cy.get('.api-diff-container > div.api-diff-form > div:nth-child(4) > label')
-            .eq(0)
-            .should('contain.text', 'Version');
+        cy.get('.api-diff-form > div:nth-child(2) > label').eq(0).should('exist').and('contain.text', 'Version');
+
+        cy.get('.api-diff-form > p').eq(0).should('contain.text', 'Compare');
+        cy.get('.api-diff-form > p').eq(1).should('exist');
+        cy.get('.api-diff-form > p').eq(1).should('contain.text', 'with');
+
+        cy.get('.api-diff-form > div:nth-child(2) > label').eq(0).should('exist').and('contain.text', 'Version');
+        cy.get('.api-diff-form > div:nth-child(2) > label').eq(0).should('contain.text', 'Version');
+        cy.get('.api-diff-form > div:nth-child(4) > label').eq(0).should('exist');
+        cy.get('.api-diff-form > div:nth-child(4) > label').eq(0).should('contain.text', 'Version');
         cy.get('.api-diff-form > button').should('exist');
-        cy.get('.api-diff-form > button').should('contain.text', 'Go');
+        cy.get('.api-diff-form > button').should('contain.text', 'Show');
     });
 
     it('Should display version in selector', () => {
-        cy.get('.content-description-container > div > div.serviceTab > div.tabs-container > span:nth-child(4) > p')
+        cy.get(
+            '#root > div > div.content > div.detail-page > div.content-description-container > div > div.tabs-swagger > div.serviceTab > div:nth-child(2) > button > span > p'
+        )
             .should('contain.text', 'Compare')
             .click();
 
