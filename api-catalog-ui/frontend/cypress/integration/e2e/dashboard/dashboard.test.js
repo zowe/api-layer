304f26ad3ef881f70c635f487232b90c681145b5
sj895092 <shobhackm9@gmail.com>
2023-06-01 17:17:56 +0200

    fixed dashboard.test.js
    Signed-off-by: sj895092 <shobhackm9@gmail.com>

--- api-catalog-ui/frontend/cypress/integration/e2e/dashboard/dashboard.test.js
+++ api-catalog-ui/frontend/cypress/integration/e2e/dashboard/dashboard.test.js
@@ -36,25 +36,16 @@ describe('>>> Dashboard test', () => {
 
         cy.get('.grid-tile-status').should('have.length.gte', 1);
 
-        cy.contains('All services are running').should('exist');
+        cy.contains('The service is running').should('exist');
 
         cy.get('.header').should('exist');
 
         cy.get('#search > div > div > input').should('exist');
-        cy.contains('Available API services').should('exist');
         cy.get('#refresh-api-button').should('exist').click();
         cy.get('.Toastify').should('have.length.gte', 1);
         cy.get('.Toastify > div> div')
             .should('have.length', 1)
             .should('contain', 'The refresh of static APIs was successful!');
-        cy.get('#search > div > div > input').as('search').type('API Mediation Layer API');
-
-        cy.get('.grid-tile').should('have.length', 1).should('contain', 'API Mediation Layer API');
-
-        cy.get('@search').clear();
-        cy.should('be.empty');
-
-        cy.get('.grid-tile').should('have.length.gte', 1);
 
         cy.get('#search > div > div > input')
             .as('search')
@@ -66,7 +57,7 @@ describe('>>> Dashboard test', () => {
 
         cy.get('@search').clear();
 
-        cy.get('#search > div > div > input').as('search').type('API Mediation Layer API');
+        cy.get('#search > div > div > input').as('search').type('API Catalog');
 
         cy.get('.grid-tile').should('have.length', 1);
 
@@ -75,9 +66,8 @@ describe('>>> Dashboard test', () => {
         cy.get('.grid-tile').should('have.length.gte', 1);
         cy.get('@search').should('have.text', '');
 
-        cy.contains('API Mediation Layer API').click();
+        cy.contains('API Catalog').click();
 
-        cy.url().should('contain', '/tile/apimediationlayer/');
         cy.get('#root > div > div.content > div.header > div.right-icons > div').should('exist').click();
         cy.get('#user-info-text').should('have.length', 1);
         cy.get('#logout-button').should('have.length', 1).should('contain', 'Log out');
