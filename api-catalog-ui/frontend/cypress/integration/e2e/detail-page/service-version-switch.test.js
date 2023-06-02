de7119ceca43ea9ac1356f0fd5a8fbaeb3d83577
Pablo Hernán Carle <pablo.carle@broadcom.com>
2023-06-02 12:27:55 +0200

    fixes for service-version-switch.test.js
    It will fail until the default version is selected in dropdown
    
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- api-catalog-ui/frontend/cypress/integration/e2e/detail-page/service-version-switch.test.js
+++ api-catalog-ui/frontend/cypress/integration/e2e/detail-page/service-version-switch.test.js
@@ -21,31 +21,34 @@ describe('>>> Service version change Test', () => {
 
         cy.get('@submitButton').click();
 
-        cy.contains('API Mediation Layer API').click();
+        cy.contains('Service Spring Onboarding Enabler sample application API').click();
 
-        cy.visit(`${Cypress.env('catalogHomePage')}/#/tile/cademoapps/discoverableclient`);
+        cy.visit(`${Cypress.env('catalogHomePage')}/#/service/discoverableclient`);
     });
 
     it('Should contain version tabs', () => {
-        cy.get('.tabs-container').should('exist');
-        cy.get('.tabs-container').should('have.length', 2);
-        cy.get('.nav-tab').should('exist');
-        cy.get('.nav-tab').should('have.length', 5);
+        cy.get('.tabs-container').should('not.exist');
         cy.get('.version-text').should('exist');
-        cy.get('.version-text').should('have.length', 4);
-        cy.get('.version-text').eq(0).should('contain.text', 'zowe.apiml.discoverableclient.ws v1.0.0');
-        cy.get('.version-text').eq(1).should('contain.text', 'zowe.apiml.discoverableclient.rest v1.0.0');
-        cy.get('.version-text').eq(3).should('contain.text', 'Compare');
+        cy.get('.version-text').should('have.length', 1);
+
+        cy.get('#version-menu').should('exist').click();
+
+        cy.get('#menu- > div > ul').should('exist');
+
+        cy.get('#menu- > div > ul > li').eq(0).should('contain.text', 'zowe.apiml.discoverableclient.ws v1.0.0');
+        cy.get('#menu- > div > ul > li').eq(1).should('contain.text', 'zowe.apiml.discoverableclient.rest v1.0.0');
+        cy.get('#menu- > div > ul > li').eq(2).should('contain.text', 'zowe.apiml.discoverableclient.rest v2.0.0');
     });
 
     it('Should pre select default version', () => {
         cy.get('.version-text').should('exist');
         cy.get('.servers').contains('discoverableclient/api/v1');
+        cy.get('#version-menu').should('exist').and('have.text', 'zowe.apiml.discoverableclient.ws v1.0.0');
     });
 
     it('Should change version when clicking version 2', () => {
-        cy.get('.nav-tab').eq(2).should('contain.text', 'zowe.apiml.discoverableclient.rest v1.0.0');
-        cy.get('.nav-tab').eq(3).click();
+        cy.get('#version-menu').should('contain.text', 'zowe.apiml.discoverableclient.rest v1.0.0').click();
+        cy.get('#menu- > div > ul').should('exist').get('li').eq(2).click();
         cy.get('.servers', { timeout: 10000 }).contains('/discoverableclient/api/v2');
     });
 });
