0bfae6881a36aadf0e19d92dfd95e0b9469a1404
Pablo Hernán Carle <pablo.carle@broadcom.com>
2023-07-24 10:43:00 +0200

    fix checkstyle
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfServiceTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/security/service/zosmf/ZosmfServiceTest.java
@@ -74,7 +74,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.doAnswer;
