18222eb592ce9f9a23a79608157e1efe79cbb1d4
Andrea Tabone <andrea.tabone@broadcom.com>
2023-08-04 10:54:41 +0200

    Read certificate from request header
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- apiml-security-common/src/main/java/org/zowe/apiml/security/common/filter/CategorizeCertsFilter.java
+++ apiml-security-common/src/main/java/org/zowe/apiml/security/common/filter/CategorizeCertsFilter.java
@@ -13,6 +13,8 @@ package org.zowe.apiml.security.common.filter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -20,25 +22,33 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * This filter processes certificates on request. It decides, which certificates are considered for client authentication
  */
 @RequiredArgsConstructor
 @Slf4j
+@Component
 public class CategorizeCertsFilter extends OncePerRequestFilter {
 
     private static final String ATTRNAME_CLIENT_AUTH_X509_CERTIFICATE = "client.auth.X509Certificate";
     private static final String ATTRNAME_JAVAX_SERVLET_REQUEST_X509_CERTIFICATE = "javax.servlet.request.X509Certificate";
     private static final String LOG_FORMAT_FILTERING_CERTIFICATES = "Filtering certificates: {} -> {}";
-
+    private static final String X_AUTH_SOURCE = "x-auth-source";
+    @Value("${apiml.security.x509.authViaHeader:false}")
+    private boolean x509AuthViaHeader;
     private final Set<String> publicKeyCertificatesBase64;
 
     public Set<String> getPublicKeyCertificatesBase64() {
@@ -53,6 +63,27 @@ public class CategorizeCertsFilter extends OncePerRequestFilter {
      */
     private void categorizeCerts(ServletRequest request) {
         X509Certificate[] certs = (X509Certificate[]) request.getAttribute(ATTRNAME_JAVAX_SERVLET_REQUEST_X509_CERTIFICATE);
+        if (x509AuthViaHeader) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            if (certs == null) {
+                certs = new X509Certificate[0];
+            }
+            String certFromHeader = httpRequest.getHeader(X_AUTH_SOURCE);
+            if (certFromHeader != null && !certFromHeader.isEmpty()) {
+                try {
+                    byte[] decodedCertData = Base64.getDecoder().decode(certFromHeader);
+                    CertificateFactory cf = CertificateFactory.getInstance("X.509");
+                    InputStream certStream = new ByteArrayInputStream(decodedCertData);
+                    X509Certificate certificate = (X509Certificate) cf.generateCertificate(certStream);
+                    certStream.close();
+                    Stream<X509Certificate> certsStream = Arrays.stream(certs);
+                    Stream<X509Certificate> newCertsStream = Stream.concat(certsStream, Stream.of(certificate));
+                    certs = newCertsStream.toArray(X509Certificate[]::new);
+                } catch (CertificateException | IOException e) {
+                    log.error("Cannot extract X509 certificate from the authentication header {}", X_AUTH_SOURCE);
+                }
+            }
+        }
         if (certs != null) {
             request.setAttribute(ATTRNAME_CLIENT_AUTH_X509_CERTIFICATE, selectCerts(certs, certificateForClientAuth));
             request.setAttribute(ATTRNAME_JAVAX_SERVLET_REQUEST_X509_CERTIFICATE, selectCerts(certs, notCertificateForClientAuth));
