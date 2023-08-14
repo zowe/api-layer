1f7ecd532153cbce695a3ca49a4e00c11c6d4ba9
Elena Kubantseva <elena.kubantseva@broadcom.com>
2023-08-14 17:10:07 +0200

    fix licenses and checkstyle errors
    Signed-off-by: Elena Kubantseva <elena.kubantseva@broadcom.com>

--- cloud-gateway-service/src/test/java/org/zowe/apiml/cloudgatewayservice/service/routing/ByBasePathTest.java
+++ cloud-gateway-service/src/test/java/org/zowe/apiml/cloudgatewayservice/service/routing/ByBasePathTest.java
@@ -1,24 +1,11 @@
 /*
- * Copyright (c) 2022 Broadcom.  All Rights Reserved.  The term
- * "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
  *
- * This software and all information contained therein is
- * confidential and proprietary and shall not be duplicated,
- * used, disclosed, or disseminated in any way except as
- * authorized by the applicable license agreement, without the
- * express written permission of Broadcom.  All authorized
- * reproductions must be marked with this language.
+ * SPDX-License-Identifier: EPL-2.0
  *
- * EXCEPT AS SET FORTH IN THE APPLICABLE LICENSE AGREEMENT, TO
- * THE EXTENT PERMITTED BY APPLICABLE LAW, BROADCOM PROVIDES THIS
- * SOFTWARE WITHOUT WARRANTY OF ANY KIND, INCLUDING WITHOUT
- * LIMITATION, ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR
- * FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT WILL BROADCOM
- * BE LIABLE TO THE END USER OR ANY THIRD PARTY FOR ANY LOSS OR
- * DAMAGE, DIRECT OR INDIRECT, FROM THE USE OF THIS SOFTWARE,
- * INCLUDING WITHOUT LIMITATION, LOST PROFITS, BUSINESS
- * INTERRUPTION, GOODWILL, OR LOST DATA, EVEN IF BROADCOM IS
- * EXPRESSLY ADVISED OF SUCH LOSS OR DAMAGE.
+ * Copyright Contributors to the Zowe Project.
  */
 
 package org.zowe.apiml.cloudgatewayservice.service.routing;
