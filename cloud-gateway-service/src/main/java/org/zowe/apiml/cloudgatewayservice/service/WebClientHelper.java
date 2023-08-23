eb7e136571118b031a2610ed231edc4606861614
Andrea Tabone <andrea.tabone@broadcom.com>
2023-08-23 16:36:50 +0200

    Add exception handling
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/service/WebClientHelper.java
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/service/WebClientHelper.java
@@ -3,19 +3,26 @@ package org.zowe.apiml.cloudgatewayservice.service;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.zowe.apiml.message.log.ApimlLogger;
+import org.zowe.apiml.product.logging.annotations.InjectApimlLogger;
 
 import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLException;
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 
+@Slf4j
 public class WebClientHelper {
+    @InjectApimlLogger
+    private final static ApimlLogger apimlLog = ApimlLogger.empty();
 
-
-    @SneakyThrows
     public static SslContext load(String keystorePath, char[] password) {
         File keyStoreFile = new File(keystorePath);
         if (keyStoreFile.exists()) {
@@ -23,15 +30,18 @@ public class WebClientHelper {
                 KeyStore keyStore = KeyStore.getInstance("PKCS12");
                 keyStore.load(is, password);
                 return initSslContext(keyStore, password);
-
+            } catch (Exception e) {
+                log.error("Exception while creating SSL context", e);
+                apimlLog.log("org.zowe.apiml.common.sslContextInitializationError", e.getMessage());
+                System.exit(1);
+                return null;
             }
         } else {
             throw new IllegalArgumentException("Not existing file: " + keystorePath);
         }
     }
 
-    @SneakyThrows
-    private static SslContext initSslContext(KeyStore keyStore, char[] password) {
+    private static SslContext initSslContext(KeyStore keyStore, char[] password) throws NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException, SSLException {
         KeyManagerFactory kmf = KeyManagerFactory.getInstance("PKIX");
         kmf.init(keyStore, password);
 
