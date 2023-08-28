563d33f7babe3585c49fabff02405279b22b391a
ms895784 <143127779+ms895784@users.noreply.github.com>
2023-08-28 11:05:33 -0400

    chore[ui]: Update UI per suggestions from UX (#3047)
    * chore[ui]: Update UI per suggestions from UX
    
    Update screens to closer align to figma.
    
    Signed-off-by: Mike Simpson <mike.simpson@broadcom.com>
    
    * style inputs and focus and hover
    
    ---------
    
    Signed-off-by: Mike Simpson <mike.simpson@broadcom.com>
    Co-authored-by: Andrea Tabone <39694626+taban03@users.noreply.github.com>

--- api-catalog-ui/frontend/src/components/Tile/Tile.jsx
+++ api-catalog-ui/frontend/src/components/Tile/Tile.jsx
@@ -7,13 +7,15 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardActionArea, CardContent, Typography } from '@material-ui/core';
 import React, { Component } from 'react';
 import Brightness1RoundedIcon from '@material-ui/icons/Brightness1Rounded';
 import ReportProblemIcon from '@material-ui/icons/ReportProblem';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import videosImg from '../../assets/images/videos.png';
 import tutorialsImg from '../../assets/images/tutorials.png';
+import swaggerImg from '../../assets/images/swagger.png';
+
 import utilFunctions, { isAPIPortal } from '../../utils/utilFunctions';
 
 export default class Tile extends Component {
@@ -25,7 +27,7 @@ export default class Tile extends Component {
         const { status } = tile;
         switch (status) {
             case 'UP':
-                return <Brightness1RoundedIcon id="success" style={{ color: 'rgb(42, 133, 78)', fontSize: '12px' }} />;
+                return '';
             case 'DOWN':
                 return <ReportProblemIcon id="danger" style={{ color: 'rgb(222, 27, 27)', fontSize: '12px' }} />;
             default:
@@ -33,23 +35,23 @@ export default class Tile extends Component {
         }
     };
 
-    getTileStatusText = (tile) => {
-        if (tile === null || tile === undefined) {
-            return 'Status unknown';
-        }
-        const apiPortalEnabled = isAPIPortal();
-        if (!apiPortalEnabled) {
-            const { status } = tile;
-            switch (status) {
-                case 'UP':
-                    return 'The service is running';
-                case 'DOWN':
-                    return 'The service is not running';
-                default:
-                    return 'Status unknown';
-            }
-        }
-    };
+    // getTileStatusText = (tile) => {
+    //     if (tile === null || tile === undefined) {
+    //         return 'Status unknown';
+    //     }
+    //     const apiPortalEnabled = isAPIPortal();
+    //     if (!apiPortalEnabled) {
+    //         const { status } = tile;
+    //         switch (status) {
+    //             case 'UP':
+    //                 return 'The service is running';
+    //             case 'DOWN':
+    //                 return 'The service is not running';
+    //             default:
+    //                 return 'Status unknown';
+    //         }
+    //     }
+    // };
 
     handleClick = () => {
         const { tile, history, storeCurrentTileId, service } = this.props;
@@ -62,56 +64,66 @@ export default class Tile extends Component {
     render() {
         const { tile, service } = this.props;
         const apiPortalEnabled = isAPIPortal();
-        const { useCasesCounter, tutorialsCounter, videosCounter } = utilFunctions(service);
+        const { useCasesCounter, tutorialsCounter, videosCounter, hasSwagger } = utilFunctions(service);
 
         return (
             <Card key={tile.id} className="grid-tile pop grid-item" onClick={this.handleClick} data-testid="tile">
-                <CardContent style={{ fontSize: '0.875em', color: 'rgb(88, 96, 110)' }} className="tile">
-                    <Typography id="tileLabel" className="grid-tile-status">
-                        {this.getTileStatus(tile)}
-                        {this.getTileStatusText(tile)}
-                    </Typography>
-                    <Typography
-                        variant="subtitle1"
-                        style={{
-                            color: 'rgb(88, 96, 110)',
-                            fontWeight: 'bold',
-                            fontSize: '1.125em',
-                        }}
-                    >
-                        {service.title}
-                    </Typography>
-                    {service.sso && (
-                        <Typography variant="h6" id="grid-tile-sso">
-                            (SSO)
+                <CardActionArea>
+                    <CardContent style={{ fontSize: '0.875em', color: 'rgb(88, 96, 110)' }} className="tile">
+                        <Typography id="tileLabel" className="grid-tile-status">
+                            {this.getTileStatus(tile)}
+                            {/* {this.getTileStatusText(tile)} */}
                         </Typography>
-                    )}
-                    {apiPortalEnabled && (
-                        <div id="media-icons">
-                            <Typography
-                                className="media-labels"
-                                id="use-cases-counter"
-                                size="medium"
-                                variant="outlined"
-                            >
-                                {useCasesCounter}
+                        <Typography
+                            variant="subtitle1"
+                            style={{
+                                fontFamily: 'Roboto',
+                                fontSize: '24px',
+                                fontWeight: '700',
+                                lineHeight: '28px',
+                                color: 'black',
+                            }}
+                        >
+                            {service.title}
+                        </Typography>
+                        {/* {service.sso && (
+                            <Typography variant="h6" id="grid-tile-sso">
+                                (SSO)
                             </Typography>
-                            <Typography
-                                className="media-labels"
-                                id="tutorials-counter"
-                                size="medium"
-                                variant="outlined"
-                            >
-                                {tutorialsCounter}
-                            </Typography>
-                            <img id="tutorials" alt="Tutorials" src={tutorialsImg} />
-                            <Typography className="fmedia-labels" id="videos-counter" size="medium" variant="outlined">
-                                {videosCounter}
-                            </Typography>
-                            <img id="videos" alt="Videos" src={videosImg} />
-                        </div>
-                    )}
-                </CardContent>
+                        )} */}
+                        {apiPortalEnabled && (
+                            <div id="media-icons">
+                                <div id="swagger">{hasSwagger && <img alt="Swagger" src={swaggerImg} />}</div>
+                                <Typography
+                                    className="media-labels"
+                                    id="use-cases-counter"
+                                    size="medium"
+                                    variant="outlined"
+                                >
+                                    {useCasesCounter}
+                                </Typography>
+                                <Typography
+                                    className="media-labels"
+                                    id="tutorials-counter"
+                                    size="medium"
+                                    variant="outlined"
+                                >
+                                    {tutorialsCounter}
+                                </Typography>
+                                <img id="tutorials" alt="Tutorials" src={tutorialsImg} />
+                                <Typography
+                                    className="fmedia-labels"
+                                    id="videos-counter"
+                                    size="medium"
+                                    variant="outlined"
+                                >
+                                    {videosCounter}
+                                </Typography>
+                                <img id="videos" alt="Videos" src={videosImg} />
+                            </div>
+                        )}
+                    </CardContent>
+                </CardActionArea>
             </Card>
         );
     }
