8d2b091593d668d38e7cbdb106d93d9d6e65b9e8
sj895092 <shobhackm9@gmail.com>
2023-09-22 16:01:26 +0200

    securing "/registry/**" endpoint with x509
    Signed-off-by: sj895092 <shobhackm9@gmail.com>

--- cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/config/WebSecurity.java
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/config/WebSecurity.java
@@ -10,31 +10,51 @@
 
 package org.zowe.apiml.cloudgatewayservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
 
 @Configuration
 public class WebSecurity {
 
+    @Value("${apiml.security.x509.serviceList.allowedUsers:-}")
+     private ArrayList usersList = new ArrayList<>();
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.x509(Customizer.withDefaults()).csrf().disable()
-            .authorizeExchange(exchange -> exchange.anyExchange().permitAll());
+
+
+        SubjectDnX509PrincipalExtractor principalExtractor = new SubjectDnX509PrincipalExtractor();
+
+        ReactiveAuthenticationManager authenticationManager = authentication -> {
+            authentication.setAuthenticated(usersList.contains(authentication.getName()));
+            return Mono.just(authentication);
+        };
+
+        http.x509(x509 ->
+            x509
+                .principalExtractor(principalExtractor)
+                .authenticationManager(authenticationManager)).authorizeExchange().pathMatchers("/registry/**").authenticated();
+
         return http.build();
     }
 
     @Bean
     @Primary
     ReactiveUserDetailsService userDetailsService() {
-        return username -> Mono.just(User.withUsername(username).password("password").authorities("user").build());
-    }
 
+        return username -> Mono.just(User.withUsername(username).password("")
+           .authorities("user") // should be enhanced for user roles
+           .build());
+    }
 
 }
