018da1d295f43e204737529b1808dd27c6b0c4c1
Jiri Aichler <jiri.aichler@broadcom.com>
2023-09-26 10:32:36 +0200

    Review

--- cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/config/WebSecurity.java
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/config/WebSecurity.java
@@ -10,7 +10,6 @@
 
 package org.zowe.apiml.cloudgatewayservice.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,7 +25,6 @@ import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.List;
 
-@Slf4j
 @Configuration
 public class WebSecurity {
 
