a57e114391c0600899839e0f799a0e826bb08fc2
Pavel Jareš <pavel.jares@broadcom.com>
2023-10-25 13:38:06 +0200

    attempt to refactor auth schema factories
    Signed-off-by: Pavel Jareš <pavel.jares@broadcom.com>

---
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/filters/AbstractAuthSchemeFactory.java
@@ -0,0 +1,105 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.cloudgatewayservice.filters;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import org.zowe.apiml.cloudgatewayservice.service.InstanceInfoService;
+import org.zowe.apiml.constants.ApimlConstants;
+import org.zowe.apiml.message.core.MessageService;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.function.Function;
+
+public abstract class AbstractAuthSchemeFactory<T, R, D> extends AbstractGatewayFilterFactory<T> {
+
+    protected final WebClient webClient;
+
+    protected final InstanceInfoService instanceInfoService;
+
+    protected final MessageService messageService;
+
+    protected AbstractAuthSchemeFactory(Class<T> configClazz, WebClient webClient, InstanceInfoService instanceInfoService, MessageService messageService) {
+        super(configClazz);
+        this.webClient = webClient;
+        this.instanceInfoService = instanceInfoService;
+        this.messageService = messageService;
+    }
+
+    protected abstract Class<R> getResponseClass();
+
+    private Mono<List<ServiceInstance>> getZaasInstances() {
+        return instanceInfoService.getServiceInstance("gateway");
+    }
+
+    protected Mono<Void> invoke(
+        List<ServiceInstance> serviceInstances,
+        Function<ServiceInstance, WebClient.RequestHeadersSpec<?>> requestCreator,
+        Function<? super R, ? extends Mono<Void>> responseProcessor
+    ) {
+        RuntimeException latestException = null;
+        for (ServiceInstance instance : serviceInstances) {
+            try {
+                return requestCreator.apply(instance)
+                    .retrieve()
+                    .onStatus(HttpStatus::is4xxClientError, (response) -> Mono.empty())
+                    .bodyToMono(getResponseClass())
+                    .flatMap(responseProcessor);
+            } catch (RuntimeException re) {
+                latestException = re;
+            }
+        }
+
+        if (latestException != null) {
+            throw latestException;
+        }
+
+        return Mono.empty();
+    }
+
+    protected abstract WebClient.RequestHeadersSpec<?> createRequest(ServerWebExchange exchange, ServiceInstance instance, D data);
+    protected abstract Mono<Void> processResponse(ServerWebExchange exchange, GatewayFilterChain chain, R response);
+
+    protected GatewayFilter createGatewayFilter(D data) {
+        return (ServerWebExchange exchange, GatewayFilterChain chain) -> getZaasInstances()
+            .flatMap(instances -> invoke(
+                instances,
+                instance -> createRequest(exchange, instance, data),
+                response -> processResponse(exchange, chain, response)
+            ));
+    }
+
+    protected ServerHttpRequest addRequestHeader(ServerWebExchange exchange, String key, String value) {
+        return exchange.getRequest().mutate()
+            .headers(headers -> headers.add(key, value))
+            .build();
+    }
+
+    protected ServerHttpRequest setRequestHeader(ServerWebExchange exchange, String headerName, String headerValue) {
+        return exchange.getRequest().mutate()
+            .header(headerName, headerValue)
+            .build();
+    }
+
+    protected ServerHttpRequest updateHeadersForError(ServerWebExchange exchange, String errorMessage) {
+        ServerHttpRequest request = addRequestHeader(exchange, ApimlConstants.AUTH_FAIL_HEADER, messageService.createMessage("org.zowe.apiml.security.ticket.generateFailed", errorMessage).mapToLogMessage());
+        exchange.getResponse().getHeaders().add(ApimlConstants.AUTH_FAIL_HEADER, messageService.createMessage("org.zowe.apiml.security.ticket.generateFailed", errorMessage).mapToLogMessage());
+        return request;
+    }
+
+}
--- cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/filters/PassticketFilterFactory.java
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/filters/PassticketFilterFactory.java
@@ -13,18 +13,16 @@ package org.zowe.apiml.cloudgatewayservice.filters;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.Data;
 import org.apache.http.HttpHeaders;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import org.zowe.apiml.cloudgatewayservice.service.InstanceInfoService;
-import org.zowe.apiml.constants.ApimlConstants;
 import org.zowe.apiml.message.core.MessageService;
 import org.zowe.apiml.ticket.TicketRequest;
 import org.zowe.apiml.ticket.TicketResponse;
@@ -34,47 +32,44 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @Service
-public class PassticketFilterFactory extends AbstractGatewayFilterFactory<PassticketFilterFactory.Config> {
-    private final WebClient webClient;
-    private final InstanceInfoService instanceInfoService;
-    private final MessageService messageService;
+public class PassticketFilterFactory extends AbstractAuthSchemeFactory<PassticketFilterFactory.Config, TicketResponse, String> {
+
     private final String ticketUrl = "%s://%s:%s/%s/api/v1/auth/ticket";
     private final ObjectWriter writer = new ObjectMapper().writer();
 
     public PassticketFilterFactory(WebClient webClient, InstanceInfoService instanceInfoService, MessageService messageService) {
-        super(Config.class);
-        this.webClient = webClient;
-        this.instanceInfoService = instanceInfoService;
-        this.messageService = messageService;
+        super(Config.class, webClient, instanceInfoService, messageService);
+    }
+
+    @Override
+    protected Class<TicketResponse> getResponseClass() {
+        return TicketResponse.class;
+    }
+
+    @Override
+    protected WebClient.RequestHeadersSpec<?> createRequest(ServerWebExchange exchange, ServiceInstance instance, String requestBody) {
+        return webClient.post()
+            .uri(String.format(ticketUrl, instance.getScheme(), instance.getHost(), instance.getPort(), instance.getServiceId().toLowerCase()))
+            .headers(headers -> headers.addAll(exchange.getRequest().getHeaders()))
+            .bodyValue(requestBody);
+    }
+
+    @Override
+    protected Mono<Void> processResponse(ServerWebExchange exchange, GatewayFilterChain chain, TicketResponse response) {
+        if (response.getTicket() == null) {
+            ServerHttpRequest request = updateHeadersForError(exchange, "Invalid or missing authentication.");
+            return chain.filter(exchange.mutate().request(request).build());
+        }
+        String encodedCredentials = Base64.getEncoder().encodeToString((response.getUserId() + ":" + response.getTicket()).getBytes(StandardCharsets.UTF_8));
+        final String headerValue = "Basic " + encodedCredentials;
+        ServerHttpRequest request = setRequestHeader(exchange, HttpHeaders.AUTHORIZATION, headerValue);
+        return chain.filter(exchange.mutate().request(request).build());
     }
 
     @Override
     public GatewayFilter apply(Config config) {
         try {
-            final String requestBody = writer.writeValueAsString(new TicketRequest(config.getApplicationName()));
-            return (ServerWebExchange exchange, GatewayFilterChain chain) ->
-                instanceInfoService.getServiceInstance("gateway").flatMap(instances -> {
-                    for (ServiceInstance instance : instances) {
-                        return webClient.post()
-                            .uri(String.format(ticketUrl, instance.getScheme(), instance.getHost(), instance.getPort(), instance.getServiceId().toLowerCase()))
-                            .headers(headers -> headers.addAll(exchange.getRequest().getHeaders()))
-                            .bodyValue(requestBody)
-                            .retrieve().onStatus(HttpStatus::is4xxClientError, (response) -> Mono.empty())
-                            .bodyToMono(TicketResponse.class)
-                            .flatMap(response -> {
-                                if (response.getTicket() == null) {
-                                    ServerHttpRequest request = updateHeadersForError(exchange, "Invalid or missing authentication.");
-                                    return chain.filter(exchange.mutate().request(request).build());
-                                }
-                                String encodedCredentials = Base64.getEncoder().encodeToString((response.getUserId() + ":" + response.getTicket()).getBytes(StandardCharsets.UTF_8));
-                                final String headerValue = "Basic " + encodedCredentials;
-                                ServerHttpRequest request = addRequestHeader(exchange, HttpHeaders.AUTHORIZATION, headerValue);
-                                return chain.filter(exchange.mutate().request(request).build());
-                            });
-                    }
-                    ServerHttpRequest request = updateHeadersForError(exchange, "All gateway service instances failed to respond.");
-                    return chain.filter(exchange.mutate().request(request).build());
-                });
+            return createGatewayFilter(writer.writeValueAsString(new TicketRequest(config.getApplicationName())));
         } catch (JsonProcessingException e) {
             return ((exchange, chain) -> {
                 ServerHttpRequest request = updateHeadersForError(exchange, e.getMessage());
@@ -83,31 +78,10 @@ public class PassticketFilterFactory extends AbstractGatewayFilterFactory<Passti
         }
     }
 
-    private ServerHttpRequest updateHeadersForError(ServerWebExchange exchange, String errorMessage) {
-        ServerHttpRequest request = addRequestHeader(exchange, ApimlConstants.AUTH_FAIL_HEADER, messageService.createMessage("org.zowe.apiml.security.ticket.generateFailed", errorMessage).mapToLogMessage());
-        exchange.getResponse().getHeaders().add(ApimlConstants.AUTH_FAIL_HEADER, messageService.createMessage("org.zowe.apiml.security.ticket.generateFailed", errorMessage).mapToLogMessage());
-        return request;
-    }
-
-    private ServerHttpRequest addRequestHeader(ServerWebExchange exchange, String key, String value) {
-        return exchange.getRequest().mutate()
-            .headers(headers -> {
-                    headers.add(key, value);
-                    headers.remove(org.springframework.http.HttpHeaders.COOKIE);
-                }
-            ).build();
-    }
-
-
+    @Data
     public static class Config {
+
         private String applicationName;
 
-        public String getApplicationName() {
-            return applicationName;
-        }
-
-        public void setApplicationName(String applicationName) {
-            this.applicationName = applicationName;
-        }
     }
 }
