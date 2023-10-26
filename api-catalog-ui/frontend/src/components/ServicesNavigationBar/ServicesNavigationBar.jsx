4d19aaed2ccc2c5e707df1058378cf59cca9c661
Andrea Tabone <39694626+taban03@users.noreply.github.com>
2023-10-26 16:44:48 +0200

    chore: UI tweaks - buttons, keyboard focus, search icon, compare (#3175)
    * UI tweaks: buttons, keyboard focus, search icon, compare (#3173)
    
    * UI enhancements per Jan
    
    * Fix font sizes
    
    * UI tweaks
    
    ---------
    
    Co-authored-by: Brian Lee <brian-mf.lee@broadcom.com>
    
    * adjust
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * fix title size
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * fix dropdown bg
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    * fix test
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    
    ---------
    
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>
    Signed-off-by: Andrea Tabone <39694626+taban03@users.noreply.github.com>
    Co-authored-by: briandavid85 <blee1006@gmail.com>
    Co-authored-by: Brian Lee <brian-mf.lee@broadcom.com>

--- api-catalog-ui/frontend/src/components/ServicesNavigationBar/ServicesNavigationBar.jsx
+++ api-catalog-ui/frontend/src/components/ServicesNavigationBar/ServicesNavigationBar.jsx
@@ -16,6 +16,7 @@ import Shield from '../ErrorBoundary/Shield/Shield';
 import SearchCriteria from '../Search/SearchCriteria';
 import { closeMobileMenu, isAPIPortal } from '../../utils/utilFunctions';
 import MenuCloseImage from '../../assets/images/xmark.svg';
+import { ReactComponent as BackArrowImage } from '../../assets/images/angles-left.svg';
 
 export default class ServicesNavigationBar extends Component {
     componentDidMount() {
@@ -93,11 +94,12 @@ export default class ServicesNavigationBar extends Component {
                 {isAPIPortal() && (
                     <IconButton
                         id="go-back-button-portal"
-                        className="button-cta"
+                        className="button-link"
                         onClick={closeMobileMenu}
                         size="medium"
                         href={dashboard}
                     >
+                        <BackArrowImage className="icon-img" alt="" />
                         {backButtonText}
                     </IconButton>
                 )}
