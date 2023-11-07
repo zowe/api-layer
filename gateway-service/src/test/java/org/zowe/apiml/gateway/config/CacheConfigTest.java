1bbde254e788842bb814c84cd176956d04329f01
Andrea Tabone <andrea.tabone@broadcom.com>
2023-11-07 18:01:33 +0100

    add test
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

---
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/config/CacheConfigTest.java
@@ -0,0 +1,35 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.gateway.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = CacheConfig.class)
+@ActiveProfiles("test")
+class CacheConfigTest {
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Test
+    void testCacheManagerIsRealImplementation() {
+        assertNotNull(cacheManager);
+        assertTrue(cacheManager instanceof JCacheCacheManager);
+    }
+}
