c28f906fc2120fdc812b40ec700b25b7a58c9f44
Pablo Hernán Carle <pablo.carle@broadcom.com>
2024-01-10 10:51:37 +0100

    rollback version of aopalliance
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/util/StartAndCleanApplications.java
+++ integration-tests/src/test/java/org/zowe/apiml/util/StartAndCleanApplications.java
@@ -10,10 +10,12 @@
 
 package org.zowe.apiml.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
 import org.zowe.apiml.util.service.FullApiMediationLayer;
 
+@Slf4j
 public class StartAndCleanApplications implements TestExecutionListener {
     private FullApiMediationLayer fullApiMediationLayer;
 
@@ -24,12 +26,14 @@ public class StartAndCleanApplications implements TestExecutionListener {
     @Override
     public void testPlanExecutionStarted(TestPlan testPlan) {
         if (!fullApiMediationLayer.runsOffPlatform()) {
+            log.info("Starting Full API Mediation Layer");
             fullApiMediationLayer.start();
         }
     }
 
     @Override
     public void testPlanExecutionFinished(TestPlan testPlan) {
+        log.info("Stopping Full API Mediation Layer");
         fullApiMediationLayer.stop();
     }
 
