4d5d7887b3eb2bf08ffba14b85fccc0d60b2b582
Pavel Jares <Pavel.Jares@broadcom.com>
2024-01-17 18:38:19 +0100

    fix

--- gateway-service/src/main/java/org/zowe/apiml/gateway/config/CacheConfig.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/config/CacheConfig.java
@@ -10,12 +10,15 @@
 
 package org.zowe.apiml.gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.cache.jcache.JCacheManagerFactoryBean;
+import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -29,9 +32,9 @@ import java.io.IOException;
 /**
  * Spring configuration to use EhCache. This context is using from application and also from tests.
  */
+@Slf4j
 @EnableCaching
 @Configuration
-@ConditionalOnProperty(value = "apiml.caching.enabled", havingValue = "true", matchIfMissing = true)
 public class CacheConfig {
 
     public static final String COMPOSITE_KEY_GENERATOR = "compositeKeyGenerator";
@@ -40,28 +43,43 @@ public class CacheConfig {
     private static final String EHCACHE_STORAGE_ENV_PARAM_NAME = "ehcache.disk.store.dir";
     private static final String APIML_CACHE_STORAGE_LOCATION_ENV_PARAM_NAME = "apiml.cache.storage.location";
 
+    @Value("${apiml.caching.enabled:true}")
+    private boolean cacheEnabled;
+
     @PostConstruct
     public void afterPropertiesSet() {
-        if (System.getProperty(EHCACHE_STORAGE_ENV_PARAM_NAME) == null) {
-            String location = System.getProperty(APIML_CACHE_STORAGE_LOCATION_ENV_PARAM_NAME);
-            if (location == null) location = System.getProperty("user.dir");
+        if (cacheEnabled) {
+            if (System.getProperty(EHCACHE_STORAGE_ENV_PARAM_NAME) == null) {
+                String location = System.getProperty(APIML_CACHE_STORAGE_LOCATION_ENV_PARAM_NAME);
+                if (location == null) location = System.getProperty("user.dir");
 
-            System.setProperty(EHCACHE_STORAGE_ENV_PARAM_NAME, location);
+                System.setProperty(EHCACHE_STORAGE_ENV_PARAM_NAME, location);
+            }
+        } else {
+            log.warn("Gateway Service is runnning in NoOp Cache mode. Do not use in production.");
         }
     }
 
     @Bean
+    @ConditionalOnProperty(value = "apiml.caching.enabled", havingValue = "true", matchIfMissing = true)
     public JCacheManagerFactoryBean cacheManagerFactoryBean() throws IOException {
         JCacheManagerFactoryBean jCacheManagerFactoryBean = new JCacheManagerFactoryBean();
         jCacheManagerFactoryBean.setCacheManagerUri(new ClassPathResource("ehcache.xml").getURI());
         return jCacheManagerFactoryBean;
     }
 
-    @Bean
+    @Bean("cacheManager")
+    @ConditionalOnProperty(value = "apiml.caching.enabled", havingValue = "true", matchIfMissing = true)
     public CacheManager cacheManager() throws IOException {
         final JCacheCacheManager jCacheCacheManager = new JCacheCacheManager();
         jCacheCacheManager.setCacheManager(cacheManagerFactoryBean().getObject());
         return jCacheCacheManager;
+    }
+
+    @ConditionalOnProperty(value = "apiml.caching.enabled", havingValue = "false")
+    @Bean("cacheManager")
+    public CacheManager cacheManagerNoOp() {
+        return new NoOpCacheManager();
     }
 
     @Bean(CacheConfig.COMPOSITE_KEY_GENERATOR)
