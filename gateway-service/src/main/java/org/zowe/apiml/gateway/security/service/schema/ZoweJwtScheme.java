0a02e206f792d2b63cf48fbfeac83e33e3534870
sj895092 <shobhackm9@gmail.com>
2024-04-15 10:42:22 +0200

    formatting

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/ZoweJwtScheme.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/schema/ZoweJwtScheme.java
@@ -31,7 +31,6 @@ import org.zowe.apiml.security.common.token.TokenNotValidException;
 
 import java.util.Optional;
 
-
 @Component
 public class ZoweJwtScheme implements IAuthenticationScheme {
     @InjectApimlLogger
@@ -106,8 +105,6 @@ public class ZoweJwtScheme implements IAuthenticationScheme {
             if (jwt != null) {
                 final RequestContext context = RequestContext.getCurrentContext();
                 JwtCommand.setCustomHeader(context,"authorization","Bearer " + jwt);
-             //   context.getRequest().getHeaderNames().
-                System.out.println("inside jwt cmd");
                 JwtCommand.setCookie(context, configurationProperties.getCookieProperties().getCookieName(), jwt);
                 if (StringUtils.isNotEmpty(customHeader)) {
                     JwtCommand.setCustomHeader(context, customHeader, jwt);
