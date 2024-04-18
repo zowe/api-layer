23f510dd5626f762bb699ba04fecc886e922855b
achmelo <a.chmelo@gmail.com>
2024-04-18 16:16:11 +0200

    use discoverable client for redirect, add unit test
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- api-catalog-ui/frontend/cypress/e2e/login/login-oauth2.cy.js
+++ api-catalog-ui/frontend/cypress/e2e/login/login-oauth2.cy.js
@@ -34,7 +34,7 @@ describe('>>> Login through Okta OK', () => {
 
         cy.location('href').should('contain', '/oauth2/v1/authorize')
 
-        cy.url().should('contain', '/application');
+        cy.url().should('contain', '/discoverableclient/api/v1/greeting');
 
         cy.getCookie('apimlAuthenticationToken').should('exist');
 
--- cloud-gateway-service/src/test/java/org/zowe/apiml/cloudgatewayservice/config/WebSecurityTest.java
+++ cloud-gateway-service/src/test/java/org/zowe/apiml/cloudgatewayservice/config/WebSecurityTest.java
@@ -17,13 +17,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.RequestPath;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.mock.http.server.reactive.MockServerHttpResponse;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
 import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.util.LinkedMultiValueMap;
@@ -32,10 +36,13 @@ import org.zowe.apiml.cloudgatewayservice.config.oidc.ClientConfiguration;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -154,5 +161,34 @@ class WebSecurityTest {
                 })
                 .verifyComplete();
         }
+    }
+
+    @Test
+    void givenValuesInCookies_thenSetTheseValuesInTheAuthorizationRequest() {
+        var resolver = mock(ServerOAuth2AuthorizationRequestResolver.class);
+        var oauth2AuthReqBuilder = OAuth2AuthorizationRequest.authorizationCode();
+        var attrs = new HashMap<String, Object>();
+        attrs.put("nonce", "nonceValue");
+        oauth2AuthReqBuilder.attributes(attrs).authorizationUri("auth-uri").clientId("okta");
+        var oauth2AuthReq = oauth2AuthReqBuilder.build();
+        var monoResp = Mono.just(oauth2AuthReq);
+        when(resolver.resolve(any(), any())).thenReturn(monoResp);
+        var requestRepository = new WebSecurity(null).new ApimlServerAuthorizationRequestRepository(resolver);
+        var exchange = mock(ServerWebExchange.class);
+        var request = mock(ServerHttpRequest.class);
+        when(exchange.getRequest()).thenReturn(request);
+        var cookieMap = new LinkedMultiValueMap<String, HttpCookie>();
+        cookieMap.add(WebSecurity.COOKIE_NONCE, new HttpCookie(WebSecurity.CONTEXT_PATH, "nonceValue"));
+        cookieMap.add(WebSecurity.COOKIE_STATE, new HttpCookie(WebSecurity.COOKIE_STATE, "stateValue"));
+        when(request.getCookies()).thenReturn(cookieMap);
+        var requestPath = mock(RequestPath.class);
+        when(requestPath.value()).thenReturn("/cloud-gateway/login/oauth2/code/okta");
+        when(request.getPath()).thenReturn(requestPath);
+
+        var response = requestRepository.loadAuthorizationRequest(exchange);
+        AtomicReference<OAuth2AuthorizationRequest> requestRef = new AtomicReference<>();
+        response.subscribe(requestRef::set);
+        assertEquals("nonceValue", requestRef.get().getAttributes().get(OidcParameterNames.NONCE));
+        assertEquals("stateValue", requestRef.get().getState());
     }
 }
