2e4e937f66c708d0fb91a3949c6a145ac0ab4660
Pavel Jares <Pavel.Jares@broadcom.com>
2024-04-19 16:54:19 +0200

    support configuration via system environment
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>

--- cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/config/oidc/ClientConfiguration.java
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/config/oidc/ClientConfiguration.java
@@ -10,6 +10,7 @@
 
 package org.zowe.apiml.cloudgatewayservice.config.oidc;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -17,17 +18,75 @@ import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "spring.security.oauth2.client", ignoreInvalidFields = true)
 public class ClientConfiguration {
 
+    private static final String SYSTEM_ENV_PREFIX = "ZWE_configs_spring_security_oauth2_client_";
+    private static final Pattern REGISTRATION_ID_PATTERN = Pattern.compile(
+        "^" + SYSTEM_ENV_PREFIX + "([^_]+)_.*$"
+    );
+
     private Map<String, Registration> registration = new HashMap<>();
     private Map<String, Provider> provider = new HashMap<>();
+
+    private String getSystemEnv(String id, String name) {
+        return System.getProperty(SYSTEM_ENV_PREFIX + id + "_" + name);
+    }
+
+    private void update(String id, String base, Consumer<String> setter) {
+        String systemEnv = getSystemEnv(id, base);
+        if (systemEnv != null) {
+            setter.accept(systemEnv);
+        }
+    }
+
+    private void update(String id, Registration registration) {
+        update(id, "registration_clientId", registration::setClientId);
+        update(id, "registration_clientSecret", registration::setClientSecret);
+        update(id, "registration_redirectUri", registration::setRedirectUri);
+
+        String scope = getSystemEnv(id, "registration_scope");
+        if (scope != null) {
+            registration.setScope(Arrays.asList(scope.split("[,]")));
+        }
+    }
+
+    private void update(String id, Provider provider) {
+        update(id, "provider_authorizationUri", provider::setAuthorizationUri);
+        update(id, "provider_tokenUri", provider::setTokenUri);
+        update(id, "provider_userInfoUri", provider::setUserInfoUri);
+        update(id, "provider_userNameAttribute", provider::setUserNameAttribute);
+        update(id, "provider_jwkSetUri", provider::setJwkSetUri);
+    }
+
+    private Set<String> getRegistrationsIdsFromSystemEnv() {
+        return System.getProperties().keySet().stream()
+            .map(key -> {
+                Matcher matcher = REGISTRATION_ID_PATTERN.matcher(String.valueOf(key));
+                if (matcher.matches()) {
+                    return matcher.group(1);
+                }
+                return null;
+            })
+            .filter(Objects::nonNull)
+            .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @PostConstruct
+    void updateWithSystemEnvironment() {
+        for (String registrationId : getRegistrationsIdsFromSystemEnv()) {
+            update(registrationId, registration.computeIfAbsent(registrationId, k -> new Registration()));
+            update(registrationId, provider.computeIfAbsent(registrationId, k -> new Provider()));
+        }
+    }
 
     public Map<String, Config> getConfigurations() {
         Map<String, Config> map = new HashMap<>();
