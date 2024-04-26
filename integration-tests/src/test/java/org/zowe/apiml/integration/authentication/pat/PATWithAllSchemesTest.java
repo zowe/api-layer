94e507c247b394fbf968b8dc05d4ca6f96411eac
sj895092 <shobhackm9@gmail.com>
2024-04-26 10:26:21 +0200

    parameterizing the tests
    Signed-off-by: sj895092 <shobhackm9@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/authentication/pat/PATWithAllSchemesTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/authentication/pat/PATWithAllSchemesTest.java
@@ -10,26 +10,33 @@
 
 package org.zowe.apiml.integration.authentication.pat;
 
+import com.nimbusds.jwt.JWTParser;
 import io.restassured.response.Response;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+
+import org.junit.jupiter.params.provider.MethodSource;
 import org.zowe.apiml.constants.ApimlConstants;
-import org.zowe.apiml.util.SecurityUtils;
 import org.zowe.apiml.util.categories.InfinispanStorageTest;
 import org.zowe.apiml.util.config.ItSslConfigFactory;
 import org.zowe.apiml.util.config.SslContext;
 import org.zowe.apiml.util.http.HttpRequestUtils;
 
+
 import java.net.URI;
-import java.util.Base64;
+
+import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.commons.lang3.StringUtils.substring;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasKey;
@@ -43,6 +50,22 @@ public class PATWithAllSchemesTest {
     private static URI URL_SAF;
     private static URI URL_PASS;
 
+    static Stream<Arguments> headers() {
+        return Stream.of(Arguments.of("header",ApimlConstants.PAT_HEADER_NAME),
+            Arguments.of("header", HttpHeaders.AUTHORIZATION.toLowerCase()),
+            Arguments.of("cookie", ApimlConstants.PAT_COOKIE_AUTH_NAME),
+            Arguments.of("cookie", COOKIE_NAME));
+    }
+
+    String updateHeaders(String headerType,String headerValue, String pat){
+
+        if((headerType.equals("header")) && (headerValue.equals("authorization"))) {
+            return ApimlConstants.BEARER_AUTHENTICATION_PREFIX + " " + pat;
+        }
+        return pat;
+    }
+
+
     @BeforeAll
    static void init() throws Exception {
         SslContext.prepareSslAuthentication(ItSslConfigFactory.integrationTests());
@@ -53,68 +76,40 @@ public class PATWithAllSchemesTest {
 
     @Nested
     class GivenPATWithZowejwtscheme {
-        @Test
+
         @InfinispanStorageTest
-        void requestWithPATinPATHeader() {
+        @ParameterizedTest
+        @MethodSource("org.zowe.apiml.integration.authentication.pat.PATWithAllSchemesTest#headers()")
+        void requestWithPATZoweZwt(String headerType,String headerValue) throws ParseException {
             Set<String> scopes = new HashSet<>();
             scopes.add("zowejwt");
             String pat = personalAccessToken(scopes);
 
-            verifyZoweZwtHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .header(ApimlConstants.PAT_HEADER_NAME, pat)
-                .when()
-                .get(URL),pat);
+            String updatedPAT = updateHeaders( headerType, headerValue,  pat);
+
+            if(headerType.equals("header"))
+            {
+                verifyZoweZwtHeaders(given()
+                    .config(SslContext.tlsWithoutCert)
+                    .header(headerValue, updatedPAT)
+                    .when()
+                    .get(URL),pat);
+            }
+            else {
+                verifyZoweZwtHeaders(given()
+                    .config(SslContext.tlsWithoutCert)
+                    .cookie(headerValue, pat)
+                    .when()
+                    .get(URL),pat);
+            }
         }
+        void verifyZoweZwtHeaders(Response response,String pat) throws ParseException {
 
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinAuthorizationHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("zowejwt");
-            String pat = personalAccessToken(scopes);
-
-            verifyZoweZwtHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .header(HttpHeaders.AUTHORIZATION,
-                    ApimlConstants.BEARER_AUTHENTICATION_PREFIX + " " + pat)
-                .when()
-                .get(URL),pat);
-        }
-
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinPATCookieHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("zowejwt");
-            String pat = personalAccessToken(scopes);
-
-            verifyZoweZwtHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .cookie(ApimlConstants.PAT_COOKIE_AUTH_NAME, pat)
-                .when()
-                .get(URL),pat);
-        }
-
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinCookieHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("zowejwt");
-            String pat = personalAccessToken(scopes);
-
-          verifyZoweZwtHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .cookie(SecurityUtils.COOKIE_NAME, pat)
-                .when()
-                .get(URL),pat);
-        }
-
-        void verifyZoweZwtHeaders(Response response,String pat) {
-            String basic = "Basic " + Base64.getEncoder().encodeToString((USERNAME + ":" + PASSWORD).getBytes());
             assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
+            String jwt = substring(response.getBody().path("headers.authorization").toString(),7);
+
             assertNotEquals(response.getBody().path("headers.authorization"),pat);
-            assertNotEquals(response.getBody().path("headers.authorization"), basic);
+            assertEquals(JWTParser.parse(jwt).getJWTClaimsSet().toJSONObject().get("iss"),"APIML");
             assertThat(response.getBody().path("headers.cookie"), containsString(COOKIE_NAME));
         }
 
@@ -123,68 +118,36 @@ public class PATWithAllSchemesTest {
     @Nested
     class GivenPATWithPassTicketscheme {
 
-        @Test
+
         @InfinispanStorageTest
-        void requestWithPATinPATCookieHeader() {
+        @ParameterizedTest
+        @MethodSource("org.zowe.apiml.integration.authentication.pat.PATWithAllSchemesTest#headers()")
+        void requestWithPATwithPassTicket(String headerType,String headerValue) {
             Set<String> scopes = new HashSet<>();
             scopes.add("dcpassticket");
             String pat = personalAccessToken(scopes);
 
-            verifyPassTicketHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .cookie(ApimlConstants.PAT_COOKIE_AUTH_NAME, pat)
-                .when()
-                .get(URL_PASS));
-        }
+            String updatedPAT = updateHeaders( headerType, headerValue,  pat);
 
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinAuthorizationHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("dcpassticket");
-            String pat = personalAccessToken(scopes);
-
-            verifyPassTicketHeaders(given()
+            if(headerType.equals("header")) {
+                verifyPassTicketHeaders(given()
                     .config(SslContext.tlsWithoutCert)
-                    .header(HttpHeaders.AUTHORIZATION,
-                        ApimlConstants.BEARER_AUTHENTICATION_PREFIX + " " + pat)
+                    .header(headerValue, updatedPAT)
                     .when()
                     .get(URL_PASS));
-        }
-
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinPATHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("dcpassticket");
-            String pat = personalAccessToken(scopes);
-
-            verifyPassTicketHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .header(ApimlConstants.PAT_HEADER_NAME, pat)
-                .when()
-                .get(URL_PASS));
-        }
-
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinCookieHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("dcpassticket");
-            String pat = personalAccessToken(scopes);
-
-            verifyPassTicketHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .cookie(SecurityUtils.COOKIE_NAME, pat)
-                .when()
-                .get(URL_PASS));
+            }
+            else {
+                verifyPassTicketHeaders(given()
+                    .config(SslContext.tlsWithoutCert)
+                    .cookie(headerValue, pat)
+                    .when()
+                    .get(URL_PASS));
+            }
         }
 
         void verifyPassTicketHeaders(Response response) {
-            String basic = "Basic " + Base64.getEncoder().encodeToString((USERNAME + ":" + PASSWORD).getBytes());
             assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
             assertThat(response.getBody().path("headers.authorization"), startsWith("Basic "));
-            assertNotEquals(response.getBody().path("headers.authorization"), basic);
             assertThat(response.getBody().path("cookies"), not(hasKey(COOKIE_NAME)));
         }
     }
@@ -192,67 +155,33 @@ public class PATWithAllSchemesTest {
     @Nested
     class GivenPATWithSAFIDTscheme {
 
-        @Test
         @InfinispanStorageTest
-        void requestWithPATinPATHeader() {
+        @ParameterizedTest
+        @MethodSource("org.zowe.apiml.integration.authentication.pat.PATWithAllSchemesTest#headers()")
+        void requestWithPATWithSafidt(String headerType,String headerValue) {
             Set<String> scopes = new HashSet<>();
             scopes.add("dcsafidt");
             String pat = personalAccessToken(scopes);
 
-            verifySafIDTHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .header(ApimlConstants.PAT_HEADER_NAME, pat)
-                .when()
-                .get(URL_SAF));
-        }
+            String updatedPAT = updateHeaders( headerType, headerValue,  pat);
 
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinAuthorizationHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("dcsafidt");
-            String pat = personalAccessToken(scopes);
-
-            verifySafIDTHeaders(given()
+            if(headerType.equals("header")) {
+                verifySafIDTHeaders(given()
                     .config(SslContext.tlsWithoutCert)
-                    .header(HttpHeaders.AUTHORIZATION,
-                        ApimlConstants.BEARER_AUTHENTICATION_PREFIX + " " + pat)
+                    .header(headerValue, updatedPAT)
                     .when()
                     .get(URL_SAF));
+            }
+            else {
+                verifySafIDTHeaders(given()
+                    .config(SslContext.tlsWithoutCert)
+                    .cookie(headerValue, pat)
+                    .when()
+                    .get(URL_SAF));
+            }
         }
-
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinPATCookieHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("dcsafidt");
-            String pat = personalAccessToken(scopes);
-
-            verifySafIDTHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .cookie(ApimlConstants.PAT_COOKIE_AUTH_NAME, pat)
-                .when()
-                .get(URL_SAF));
-        }
-
-        @Test
-        @InfinispanStorageTest
-        void requestWithPATinCookieHeader() {
-            Set<String> scopes = new HashSet<>();
-            scopes.add("dcsafidt");
-            String pat = personalAccessToken(scopes);
-
-            verifySafIDTHeaders(given()
-                .config(SslContext.tlsWithoutCert)
-                .cookie(SecurityUtils.COOKIE_NAME, pat)
-                .when()
-                .get(URL_SAF));
-        }
-
         void verifySafIDTHeaders(Response response) {
-            String basic = "Basic " + Base64.getEncoder().encodeToString((USERNAME + ":" + PASSWORD).getBytes());
             assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
-            assertNotEquals(response.getBody().path("headers.authorization"), basic);
             assertThat(response.getBody().path("headers"), hasKey("x-saf-token"));
         }
     }
