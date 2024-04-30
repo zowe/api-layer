5585231c0a5fcc523fd58d12c6ba7ee2afdc620d
ShobhaJayanna <36433611+Shobhajayanna@users.noreply.github.com>
2024-04-30 13:54:07 +0200

    fix: Fixes the issue when PAT passed as authorization header with auth scheme zoweJwt (#3505)
    * fixing the bug by adding cookie headers
    
    * replacing PAT with JWT in the authorization header
    
    * replacing PAT with JWT in the authorization header
    
    * formatting
    
    * formatting
    
    * formatting
    
    * fixing the failing tests
    
    * fixing review comments
    
    * fixing review comments
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * adding integration tests for PAT with all authentication schemes
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * parameterizing the tests
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * fix issues
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * refactor
    
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>
    
    * refactor
    
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>
    
    * fixing comments
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * fixing response validation
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * adding jwt validation
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * addressing sonar issues
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    
    * make JUnit more accurate
    
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>
    
    ---------
    
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>
    Co-authored-by: Pavel Jares <Pavel.Jares@broadcom.com>

---
+++ integration-tests/src/test/java/org/zowe/apiml/integration/authentication/pat/PATWithAllSchemesTest.java
@@ -0,0 +1,106 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.integration.authentication.pat;
+
+import com.nimbusds.jwt.JWTParser;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.groovy.util.Arrays;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.zowe.apiml.constants.ApimlConstants;
+import org.zowe.apiml.util.categories.InfinispanStorageTest;
+import org.zowe.apiml.util.config.ItSslConfigFactory;
+import org.zowe.apiml.util.config.SslContext;
+import org.zowe.apiml.util.http.HttpRequestUtils;
+
+import java.net.URI;
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.zowe.apiml.util.SecurityUtils.COOKIE_NAME;
+import static org.zowe.apiml.util.SecurityUtils.personalAccessToken;
+import static org.zowe.apiml.util.requests.Endpoints.*;
+
+ class PATWithAllSchemesTest {
+
+    static Stream<Arguments> authentication() {
+        return Stream.of(
+            Arguments.of("PAT header", (BiFunction<RequestSpecification, String, RequestSpecification>) (rs, token) -> rs.header(ApimlConstants.PAT_HEADER_NAME, token)),
+            Arguments.of("authorization header", (BiFunction<RequestSpecification, String, RequestSpecification>) (rs, token) -> rs.header(HttpHeaders.AUTHORIZATION, ApimlConstants.BEARER_AUTHENTICATION_PREFIX + " " + token)),
+            Arguments.of("PAT cookie", (BiFunction<RequestSpecification, String, RequestSpecification>) (rs, token) -> rs.cookie(ApimlConstants.PAT_COOKIE_AUTH_NAME, token)),
+            Arguments.of("APIML cookie", (BiFunction<RequestSpecification, String, RequestSpecification>) (rs, token) -> rs.cookie(COOKIE_NAME, token))
+        );
+    }
+
+    static Stream<Arguments> schemas() {
+        return Stream.of(
+            Arguments.of("zowejwt", HttpRequestUtils.getUriFromGateway(ZOWE_JWT_REQUEST), (Consumer<Response>) r -> {
+                assertEquals(HttpStatus.SC_OK, r.getStatusCode() );
+                assertThat(r.getBody().path("headers.cookie"), containsString(COOKIE_NAME));
+                String jwt = r.getBody().path("headers.authorization").toString();
+                try {
+                   String issuer = JWTParser.parse(jwt.substring(ApimlConstants.BEARER_AUTHENTICATION_PREFIX.length()).trim()).getJWTClaimsSet().toJSONObject().get("iss").toString();
+                    assertEquals("APIML", issuer);
+                } catch (ParseException e) {
+                    fail(e);
+                }
+            }),
+            Arguments.of("dcpassticket", HttpRequestUtils.getUriFromGateway(REQUEST_INFO_ENDPOINT), (Consumer<Response>) r -> {
+                assertEquals(HttpStatus.SC_OK, r.getStatusCode() );
+                assertThat(r.getBody().path("headers.authorization"), startsWith("Basic "));
+                assertThat(r.getBody().path("cookies"), not(hasKey(COOKIE_NAME)));
+            }),
+            Arguments.of("dcsafidt", HttpRequestUtils.getUriFromGateway(SAF_IDT_REQUEST), (Consumer<Response>) r -> {
+                assertEquals(HttpStatus.SC_OK, r.getStatusCode() );
+                assertThat(r.getBody().path("headers"), hasKey("x-saf-token"));
+            })
+        );
+    }
+
+    static Stream<Arguments> authSchemas() {
+        return authentication().flatMap(a -> schemas().map(s ->
+            Arguments.of(Arrays.concat(a.get(), s.get()))
+        ));
+    }
+
+    @BeforeAll
+    static void init() throws Exception {
+        SslContext.prepareSslAuthentication(ItSslConfigFactory.integrationTests());
+    }
+
+    @InfinispanStorageTest
+    @ParameterizedTest(name = "Test service with {0} schema with the credentials stored in {2}")
+    @MethodSource("org.zowe.apiml.integration.authentication.pat.PATWithAllSchemesTest#authSchemas")
+    void requestWithPAT(
+        String credentialContainer, BiFunction<RequestSpecification, String, RequestSpecification> authenticationAction,
+        String name, URI urlSpecification, Consumer<Response> validation) {
+        String pat = personalAccessToken(Collections.singleton(name));
+
+        validation.accept(authenticationAction.apply(given(), pat)
+            .config(SslContext.tlsWithoutCert)
+        .when()
+            .get(urlSpecification)
+        );
+    }
+}
