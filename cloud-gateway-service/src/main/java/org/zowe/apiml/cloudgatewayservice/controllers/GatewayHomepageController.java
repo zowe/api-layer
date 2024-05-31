805ffc56e31fef8b79efe3b8e3098f7793ecf89a
Andrea Tabone <andrea.tabone@broadcom.com>
2024-05-31 10:34:58 +0200

    fix
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/controllers/GatewayHomepageController.java
+++ cloud-gateway-service/src/main/java/org/zowe/apiml/cloudgatewayservice/controllers/GatewayHomepageController.java
@@ -1,5 +1,3 @@
-package org.zowe.apiml.cloudgatewayservice.controllers;
-
 /*
  * This program and the accompanying materials are made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution, and is available at
@@ -10,6 +8,7 @@ package org.zowe.apiml.cloudgatewayservice.controllers;
  * Copyright Contributors to the Zowe Project.
  */
 
+package org.zowe.apiml.cloudgatewayservice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
