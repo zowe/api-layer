bd9aba46efaa18bd8188cd723f79ac7361564586
Pavel Jares <Pavel.Jares@broadcom.com>
2024-06-04 09:08:14 +0200

    wip
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/config/AuthEndpointConfig.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/config/AuthEndpointConfig.java
@@ -10,21 +10,17 @@
 
 package org.zowe.apiml.gateway.config;
 
-import com.netflix.discovery.DiscoveryClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.integration.webflux.dsl.WebFlux;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
@@ -39,56 +35,35 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @RequiredArgsConstructor
 public class AuthEndpointConfig {
 
-    private final DiscoveryClient discoveryClient;
+    private Mono<ServerResponse> resend(WebClient webClient, ServerRequest request, String path, String body) {
+        return webClient
+            .method(request.method())
+            .uri("lb://zaas/zaas" + path)
+            .headers(headers -> headers.addAll(request.headers().asHttpHeaders()))
+            .exchangeToMono(clientResponse -> {
+                var builder = ServerResponse.status(clientResponse.statusCode());
+                clientResponse.bodyToMono(String.class).flatMap(b -> {
+                    builder.bodyValue(b);
+                    return Mono.empty();
+                });
+                return builder.build();
+            });
+    }
+
+    private HandlerFunction<ServerResponse> resendTo(WebClient webClient, String path) {
+        return request -> request.bodyToMono(String.class)
+            .flatMap(body -> resend(webClient, request, path, body));
+    }
 
     @Bean
-    public RouterFunction<ServerResponse> routes(
-            //@Qualifier("webClientLoadBalanced")
-            WebClient webClient/*PostHandler postController*/,
-            ReactiveLoadBalancer.Factory<ServiceInstance> serviceInstanceFactory,
-            HttpClient httpClient
-    ) {
+    public RouterFunction<ServerResponse> routes(HttpClient httpClient, ReactiveLoadBalancer.Factory<ServiceInstance> serviceInstanceFactory) {
+        WebClient webClient = WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .filter(new ReactorLoadBalancerExchangeFilterFunction(serviceInstanceFactory, Collections.emptyList()))
+            .build();
 
-        return route(GET("/gateway/api/v1/auth/query"), request -> {
-                return Mono.empty();
-            })
-            .andRoute(POST("/gateway/api/v1/auth/login"), request -> {
-
-                System.out.println(discoveryClient.getApplication("zaas").size());
-
-                //WebFlux.outboundGateway("lb://zaas/zaas/api/v1/auth/login").
-
-                return request.bodyToMono(String.class)
-                        .flatMap(body -> {
-                            return  WebClient.builder()
-                                    .clientConnector(new ReactorClientHttpConnector(httpClient))
-                                    .filter(new ReactorLoadBalancerExchangeFilterFunction(serviceInstanceFactory, Collections.emptyList()))
-                                    .build()
-                                    .post()
-                                    .uri("lb://zaas/zaas/api/v1/auth/login")
-                                    //.uri("https://zaas/zaas/api/v1/auth/login")
-                                    //.uri("https://localhost:10023/zaas/api/v1/auth/login")
-                                    .headers(headers -> {
-                                        headers.add(HttpHeaders.AUTHORIZATION, request.headers().firstHeader(HttpHeaders.AUTHORIZATION));
-                                        headers.add(HttpHeaders.COOKIE, request.headers().firstHeader(HttpHeaders.COOKIE));
-                                    })
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    //.body(BodyInserters.fromValue(body))
-                                    //.uri("https://localhost:10023/zaas/api/v1/auth/login")
-                                    //.body(BodyInserters.fromValue(request.))
-                                    //.body(request.bo)
-                                    .exchangeToMono(clientResponse -> {
-                                        clientResponse.bodyToMono(String.class).flatMap(b -> {
-                                            System.out.println(b);
-                                            return Mono.empty();
-                                        });
-                                        var builder = ServerResponse.status(clientResponse.statusCode());
-                                        //builder.bodyValue(BodyInserters.f)
-                                        return builder.build();
-                                    });
-                        });
-
-            });
+        return route(GET("/gateway/api/v1/auth/query"), resendTo(webClient, "/api/v1/auth/query"))
+            .andRoute(POST("/gateway/api/v1/auth/login"), resendTo(webClient, "/api/v1/auth/login"));
     }
 
 }
