e543601261258c43138ba5fa06f4a6a31c324d6f
Pablo Hernán Carle <pablo.carle@broadcom.com>
2024-06-05 13:14:12 +0200

    fix some tests, 2 failing
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- apiml-security-common/src/main/java/org/zowe/apiml/security/common/verify/TrustedCertificatesProvider.java
+++ apiml-security-common/src/main/java/org/zowe/apiml/security/common/verify/TrustedCertificatesProvider.java
@@ -18,7 +18,6 @@ import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -45,7 +44,6 @@ public class TrustedCertificatesProvider {
     @InjectApimlLogger
     private final ApimlLogger apimlLog = ApimlLogger.empty();
 
-    @Autowired
     public TrustedCertificatesProvider(@Qualifier("secureHttpClientWithoutKeystore") CloseableHttpClient httpClient) {
         this.httpClient = httpClient;
     }
@@ -57,7 +55,7 @@ public class TrustedCertificatesProvider {
      * @param certificatesEndpoint Given full URL to the remote proxy gateway certificates endpoint
      * @return List of certificates or empty list
      */
-    @Cacheable(value = "trustedCertificates", key = "#certificatesEndpoint", unless = "#result.isEmpty()")
+    @Cacheable(value = "trustedCertificates", unless = "#result.isEmpty()")
     public List<Certificate> getTrustedCerts(String certificatesEndpoint) {
         List<Certificate> trustedCerts = new ArrayList<>();
         String pem = callCertificatesEndpoint(certificatesEndpoint);
