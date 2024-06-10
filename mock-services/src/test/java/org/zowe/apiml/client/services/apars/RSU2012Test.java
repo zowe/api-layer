871ac577e228cee68ca7c2f5a3f658c38a341a60
Pablo Hernán Carle <pablo.carle@broadcom.com>
2024-06-10 08:51:40 +0200

    fix checkstyle
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- mock-services/src/test/java/org/zowe/apiml/client/services/apars/RSU2012Test.java
+++ mock-services/src/test/java/org/zowe/apiml/client/services/apars/RSU2012Test.java
@@ -10,6 +10,8 @@
 
 package org.zowe.apiml.client.services.apars;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -20,13 +22,17 @@ import org.mockito.ArgumentCaptor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class RSU2012Test {
     private static final String SERVICE = "authentication";
