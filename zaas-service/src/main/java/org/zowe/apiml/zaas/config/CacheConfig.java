dc2100af2ecfc79e87c93209a2035f31ed4a1dd1
ac892247 <a.chmelo@gmail.com>
2024-06-21 11:26:26 +0200

    replace xml config with code
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- zaas-service/src/main/java/org/zowe/apiml/zaas/config/CacheConfig.java
+++ zaas-service/src/main/java/org/zowe/apiml/zaas/config/CacheConfig.java
@@ -13,6 +13,19 @@ package org.zowe.apiml.zaas.config;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.core.config.DefaultConfiguration;
+import org.ehcache.impl.config.persistence.DefaultPersistenceConfiguration;
+import org.ehcache.impl.config.store.disk.OffHeapDiskStoreConfiguration;
+import org.ehcache.impl.config.store.disk.OffHeapDiskStoreProviderConfiguration;
+import org.ehcache.impl.copy.IdentityCopier;
+import org.ehcache.impl.copy.SerializingCopier;
+import org.ehcache.jsr107.EhcacheCachingProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -20,19 +33,24 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.jcache.JCacheCacheManager;
-import org.springframework.cache.jcache.JCacheManagerFactoryBean;
 import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 import org.zowe.apiml.cache.CompositeKeyGenerator;
 import org.zowe.apiml.cache.CompositeKeyGeneratorWithoutLast;
 import org.zowe.apiml.product.gateway.GatewayClient;
+import org.zowe.apiml.security.common.token.TokenAuthentication;
 import org.zowe.apiml.util.CacheUtils;
 import org.zowe.apiml.zaas.cache.CachingServiceClient;
+import org.zowe.apiml.zaas.security.service.schema.source.AuthSource;
 
-import java.io.IOException;
+import javax.cache.Caching;
+import java.io.File;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Spring configuration to use EhCache. This context is using from application and also from tests.
@@ -52,6 +70,9 @@ public class CacheConfig {
     @Value("${apiml.caching.enabled:true}")
     private boolean cacheEnabled;
 
+    @Value("${apiml.cache.storage.location:./ehcache}")
+    private String cacheDirectory;
+
     @PostConstruct
     public void afterPropertiesSet() {
         if (cacheEnabled) {
@@ -63,26 +84,93 @@ public class CacheConfig {
             }
         } else {
             log.warn("ZAAS is running in NoOp Cache mode. Do not use in production. " +
-               "To enable caching set configuration property apiml.caching.enabled to true."
+                "To enable caching set configuration property apiml.caching.enabled to true."
             );
         }
     }
 
-    @Bean
-    @ConditionalOnProperty(value = "apiml.caching.enabled", havingValue = "true", matchIfMissing = true)
-    public JCacheManagerFactoryBean cacheManagerFactoryBean() throws IOException {
-        JCacheManagerFactoryBean jCacheManagerFactoryBean = new JCacheManagerFactoryBean();
-        jCacheManagerFactoryBean.setCacheManagerUri(new ClassPathResource("ehcache.xml").getURI());
-        return jCacheManagerFactoryBean;
-    }
-
     @Bean("cacheManager")
     @ConditionalOnProperty(value = "apiml.caching.enabled", havingValue = "true", matchIfMissing = true)
-    public CacheManager cacheManager() throws IOException {
-        final JCacheCacheManager jCacheCacheManager = new JCacheCacheManager();
-        jCacheCacheManager.setCacheManager(cacheManagerFactoryBean().getObject());
-        return jCacheCacheManager;
+    public CacheManager cacheManager() {
+        var caches = new HashMap<String, CacheConfiguration<?, ?>>();
+
+        var invalidatedJwtTokensConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, Boolean.class, ResourcePoolsBuilder.newResourcePoolsBuilder().disk(10, MemoryUnit.MB).heap(1, MemoryUnit.MB)
+            ).withService(new OffHeapDiskStoreConfiguration("pool1", 1, 1))
+            .withKeyCopier(IdentityCopier.identityCopier())
+            .withValueCopier(IdentityCopier.identityCopier())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofDays(1)))
+            .build();
+
+        caches.put("invalidatedJwtTokens", invalidatedJwtTokensConf);
+
+        var validationJwtTokenConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, TokenAuthentication.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1000, EntryUnit.ENTRIES)
+            )
+            .withKeyCopier(IdentityCopier.identityCopier())
+            .withValueCopier(SerializingCopier.asCopierClass())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofDays(1))).build();
+        caches.put("validationJwtToken", validationJwtTokenConf);
+
+        var zosmfInfoConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, String.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10, EntryUnit.ENTRIES)
+            )
+            .withKeyCopier(IdentityCopier.identityCopier())
+            .withValueCopier(IdentityCopier.identityCopier())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofHours(1))).build();
+        caches.put("zosmfInfo", zosmfInfoConf);
+
+        var zosmfAuthenticationEndpointConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, Boolean.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10, EntryUnit.ENTRIES)
+            )
+            .withKeyCopier(IdentityCopier.identityCopier())
+            .withValueCopier(IdentityCopier.identityCopier())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofHours(1))).build();
+        caches.put("zosmfAuthenticationEndpoint", zosmfAuthenticationEndpointConf);
+
+        var zosmfJwtEndpointConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                HttpHeaders.class, Boolean.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10, EntryUnit.ENTRIES)
+            )
+            .withKeyCopier(SerializingCopier.asCopierClass())
+            .withValueCopier(IdentityCopier.identityCopier())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofHours(1))).build();
+        caches.put("zosmfJwtEndpoint", zosmfJwtEndpointConf);
+
+        var validationOIDCTokenConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                AuthSource.class, Boolean.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1000, EntryUnit.ENTRIES)
+            )
+            .withKeyCopier(IdentityCopier.identityCopier())
+            .withValueCopier(IdentityCopier.identityCopier())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(20))).build();
+        caches.put("validationOIDCToken", validationOIDCTokenConf);
+
+        var parseOIDCTokenConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                AuthSource.class, AuthSource.Parsed.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1000, EntryUnit.ENTRIES)
+            )
+            .withKeyCopier(IdentityCopier.identityCopier())
+            .withValueCopier(IdentityCopier.identityCopier())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(20))).build();
+        caches.put("parseOIDCToken", parseOIDCTokenConf);
+
+        var trustedCertificatesConf = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, List.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1000, EntryUnit.ENTRIES)
+            )
+            .withKeyCopier(IdentityCopier.identityCopier())
+            .withValueCopier(IdentityCopier.identityCopier())
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofHours(1))).build();
+        caches.put("trustedCertificates", trustedCertificatesConf);
+
+        EhcacheCachingProvider provider = (EhcacheCachingProvider) Caching.getCachingProvider("org.ehcache.jsr107.EhcacheCachingProvider");
+        var serviceProvider = new OffHeapDiskStoreProviderConfiguration("pool1");
+        var localPersStore = new DefaultPersistenceConfiguration(new File(cacheDirectory));
+
+        org.ehcache.config.Configuration configuration = new DefaultConfiguration(caches, provider.getDefaultClassLoader(), serviceProvider, localPersStore);
+
+        var cacheManager = provider.getCacheManager(provider.getDefaultURI(), configuration);
+
+        return new JCacheCacheManager(cacheManager);
     }
+
 
     @ConditionalOnProperty(value = "apiml.caching.enabled", havingValue = "false")
     @Bean("cacheManager")
