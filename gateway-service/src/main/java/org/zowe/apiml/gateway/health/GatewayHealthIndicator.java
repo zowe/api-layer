d8c6b32536c35514fa4f6e2a18dd93d9dd4038ee
ac892247 <a.chmelo@gmail.com>
2024-06-28 13:52:16 +0200

    remove copy-pasted code from spring common 3.1
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/health/GatewayHealthIndicator.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/health/GatewayHealthIndicator.java
@@ -10,7 +10,6 @@
 
 package org.zowe.apiml.gateway.health;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
@@ -18,15 +17,10 @@ import org.springframework.boot.actuate.health.Status;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.zowe.apiml.gateway.security.login.Providers;
 import org.zowe.apiml.message.log.ApimlLogger;
 import org.zowe.apiml.message.yaml.YamlMessageServiceInstance;
 import org.zowe.apiml.product.constants.CoreService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 import static org.springframework.boot.actuate.health.Status.DOWN;
 import static org.springframework.boot.actuate.health.Status.UP;
@@ -35,11 +29,10 @@ import static org.springframework.boot.actuate.health.Status.UP;
  * Gateway health information (/application/health)
  */
 @Component
-@Slf4j
 public class GatewayHealthIndicator extends AbstractHealthIndicator {
     private final DiscoveryClient discoveryClient;
     private final Providers loginProviders;
-    private final String apiCatalogServiceId;
+    private String apiCatalogServiceId;
 
     private final ApimlLogger apimlLog = ApimlLogger.of(GatewayHealthIndicator.class,
             YamlMessageServiceInstance.getInstance());
@@ -58,6 +51,7 @@ public class GatewayHealthIndicator extends AbstractHealthIndicator {
     protected void doHealthCheck(Health.Builder builder) {
         boolean anyCatalogIsAvailable = apiCatalogServiceId != null && !apiCatalogServiceId.isEmpty();
         boolean apiCatalogUp = !this.discoveryClient.getInstances(apiCatalogServiceId).isEmpty();
+
         // When DS goes 'down' after it was already 'up', the new status is not shown. This is probably feature of
         // Eureka client which caches the status of services. When DS is down the cache is not refreshed.
         boolean discoveryUp = !this.discoveryClient.getInstances(CoreService.DISCOVERY.getServiceId()).isEmpty();
@@ -72,18 +66,11 @@ public class GatewayHealthIndicator extends AbstractHealthIndicator {
         }
 
         int gatewayCount = this.discoveryClient.getInstances(CoreService.GATEWAY.getServiceId()).size();
-        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
-        boolean isAuthenticated = request.getUserPrincipal() != null; // Check if the user is authenticated
-        log.error("AUTHENTICATED {}", isAuthenticated);
-        if (isAuthenticated) {
-            builder.status(toStatus(discoveryUp))
-                .withDetail(CoreService.DISCOVERY.getServiceId(), toStatus(discoveryUp).getCode())
-                .withDetail(CoreService.AUTH.getServiceId(), toStatus(authUp).getCode())
-                .withDetail("gatewayCount", gatewayCount);
-        } else {
-            builder.status(toStatus(discoveryUp))
-                .withDetail("gatewayCount", gatewayCount);
-        }
+
+        builder.status(toStatus(discoveryUp))
+            .withDetail(CoreService.DISCOVERY.getServiceId(), toStatus(discoveryUp).getCode())
+            .withDetail(CoreService.AUTH.getServiceId(), toStatus(authUp).getCode())
+            .withDetail("gatewayCount", gatewayCount);
 
         if (anyCatalogIsAvailable) {
             builder.withDetail(CoreService.API_CATALOG.getServiceId(), toStatus(apiCatalogUp).getCode());
