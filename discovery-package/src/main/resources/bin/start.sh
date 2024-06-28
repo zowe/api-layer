fe6af2eb7306174c5ff67ba3da63bde3afbb649c
Pablo Hernán Carle <pablo.carle@broadcom.com>
2024-06-28 16:00:43 +0200

    add outofmemory flag
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- discovery-package/src/main/resources/bin/start.sh
+++ discovery-package/src/main/resources/bin/start.sh
@@ -195,6 +195,7 @@ DISCOVERY_CODE=AD
 _BPXK_AUTOCVT=OFF
 _BPX_JOBNAME=${ZWE_zowe_job_prefix}${DISCOVERY_CODE} java \
     -Xms${ZWE_configs_heap_init:-32}m -Xmx${ZWE_configs_heap_max:-512}m \
+    -XX:+ExitOnOutOfMemoryError \
     ${QUICK_START} \
     ${ADD_OPENS} \
     -Dibm.serversocket.recover=true \
