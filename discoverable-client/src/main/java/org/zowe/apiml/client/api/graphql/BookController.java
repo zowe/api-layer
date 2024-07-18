311037d47157d1fdaaca7904e34cda6810ea92c1
Olha <olya.kishkinova@gmail.com>
2024-07-18 16:31:05 +0200

    commit before merge

--- discoverable-client/src/main/java/org/zowe/apiml/client/api/graphql/BookController.java
+++ discoverable-client/src/main/java/org/zowe/apiml/client/api/graphql/BookController.java
@@ -5,7 +5,6 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zowe.apiml.client.model.graphql.Author;
 import org.zowe.apiml.client.model.graphql.Book;
@@ -15,6 +14,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/api/v3/graphql")
 public class BookController {
+
     @QueryMapping
     public Book bookById(@Argument String id) {
         return Book.getById(id);
@@ -44,9 +44,5 @@ public class BookController {
     public Book deleteBook(@Argument String bookId) {
         return Book.deleteBook(bookId);
     }
-
-
-
-
 
 }
