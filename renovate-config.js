420f41893081f6615e5fc9ee2ac6a1107ea21773
mirek163 <95099003+mirek163@users.noreply.github.com>
2024-07-26 19:49:19 +0200

    Addressing review
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>

---
+++ renovate-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    extends: ["group:allNonMajor", "config:recommended"],
+    dependencyDashboard: true,
+    logFile: "renovate.log",
+    repositories: ['zowe/api-layer'],
+    baseBranches: ["v2.x.x","v3.x.x"],
+    assignees: [],
+    printConfig: true,
+    prHourlyLimit: 0, // removes rate limit for PR creation per hour
+    recreateWhen: "always", // recreates all closed or blocking PRs not just immortals
+    npmrc: "legacy-peer-deps=true", //for updating lock-files
+    npmrcMerge: true //be combined with a "global" npmrc
+};
+
+// To disable package example:
+//> "packageRules": [
+//     {
+//         "matchPackageNames": ["neutrino"],
+//         "enabled": false
+//     }
+// ]
+
+// Delays creating a PR for x days after detecting a dependency update, but creates the temporary branch right away.
+// If the update is removed from the registry within those x days, the bot deletes the branch.
+// (to adress situation if author of dependency decide to delete pull)
+//> minimumReleaseAge
