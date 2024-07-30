fcd65f39dac3fe7cd8cde289c15ff989b84e046f
mirek163 <95099003+mirek163@users.noreply.github.com>
2024-07-30 15:32:44 +0200

    Update renovate-config.js
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>

--- renovate-config.js
+++ renovate-config.js
@@ -5,7 +5,6 @@ module.exports = {
     baseBranches: ["v2.x.x","v3.x.x"],
     printConfig: true,
     prHourlyLimit: 0, // removes rate limit for PR creation per hour
-    recreateWhen: "always", // recreates all closed or blocking PRs not just immortals
     npmrc: "legacy-peer-deps=true", //for updating lock-files
     npmrcMerge: true //be combined with a "global" npmrc
 };
