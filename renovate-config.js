c3f7309f98f8d48e32aa476e5a12943d82b0bddf
mirek163 <95099003+mirek163@users.noreply.github.com>
2024-07-31 13:36:48 +0200

    Adding prTitle & label
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>

--- renovate-config.js
+++ renovate-config.js
@@ -4,6 +4,8 @@ module.exports = {
     repositories: ['zowe/api-layer'],
     baseBranches: ["v2.x.x","v3.x.x"],
     printConfig: true,
+    labels: ["dependencies"],
+    prTitle: "chore: Update dependency {{depName}} to v{{newVersion}}",
     prHourlyLimit: 0, // removes rate limit for PR creation per hour
     npmrc: "legacy-peer-deps=true", //for updating lock-files
     npmrcMerge: true //be combined with a "global" npmrc
