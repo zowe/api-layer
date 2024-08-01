bbf51e8498dffcb583a388975062337318901e6d
Pavel Jares <Pavel.Jares@broadcom.com>
2024-08-01 17:03:32 +0200

    wip
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>

--- gateway-service/src/test/java/org/zowe/apiml/gateway/security/service/token/OIDCTokenProviderEndpointTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/security/service/token/OIDCTokenProviderEndpointTest.java
@@ -11,45 +11,65 @@
 package org.zowe.apiml.gateway.security.service.token;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpStatus;
+import org.apache.http.Header;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicStatusLine;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.zowe.apiml.acceptance.common.Service;
 import org.zowe.apiml.acceptance.config.ApimlRoutingConfig;
 import org.zowe.apiml.acceptance.config.DiscoveryClientTestConfig;
 import org.zowe.apiml.acceptance.config.GatewayOverrideConfig;
 import org.zowe.apiml.acceptance.config.GatewayTestApplication;
 import org.zowe.apiml.acceptance.netflix.ApplicationRegistry;
+import org.zowe.apiml.acceptance.netflix.MetadataBuilder;
+import org.zowe.apiml.gateway.security.mapping.AuthenticationMapper;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.zowe.apiml.constants.ApimlConstants.BEARER_AUTHENTICATION_PREFIX;
+import static org.zowe.apiml.constants.ApimlConstants.HEADER_OIDC_TOKEN;
 
 @SpringBootTest(classes = GatewayTestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
         "apiml.security.oidc.validationType=endpoint",
         "apiml.security.oidc.enabled=true",
-        "apiml.health.protected=true",
-        "apiml.security.oidc.userInfo.uri=https://localhost/user/info"
+        "apiml.security.oidc.userInfo.uri=https://localhost/user/info",
+        "apiml.security.filterChainConfiguration=new"
     })
-//@Import(OIDCTokenProviderEndpointTest.OidcProviderTestController.class)
-@Import({GatewayOverrideConfig.class, DiscoveryClientTestConfig.class, ApimlRoutingConfig.class, OIDCTokenProviderEndpoint.class, OIDCTokenProviderEndpointTest.OidcProviderTestController.class})
+@Import({GatewayOverrideConfig.class, DiscoveryClientTestConfig.class, ApimlRoutingConfig.class, OIDCTokenProviderEndpoint.class, OIDCTokenProviderEndpointTest.Config.class})
 class OIDCTokenProviderEndpointTest {
 
-    private final static String VALID_TOKEN = "eyJ0eXAiOiJKV1QiLCJub25jZSI6ImVUbTV0TDhMeFhoUTUzVjdVbDVKSmlpZ0plNy13V1ZHUEpRVTFwelZqZ2MiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1HTHFqOThWTkxvWGFGZnBKQ0JwZ0I0SmFLcyIsImtpZCI6Ik1HTHFqOThWTkxvWGFGZnBKQ0JwZ0I0SmFLcyJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xMTk0ZGYxNi0zYWUwLTQ5YWEtYjQ4Yi01YzRkYTZlMTM2ODkvIiwiaWF0IjoxNzIyNDI5NTI3LCJuYmYiOjE3MjI0Mjk1MjcsImV4cCI6MTcyMjQzNDgxOSwiYWNjdCI6MCwiYWNyIjoiMSIsImFjcnMiOlsidXJuOnVzZXI6cmVnaXN0ZXJzZWN1cml0eWluZm8iXSwiYWlvIjoiQVRRQXkvOFhBQUFBVzJvNGFIdFZBQnZEamFJcmVaNytuSmQ1dmJ1VGo3RWh5QjNONUZTb2xnSGNjYkNnR2FkMHJZTUdKRUVJV1QwUCIsImFtciI6WyJwd2QiXSwiYXBwX2Rpc3BsYXluYW1lIjoiTUZELU9yaW9uIiwiYXBwaWQiOiJkZTQ2ODM2My1iMGM3LTQ5YmQtYjVjYS1kYzk5OTAxNGM0Y2IiLCJhcHBpZGFjciI6IjAiLCJmYW1pbHlfbmFtZSI6IkN1bWFyYXYiLCJnaXZlbl9uYW1lIjoiQWxleGFuZHIiLCJpZHR5cCI6InVzZXIiLCJpcGFkZHIiOiIxOTIuMTkuMTUyLjIwOSIsIm5hbWUiOiJBbGV4YW5kciBDdW1hcmF2Iiwib2lkIjoiOWM0OWEwMmYtY2JkMi00NmFmLWE1MzAtOTc5YjE4MTI1NmNmIiwicGxhdGYiOiIzIiwicHVpZCI6IjEwMDMyMDAyRDMyOTExNEIiLCJyaCI6IjAuQVMwQUZ0LVVFZUE2cWttMGkxeE5wdUUyaVFNQUFBQUFBQUFBd0FBQUFBQUFBQUF0QURBLiIsInNjcCI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoiVjZSWFVaRjI3WTNDUXFVdnFTcUxlTVVMTUtVUkFxU0t3RGNsemF5aGNCOCIsInRlbmFudF9yZWdpb25fc2NvcGUiOiJOQSIsInRpZCI6IjExOTRkZjE2LTNhZTAtNDlhYS1iNDhiLTVjNGRhNmUxMzY4OSIsInVuaXF1ZV9uYW1lIjoiYWM4OTMyNjZAYnJvYWRjb20ubmV0IiwidXBuIjoiYWM4OTMyNjZAYnJvYWRjb20ubmV0IiwidXRpIjoiMmJyS2d2ZzFua0NtQlRDcVhONHlBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il0sInhtc19pZHJlbCI6IjEgMjgiLCJ4bXNfc3QiOnsic3ViIjoiR25SemhMcGtNZmNCVFQ0dlQyYzJOVWhkYlBaOU5tckFJSnpCS0RoNUg4YyJ9LCJ4bXNfdGNkdCI6MTQwMjUwNzgwOX0.gp6CvZp0dwuSMG0yCh7g7M-LoH9SPhdy_Du3mvH1ItolXIvCHf4P993_xrnZ3pyOaxgNne7kgdLS16qy5FsBEidksAg7xtwKAwnCFNnPvgx-YJwVsak_PByj1FEcgGHnqgvtlGqmz_lB1R26Eek627Aij3ojk9tsmGn45e--tSi_jNUptjDUTlrjmSkVKmS1ae63PfYuNGTg3S2XiZE-Tt08Kyax4oQdt3G-40tCS0F-aNobwLjiDqkffu3SW4tmUuzolrTDXtlo8V4b8R5PCs0Qa3yxt5E4vLRFRiApka73Gxd8ERL9AVw-MgYx1sQjoGYAMW7jXtka4QYLQ26lDw";
-  //  private final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKFZ5zZ77xw";
-    private final static String INVALID_TOKEN = "eyJraWQiOiJMY3hja2tvcjk0cWtydW54SFA3VGtpYjU0N3J6bWtYdnNZVi1uYzZVLU40IiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULlExakp2UkZ0dUhFUFpGTXNmM3A0enQ5aHBRRHZrSU1CQ3RneU9IcTdlaEkiLCJpc3MiOiJodHRwczovL2Rldi05NTcyNzY4Ni5va3RhLmNvbS9vYXV0aDIvZGVmYXVsdCIsImF1ZCI6ImFwaTovL2RlZmF1bHQiLCJpYXQiOjE2OTcwNjA3NzMsImV4cCI6MTY5NzA2NDM3MywiY2lkIjoiMG9hNmE0OG1uaVhBcUVNcng1ZDciLCJ1aWQiOiIwMHU5OTExOGgxNmtQT1dBbTVkNyIsInNjcCI6WyJvcGVuaWQiXSwiYXV0aF90aW1lIjoxNjk3MDYwMDY0LCJzdWIiOiJzajg5NTA5MkBicm9hZGNvbS5uZXQiLCJncm91cHMiOlsiRXZlcnlvbmUiXX0.Cuf1JVq_NnfBxaCwiLsR5O6DBmVV1fj9utAfKWIF1hlek2hCJsDLQM4ii_ucQ0MM1V3nVE1ZatPB-W7ImWPlGz7NeNBv7jEV9DkX70hchCjPHyYpaUhAieTG75obdufiFpI55bz3qH5cPRvsKv0OKKI9T8D7GjEWsOhv6CevJJZZvgCFLGFfnacKLOY5fEBN82bdmCulNfPVrXF23rOregFjOBJ1cKWfjmB0UGWgI8VBGGemMNm3ACX3OYpTOek2PBfoCIZWOSGnLZumFTYA0F_3DsWYhIJNoFv16_EBBJcp_C0BYE_fiuXzeB0fieNUXASsKp591XJMflDQS_Zt1g";
+    private final static String MF_USER = "USER";
+
+    private final static String VALID_TOKEN = "ewogICJ0eXAiOiAiSldUIiwKICAibm9uY2UiOiAiYVZhbHVlVG9CZVZlcmlmaWVkIiwKICAiYWxnIjogIlJTMjU2IiwKICAia2lkIjogIlNlQ1JldEtleSIKfQ.ewogICJhdWQiOiAiMDAwMDAwMDMtMDAwMC0wMDAwLWMwMDAtMDAwMDAwMDAwMDAwIiwKICAiaXNzIjogImh0dHBzOi8vb2lkYy5wcm92aWRlci5vcmcvYXBwIiwKICAiaWF0IjogMTcyMjUxNDEyOSwKICAibmJmIjogMTcyMjUxNDEyOSwKICAiZXhwIjogODcyMjUxODEyNSwKICAic3ViIjogIm9pZGMudXNlcm5hbWUiCn0.c29tZVNpZ25lZEhhc2hDb2Rl";
+    private final static String INVALID_TOKEN = "ewogICJ0eXAiOiAiSldUIiwKICAibm9uY2UiOiAiYVZhbHVlVG9CZVZlcmlmaWVkIiwKICAiYWxnIjogIlJTMjU2IiwKICAia2lkIjogIlNlQ1JldEtleSIKfQ.ewogICJhdWQiOiAiMDAwMDAwMDMtMDAwMC0wMDAwLWMwMDAtMDAwMDAwMDAwMDAwIiwKICAiaXNzIjogImh0dHBzOi8vb2lkYy5wcm92aWRlci5vcmcvYXBwIiwKICAiaWF0IjogMTcyMjUxNDEyOSwKICAibmJmIjogMTcyMjUxNDEyOSwKICAiZXhwIjogODcyMjUxODEyNSwKICAic3ViIjogIm9pZGMudXNlcm5hbWUiLAogICJ0eXBlIjogImludmFsaWQiCn0.c29tZVNpZ25lZEhhc2hDb2Rl";
 
     @Autowired
     OIDCTokenProviderEndpoint oidcTokenProviderEndpoint;
@@ -57,28 +77,84 @@ class OIDCTokenProviderEndpointTest {
     @Autowired
     protected ApplicationRegistry applicationRegistry;
 
+    @Autowired
+    @Qualifier("mockProxy")
+    protected CloseableHttpClient mockClient;
+
     @LocalServerPort
     protected int port;
 
     protected String basePath;
 
+    private CloseableHttpResponse createResponse(int responseCode, String body, Header...headers) {
+        CloseableHttpResponse response = mock(CloseableHttpResponse.class);
+        doReturn(new BasicStatusLine(new ProtocolVersion("http", 1, 1), responseCode, "")).when(response).getStatusLine();
+        doReturn(new StringEntity(body, ContentType.APPLICATION_JSON)).when(response).getEntity();
+        doReturn(headers).when(response).getAllHeaders();
+        return response;
+    }
+
+    private Header[] copyHeaders(HttpUriRequest request, String...headerNames) {
+        return Arrays.stream(headerNames)
+            .map(request::getFirstHeader)
+            .filter(Objects::nonNull)
+            .toArray(Header[]::new);
+    }
+
     @BeforeEach
-    public void setBasePath() {
+    public void setBasePath() throws IOException {
         basePath = String.format("https://localhost:%d", port);
-        ReflectionTestUtils.setField(oidcTokenProviderEndpoint, "endpointUrl", "https://localhost:" + port + "/user/info");
+        ReflectionTestUtils.setField(oidcTokenProviderEndpoint, "endpointUrl", "https://oidc.provider.com/user/info");
+        ReflectionTestUtils.setField(oidcTokenProviderEndpoint, "secureHttpClientWithKeystore", mockClient);
+
+        doAnswer(invocation -> {
+            HttpUriRequest request = invocation.getArgument(0);
+            switch (request.getURI().getPath()) {
+            case "/user/info":
+                String authHeader = request.getFirstHeader(HttpHeaders.AUTHORIZATION).getValue();
+                if (StringUtils.equals(BEARER_AUTHENTICATION_PREFIX + " " + VALID_TOKEN, authHeader)) {
+                    return createResponse(SC_OK, "{\"detail\":\"information\")");
+                }
+                return createResponse(SC_UNAUTHORIZED, "{\"error\":\"message\")");
+            case "/request":
+                return createResponse(SC_OK, "{\"status\":\"ok\"}", copyHeaders(request, HttpHeaders.AUTHORIZATION, HEADER_OIDC_TOKEN));
+            default:
+                fail("Unknown endpoint");
+            }
+            return null;
+        }).when(mockClient).execute(any());
+
+        MetadataBuilder serviceMetadataBuilder = MetadataBuilder.defaultInstance().withZoweJwt();
+        Service service = new Service("app", "/app/api/v1/**", "api/v1");
+        applicationRegistry.addApplication(service, serviceMetadataBuilder, false);
+        applicationRegistry.setCurrentApplication("app");
     }
 
     @Test
-    void givenValidToken_thenAccessEndpoint() {
-     //   ReflectionTestUtils.setField(oidcTokenProviderEndpoint, "clock", new FixedClock(new Date(Instant.ofEpochSecond(1697060773 + 1000L).toEpochMilli())));
+    void givenValidTokenWithoutMapping_thenSetOidcToken() {
+        Config.mfUserExists = false;
         given()
             .header(HttpHeaders.AUTHORIZATION, BEARER_AUTHENTICATION_PREFIX + " " + VALID_TOKEN)
         .when()
-            .get(basePath + "/gateway/api/v1/request")
-          //  .get(basePath + "/gateway/api/v1/conformance/gateway")
-          //    .get(basePath + "/application/health")
+            .get(basePath + "/app/api/v1/request")
         .then()
-            .statusCode(is(HttpStatus.SC_OK));
+            .statusCode(is(SC_OK))
+            .header(HEADER_OIDC_TOKEN, VALID_TOKEN)
+            .header(HttpHeaders.AUTHORIZATION, nullValue());
+    }
+
+    @Test
+    void givenValidTokenWithMapping_thenSetOidcToken() {
+        Config.mfUserExists = true;
+        given()
+            .header(HttpHeaders.AUTHORIZATION, BEARER_AUTHENTICATION_PREFIX + " " + VALID_TOKEN)
+        .when()
+            .get(basePath + "/app/api/v1/request")
+        .then()
+            .statusCode(is(SC_OK))
+            .header(HttpHeaders.AUTHORIZATION, CoreMatchers.startsWith(BEARER_AUTHENTICATION_PREFIX))
+            .header(HttpHeaders.AUTHORIZATION, not(containsString(VALID_TOKEN)))
+            .header(HEADER_OIDC_TOKEN, nullValue());
     }
 
     @Test
@@ -86,25 +162,22 @@ class OIDCTokenProviderEndpointTest {
         given()
             .header(HttpHeaders.AUTHORIZATION, BEARER_AUTHENTICATION_PREFIX + " " + INVALID_TOKEN)
         .when()
-            .get(basePath + "/application/health")
+            .get(basePath + "/app/api/v1/request")
         .then()
-            .statusCode(is(HttpStatus.SC_UNAUTHORIZED));
+            .statusCode(is(SC_OK))
+            .header(HttpHeaders.AUTHORIZATION, INVALID_TOKEN)
+            .header(HEADER_OIDC_TOKEN, nullValue());
     }
 
-    @Controller
-    static class OidcProviderTestController {
+    @Configuration
+    static class Config {
 
-        @GetMapping("/user/info")
-        ResponseEntity<String> verify(@RequestHeader(HttpHeaders.AUTHORIZATION) String authenticationHeader) {
-            if (StringUtils.equals(BEARER_AUTHENTICATION_PREFIX + " " + VALID_TOKEN, authenticationHeader)) {
-                return ResponseEntity.ok("{\"detail\":\"information\")");
-            }
-            return ResponseEntity.status(HttpStatus.SC_UNAUTHORIZED).body("{\"error\":\"message\")");
-        }
+        private static boolean mfUserExists;
 
-        @GetMapping("/request")
-        ResponseEntity<String> getRequest(HttpServletRequest httpServletRequest) {
-            return ResponseEntity.ok(httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION));
+        @Bean
+        @Primary
+        public AuthenticationMapper oidcMapper() {
+            return authSource -> mfUserExists && VALID_TOKEN.equals(authSource.getRawSource()) ? MF_USER : null;
         }
 
     }
