a9eab7b78e65ae4de7616fc725ee1648d314a8ce
mirek163 <95099003+mirek163@users.noreply.github.com>
2024-08-06 13:14:48 +0200

    Adding Renovate with configs (#3667)
    * Adding Renovate with configs
    
    * Addressing review
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Addressing review
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Update renovate-config.js
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Adressing review
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Update renovate-config.js
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Update renovate-bot.yml
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Adding prTitle & label
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * changing deprecated prTitle
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Switch to github_token
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * WIP
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    * Update renovate-config.js
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: mirek163 <95099003+mirek163@users.noreply.github.com>
    Co-authored-by: Elena Kubantseva <elena.kubantseva@broadcom.com>

---
+++ renovate-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ["config:recommended", "group:allNonMajor"],
+    major:
+        { dependencyDashboardApproval: true },
+    dependencyDashboard: true,
+    repositories: ['zowe/api-layer'],
+    baseBranches: ["v2.x.x","v3.x.x"],
+    printConfig: true,
+    labels: ["dependencies"],
+    commitMessagePrefix:
+        'chore: ',
+    prHourlyLimit: 0, // removes rate limit for PR creation per hour
+    npmrc: "legacy-peer-deps=true", //for updating lock-files
+    npmrcMerge: true //be combined with a "global" npmrc
+};
