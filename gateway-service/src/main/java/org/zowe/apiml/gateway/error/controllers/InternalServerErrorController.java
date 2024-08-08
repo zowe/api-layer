f852c40dab159f35ce13abab19d7430a559532c1
Pavel Jareš <Pavel.Jares@broadcom.com>
2024-08-08 16:41:09 +0200

    revert InternalServerErrorController

--- gateway-service/src/main/java/org/zowe/apiml/gateway/error/controllers/InternalServerErrorController.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/error/controllers/InternalServerErrorController.java
@@ -10,7 +10,6 @@
 
 package org.zowe.apiml.gateway.error.controllers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
@@ -37,7 +36,6 @@ import java.util.List;
 @Controller
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Primary
-@Slf4j
 public class InternalServerErrorController implements ApimlErrorController {
     public static final String ERROR_ENDPOINT = "/internal_error";
 
@@ -85,7 +83,6 @@ public class InternalServerErrorController implements ApimlErrorController {
 
     private ResponseEntity<ApiMessageView> createResponseForInternalError(HttpServletRequest request, Throwable exc) {
         final int status = ErrorUtils.getErrorStatus(request);
-        log.debug("error creating the response for path {} and the status is ", request.getPathInfo(), status);
         Message message = messageService.createMessage("org.zowe.apiml.common.internalRequestError",
             ErrorUtils.getGatewayUri(request),
             ExceptionUtils.getMessage(exc),
