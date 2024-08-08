9fc597607639507fd4fb80af50d09679d95c1cf0
Pavel Jareš <58428711+pj892031@users.noreply.github.com>
2024-08-08 17:09:35 +0200

    feat: Validation of OIDC token via user info endpoint (v2) (#3670)
    Signed-off-by: Pavel Jares <Pavel.Jares@broadcom.com>
    Signed-off-by: sj895092 <shobhackm9@gmail.com>
    Co-authored-by: ShobhaJayanna <36433611+shobhajayanna@users.noreply.github.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/controllers/AuthController.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/controllers/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.zowe.apiml.gateway.security.service.AuthenticationService;
 import org.zowe.apiml.gateway.security.service.JwtSecurity;
-import org.zowe.apiml.gateway.security.service.token.OIDCTokenProvider;
+import org.zowe.apiml.gateway.security.service.token.OIDCTokenProviderJWK;
 import org.zowe.apiml.gateway.security.service.zosmf.ZosmfService;
 import org.zowe.apiml.gateway.security.webfinger.WebFingerProvider;
 import org.zowe.apiml.gateway.security.webfinger.WebFingerResponse;
@@ -69,7 +69,7 @@ public class AuthController {
     private final AccessTokenProvider tokenProvider;
 
     @Nullable
-    private final OIDCTokenProvider oidcProvider;
+    private final OIDCTokenProviderJWK oidcTokenProviderJWK;
     private final WebFingerProvider webFingerProvider;
 
     private static final String TOKEN_KEY = "token";
@@ -218,9 +218,11 @@ public class AuthController {
         }
         Optional<JWK> key = jwtSecurity.getJwkPublicKey();
         key.ifPresent(keys::add);
-        JWKSet oidcSet = oidcProvider.getJwkSet();
-        if (oidcSet != null) {
-            keys.addAll(oidcSet.getKeys());
+        if (oidcTokenProviderJWK != null) {
+            JWKSet oidcSet = oidcTokenProviderJWK.getJwkSet();
+            if (oidcSet != null) {
+                keys.addAll(oidcSet.getKeys());
+            }
         }
         return new JWKSet(keys).toJSONObject(true);
     }
@@ -293,7 +295,7 @@ public class AuthController {
     @HystrixCommand
     public ResponseEntity<String> validateOIDCToken(@RequestBody ValidateRequestModel validateRequestModel) {
         String token = validateRequestModel.getToken();
-        if (oidcProvider != null && oidcProvider.isValid(token)) {
+        if (oidcTokenProviderJWK != null && oidcTokenProviderJWK.isValid(token)) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
