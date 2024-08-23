e413a9d74294b1f407618e6286d22a7a8262272b
Nafi Xhafa <164854562+nxhafa@users.noreply.github.com>
2024-08-23 16:49:20 +0200

    chore: adding e2e test for rendering swagger (#3696)
    * fix: adding e2e test for rendering swagger
    
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>
    
    * fix: updating service homepage url in the e2e test
    
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>
    
    * using custom command in e2e cypress tests
    
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>
    
    ---------
    
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>
    Co-authored-by: nx673747 <nafi.xhafa@broadcom.com>

--- api-catalog-ui/frontend/cypress/support/commands.js
+++ api-catalog-ui/frontend/cypress/support/commands.js
@@ -25,3 +25,16 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 import 'cypress-file-upload';
+
+Cypress.Commands.add("login", (username, password) => {
+    cy.visit(`${Cypress.env('catalogHomePage')}/#/login`);
+    cy.url().should('contain', '/login');
+
+    cy.get('button[type="submit"').as('submitButton').should('exist');
+
+    cy.get('#username').type(username);
+    cy.get('input[name="password"]').type(password);
+
+    cy.get('@submitButton').should('not.be.disabled');
+    cy.get('@submitButton').click();
+});
