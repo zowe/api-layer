a9fcc3d3f58788349c124810b3bb9a16a849d093
ac892247 <a.chmelo@gmail.com>
2024-08-27 10:17:33 +0200

    configure websocket connection
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/websocket/ApimlWebSocketClient.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/websocket/ApimlWebSocketClient.java
@@ -10,23 +10,43 @@
 
 package org.zowe.apiml.gateway.websocket;
 
+import jakarta.websocket.ClientEndpointConfig;
 import jakarta.websocket.Session;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.websocket.Constants;
 import org.apache.tomcat.websocket.WsWebSocketContainer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.socket.HandshakeInfo;
 import org.springframework.web.reactive.socket.adapter.StandardWebSocketSession;
 import org.springframework.web.reactive.socket.client.StandardWebSocketClient;
 import reactor.core.publisher.Sinks;
 
+import java.util.List;
+
+@Slf4j
 public class ApimlWebSocketClient extends StandardWebSocketClient {
 
-    public ApimlWebSocketClient() {
-        super(new WsWebSocketContainer());
+    @Value("${server.webSocket.connectTimeout:45000}")
+    private String ioTimeout = "45000";
+
+    public ApimlWebSocketClient(WsWebSocketContainer webSocketContainer) {
+        super(webSocketContainer);
     }
 
     @Override
     protected StandardWebSocketSession createWebSocketSession(
         Session session, HandshakeInfo info, Sinks.Empty<Void> completionSink) {
-
         return new ApimlWebSocketSession(session, info, bufferFactory(), completionSink);
     }
+
+    @Override
+    protected ClientEndpointConfig createEndpointConfig(ClientEndpointConfig.Configurator configurator, List<String> subProtocols) {
+        var config = ClientEndpointConfig.Builder.create()
+            .configurator(configurator)
+            .preferredSubprotocols(subProtocols)
+            .build();
+        config.getUserProperties().put(Constants.IO_TIMEOUT_MS_PROPERTY, ioTimeout);
+        return config;
+    }
+
 }
