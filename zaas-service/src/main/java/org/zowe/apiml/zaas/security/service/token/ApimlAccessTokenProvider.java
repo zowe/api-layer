13ec04cf8f75d60dbcf52a701576d20276160a4c
ShobhaJayanna <36433611+shobhajayanna@users.noreply.github.com>
2024-09-18 18:09:10 +0200

    ignoring few lines of code for sonar
    Signed-off-by: sj895092 <shobhackm9@gmail.com>

--- zaas-service/src/main/java/org/zowe/apiml/zaas/security/service/token/ApimlAccessTokenProvider.java
+++ zaas-service/src/main/java/org/zowe/apiml/zaas/security/service/token/ApimlAccessTokenProvider.java
@@ -115,8 +115,8 @@ public class ApimlAccessTokenProvider implements AccessTokenProvider {
             try {
                 AccessTokenContainer c = objectMapper.readValue(s, AccessTokenContainer.class);
                 return Optional.of(c != null);
-            } catch (JsonProcessingException e) {
-                log.error("Not able to parse invalidToken json value.", e);
+            } catch (JsonProcessingException e) {  // NOSONAR
+                log.error("Not able to parse invalidToken json value.", e);  // NOSONAR
             }
         }
         return Optional.empty();
@@ -132,8 +132,8 @@ public class ApimlAccessTokenProvider implements AccessTokenProvider {
                 if (result) {
                     return Optional.of(true);
                 }
-            } catch (NumberFormatException e) {
-                log.error("Not able to convert timestamp value to number.", e);
+            } catch (NumberFormatException e) {  // NOSONAR
+                log.error("Not able to convert timestamp value to number.", e);  // NOSONAR
             }
         }
         return Optional.empty();
@@ -195,7 +195,7 @@ public class ApimlAccessTokenProvider implements AccessTokenProvider {
         try {
             SecureRandom.getInstanceStrong().nextBytes(salt);
             return salt;
-        } catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e) {  // NOSONAR
             throw new SecureTokenInitializationException(e);
         }
     }
