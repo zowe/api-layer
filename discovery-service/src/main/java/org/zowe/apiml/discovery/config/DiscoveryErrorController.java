c20af89abd82a4e4d59f69a98af702821ebf8130
Pablo Hernán Carle <pablo.carle@broadcom.com>
2024-09-26 12:09:55 +0200

    sonar
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- discovery-service/src/main/java/org/zowe/apiml/discovery/config/DiscoveryErrorController.java
+++ discovery-service/src/main/java/org/zowe/apiml/discovery/config/DiscoveryErrorController.java
@@ -21,14 +21,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 import java.util.Map;
 
 @Primary
 @Controller
-@RequestMapping(value = "${server.error.path:${error.path:/error}}", method = RequestMethod.GET)
+@RequestMapping(value = "${server.error.path:${error.path:/error}}") // NOSONAR - No security risk, the controller cleans body for specified endpoint patterns
 public class DiscoveryErrorController extends BasicErrorController {
 
     public DiscoveryErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties, List<ErrorViewResolver> errorViewResolvers) {
