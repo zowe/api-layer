19878f01f9ce1cc8eb18fd2390da824fca36a9bb
Pablo Hernán Carle <pablo.carle@broadcom.com>
2024-09-26 11:25:46 +0200

    specify method
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- discovery-service/src/main/java/org/zowe/apiml/discovery/config/DiscoveryErrorController.java
+++ discovery-service/src/main/java/org/zowe/apiml/discovery/config/DiscoveryErrorController.java
@@ -21,13 +21,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 import java.util.Map;
 
 @Primary
 @Controller
-@RequestMapping("${server.error.path:${error.path:/error}}")
+@RequestMapping(value = "${server.error.path:${error.path:/error}}", method = RequestMethod.GET)
 public class DiscoveryErrorController extends BasicErrorController {
 
     public DiscoveryErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties, List<ErrorViewResolver> errorViewResolvers) {
