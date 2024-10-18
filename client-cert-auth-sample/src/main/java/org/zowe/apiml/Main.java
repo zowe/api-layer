2c744067602f2959b231378feb47f8e90f79f760
ac892247 <a.chmelo@gmail.com>
2024-10-18 13:43:24 +0200

    unit test with https server
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- client-cert-auth-sample/src/main/java/org/zowe/apiml/Main.java
+++ client-cert-auth-sample/src/main/java/org/zowe/apiml/Main.java
@@ -1,38 +1,33 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
 package org.zowe.apiml;
 
 import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
-import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.http.Header;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.config.Registry;
-import org.apache.hc.core5.http.config.RegistryBuilder;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
 
-
-import javax.net.ssl.SSLContext;
-
-import java.awt.desktop.ScreenSleepEvent;
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.security.Key;
 import java.security.KeyStore;
 import java.security.cert.Certificate;
 
 public class Main {
-    //private static final String API_URL = "https://hostname:port/bcmdbmdsh/api/v1/gen/agents?flattenResults=false"; // Replace with your API URL
-    private static final String API_URL = "https://hostname:port/gateway/api/v1/auth/login"; // Replace with your API URL
-    private static final String CLIENT_CERT_PATH = "/path/to/keystore.p12"; // Replace with your client cert path
+
+    private static final String API_URL = "https://localhost:8080/gateway/api/v1/auth/login"; // Replace with your API URL
+    private static final String CLIENT_CERT_PATH = "../keystore/client_cert/client-certs.p12"; // Replace with your client cert path
     private static final String CLIENT_CERT_PASSWORD = "password"; // Replace with your cert password
-    private static final String CLIENT_CERT_ALIAS = "mycert"; // Replace with your signed client cert alias
-    private static final String PRIVATE_KEY_ALIAS = "server"; // Replace with your private key alias
+    private static final String CLIENT_CERT_ALIAS = "apimtst"; // Replace with your signed client cert alias
+    private static final String PRIVATE_KEY_ALIAS = "apimtst"; // Replace with your private key alias
 
 
     public static void main(String[] args) {
@@ -43,66 +38,41 @@ public class Main {
                 keyStore.load(keyStoreStream, CLIENT_CERT_PASSWORD.toCharArray());
             }
 
-            Key key = keyStore.getKey(PRIVATE_KEY_ALIAS, CLIENT_CERT_PASSWORD.toCharArray()); // Load private key from original keystore
-            Certificate cert = keyStore.getCertificate(CLIENT_CERT_ALIAS); // Load signed certificate from original keystore
+            var key = keyStore.getKey(PRIVATE_KEY_ALIAS, CLIENT_CERT_PASSWORD.toCharArray()); // Load private key from original keystore
+            var cert = keyStore.getCertificate(CLIENT_CERT_ALIAS); // Load signed certificate from original keystore
 
             // Create new keystore
-            KeyStore newKeyStore = KeyStore.getInstance("PKCS12");
+            var newKeyStore = KeyStore.getInstance("PKCS12");
             newKeyStore.load(null);
             newKeyStore.setKeyEntry(PRIVATE_KEY_ALIAS, key, CLIENT_CERT_PASSWORD.toCharArray(), new Certificate[]{ cert }); // Create an entry with private key + signed certificate
 
             // Create SSL context with the client certificate
-//            SSLContext sslContext = SSLContexts.custom()
-//                .loadKeyMaterial(newKeyStore, CLIENT_CERT_PASSWORD.toCharArray())
-//                .build();
-            var sslContext = new SSLContextBuilder()
+            var sslContext = new SSLContextBuilder().loadTrustMaterial((chain,type)->true)
                 .loadKeyMaterial(newKeyStore, CLIENT_CERT_PASSWORD.toCharArray()).build();
-
-            // Create an HTTP client with the SSL context
-            HttpClientBuilder clientBuilder = HttpClientBuilder.create();
-
             var sslsf = new DefaultClientTlsStrategy(sslContext);
-//            var socketFactoryRegistry =
-//                RegistryBuilder.<DefaultClientTlsStrategy> create()
-//                    .register("https", sslsf)
-//                    .build();
+
 
             var connectionManager = BasicHttpClientConnectionManager.create((s)->sslsf);
-            var clientBuilder1 = clientBuilder.setConnectionManager(connectionManager);
 
+            var clientBuilder = HttpClientBuilder.create().setConnectionManager(connectionManager);
 
-            try (CloseableHttpClient httpClient = clientBuilder1.build()) {
-
-//                // Create a GET request
-//                HttpGet httpGet = new HttpGet(API_URL);
+            try (var httpClient = clientBuilder.build()) {
 
                 // Create a POST request
-                HttpPost httpPost = new HttpPost(API_URL);
+                var httpPost = new HttpPost(API_URL);
 
                 // Execute the request
-                HttpResponse response = httpClient.execute(httpPost, res -> res);
+                var response = httpClient.execute(httpPost, res -> res);
 
                 // Print the response status
                 System.out.println("Response Code: " + response.getCode());
 
                 // Print headers
-                Header[] headers = response.getHeaders();
-                for (Header header : headers) {
+                var headers = response.getHeaders();
+                for (var header : headers) {
                     System.out.println("Key : " + header.getName()
                         + " ,Value : " + header.getValue());
                 }
-
-                // You can read the response body if needed
-//                HttpEntity entity = response.getEntity();
-//                StringBuilder builder = new StringBuilder();
-//                try (BufferedInputStream is = new BufferedInputStream(entity.getContent())) {
-//                    byte[] contents = new byte[1024];
-//                    int bytesRead = 0;
-//                    while ((bytesRead = is.read(contents)) != -1) {
-//                        builder.append(new String(contents, 0, bytesRead));
-//                    }
-//                }
-//                System.out.println("Response Body: " + builder.toString());
             }
         } catch (Exception e) {
             e.printStackTrace();
---
+++ client-cert-auth-sample/src/test/java/org/zowe/apiml/MainTest.java
@@ -0,0 +1,103 @@
+package org.zowe.apiml;
+
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsExchange;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.net.InetSocketAddress;
+import java.security.KeyStore;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class MainTest {
+
+    static HttpsServer httpServer;
+    static AssertionError error;
+
+    @BeforeAll
+    static void setup() throws Exception {
+        var inetAddress = new InetSocketAddress("127.0.0.1", 8080);
+        httpServer = HttpsServer.create(inetAddress, 0);
+
+        var sslContext = SSLContext.getInstance("TLS");
+        var ks = KeyStore.getInstance("PKCS12");
+        try (var fis = new FileInputStream("../keystore/localhost/localhost.keystore.p12")) {
+            ks.load(fis, "password".toCharArray());
+        }
+        var km = KeyManagerFactory.getInstance("SunX509");
+        km.init(ks, "password".toCharArray());
+        var ts = KeyStore.getInstance("PKCS12");
+        try (var fis = new FileInputStream("../keystore/localhost/localhost.truststore.p12")) {
+            ts.load(fis, "password".toCharArray());
+        }
+        var tm = TrustManagerFactory.getInstance("SunX509");
+        tm.init(ts);
+
+        sslContext.init(km.getKeyManagers(), tm.getTrustManagers(), null);
+        sslContext.getDefaultSSLParameters().setNeedClientAuth(true);
+        sslContext.getDefaultSSLParameters().setWantClientAuth(true);
+
+
+        var httpsConfigurator = new TestHttpsConfigurator(sslContext);
+        httpServer.setHttpsConfigurator(httpsConfigurator);
+        httpServer.createContext("/gateway/api/v1/auth/login", exchange -> {
+
+            exchange.sendResponseHeaders(204, 0);
+            var clientCert = ((HttpsExchange) exchange).getSSLSession().getPeerCertificates();
+            try {
+                // client certificate must be present at this stage
+                assertNotNull(clientCert);
+            } catch (AssertionError e) {
+                error = e;
+            }
+            exchange.close();
+        });
+        httpServer.start();
+
+    }
+
+    @AfterAll
+    static void tearDown() {
+
+        httpServer.stop(0);
+        if (error != null) {
+            throw error;
+        }
+    }
+
+    @Test
+    void givenHttpsRequestWithClientCertificate_thenPeerCertificateMustBeAvailable() {
+        // Assertion is done on the server to make sure that client certificate was delivered.
+        // Assertion error is then rethrown in the tear down method in case certificate was not present.
+        Main.main(null);
+    }
+
+    static class TestHttpsConfigurator extends HttpsConfigurator {
+        /**
+         * Creates a Https configuration, with the given {@link SSLContext}.
+         *
+         * @param context the {@code SSLContext} to use for this configurator
+         * @throws NullPointerException if no {@code SSLContext} supplied
+         */
+        public TestHttpsConfigurator(SSLContext context) {
+            super(context);
+        }
+
+        @Override
+        public void configure(HttpsParameters params) {
+            var parms = getSSLContext().getDefaultSSLParameters();
+            parms.setNeedClientAuth(true);
+            params.setWantClientAuth(true);
+            params.setSSLParameters(parms);
+        }
+    }
+
+}
