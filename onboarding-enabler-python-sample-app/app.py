da28d3af454cdcf5324d62a6b10cf87e3f28641e
Andrea Tabone <andrea.tabone@broadcom.com>
2024-10-24 14:01:53 +0200

    refactoring
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

--- onboarding-enabler-python-sample-app/app.py
+++ onboarding-enabler-python-sample-app/app.py
@@ -1,9 +1,20 @@
+"""
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+"""
+
 import importlib.util
 import sys
 import os
 from flask import Flask, jsonify
 import ssl
-
+import yaml
+import json
 # Add the parent directory of 'onboarding-enabler-python' to sys.path
 base_directory = os.path.dirname(os.path.abspath(__file__))
 parent_directory = os.path.abspath(os.path.join(base_directory, '..'))
@@ -32,8 +43,9 @@ enabler = PythonEnabler(config_file=config_file_path)
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
 ssl_context.load_cert_chain(
     "../keystore/localhost/localhost.keystore.cer",  # Path to your certificate file
-    "../keystore/localhost/localhost.keystore.key"   # Path to your key file
+    "../keystore/localhost/localhost.keystore.key"  # Path to your key file
 )
+
 
 @app.route("/registerInfo", methods=['GET'])
 def register_python_enabler():
@@ -41,16 +53,45 @@ def register_python_enabler():
     enabler.register()
     return jsonify({"message": "Registered with Python eureka client to Discovery service"})
 
+
 @app.route("/hello", methods=['GET'])
 def hello():
     """Simple hello endpoint for testing."""
     return jsonify({"message": "Hello world in swagger"})
+
+
+@app.route("/apidoc", methods=['GET'])
+def getSwagger():
+    with open('pythonSwagger.json') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        response = app.response_class(
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 @app.route("/unregisterInfo", methods=['GET'])
 def unregister_python_enabler():
     """Endpoint to manually unregister the service."""
     enabler.unregister()
     return jsonify({"message": "Unregistered Python eureka client from Discovery service"})
+
+
+# investigate how to get /python_enabler_service/application/info link on discovery service
+# localhost:10018/python_enabler_service/application/info works if routed correctly on line 71, but link doesn't appear in discovery service.
+# Only localhost:10018/application/info appears in discovery service
+@app.route("/application/info", methods=['GET'])
+def getApplicationInfo():
+    data = {"build": {"name": "python-service", "operatingSystem": "Mac OS X (11.6.7)", "time": 1660222556.497000000,
+                      "machine": "Amandas-MacBook-Pro.local", "number": "n/a", "version": "2.3.3-SNAPSHOT", "by": "<userId>",
+                      "group": "api-layer", "artifact": "python-service"}}
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == "__main__":
     # Load SSL configuration
