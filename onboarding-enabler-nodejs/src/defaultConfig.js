09db3c3758bc843df375ca3a989bd9969fc5c8da
nx673747 <nafi.xhafa@broadcom.com>
2024-11-14 16:03:21 +0100

    initial commit; removing eureka-js-client dependency and copying over the src files
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>

---
+++ onboarding-enabler-nodejs/src/defaultConfig.js
@@ -0,0 +1,23 @@
+// Default configuration values:
+export default {
+  requestMiddleware: (request, done) => done(request),
+  shouldUseDelta: false,
+  eureka: {
+    heartbeatInterval: 30000,
+    registryFetchInterval: 30000,
+    maxRetries: 3,
+    requestRetryDelay: 500,
+    fetchRegistry: true,
+    filterUpInstances: true,
+    servicePath: '/eureka/v2/apps/',
+    ssl: false,
+    useDns: false,
+    preferSameZone: true,
+    clusterRefreshInterval: 300000,
+    fetchMetadata: true,
+    registerWithEureka: true,
+    useLocalMetadata: false,
+    preferIpAddress: false,
+  },
+  instance: {},
+};
