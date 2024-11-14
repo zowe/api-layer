14235ac67379ac5cde1d7b0ee65ad106aa7666e5
Olha <olya.kishkinova@gmail.com>
2024-11-14 17:17:31 +0100

    remove imports

--- gateway-service/src/main/java/org/zowe/apiml/gateway/filters/InMemoryRateLimiter.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/filters/InMemoryRateLimiter.java
@@ -14,10 +14,8 @@ import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
