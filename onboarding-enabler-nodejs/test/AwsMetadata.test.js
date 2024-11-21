b461a5ccd01c7c3256c2d8853e9b451c2a6112a0
Pavel Jareš <Pavel.Jares@broadcom.com>
2024-11-21 14:56:34 +0100

    missing files
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- onboarding-enabler-nodejs/test/AwsMetadata.test.js
+++ onboarding-enabler-nodejs/test/AwsMetadata.test.js
@@ -17,60 +17,31 @@ describe('AWS Metadata client', () => {
       global.request.get.restore();
     });
 
-    it('should call metadata URIs', () => {
-      const requestStub = sinon.stub(global, 'get');
+    it('should call metadata URIs', async() => {
+      const requestStub = sinon.stub(global, 'fetch');
 
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/ami-id',
-      }).yields(null, { statusCode: 200 }, 'ami-123');
+      const mock = (url, body) => {
+        requestStub.withArgs(url).returns(Promise.resolve({
+          error: () => null,
+          statusCode: 200,
+          body: () => body,
+        }));
+      };
 
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/instance-id',
-      }).yields(null, { statusCode: 200 }, 'i123');
+      mock('http://127.0.0.1:8888/latest/meta-data/ami-id', 'ami-123');
+      mock('http://127.0.0.1:8888/latest/meta-data/instance-id', 'i123');
+      mock('http://127.0.0.1:8888/latest/meta-data/instance-type', 'medium');
+      mock('http://127.0.0.1:8888/latest/meta-data/local-ipv4', '1.1.1.1');
+      mock('http://127.0.0.1:8888/latest/meta-data/local-hostname', 'ip-127-0-0-1');
+      mock('http://127.0.0.1:8888/latest/meta-data/placement/availability-zone', 'fake-1');
+      mock('http://127.0.0.1:8888/latest/meta-data/public-hostname', 'ec2-127-0-0-1');
+      mock('http://127.0.0.1:8888/latest/meta-data/public-ipv4', '2.2.2.2');
+      mock('http://127.0.0.1:8888/latest/meta-data/mac', 'AB:CD:EF:GH:IJ');
+      mock('http://127.0.0.1:8888/latest/dynamic/instance-identity/document', '{"accountId":"123456"}');
+      mock('http://127.0.0.1:8888/latest/meta-data/network/interfaces/macs/AB:CD:EF:GH:IJ/vpc-id', 'vpc123');
 
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/instance-type',
-      }).yields(null, { statusCode: 200 }, 'medium');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/local-ipv4',
-      }).yields(null, { statusCode: 200 }, '1.1.1.1');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/local-hostname',
-      }).yields(null, { statusCode: 200 }, 'ip-127-0-0-1');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/placement/availability-zone',
-      }).yields(null, { statusCode: 200 }, 'fake-1');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/public-hostname',
-      }).yields(null, { statusCode: 200 }, 'ec2-127-0-0-1');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/public-ipv4',
-      }).yields(null, { statusCode: 200 }, '2.2.2.2');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/mac',
-      }).yields(null, { statusCode: 200 }, 'AB:CD:EF:GH:IJ');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/dynamic/instance-identity/document',
-      }).yields(null, { statusCode: 200 }, '{"accountId":"123456"}');
-
-      requestStub.withArgs({
-        url: 'http://127.0.0.1:8888/latest/meta-data/network/interfaces/macs/AB:CD:EF:GH:IJ/vpc-id',
-      }).yields(null, { statusCode: 200 }, 'vpc123');
-
-      const fetchCb = sinon.spy();
-      client.fetchMetadata(fetchCb);
-
-      expect(global.request.get).to.have.been.callCount(11);
-
-      expect(fetchCb).to.have.been.calledWithMatch({
-        accountId: '123456',
+      const expected = {
+        accountId: '123456Z',
         'ami-id': 'ami-123',
         'availability-zone': 'fake-1',
         'instance-id': 'i123',
@@ -81,7 +52,43 @@ describe('AWS Metadata client', () => {
         'public-hostname': 'ec2-127-0-0-1',
         'public-ipv4': '2.2.2.2',
         'vpc-id': 'vpc123',
+      };
+
+      new Promise((resolve) => {
+        client.fetchMetadata(data => {
+          resolve(data);
+        });
+      }).then(actual => {
+        console.log('expected');
+        console.log(expected);
+        expect(actual).to.deep.equal(expected);
+        console.log('ok');
+      }, reason => {
+        throw new Error(reason);
       });
+
+      //
+      // const fetchCb = sinon.spy();
+      // client.fetchMetadata(fetchCb);
+      //
+      // console.log('arg');
+      // console.log(fetchCb.lastCall);
+      //
+      // // expect(requestStub).to.have.been.callCount(11);
+      //
+      // expect(fetchCb).to.have.been.calledWithMatch({
+      //   accountId: '123456',
+      //   'ami-id': 'ami-123',
+      //   'availability-zone': 'fake-1',
+      //   'instance-id': 'i123',
+      //   'instance-type': 'medium',
+      //   'local-hostname': 'ip-127-0-0-1',
+      //   'local-ipv4': '1.1.1.1',
+      //   mac: 'AB:CD:EF:GH:IJ',
+      //   'public-hostname': 'ec2-127-0-0-1',
+      //   'public-ipv4': '2.2.2.2',
+      //   'vpc-id': 'vpc123',
+      // });
     });
 
     it('should call metadata URIs and filter out null and undefined values', () => {
