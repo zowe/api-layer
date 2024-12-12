b83df3bf32b99f9c341ed43d3ed439078b9fc0c5
Richard Salac <richard.salac@broadcom.com>
2024-12-12 10:34:01 +0100

    cr

--- apiml-security-common/src/main/java/org/zowe/apiml/security/common/verify/CertificateValidator.java
+++ apiml-security-common/src/main/java/org/zowe/apiml/security/common/verify/CertificateValidator.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
@@ -55,6 +56,11 @@ public class CertificateValidator {
      * @return true if all given certificates are known false otherwise
      */
     public boolean isTrusted(X509Certificate[] certs) {
+        if (ObjectUtils.isEmpty(proxyCertificatesEndpoint)) {
+            log.debug("No endpoint configured to retrieve trusted certificates. Provide URL via apiml.security.x509.certificatesUrls");
+            return false;
+        }
+
         List<Certificate> trustedCerts = trustedCertificatesProvider.getTrustedCerts(proxyCertificatesEndpoint);
         for (X509Certificate cert : certs) {
             if (!trustedCerts.contains(cert)) {
