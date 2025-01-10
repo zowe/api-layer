22e074e86197a92844c159df18fbf5f29c3bb506
Pavel Jareš <Pavel.Jares@broadcom.com>
2025-01-10 10:37:26 +0100

    Revert "update of deprecated annotation @MockBean"
    This reverts commit 50ba76f61070f5eba75e5ffc12a708e36e387742.

--- discoverable-client/src/test/java/org/zowe/apiml/client/api/ZaasClientTestControllerTest.java
+++ discoverable-client/src/test/java/org/zowe/apiml/client/api/ZaasClientTestControllerTest.java
@@ -11,13 +11,12 @@
 package org.zowe.apiml.client.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.web.servlet.MockMvc;
 import org.zowe.apiml.client.configuration.ApplicationConfiguration;
@@ -26,6 +25,8 @@ import org.zowe.apiml.client.configuration.SpringComponentsConfiguration;
 import org.zowe.apiml.zaasclient.exception.ZaasClientErrorCodes;
 import org.zowe.apiml.zaasclient.exception.ZaasClientException;
 import org.zowe.apiml.zaasclient.service.ZaasClient;
+
+import jakarta.servlet.http.Cookie;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -40,7 +41,7 @@ class ZaasClientTestControllerTest {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
-    @MockitoBean
+    @MockBean
     private ZaasClient zaasClient;
 
     private static final String TOKEN_PREFIX = "apimlAuthenticationToken";
