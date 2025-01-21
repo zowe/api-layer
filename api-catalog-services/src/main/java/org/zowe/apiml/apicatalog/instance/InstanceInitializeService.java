b565a12b4047e0d50b49e23ae48ac67f3c50fc75
Pavel Jareš <Pavel.Jares@broadcom.com>
2025-01-21 16:17:38 +0100

    revert the behaviour about missing catalog instance
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/instance/InstanceInitializeService.java
+++ api-catalog-services/src/main/java/org/zowe/apiml/apicatalog/instance/InstanceInitializeService.java
@@ -78,8 +78,6 @@ public class InstanceInitializeService {
                 getAllInstances(apiCatalogInstance);
                 instanceRefreshService.start();
             }
-        } catch (RetryException re) {
-            throw re;
         } catch (InstanceInitializationException | GatewayNotAvailableException e) {
             throw new RetryException(e.getMessage());
         } catch (Exception e) {
