f9653e130e82f5355ebbb39815895d41bfe8dbc1
ac892247 <a.chmelo@gmail.com>
2025-02-06 18:54:48 +0100

    add eureka endpoints
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- apiml/src/main/java/org/zowe/apiml/MyApplicationsResource.java
+++ apiml/src/main/java/org/zowe/apiml/MyApplicationsResource.java
@@ -1,7 +1,15 @@
 package org.zowe.apiml;
 
+import com.netflix.appinfo.EurekaAccept;
+import com.netflix.eureka.resources.ApplicationResource;
 import com.netflix.eureka.resources.ApplicationsResource;
+import jakarta.annotation.Nullable;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +22,35 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/eureka/apps")
 @DependsOn("modulithConfig")
 public class MyApplicationsResource extends ApplicationsResource {
+    private static final String HEADER_ACCEPT = "Accept";
+    private static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
 
-    @GetMapping
-    public Mono<Response> apps() {
-        return Mono.just(super.getContainers(null, null, null, null, null, null));
+
+
+    @GetMapping("/{appId}")
+    public Mono<ApplicationResource> getAppResource(@PathParam("version") String version,
+                                                    @PathParam("appId") String appId) {
+        return Mono.just(super.getApplicationResource(version, appId));
     }
 
+    @GetMapping
+    public Mono<Response> getAllContainer(@PathParam("version") String version,
+                                        @HeaderParam(HEADER_ACCEPT) String acceptHeader,
+                                        @HeaderParam(HEADER_ACCEPT_ENCODING) String acceptEncoding,
+                                        @HeaderParam(EurekaAccept.HTTP_X_EUREKA_ACCEPT) String eurekaAccept,
+                                        @Context UriInfo uriInfo,
+                                        @Nullable @QueryParam("regions") String regionsStr) {
 
+        return Mono.just(super.getContainers(version, acceptHeader, acceptEncoding, eurekaAccept, uriInfo, regionsStr));
+    }
+
+    @GetMapping("/delta")
+    public Mono<Response> getContainerDiff(@PathParam("version") String version,
+                                                   @HeaderParam(HEADER_ACCEPT) String acceptHeader,
+                                                   @HeaderParam(HEADER_ACCEPT_ENCODING) String acceptEncoding,
+                                                   @HeaderParam(EurekaAccept.HTTP_X_EUREKA_ACCEPT) String eurekaAccept,
+                                                   @Context UriInfo uriInfo,
+                                                   @Nullable @QueryParam("regions") String regionsStr) {
+        return Mono.just(super.getContainerDifferential(version, acceptHeader, acceptEncoding, eurekaAccept, uriInfo, regionsStr));
+    }
 }
