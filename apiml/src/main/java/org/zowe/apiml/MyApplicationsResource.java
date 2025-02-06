05d42cc71159f32f3c2bf4189d4edd9d3cea203f
ac892247 <a.chmelo@gmail.com>
2025-02-06 20:21:17 +0100

    return json
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- apiml/src/main/java/org/zowe/apiml/MyApplicationsResource.java
+++ apiml/src/main/java/org/zowe/apiml/MyApplicationsResource.java
@@ -1,19 +1,28 @@
 package org.zowe.apiml;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.appinfo.EurekaAccept;
+import com.netflix.discovery.converters.jackson.EurekaJsonJacksonCodec;
+import com.netflix.discovery.shared.Applications;
 import com.netflix.eureka.resources.ApplicationResource;
 import com.netflix.eureka.resources.ApplicationsResource;
 import jakarta.annotation.Nullable;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/eureka/apps")
 @DependsOn("modulithConfig")
+@Slf4j
 public class MyApplicationsResource extends ApplicationsResource {
     private static final String HEADER_ACCEPT = "Accept";
     private static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
@@ -33,7 +42,16 @@ public class MyApplicationsResource extends ApplicationsResource {
         @Nullable @RequestParam(value = "regions", required = false) String regionsStr) {
 
         var res = super.getContainers(null, acceptHeader, acceptEncoding, eurekaAccept, null, regionsStr);
-        return Mono.just(res.getEntity());
+        ObjectMapper mapper = new EurekaJsonJacksonCodec().getObjectMapper(Applications.class);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        try {
+            var applications = mapper.readValue((String)res.getEntity(), Applications.class);
+            return Mono.just(applications);
+        } catch (IOException e) {
+           log.error(e.getMessage(), e);
+        }
+        return Mono.error(new RuntimeException("Could not get applications"));
     }
 
     @GetMapping("/delta")
