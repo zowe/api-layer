04590d13e8a30af2b4d7743ff844e53dc41f9da8
nx673747 <nafi.xhafa@broadcom.com>
2025-03-21 10:50:21 +0100

    initial commit; adding base for apiml service
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>

---
+++ apiml/src/main/java/org/zowe/apiml/ApimlApplication.java
@@ -0,0 +1,12 @@
+package org.zowe.apiml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApimlApplication.class, args);
+    }
+}
