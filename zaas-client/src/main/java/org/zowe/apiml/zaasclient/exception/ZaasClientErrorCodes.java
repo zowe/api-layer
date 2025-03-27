518466fe3282e2b48925de9ea0cdeec97d9a8c0e
ShobhaJayanna <36433611+shobhajayanna@users.noreply.github.com>
2025-03-27 14:15:25 +0100

    change error code to ZWEAS418E
    Signed-off-by: sj895092 <shobhackm9@gmail.com>

--- zaas-client/src/main/java/org/zowe/apiml/zaasclient/exception/ZaasClientErrorCodes.java
+++ zaas-client/src/main/java/org/zowe/apiml/zaasclient/exception/ZaasClientErrorCodes.java
@@ -25,7 +25,7 @@ public enum ZaasClientErrorCodes {
         "and application ID is configured properly by referring to  Using PassTickets in the guide for your security provider", 400),
     TOKEN_NOT_PROVIDED("ZWEAS401E", "Token is not provided", 401),
     SERVICE_UNAVAILABLE("ZWEAS404E", "Gateway service is unavailable", 404),
-    INVALID_REQUEST_AUTHENTICATION("ZWEAS500E", "The generation of the PassTicket failed. Verify that the user and the application name are valid, and check that corresponding permissions have been set up.", 500),
+    INVALID_REQUEST_AUTHENTICATION("ZWEAS418E", "The generation of the PassTicket failed. Verify that the user and the application name are valid, and check that corresponding permissions have been set up.", 500),
     EXPIRED_PASSWORD("ZWEAT412E", "The specified password is expired", 401),
     APPLICATION_NAME_NOT_FOUND("ZWEAS417E", "The application name wasn't found", 404);
 
