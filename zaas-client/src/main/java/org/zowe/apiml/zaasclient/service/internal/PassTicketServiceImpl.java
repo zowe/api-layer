0bc276256f062bf0f875c396bc6a57b0e8d04a5d
sj895092 <shobhackm9@gmail.com>
2025-03-27 14:59:32 +0100

    change error code
    Signed-off-by: sj895092 <shobhackm9@gmail.com>

--- zaas-client/src/main/java/org/zowe/apiml/zaasclient/service/internal/PassTicketServiceImpl.java
+++ zaas-client/src/main/java/org/zowe/apiml/zaasclient/service/internal/PassTicketServiceImpl.java
@@ -78,7 +78,7 @@ class PassTicketServiceImpl implements PassTicketService {
             } else if (statusCode == 400) {
                 throw new ZaasClientException(ZaasClientErrorCodes.BAD_REQUEST, obtainedMessage);
             } else if (statusCode == 500) {
-                throw new ZaasClientException(ZaasClientErrorCodes.INVALID_REQUEST_AUTHENTICATION, obtainedMessage);
+                throw new ZaasClientException(ZaasClientErrorCodes.INTERNAL_SERVER_ERROR, obtainedMessage);
             } else {
                 throw new ZaasClientException(ZaasClientErrorCodes.GENERIC_EXCEPTION, obtainedMessage);
             }
