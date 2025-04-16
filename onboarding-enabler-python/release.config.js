ea2d59124569dcffd55194b590e96b6f2b4820e2
Andrea Tabone <andrea.tabone@broadcom.com>
2025-04-16 16:25:31 +0200

    add octo-release config
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

---
+++ onboarding-enabler-python/release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    branches: [
+        {
+            name: "main",
+            level: "none",
+            prerelease: true
+        },
+        {
+            name: "zowe-v?-lts",
+            level: "patch"
+        }
+    ],
+    plugins: [
+        "@octorelease/changelog",
+        "@octorelease/pypi",
+        ["@octorelease/github", {
+            publishRelease: true
+        }],
+        "@octorelease/git"
+    ]
+};
