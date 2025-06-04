0d2042c24e0f4fd5ce1ab73c8a725aacfd23b2d9
nx673747 <nafi.xhafa@broadcom.com>
2025-06-04 10:55:54 +0200

    adjust license
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>

--- apiml-utility/src/main/java/org/zowe/apiml/product/logging/ApimlDuplicateMessagesFilter.java
+++ apiml-utility/src/main/java/org/zowe/apiml/product/logging/ApimlDuplicateMessagesFilter.java
@@ -1,13 +1,11 @@
 /*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
  *
- *  * This program and the accompanying materials are made available under the terms of the
- *  * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- *  * https://www.eclipse.org/legal/epl-v20.html
- *  *
- *  * SPDX-License-Identifier: EPL-2.0
- *  *
- *  * Copyright Contributors to the Zowe Project.
+ * SPDX-License-Identifier: EPL-2.0
  *
+ * Copyright Contributors to the Zowe Project.
  */
 
 package org.zowe.apiml.product.logging;
@@ -47,10 +45,10 @@ public class ApimlDuplicateMessagesFilter extends DuplicateMessageFilter {
     private String getLogMessage(String format, Object[] params, Throwable t) {
         FormattingTuple formattingTuple = getFormattedMessage(format, params, t);
         return new StringBuilder()
-            .append(formattingTuple.getMessage())
-            .append(formattingTuple.getThrowable() == null ? "" : "\n")
-            .append(String.join("\n", ExceptionUtils.getStackFrames(formattingTuple.getThrowable())))
-            .toString();
+                .append(formattingTuple.getMessage())
+                .append(formattingTuple.getThrowable() == null ? "" : "\n")
+                .append(String.join("\n", ExceptionUtils.getStackFrames(formattingTuple.getThrowable())))
+                .toString();
     }
 
     private FormattingTuple getFormattedMessage(String format, Object[] params, Throwable t) {
