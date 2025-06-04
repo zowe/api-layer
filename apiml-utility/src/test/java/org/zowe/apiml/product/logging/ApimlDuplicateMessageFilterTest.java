15324e91a695f5efb768070ec582e4a1b24a314e
nx673747 <nafi.xhafa@broadcom.com>
2025-06-04 10:59:54 +0200

    adjust license for test class
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>

--- apiml-utility/src/test/java/org/zowe/apiml/product/logging/ApimlDuplicateMessageFilterTest.java
+++ apiml-utility/src/test/java/org/zowe/apiml/product/logging/ApimlDuplicateMessageFilterTest.java
@@ -1,13 +1,11 @@
 /*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
  *
- *  * This program and the accompanying materials are made available under the terms of the
- *  * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- *  * https://www.eclipse.org/legal/epl-v20.html
- *  *
- *  * SPDX-License-Identifier: EPL-2.0
- *  *
- *  * Copyright Contributors to the Zowe Project.
+ * SPDX-License-Identifier: EPL-2.0
  *
+ * Copyright Contributors to the Zowe Project.
  */
 
 package org.zowe.apiml.product.logging;
@@ -51,17 +49,17 @@ class ApimlDuplicateMessageFilterTest {
 
             // No args
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                null, null, null), "Expected FilterReply.NEUTRAL");
+                    null, null, null), "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                null, null, exception), "Expected FilterReply.NEUTRAL");
+                    null, null, exception), "Expected FilterReply.NEUTRAL");
 
             // With args
             assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                null, new Object[]{1}, null), "Expected FilterReply.DENY");
+                    null, new Object[]{1}, null), "Expected FilterReply.DENY");
             assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                null, new Object[]{1, exception}, null), "Expected FilterReply.DENY");
+                    null, new Object[]{1, exception}, null), "Expected FilterReply.DENY");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                null, new Object[]{1, new RuntimeException("exception in arguments")}, new RuntimeException("outside exception")), "Expected FilterReply.NEUTRAL");
+                    null, new Object[]{1, new RuntimeException("exception in arguments")}, new RuntimeException("outside exception")), "Expected FilterReply.NEUTRAL");
         }
 
         @Test
@@ -73,17 +71,17 @@ class ApimlDuplicateMessageFilterTest {
 
             // No args
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "", null, null), "Expected FilterReply.NEUTRAL");
+                    "", null, null), "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "", null, exception), "Expected FilterReply.NEUTRAL");
+                    "", null, exception), "Expected FilterReply.NEUTRAL");
 
             // With args
             assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "", new Object[]{1}, null), "Expected FilterReply.DENY");
+                    "", new Object[]{1}, null), "Expected FilterReply.DENY");
             assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "", new Object[]{1, exception}, null), "Expected FilterReply.DENY");
+                    "", new Object[]{1, exception}, null), "Expected FilterReply.DENY");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "", new Object[]{1, new RuntimeException("exception in arguments")}, new RuntimeException("outside exception")), "Expected FilterReply.NEUTRAL");
+                    "", new Object[]{1, new RuntimeException("exception in arguments")}, new RuntimeException("outside exception")), "Expected FilterReply.NEUTRAL");
         }
 
         @Test
@@ -95,15 +93,15 @@ class ApimlDuplicateMessageFilterTest {
 
             // No exception
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "First Message", null, null), "Expected FilterReply.NEUTRAL");
+                    "First Message", null, null), "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "Second Message", null, null), "Expected FilterReply.NEUTRAL");
+                    "Second Message", null, null), "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "First Message", new Object[0], null), "Expected FilterReply.DENY");
+                    "First Message", new Object[0], null), "Expected FilterReply.DENY");
 
             // With Exception
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null,
-                "Second Message", new Object[]{}, exception), "Expected FilterReply.NEUTRAL");
+                    "Second Message", new Object[]{}, exception), "Expected FilterReply.NEUTRAL");
         }
 
         @Test
@@ -114,17 +112,17 @@ class ApimlDuplicateMessageFilterTest {
             RuntimeException exception = new RuntimeException("my exception");
 
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message", new Object[]{1}, null),
-                "Expected FilterReply.NEUTRAL");
+                    "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message {}", new Object[]{1}, null),
-                "Expected FilterReply.NEUTRAL");
+                    "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null, String.format("Message %s", 1), null, null),
-                "Expected FilterReply.DENY");
+                    "Expected FilterReply.DENY");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message {}", new Object[]{1, exception}, null),
-                "Expected FilterReply.NEUTRAL");
+                    "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message {}", new Object[]{1}, exception),
-                "Expected FilterReply.DENY");
+                    "Expected FilterReply.DENY");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message {} {}",
-                new Object[]{1, new RuntimeException("Exception inside formatted message")}, exception), "Expected FilterReply.NEUTRAL");
+                    new Object[]{1, new RuntimeException("Exception inside formatted message")}, exception), "Expected FilterReply.NEUTRAL");
         }
 
         @Test
@@ -140,19 +138,19 @@ class ApimlDuplicateMessageFilterTest {
                 // "Message 0" and "Message 1" get dropped from cache because cache reaches the capacity
                 // "Message 2", "Message 3", "Message 4", "Message 5", "Message 6" remain in cache
                 assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message " + i, null, null),
-                    "Expected FilterReply.NEUTRAL");
+                        "Expected FilterReply.NEUTRAL");
             }
 
             for (int i = cacheSize - 1; i >= margin; i--) {
                 // "Message 4", "Message 3", "Message 2" do not pass filtering because they are in cache
                 assertEquals(FilterReply.DENY, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message {}", new Object[]{i}, null),
-                    "Expected FilterReply.DENY");
+                        "Expected FilterReply.DENY");
             }
 
             for (int i = margin - 1; i >= 0; i--) {
                 // "Message 1", "Message 0" pass filtering because they are not in cache
                 assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message " + i, null, null),
-                    "Expected FilterReply.NEUTRAL");
+                        "Expected FilterReply.NEUTRAL");
             }
         }
 
@@ -164,9 +162,9 @@ class ApimlDuplicateMessageFilterTest {
             RuntimeException filterException = new RuntimeException("Token is not valid");
 
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message {}", new Object[]{1}, null),
-                "Expected FilterReply.NEUTRAL");
+                    "Expected FilterReply.NEUTRAL");
             assertEquals(FilterReply.NEUTRAL, apimlDuplicateMessagesFilter.decide(null, logger, null, "Message {}", new Object[]{1}, filterException),
-                "Expected FilterReply.NEUTRAL");
+                    "Expected FilterReply.NEUTRAL");
         }
 
     }
