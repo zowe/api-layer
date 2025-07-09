bc905c64dafa0634f15a53660e01f883266e9664
Pablo Carle <pablo.carle@broadcom.com>
2025-07-09 11:18:55 +0200

    back to not replicate statically onboarded services (not gw)
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- discovery-service/src/main/java/org/zowe/apiml/discovery/staticdef/StaticServicesRegistrationService.java
+++ discovery-service/src/main/java/org/zowe/apiml/discovery/staticdef/StaticServicesRegistrationService.java
@@ -97,7 +97,7 @@ public class StaticServicesRegistrationService {
         for (InstanceInfo instanceInfo : result.getInstances()) {
             result.getRegisteredServices().add(instanceInfo.getInstanceId());
             staticInstances.add(instanceInfo);
-            registry.registerStatically(instanceInfo, false, true);
+            registry.registerStatically(instanceInfo, false, false);
         }
 
         return result;
