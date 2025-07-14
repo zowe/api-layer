f8ed3952b828a6b363afdb2b27f82ed48484ca0f
Richard Salac <richard.salac@broadcom.com>
2025-07-14 10:32:35 +0200

    chore: remove x-forward-* headers proxy tests from platform it
    Signed-off-by: Richard Salac <richard.salac@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/proxy/XForwardHeadersProxyTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/proxy/XForwardHeadersProxyTest.java
@@ -32,7 +32,7 @@ import static org.zowe.apiml.util.SecurityUtils.COOKIE_NAME;
 import static org.zowe.apiml.util.SecurityUtils.gatewayToken;
 import static org.zowe.apiml.util.requests.Endpoints.REQUEST_INFO_ENDPOINT;
 
-@Tag("CloudGatewayCentralRegistry")
+@Tag("CloudGatewayCentralRegistryXForwardHeaders")
 @Slf4j
 class XForwardHeadersProxyTest {
 
