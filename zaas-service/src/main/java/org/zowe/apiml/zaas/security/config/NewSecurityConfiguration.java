0a11d5355b57332a9095dd5da91f547d7e37fe55
Andrea Tabone <andrea.tabone@broadcom.com>
2025-07-15 16:54:44 +0200

    address sonar issue + polishing
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

--- zaas-service/src/main/java/org/zowe/apiml/zaas/security/config/NewSecurityConfiguration.java
+++ zaas-service/src/main/java/org/zowe/apiml/zaas/security/config/NewSecurityConfiguration.java
@@ -158,7 +158,7 @@ public class NewSecurityConfiguration {
 
         private class CustomSecurityFilters extends AbstractHttpConfigurer<CustomSecurityFilters, HttpSecurity> {
             @Override
-            public void configure(HttpSecurity http) throws Exception {
+            public void configure(HttpSecurity http) {
                 AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
                 //drive filter order this way
                 http.addFilterBefore(new CategorizeCertsFilter(publicKeyCertificatesBase64, certificateValidator), org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter.class)
@@ -193,7 +193,7 @@ public class NewSecurityConfiguration {
     /**
      * Secures endpoints:
      *   - /auth/access-token/generate
-     *
+     * <p>
      * Requires authentication by a client certificate forwarded form Gateway or basic authentication, supports credentials in header and body.
      * The request is fulfilled by the filter chain only, there is no controller to handle it.
      * Order of custom filters:
@@ -228,7 +228,7 @@ public class NewSecurityConfiguration {
 
         private class CustomSecurityFilters extends AbstractHttpConfigurer<CustomSecurityFilters, HttpSecurity> {
             @Override
-            public void configure(HttpSecurity http) throws Exception {
+            public void configure(HttpSecurity http) {
                 AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
                 //drive filter order this way
                 http.addFilterBefore(new CategorizeCertsFilter(publicKeyCertificatesBase64, certificateValidator), org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter.class)
@@ -361,7 +361,7 @@ public class NewSecurityConfiguration {
 
             private class CustomSecurityFilters extends AbstractHttpConfigurer<CustomSecurityFilters, HttpSecurity> {
                 @Override
-                public void configure(HttpSecurity http) throws Exception {
+                public void configure(HttpSecurity http) {
                     AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
                     http.addFilterBefore(queryFilter("/**", authenticationManager), UsernamePasswordAuthenticationFilter.class);
                 }
@@ -406,7 +406,7 @@ public class NewSecurityConfiguration {
 
             private class CustomSecurityFilters extends AbstractHttpConfigurer<CustomSecurityFilters, HttpSecurity> {
                 @Override
-                public void configure(HttpSecurity http) throws Exception {
+                public void configure(HttpSecurity http) {
                     AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
                     http.addFilterBefore(ticketFilter("/**", authenticationManager), UsernamePasswordAuthenticationFilter.class);
                 }
@@ -444,7 +444,7 @@ public class NewSecurityConfiguration {
                 ))).authorizeHttpRequests(requests -> requests
                         .anyRequest().authenticated())
                     .authenticationProvider(tokenAuthenticationProvider)
-                    .logout(logout -> logout.disable()) // logout filter in this chain not needed
+                    .logout(AbstractHttpConfigurer::disable) // logout filter in this chain not needed
                     .x509(x509 -> x509 //default x509 filter, authenticates trusted cert, refreshFilter(..) depends on this
                         .userDetailsService(new SimpleUserDetailService()))
                     .with(new CustomSecurityFilters(), Customizer.withDefaults());
@@ -454,7 +454,7 @@ public class NewSecurityConfiguration {
 
             private class CustomSecurityFilters extends AbstractHttpConfigurer<CustomSecurityFilters, HttpSecurity> {
                 @Override
-                public void configure(HttpSecurity http) throws Exception {
+                public void configure(HttpSecurity http) {
                     AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
                     http.addFilterBefore(refreshFilter("/**", authenticationManager), UsernamePasswordAuthenticationFilter.class);
                 }
@@ -579,6 +579,12 @@ public class NewSecurityConfiguration {
                         handlerInitializer.getResourceAccessExceptionHandler(),
                         new String[] {"/"});
                 }
+
+                private X509ForwardingAwareAuthenticationFilter x509ForwardingAwareAuthenticationFilter() {
+                    return new X509AuthAwareFilter("/**",
+                        handlerInitializer.getAuthenticationFailureHandler(),
+                        x509AuthenticationProvider);
+                }
             }
 
             private CategorizeCertsFilter reversedCategorizeCertFilter() {
@@ -586,12 +592,6 @@ public class NewSecurityConfiguration {
                 out.setCertificateForClientAuth(crt -> out.getPublicKeyCertificatesBase64().contains(CategorizeCertsFilter.base64EncodePublicKey(crt)));
                 out.setApimlCertificate(crt -> !out.getPublicKeyCertificatesBase64().contains(CategorizeCertsFilter.base64EncodePublicKey(crt)));
                 return out;
-            }
-
-            private X509ForwardingAwareAuthenticationFilter x509ForwardingAwareAuthenticationFilter() {
-                return new X509AuthAwareFilter("/**",
-                    handlerInitializer.getAuthenticationFailureHandler(),
-                    x509AuthenticationProvider);
             }
         }
 
