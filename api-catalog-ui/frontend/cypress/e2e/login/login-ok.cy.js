27966670fe870778992f9920b8ff91691086914d
ac892247 <a.chmelo@gmail.com>
2025-07-31 13:21:48 +0200

    look for text in login page
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- api-catalog-ui/frontend/cypress/e2e/login/login-ok.cy.js
+++ api-catalog-ui/frontend/cypress/e2e/login/login-ok.cy.js
@@ -27,7 +27,7 @@ describe('>>> Login ok page test', () => {
         cy.get('li[data-testid="logout"]').click();
         cy.contains('API Catalog');
 
-        cy.contains('Version: ');
+        cy.contains('Please enter your mainframe username and password');
 
         cy.getCookie('apimlAuthenticationToken').should('not.exist');
     });
