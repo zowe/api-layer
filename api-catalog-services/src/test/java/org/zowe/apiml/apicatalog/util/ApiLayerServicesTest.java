a2eb59f016d0f653a4c710714d05e2fdbe356e32
hrishikesh-nalawade <hrishikesh.nalawade.17@gmail.com>
2025-08-21 21:18:10 +0530

    license info
    Signed-off-by: hrishikesh-nalawade <hrishikesh.nalawade.17@gmail.com>

--- api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/util/ApiLayerServicesTest.java
+++ api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/util/ApiLayerServicesTest.java
@@ -1,3 +1,12 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package org.zowe.apiml.apicatalog.util;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +18,6 @@ import org.zowe.apiml.apicatalog.config.ApiLayerServices;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ApiLayerServicesTest {
-
 
     @Test
     public void testEnumConstruction_shouldHaveCorrectServiceIds() {
