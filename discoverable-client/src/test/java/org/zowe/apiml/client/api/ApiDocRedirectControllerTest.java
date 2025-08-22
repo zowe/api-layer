a951be77ffca4f74c4adfdc63c91365af8cd86a8
Andrea Tabone <39694626+taban03@users.noreply.github.com>
2025-08-22 13:04:15 +0200

    fix: backport tests for swagger redirection (#4288)
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

---
+++ discoverable-client/src/test/java/org/zowe/apiml/client/api/ApiDocRedirectControllerTest.java
@@ -0,0 +1,42 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.client.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.zowe.apiml.client.configuration.SecurityConfiguration;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = {ApiDocRedirectController.class})
+@Import(SecurityConfiguration.class)
+class ApiDocRedirectControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void callApiDocRedirect() throws Exception {
+        this.mockMvc.perform(get("/docs/redirect"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(header().string("Location", containsString("/v3/api-docs/apiv2")));
+    }
+
+}
