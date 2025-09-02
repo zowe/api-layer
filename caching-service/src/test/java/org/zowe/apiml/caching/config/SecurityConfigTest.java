797387e6b56af337824513352652e5b3145a08c1
ac892247 <a.chmelo@gmail.com>
2025-09-02 14:37:38 +0200

    test ssl verification disabled
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

---
+++ caching-service/src/test/java/org/zowe/apiml/caching/config/SecurityConfigTest.java
@@ -0,0 +1,115 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.caching.config;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.zowe.apiml.caching.CachingServiceApplication;
+import org.zowe.apiml.util.config.SslContext;
+import org.zowe.apiml.util.config.SslContextConfigurer;
+
+import static io.restassured.RestAssured.given;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SecurityConfigTest {
+
+    @BeforeAll
+    static void init() throws Exception {
+        RestAssured.useRelaxedHTTPSValidation();
+        SslContextConfigurer configurer = new SslContextConfigurer("password".toCharArray(), "../keystore/client_cert/client-certs.p12", "../keystore/localhost/localhost.keystore.p12");
+        SslContext.prepareSslAuthentication(configurer);
+    }
+
+    private String getUri(String scheme, String hostname, int port, String path) {
+        return String.format("%s://%s:%d/%s", scheme, hostname, port, path);
+    }
+
+    @Nested
+    @TestPropertySource(
+        properties = {
+            "apiml.service.ssl.verifySslCertificatesOfServices=false",
+            "apiml.service.ssl.nonStrictVerifySslCertificatesOfServices=true"
+        }
+    )
+    @DirtiesContext
+    @SpringBootTest(
+        classes = CachingServiceApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+    )
+    class GivenDisabledSSLVerification {
+
+        @Value("${apiml.service.hostname:localhost}")
+        String hostname;
+        @LocalServerPort
+        int port;
+
+        @Test
+        void thenDoNotRequireAuth() {
+            given()
+                .header(new Header("X-CS-Service-ID", "apimtst"))
+                .get(getUri("https", hostname, port, "cachingservice/api/v1/cache"))
+                .then()
+                .log().ifValidationFails()
+                .statusCode(HttpStatus.OK.value());
+        }
+    }
+
+    @Nested
+    @TestPropertySource(
+        properties = {
+            "apiml.service.ssl.verifySslCertificatesOfServices=true"
+        }
+    )
+    @DirtiesContext
+    @SpringBootTest(
+        classes = CachingServiceApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+    )
+    class GivenEnabledSSLVerification {
+
+        @Value("${apiml.service.hostname:localhost}")
+        String hostname;
+        @LocalServerPort
+        int port;
+
+        @Test
+        void whenNoClientCertificate_thenReturnUnauthorized() {
+            given()
+                .header(new Header("X-CS-Service-ID", "apimtst"))
+                .get(getUri("https", hostname, port, "cachingservice/api/v1/cache"))
+                .then()
+                .log().ifValidationFails()
+                .statusCode(HttpStatus.FORBIDDEN.value());
+        }
+
+        @Test
+        void whenClientCertificate_thenReturnOk() {
+
+            given()
+                .config(SslContext.clientCertApiml)
+                .header(new Header("X-CS-Service-ID", "apimtst"))
+                .get(getUri("https", hostname, port, "cachingservice/api/v1/cache"))
+                .then()
+                .log().ifValidationFails()
+                .statusCode(HttpStatus.OK.value());
+        }
+    }
+}
