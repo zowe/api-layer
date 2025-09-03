87e44c0bbfa4eacbfb12df1e242ed97dbaf0069d
ac892247 <a.chmelo@gmail.com>
2025-09-03 13:02:43 +0200

    move back
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- caching-service/src/test/java/org/zowe/apiml/caching/config/SecurityConfigTest.java
+++ caching-service/src/test/java/org/zowe/apiml/caching/config/SecurityConfigTest.java
@@ -8,7 +8,7 @@
  * Copyright Contributors to the Zowe Project.
  */
 
-package org.zowe.apiml.caching.functional;
+package org.zowe.apiml.caching.config;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Header;
