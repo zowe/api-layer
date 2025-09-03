72749d8c71fe6ef4abedf20ce65371631817cc5b
ac892247 <a.chmelo@gmail.com>
2025-09-03 10:57:56 +0200

    reset context before test
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- caching-service/src/test/java/org/zowe/apiml/caching/functional/SecurityConfigTest.java
+++ caching-service/src/test/java/org/zowe/apiml/caching/functional/SecurityConfigTest.java
@@ -33,6 +33,7 @@ public class SecurityConfigTest {
 
     @BeforeAll
     static void init() throws Exception {
+        SslContext.reset();
         RestAssured.useRelaxedHTTPSValidation();
         SslContextConfigurer configurer = new SslContextConfigurer("password".toCharArray(), "../keystore/client_cert/client-certs.p12", "../keystore/localhost/localhost.keystore.p12");
         SslContext.prepareSslAuthentication(configurer);
