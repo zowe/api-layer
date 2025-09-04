4399b01b9ab89c16d25b1e99e202cc9063952e45
Richard Salač <richard.salac@broadcom.com>
2025-09-04 10:42:26 +0200

    feat: Support configurable username field for oidc tokens (#4300)
    Signed-off-by: Richard Salac <richard.salac@broadcom.com>

--- zaas-service/src/main/java/org/zowe/apiml/zaas/security/service/JwtUtils.java
+++ zaas-service/src/main/java/org/zowe/apiml/zaas/security/service/JwtUtils.java
@@ -10,14 +10,22 @@
 
 package org.zowe.apiml.zaas.security.service;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.zowe.apiml.security.common.token.TokenExpireException;
+import org.zowe.apiml.security.common.token.TokenFormatNotValidException;
 import org.zowe.apiml.security.common.token.TokenNotValidException;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @UtilityClass
@@ -85,6 +93,44 @@ public class JwtUtils {
 
         log.debug(TOKEN_IS_NOT_VALID_DUE_TO, exception.getMessage());
         return new TokenNotValidException("An internal error occurred while validating the token therefore the token is no longer valid.", exception);
+    }
+
+    /**
+     * Extracts value of a field from an OIDC token. The value is extracted from a custom path which supports nested objects.
+     * @param token to extract the field from
+     * @param pathToField list of strings representing path to the field
+     * @return userId extracted from the token
+     *
+     * @throws TokenFormatNotValidException in case of the field value cannot be extracted from the token, is null, or empty
+     */
+    @SuppressWarnings("rawtypes")
+    public static String getFieldValueFromToken(String token, List<String> pathToField) throws TokenFormatNotValidException {
+        if (token == null || pathToField == null || pathToField.isEmpty() || StringUtils.isBlank(pathToField.get(0))) {
+            throw new IllegalArgumentException("Token and field path most not be null or empty");
+        }
+
+        try {
+            Claims claims = getJwtClaims(token);
+            String fieldValue;
+            if (pathToField.size() == 1) {
+                fieldValue = claims.get(pathToField.get(0), String.class);
+            } else {
+                var iterator = pathToField.iterator();
+                var key = iterator.next();
+                Map val = claims.get(key, Map.class);
+                while (iterator.hasNext()) {
+                    key = iterator.next();
+                    if (iterator.hasNext()) {
+                        val = (Map) val.get(key);
+                    }
+                }
+                fieldValue = (String) val.get(key);
+            }
+            if (StringUtils.isBlank(fieldValue)) throw new IllegalArgumentException();
+            return fieldValue;
+        } catch (Exception e) {
+            throw new TokenFormatNotValidException(String.format("Cannot extract value from field %s. The field does not exists, is empty, or is na object.", String.join(".", pathToField)));
+        }
     }
 
 }
