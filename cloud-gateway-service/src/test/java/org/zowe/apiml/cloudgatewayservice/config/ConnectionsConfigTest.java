dfdb8c46bbff5421c9a99cc8ebeeb93eb6c57406
Pablo Carle <pablo.carle@broadcom.com>
2025-10-01 14:22:19 +0200

    remove unneeded test
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- cloud-gateway-service/src/test/java/org/zowe/apiml/cloudgatewayservice/config/ConnectionsConfigTest.java
+++ cloud-gateway-service/src/test/java/org/zowe/apiml/cloudgatewayservice/config/ConnectionsConfigTest.java
@@ -14,14 +14,11 @@ import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.HealthCheckHandler;
 import com.netflix.discovery.EurekaClientConfig;
 import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClient;
-import io.netty.handler.ssl.SslContextBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
@@ -32,24 +29,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.zowe.apiml.product.web.HttpConfig;
 import org.zowe.apiml.util.CorsUtils;
 
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509KeyManager;
-import java.io.IOException;
 import java.lang.reflect.Field;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ComponentScan(basePackages = "org.zowe.apiml.cloudgatewayservice")
@@ -58,9 +46,6 @@ class ConnectionsConfigTest {
 
     @Autowired
     private ConnectionsConfig connectionsConfig;
-
-    @Autowired
-    private RoutingConfig routingConfig;
 
     @Nested
     class WhenCreateEurekaJerseyClientBuilder {
@@ -131,85 +116,6 @@ class ConnectionsConfigTest {
             assertThat(ReflectionTestUtils.getField(connectionsConfig, "keyStorePassword")).isNull();
             assertThat(ReflectionTestUtils.getField(connectionsConfig, "trustStorePassword")).isNull();
         }
-    }
-
-    @ExtendWith(MockitoExtension.class)
-    @Nested
-    class ConnectionUtilTest {
-
-        @Mock
-        private HttpConfig httpConfig;
-        @Mock
-        private SslContextBuilder builder;
-
-        @BeforeEach
-        void setUp() {
-            when(httpConfig.getTrustStoreType()).thenReturn("PKCS12");
-            when(httpConfig.getTrustStore()).thenReturn("../keystore/localhost/localhost.truststore.p12");
-            when(httpConfig.getTrustStorePassword()).thenReturn("password".toCharArray()); //NOSONAR
-        }
-
-        @Test
-        void onGetSslContextWithKeystore_thenUse() throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
-            when(httpConfig.getKeyStoreType()).thenReturn("PKCS12");
-            when(httpConfig.getKeyStore()).thenReturn("../keystore/localhost/localhost.keystore.p12");
-            when(httpConfig.getKeyStorePassword()).thenReturn("password".toCharArray()); //NOSONAR
-
-            try (MockedStatic<SslContextBuilder> sslContextBuilder = Mockito.mockStatic(SslContextBuilder.class)) {
-                sslContextBuilder.when(SslContextBuilder::forClient).thenReturn(builder);
-
-                connectionsConfig.sslContext(true);
-
-                verify(builder, times(1)).trustManager(any(TrustManagerFactory.class));
-                verify(builder, never()).endpointIdentificationAlgorithm(any());
-                verify(builder, times(1)).keyManager(any(X509KeyManager.class));
-                verify(builder, times(1)).keyManager((X509KeyManager) argThat(x509KeyManager -> {
-                    X509KeyManager m = (X509KeyManager) x509KeyManager;
-                    assertNotNull(m.getPrivateKey("localhost"));
-                    assertTrue(m.getCertificateChain("localhost").length > 0);
-                    return true;
-                }));
-
-            }
-
-        }
-
-        @Test
-        void onGetSslContextWithoutKeystore_thenEmpty() throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
-            try (MockedStatic<SslContextBuilder> sslContextBuilder = Mockito.mockStatic(SslContextBuilder.class)) {
-                sslContextBuilder.when(SslContextBuilder::forClient).thenReturn(builder);
-
-                connectionsConfig.sslContext(false);
-
-                verify(builder, times(1)).trustManager(any(TrustManagerFactory.class));
-                verify(builder, never()).endpointIdentificationAlgorithm(any());
-                verify(builder, times(1)).keyManager(any(KeyManagerFactory.class));
-                verify(builder, times(1)).keyManager((KeyManagerFactory) argThat(keyManagerFactory -> {
-                    KeyManagerFactory f = (KeyManagerFactory) keyManagerFactory;
-                    assertTrue(f.getKeyManagers().length > 0);
-                    return true;
-                }));
-
-            }
-
-        }
-
-        @Test
-        void whenNonStrict_thenDisableEndpointIdentificationAlgorithm() throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
-            when(httpConfig.isVerifySslCertificatesOfServices()).thenReturn(true);
-            when(httpConfig.isNonStrictVerifySslCertificatesOfServices()).thenReturn(true);
-
-            try (MockedStatic<SslContextBuilder> sslContextBuilder = Mockito.mockStatic(SslContextBuilder.class)) {
-                sslContextBuilder.when(SslContextBuilder::forClient).thenReturn(builder);
-
-                connectionsConfig.sslContext(false);
-
-                verify(builder, times(1)).trustManager(any(TrustManagerFactory.class));
-                verify(builder, times(1)).endpointIdentificationAlgorithm(isNull());
-            }
-
-        }
-
     }
 
     @Nested
