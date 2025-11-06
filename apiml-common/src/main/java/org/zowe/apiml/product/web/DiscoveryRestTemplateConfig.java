f77953d45be33cc508791bb09eff22507b25676f
ac892247 <a.chmelo@gmail.com>
2025-11-06 14:27:06 +0100

    close expired connections
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- apiml-common/src/main/java/org/zowe/apiml/product/web/DiscoveryRestTemplateConfig.java
+++ apiml-common/src/main/java/org/zowe/apiml/product/web/DiscoveryRestTemplateConfig.java
@@ -10,20 +10,31 @@
 
 package org.zowe.apiml.product.web;
 
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.netflix.eureka.RestClientTimeoutProperties;
-import org.springframework.cloud.netflix.eureka.http.DefaultEurekaClientHttpRequestFactorySupplier;
+import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
 import org.springframework.cloud.netflix.eureka.http.RestClientDiscoveryClientOptionalArgs;
 import org.springframework.cloud.netflix.eureka.http.RestClientTransportClientFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.zowe.apiml.message.log.ApimlLogger;
 import org.zowe.apiml.message.yaml.YamlMessageServiceInstance;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class DiscoveryRestTemplateConfig {
@@ -33,6 +44,10 @@ public class DiscoveryRestTemplateConfig {
     @Value("${server.attlsClient.enabled:false}")
     private boolean isClientAttlsEnabled;
 
+    private static int connectTimeout = 180000;
+    private static int requestTimeout = 180000;
+    private static int socketTimeout = 180000;
+
     @Bean
     RestClientTransportClientFactories restTemplateTransportClientFactories(RestClientDiscoveryClientOptionalArgs restClientDiscoveryClientOptionalArgs) {
         return new RestClientTransportClientFactories(restClientDiscoveryClientOptionalArgs);
@@ -40,8 +55,8 @@ public class DiscoveryRestTemplateConfig {
 
     @Bean
     RestClientDiscoveryClientOptionalArgs defaultArgs(@Value("${eureka.client.serviceUrl.defaultZone}") String eurekaServerUrl,
-                                                             @Qualifier("secureSslContext") SSLContext secureSslContext,
-                                                             HostnameVerifier secureHostnameVerifier
+                                                      @Qualifier("secureSslContext") SSLContext secureSslContext,
+                                                      HostnameVerifier secureHostnameVerifier
     ) {
         RestClientDiscoveryClientOptionalArgs clientArgs = new RestClientDiscoveryClientOptionalArgs(getDefaultEurekaClientHttpRequestFactorySupplier(), RestClient::builder);
 
@@ -57,12 +72,40 @@ public class DiscoveryRestTemplateConfig {
         return clientArgs;
     }
 
-    private static DefaultEurekaClientHttpRequestFactorySupplier getDefaultEurekaClientHttpRequestFactorySupplier() {
-        RestClientTimeoutProperties properties = new RestClientTimeoutProperties();
-        properties.setConnectTimeout(180000);
-        properties.setConnectRequestTimeout(180000);
-        properties.setSocketTimeout(180000);
-        return new DefaultEurekaClientHttpRequestFactorySupplier(properties);
+    public static EurekaClientHttpRequestFactorySupplier getDefaultEurekaClientHttpRequestFactorySupplier() {
+        return (sslContext, hostnameVerifier) -> {
+            var requestFactory = new HttpComponentsClientHttpRequestFactory();
+            var httpClientBuilder = HttpClients
+                .custom()
+                .evictExpiredConnections()
+                .evictIdleConnections(TimeValue.ofSeconds(30))
+                .setConnectionManager(buildConnectionManager(sslContext, hostnameVerifier));
+            RequestConfig.Builder requestConfigBuilder = RequestConfig.custom();
+
+            requestConfigBuilder.setConnectionRequestTimeout(
+                Timeout.of(requestTimeout, TimeUnit.MILLISECONDS));
+
+            httpClientBuilder.setDefaultRequestConfig(requestConfigBuilder.build());
+
+            CloseableHttpClient httpClient = httpClientBuilder.build();
+
+            requestFactory.setHttpClient(httpClient);
+            return requestFactory;
+        };
     }
+
+    private static HttpClientConnectionManager buildConnectionManager(SSLContext sslContext, HostnameVerifier hostnameVerifier) {
+        PoolingHttpClientConnectionManagerBuilder connectionManagerBuilder = PoolingHttpClientConnectionManagerBuilder
+            .create();
+        connectionManagerBuilder.setTlsSocketStrategy(new DefaultClientTlsStrategy(sslContext, hostnameVerifier));
+        connectionManagerBuilder.setDefaultSocketConfig(SocketConfig.custom()
+            .setSoTimeout(Timeout.of(socketTimeout, TimeUnit.MILLISECONDS))
+            .build());
+        connectionManagerBuilder.setDefaultConnectionConfig(ConnectionConfig.custom()
+            .setConnectTimeout(Timeout.of(connectTimeout, TimeUnit.MILLISECONDS))
+            .build());
+        return connectionManagerBuilder.build();
+    }
+
 
 }
