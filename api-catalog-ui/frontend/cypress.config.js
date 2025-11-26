5a2087da89680228a528a7d6ec0561238afda21d
Andrea Tabone <39694626+taban03@users.noreply.github.com>
2025-11-26 17:00:15 +0100

    chore: run ITs towards Auth0 provider (#4385)
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>
    Co-authored-by: Pablo Carle <pablocarle@users.noreply.github.com>

--- api-catalog-ui/frontend/cypress.config.js
+++ api-catalog-ui/frontend/cypress.config.js
@@ -14,8 +14,8 @@ module.exports = defineConfig({
     env: {
         catalogHomePage: 'https://localhost:10010/apicatalog/ui/v1',
         loginUrl: 'https://localhost:10010/apicatalog/api/v1/auth/login',
-        gatewayOktaRedirect:
-            'https://localhost:10010/gateway/oauth2/authorization/okta?returnUrl=https%3A%2F%2Flocalhost%3A10010%2Fapplication',
+        gatewayAuth0Redirect:
+            'https://localhost:10010/gateway/oauth2/authorization/auth0?returnUrl=https%3A%2F%2Flocalhost%3A10010%2Fapplication',
         username: 'USER',
         password: 'validPassword',
         microservices: true,
