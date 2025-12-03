e57b6486b5556fd8f5c5b7ff1429447a4ae8016f
Pavel Jareš <Pavel.Jares@broadcom.com>
2025-12-03 16:10:07 +0100

    stop caching of salt on Gateway side
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/token/ApimlAccessTokenProvider.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/security/service/token/ApimlAccessTokenProvider.java
@@ -49,8 +49,6 @@ public class ApimlAccessTokenProvider implements AccessTokenProvider {
     @Qualifier("oidcJwkMapper")
     private final ObjectMapper objectMapper;
 
-    private byte[] salt;
-
     public void invalidateToken(String token) throws CachingServiceClientException, JsonProcessingException {
         String hashedValue = getHash(token);
         QueryResponse queryResponse = authenticationService.parseJwtWithSignature(token);
@@ -181,11 +179,7 @@ public class ApimlAccessTokenProvider implements AccessTokenProvider {
     }
 
     public byte[] getSalt() throws CachingServiceClientException {
-        if (this.salt != null) {
-            return this.salt;
-        }
-        this.salt = initializeSalt().getBytes();
-        return this.salt;
+        return initializeSalt().getBytes();
     }
 
     private void storeSalt(byte[] salt) throws CachingServiceClientException {
