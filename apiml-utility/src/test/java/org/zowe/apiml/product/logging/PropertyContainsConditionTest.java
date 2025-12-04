33d61d1cce73c1a7cb00c2a79088e3cf42ee58b6
Pablo Carle <pablo.carle@broadcom.com>
2025-12-04 13:05:54 +0100

    add test coverage
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- apiml-utility/src/test/java/org/zowe/apiml/product/logging/PropertyContainsConditionTest.java
+++ apiml-utility/src/test/java/org/zowe/apiml/product/logging/PropertyContainsConditionTest.java
@@ -11,6 +11,7 @@
 package org.zowe.apiml.product.logging;
 
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -18,7 +19,14 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class PropertyContainsConditionTest {
@@ -44,6 +52,45 @@ class PropertyContainsConditionTest {
             propertyContainsCondition.setValue(searchedForValue);
             lenient().doReturn(returnedValue).when(propertyContainsCondition).p(key);
             assertEquals(expectedResult, propertyContainsCondition.evaluate(), "Expected " + returnedValue + " to " + (expectedResult ? " contain " : " not contain ") + searchedForValue);
+        }
+
+    }
+
+    @Nested
+    class OnStart {
+
+        @Test
+        void whenValues_thenSucess() {
+            propertyContainsCondition.key = "key";
+            propertyContainsCondition.value = "value";
+
+            propertyContainsCondition.start();
+
+            verify(propertyContainsCondition, never()).addError(anyString());
+        }
+
+        @Test
+        void whenMissingKey_thenError() {
+            doNothing().when(propertyContainsCondition).addError(any());
+
+            propertyContainsCondition.key = null;
+            propertyContainsCondition.value = "value";
+
+            propertyContainsCondition.start();
+
+            verify(propertyContainsCondition, times(1)).addError(argThat(message -> message.contains("'key'")));
+        }
+
+        @Test
+        void whenMissingValue_thenError() {
+            doNothing().when(propertyContainsCondition).addError(any());
+
+            propertyContainsCondition.key = "key";
+            propertyContainsCondition.value = null;
+
+            propertyContainsCondition.start();
+
+            verify(propertyContainsCondition, times(1)).addError(argThat(message -> message.contains("'value'")));
         }
 
     }
