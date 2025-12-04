08ce0ad8adcef28f1128f48a4b58a879c56cf3db
Pablo Carle <pablo.carle@broadcom.com>
2025-12-04 10:53:31 +0100

    add test
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- apiml-utility/src/test/java/org/zowe/apiml/product/logging/PropertyContainsConditionTest.java
+++ apiml-utility/src/test/java/org/zowe/apiml/product/logging/PropertyContainsConditionTest.java
@@ -10,20 +10,39 @@
 
 package org.zowe.apiml.product.logging;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 class PropertyContainsConditionTest {
 
-    @Test
-    void testEvaluate() {
+    @Spy
+    private PropertyContainsCondition propertyContainsCondition;
 
-    }
+    @Nested
+    class OnEvaluate {
 
-    @Test
-    void testStart() {
+        @ParameterizedTest
+        @CsvSource(value = {
+            "abc,b,true",
+            "abc,a,true",
+            "abc,c,true",
+            "abc,d,false",
+            "abc,B,false"
+        }, delimiter = ',')
+        void expectResult(String returnedValue, String searchedForValue, boolean expectedResult) {
+            propertyContainsCondition.setKey("a.key");
+            propertyContainsCondition.setValue(searchedForValue);
+            doReturn(returnedValue).when(propertyContainsCondition).p("a.key");
+            assertEquals(expectedResult, propertyContainsCondition.evaluate(), "Expected " + returnedValue + " to " + (expectedResult ? " contain " : " not contain ") + searchedForValue);
+        }
 
     }
 
