ca2661e17de67598b5f4ea1b1c7fe3fe858c0164
Elena Kubantseva <elena.kubantseva@broadcom.com>
2025-12-15 18:35:52 +0100

    proper mock
    Signed-off-by: Elena Kubantseva <elena.kubantseva@broadcom.com>

--- apiml/src/test/java/org/zowe/apiml/acceptance/AttlsConfigTest.java
+++ apiml/src/test/java/org/zowe/apiml/acceptance/AttlsConfigTest.java
@@ -51,6 +51,7 @@ import org.zowe.commons.attls.UnknownEnumValueException;
 
 import javax.net.ssl.SSLException;
 
+import java.util.Base64;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -288,29 +289,28 @@ class AttlsConfigTest {
 
         @Test
         void testLoginEndpoint_mockedAttls() throws IoctlCallException, UnknownEnumValueException {
-            when(attlsContext.getCertificate()).thenReturn(("-----BEGIN CERTIFICATE-----\n" + //
-                                "MIID3jCCAsagAwIBAgIULApMeb1+40+ifLXNVf1mqwsNlt4wDQYJKoZIhvcNAQEL\n" + //
-                                "BQAwYDELMAkGA1UEBhMCQ1oxEDAOBgNVBAgMB0N6ZWNoaWExDzANBgNVBAcMBlBy\n" + //
-                                "YWd1ZTEMMAoGA1UECgwDT01GMQ0wCwYDVQQLDARab3dlMREwDwYDVQQDDAhBUElN\n" + //
-                                "TCBDQTAeFw0yMzA2MDIxMjQ4NDBaFw0yOTA1MzExMjQ4NDBaMF8xCzAJBgNVBAYT\n" + //
-                                "AkNaMRAwDgYDVQQIDAdDemVjaGlhMQ8wDQYDVQQHDAZQcmFndWUxDDAKBgNVBAoM\n" + //
-                                "A09NRjENMAsGA1UECwwEWm93ZTEQMA4GA1UEAwwHQVBJTVRTVDCCASIwDQYJKoZI\n" + //
-                                "hvcNAQEBBQADggEPADCCAQoCggEBAJ6L+6l6mfxByy/VrHQ881xkW/GWQQndocPH\n" + //
-                                "i5Em15P+/ZQToYBTfLPUqGXcPnILg+PrjMtTHBCHO03pIuJxFXqrWfsaxR/O7zhp\n" + //
-                                "BSTt+iT6/kMBhPdF4sJF2VQo1sGBa79hIn3StvD3hKba/5Rzx8i+WXpKNeCzYRoZ\n" + //
-                                "BLYH/MLAokgabf0iWjzrwy9STBvZ0uPON4iBhz6bYh0wTra90j0dDjsetTBMOrm9\n" + //
-                                "gO/sj7RD2KBQUM+mMiny5w4AWjvDChfzGEc37f/Ur2FyCqwY7k4oNS2tMtPQKemg\n" + //
-                                "4CtmFsWLL3Vb7e6fwoCNFLsmJumsd13u2HCmnV5YT13ZL8xphqkCAwEAAaOBkDCB\n" + //
-                                "jTALBgNVHQ8EBAMCBeAwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMB8G\n" + //
-                                "A1UdEQQYMBaCCWxvY2FsaG9zdIIJMTI3LjAuMC4xMB0GA1UdDgQWBBQ3GrkUuyvH\n" + //
-                                "QmPRECqdzcR3qmQSHzAfBgNVHSMEGDAWgBT78hIus4SCXxMW8T9T0AEIe7HZNjAN\n" + //
-                                "BgkqhkiG9w0BAQsFAAOCAQEAHAzeBownnYY9kSF6fif+dXw2miRTNkhRRc6ZIlij\n" + //
-                                "Jy+d5ZysrR0yUTeW11raltGiX2gcCtg5GZp+ODgiqSMJN3mV1bIpKiuBhODKHlMz\n" + //
-                                "pg8v4ebjIHd1buO8KbOlR8zKv4kMFiGqdfWW6W3BZy3w3RCOnWhts2Y4O+XZ4Gri\n" + //
-                                "Yjiwkwf1IY7xv7HBJ4BsbUwxjxMcxa1HNqE8oAqEtiFxRmPkAi+g1lijvF26AKZd\n" + //
-                                "WxKFTLJV1HxUsa5l8b7cHN9yya6IVixVcB9Cla06Rg7dkaI4Deb5JCxFXjoznDKY\n" + //
-                                "kv8ZumkzQI9Ov90d1FYyVr7VWPEun/XV2XmH9nGHWyJSkA==\n" + //
-                                "-----END CERTIFICATE-----").getBytes());
+            String cert = "MIID3jCCAsagAwIBAgIULApMeb1+40+ifLXNVf1mqwsNlt4wDQYJKoZIhvcNAQEL" +
+                "BQAwYDELMAkGA1UEBhMCQ1oxEDAOBgNVBAgMB0N6ZWNoaWExDzANBgNVBAcMBlBy" +
+                "YWd1ZTEMMAoGA1UECgwDT01GMQ0wCwYDVQQLDARab3dlMREwDwYDVQQDDAhBUElN" +
+                "TCBDQTAeFw0yMzA2MDIxMjQ4NDBaFw0yOTA1MzExMjQ4NDBaMF8xCzAJBgNVBAYT" +
+                "AkNaMRAwDgYDVQQIDAdDemVjaGlhMQ8wDQYDVQQHDAZQcmFndWUxDDAKBgNVBAoM" +
+                "A09NRjENMAsGA1UECwwEWm93ZTEQMA4GA1UEAwwHQVBJTVRTVDCCASIwDQYJKoZI" +
+                "hvcNAQEBBQADggEPADCCAQoCggEBAJ6L+6l6mfxByy/VrHQ881xkW/GWQQndocPH" +
+                "i5Em15P+/ZQToYBTfLPUqGXcPnILg+PrjMtTHBCHO03pIuJxFXqrWfsaxR/O7zhp" +
+                "BSTt+iT6/kMBhPdF4sJF2VQo1sGBa79hIn3StvD3hKba/5Rzx8i+WXpKNeCzYRoZ" +
+                "BLYH/MLAokgabf0iWjzrwy9STBvZ0uPON4iBhz6bYh0wTra90j0dDjsetTBMOrm9" +
+                "gO/sj7RD2KBQUM+mMiny5w4AWjvDChfzGEc37f/Ur2FyCqwY7k4oNS2tMtPQKemg" +
+                "4CtmFsWLL3Vb7e6fwoCNFLsmJumsd13u2HCmnV5YT13ZL8xphqkCAwEAAaOBkDCB" +
+                "jTALBgNVHQ8EBAMCBeAwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMB8G" +
+                "A1UdEQQYMBaCCWxvY2FsaG9zdIIJMTI3LjAuMC4xMB0GA1UdDgQWBBQ3GrkUuyvH" +
+                "QmPRECqdzcR3qmQSHzAfBgNVHSMEGDAWgBT78hIus4SCXxMW8T9T0AEIe7HZNjAN" +
+                "BgkqhkiG9w0BAQsFAAOCAQEAHAzeBownnYY9kSF6fif+dXw2miRTNkhRRc6ZIlij" +
+                "Jy+d5ZysrR0yUTeW11raltGiX2gcCtg5GZp+ODgiqSMJN3mV1bIpKiuBhODKHlMz" +
+                "pg8v4ebjIHd1buO8KbOlR8zKv4kMFiGqdfWW6W3BZy3w3RCOnWhts2Y4O+XZ4Gri" +
+                "Yjiwkwf1IY7xv7HBJ4BsbUwxjxMcxa1HNqE8oAqEtiFxRmPkAi+g1lijvF26AKZd" +
+                "WxKFTLJV1HxUsa5l8b7cHN9yya6IVixVcB9Cla06Rg7dkaI4Deb5JCxFXjoznDKY" +
+                "kv8ZumkzQI9Ov90d1FYyVr7VWPEun/XV2XmH9nGHWyJSkA==";
+            when(attlsContext.getCertificate()).thenReturn(Base64.getDecoder().decode(cert));
             when(attlsContext.getStatConn()).thenReturn(StatConn.SECURE);
 
             given()
--- apiml/src/test/java/org/zowe/apiml/acceptance/StartupMessageAcceptanceTest.java
+++ apiml/src/test/java/org/zowe/apiml/acceptance/StartupMessageAcceptanceTest.java
@@ -24,11 +24,17 @@ import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegis
 import org.springframework.cloud.netflix.eureka.server.event.EurekaRegistryAvailableEvent;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.zowe.apiml.discovery.ApimlInstanceRegistry;
-import org.zowe.apiml.filter.AttlsHttpHandler;
 import org.zowe.apiml.product.web.ApimlTomcatCustomizer;
 import org.zowe.apiml.zaas.ZaasServiceAvailableEvent;
+import org.zowe.commons.attls.AttlsContext;
+import org.zowe.commons.attls.InboundAttls;
+import org.zowe.commons.attls.IoctlCallException;
+import org.zowe.commons.attls.StatConn;
+import org.zowe.commons.attls.UnknownEnumValueException;
 
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 import static org.awaitility.Awaitility.await;
@@ -81,18 +87,48 @@ public class StartupMessageAcceptanceTest {
     @Nested
     @ActiveProfiles({"attlsClient", "attlsServer"})
     class GivenAttlsProfile extends BaseStartupTest {
-        @MockitoBean
-        private AttlsHttpHandler attlsHttpHandler;
+
+        private static final String CERTIFICATE = "MIID3jCCAsagAwIBAgIULApMeb1+40+ifLXNVf1mqwsNlt4wDQYJKoZIhvcNAQEL" +
+            "BQAwYDELMAkGA1UEBhMCQ1oxEDAOBgNVBAgMB0N6ZWNoaWExDzANBgNVBAcMBlBy" +
+            "YWd1ZTEMMAoGA1UECgwDT01GMQ0wCwYDVQQLDARab3dlMREwDwYDVQQDDAhBUElN" +
+            "TCBDQTAeFw0yMzA2MDIxMjQ4NDBaFw0yOTA1MzExMjQ4NDBaMF8xCzAJBgNVBAYT" +
+            "AkNaMRAwDgYDVQQIDAdDemVjaGlhMQ8wDQYDVQQHDAZQcmFndWUxDDAKBgNVBAoM" +
+            "A09NRjENMAsGA1UECwwEWm93ZTEQMA4GA1UEAwwHQVBJTVRTVDCCASIwDQYJKoZI" +
+            "hvcNAQEBBQADggEPADCCAQoCggEBAJ6L+6l6mfxByy/VrHQ881xkW/GWQQndocPH" +
+            "i5Em15P+/ZQToYBTfLPUqGXcPnILg+PrjMtTHBCHO03pIuJxFXqrWfsaxR/O7zhp" +
+            "BSTt+iT6/kMBhPdF4sJF2VQo1sGBa79hIn3StvD3hKba/5Rzx8i+WXpKNeCzYRoZ" +
+            "BLYH/MLAokgabf0iWjzrwy9STBvZ0uPON4iBhz6bYh0wTra90j0dDjsetTBMOrm9" +
+            "gO/sj7RD2KBQUM+mMiny5w4AWjvDChfzGEc37f/Ur2FyCqwY7k4oNS2tMtPQKemg" +
+            "4CtmFsWLL3Vb7e6fwoCNFLsmJumsd13u2HCmnV5YT13ZL8xphqkCAwEAAaOBkDCB" +
+            "jTALBgNVHQ8EBAMCBeAwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMB8G" +
+            "A1UdEQQYMBaCCWxvY2FsaG9zdIIJMTI3LjAuMC4xMB0GA1UdDgQWBBQ3GrkUuyvH" +
+            "QmPRECqdzcR3qmQSHzAfBgNVHSMEGDAWgBT78hIus4SCXxMW8T9T0AEIe7HZNjAN" +
+            "BgkqhkiG9w0BAQsFAAOCAQEAHAzeBownnYY9kSF6fif+dXw2miRTNkhRRc6ZIlij" +
+            "Jy+d5ZysrR0yUTeW11raltGiX2gcCtg5GZp+ODgiqSMJN3mV1bIpKiuBhODKHlMz" +
+            "pg8v4ebjIHd1buO8KbOlR8zKv4kMFiGqdfWW6W3BZy3w3RCOnWhts2Y4O+XZ4Gri" +
+            "Yjiwkwf1IY7xv7HBJ4BsbUwxjxMcxa1HNqE8oAqEtiFxRmPkAi+g1lijvF26AKZd" +
+            "WxKFTLJV1HxUsa5l8b7cHN9yya6IVixVcB9Cla06Rg7dkaI4Deb5JCxFXjoznDKY" +
+            "kv8ZumkzQI9Ov90d1FYyVr7VWPEun/XV2XmH9nGHWyJSkA==";
+
         @MockitoBean
         private ApimlTomcatCustomizer apimlTomcatCustomizer;
         @MockitoBean
         private ApimlInstanceRegistry apimlInstanceRegistry;
+        @Mock
+        private AttlsContext attlsContext;
+        @Mock
+        private ThreadLocal<AttlsContext> threadLocal;
 
         @Test
-        void whenFullyStartedUp_thenEmitMessage(CapturedOutput output) {
-            // Prevent use of native code but verify it calls the customizer
+        void whenFullyStartedUp_thenEmitMessage(CapturedOutput output) throws IoctlCallException, UnknownEnumValueException {
+            // Prevent use of native code
             when(apimlInstanceRegistry.getApplications()).thenReturn(applicationRegistry.getApplications());
             doNothing().when(apimlTomcatCustomizer).customize(any());
+            ReflectionTestUtils.setField(InboundAttls.class, "contexts", threadLocal);
+            lenient().when(threadLocal.get()).thenReturn(attlsContext);
+            lenient().when(attlsContext.getCertificate()).thenReturn(Base64.getDecoder().decode(CERTIFICATE));
+            lenient().when(attlsContext.getStatConn()).thenReturn(StatConn.SECURE);
+
             verifyStartupMessage(output);
         }
     }
