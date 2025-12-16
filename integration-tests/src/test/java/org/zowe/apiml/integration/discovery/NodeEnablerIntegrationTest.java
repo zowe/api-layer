c1fb1090a6364f66cfdc0b89c2de9485ff077c79
Andrea Tabone <andrea.tabone@broadcom.com>
2025-12-16 17:46:32 +0100

    wait
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/discovery/NodeEnablerIntegrationTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/discovery/NodeEnablerIntegrationTest.java
@@ -12,15 +12,20 @@ package org.zowe.apiml.integration.discovery;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.zowe.apiml.util.TestWithStartedInstances;
 import org.zowe.apiml.util.categories.*;
 import org.zowe.apiml.util.http.HttpRequestUtils;
 
 import java.net.URI;
 
 import static io.restassured.RestAssured.given;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -31,13 +36,59 @@ import static org.hamcrest.Matchers.is;
 @GatewayTest
 @NotAttlsTest
 @NodeEnablerTest
-class NodeEnablerIntegrationTest {
-
+class NodeEnablerIntegrationTest implements TestWithStartedInstances {
     private static final String APP_INFO_HEALTH = "/hwexpress/api/v1/status/";
+
+    private static final URI MEDIATION_CLIENT_URI = HttpRequestUtils.getUriFromGateway(APP_INFO_HEALTH);
+
 
     @BeforeAll
     public static void beforeClass() {
         RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    @Nested
+    class WhenIntegratingWithDiscoveryService {
+        @Nested
+        class GivenValidService {
+            @Test
+            void verifyRegistrationAndUnregistration() {
+                isRegistered(false, MEDIATION_CLIENT_URI);
+
+                register(MEDIATION_CLIENT_URI);
+                isRegistered(true, MEDIATION_CLIENT_URI);
+            }
+        }
+    }
+
+    private void isRegistered(boolean expectedRegistrationState, URI uri) {
+        // It can take some time for (un)registration to complete
+        await()
+            .atMost(5, MINUTES)
+            .pollDelay(0, SECONDS)
+            .pollInterval(1, SECONDS)
+            .until(() -> registeredStateAsExpected(expectedRegistrationState, uri));
+    }
+
+    private boolean registeredStateAsExpected(boolean expectedRegistrationState, URI uri) {
+        try {
+            given()
+                .when()
+                .get(uri)
+                .then()
+                .statusCode(is(SC_OK));
+            return true;
+        } catch (AssertionError e) {
+            return false;
+        }
+    }
+
+    private void register(URI uri) {
+        given()
+            .when()
+            .post(uri)
+            .then()
+            .statusCode(is(SC_OK));
     }
 
     @Test
