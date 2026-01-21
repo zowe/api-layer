0c687a866a9249908e59efbae8bd6bcece2e7abc
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-01-21 17:22:08 +0100

    fix
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/ha/GatewayMultipleInstancesTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/ha/GatewayMultipleInstancesTest.java
@@ -55,7 +55,7 @@ public class GatewayMultipleInstancesTest {
             void gatewayInstancesAreRegistered() {
                 assumeTrue(haGatewayRequests.existing() > 1 && haDiscoveryRequests.existing() > 1);
 
-                assertThat(haDiscoveryRequests.getAmountOfRegisteredInstancesForService(0, Apps.GATEWAY), is(2));
+                assertThat(haDiscoveryRequests.getAmountOfRegisteredInstancesForService(0, Apps.GATEWAY), is(3));
             }
 
         }
