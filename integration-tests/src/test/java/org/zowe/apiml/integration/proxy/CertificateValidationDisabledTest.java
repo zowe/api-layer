772fc9b694958939eeb6d3188e0368108355c236
ac892247 <a.chmelo@gmail.com>
2026-01-23 13:41:17 +0100

    relaxed tls validation for test
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/proxy/CertificateValidationDisabledTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/proxy/CertificateValidationDisabledTest.java
@@ -3,15 +3,19 @@ package org.zowe.apiml.integration.proxy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
 import org.zowe.apiml.util.http.HttpRequestUtils;
 import java.net.URI;
 import org.zowe.apiml.util.requests.Endpoints;
+
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
 
 @Tag("UnknownHostnamesTest")
 public class CertificateValidationDisabledTest {
     @Test
     void givenRequestToServiceWithInvalidHostname_thenRequestIsSuccessful() {
+        RestAssured.useRelaxedHTTPSValidation();
         URI uri = HttpRequestUtils.getUriFromGateway(Endpoints.DISCOVERABLE_GREET);
         given()
             .log().ifValidationFails()
