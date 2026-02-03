eec830489fbe163858bb6c88e26c863158764c0c
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-02-03 15:59:14 +0100

    increase timeout for Redis
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/startup/impl/ApiMediationLayerStartupChecker.java
+++ integration-tests/src/test/java/org/zowe/apiml/startup/impl/ApiMediationLayerStartupChecker.java
@@ -400,7 +400,7 @@ public class ApiMediationLayerStartupChecker {
 
         public void waitUntilReady() {
             awaitFor(this::areAllInstancesOnboarded, 8);
-            awaitFor(this::areDiscoveryInSync, 2);
+            awaitFor(this::areDiscoveryInSync, 3);
             awaitFor(this::areAllInstancesRegistryUpToDate, 3);
             awaitFor(this::areAllInstancesAreUp, 2);
             awaitFor(this::isAuthUp, 1);
