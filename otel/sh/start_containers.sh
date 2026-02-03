4b9549e57572541aa1feab8f5f68833d3c0f965b
Richard Salac <richard.salac@broadcom.com>
2026-02-03 15:08:59 +0100

    refactor otel gh action into scripts
    Signed-off-by: Richard Salac <richard.salac@broadcom.com>

---
+++ otel/sh/start_containers.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+chmod -R 777 otel-*
+docker compose up -d
+
+echo "Checking OpenTelemetry Golden Tester..."
+curl -s -v -w "\n" http://localhost:5318/v1/metrics -H "Content-Type: application/json" -d "{}" \
+    --fail \
+    --retry-all-errors \
+    --retry-delay 10 \
+    --retry 3
+if [ "$?" -eq 0 ]; then
+  echo "OpenTelemetry Golden Tester is ready!"
+else
+  echo "::error::OpenTelemetry Golden Tester startup failed"
+  docker compose stop
+  exit 1
+fi
+
+echo ""
+echo "Checking OpenTelemetry Collector..."
+curl -s -v -w "\n" /dev/null http://localhost:4318/v1/metrics -H "Content-Type: application/json" -d "{}" \
+    --fail \
+    --retry-all-errors \
+    --retry-delay 10 \
+    --retry 3
+if [ $? -eq 0 ]; then
+  echo "OpenTelemetry Collector is ready!"
+else
+  echo "::error::OpenTelemetry Collector startup failed"
+  docker compose stop
+  exit 1
+fi
