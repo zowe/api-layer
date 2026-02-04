c3b933cf415a319385213a04b21f13376ad708d3
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-02-04 14:34:49 +0100

    caching service using cluster to evaluate state
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/health/CachesHealthIndicator.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/health/CachesHealthIndicator.java
@@ -10,7 +10,12 @@
 
 package org.zowe.apiml.caching.health;
 
+import org.apache.commons.lang3.StringUtils;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.remoting.transport.Address;
 import org.infinispan.spring.embedded.provider.SpringEmbeddedCacheManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.Status;
@@ -19,13 +24,18 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.zowe.apiml.caching.service.infinispan.config.LazyCacheManager;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 @ConditionalOnProperty(name = "caching.storage.mode", havingValue = "infinispan")
 public class CachesHealthIndicator extends AbstractHealthIndicator {
+
+    @Value("${caching.storage.infinispan.initialHosts:}")
+    private String initialHosts;
 
     private final AtomicReference<CacheManager> cacheManager = new AtomicReference<>();
 
@@ -40,9 +50,9 @@ public class CachesHealthIndicator extends AbstractHealthIndicator {
         boolean health = true;
         if (cm instanceof SpringEmbeddedCacheManager springEmbeddedCacheManager) {
             var nativeCacheManager = springEmbeddedCacheManager.getNativeCacheManager();
-            var status = nativeCacheManager.getStatus();
-
             var infinispan = new HashMap<String, Object>();
+
+            var status = nativeCacheManager.getStatus();
             infinispan.put("status", status);
 
             health &= status.allowInvocations();
@@ -53,6 +63,18 @@ public class CachesHealthIndicator extends AbstractHealthIndicator {
                 health &= cacheStatus.allowInvocations();
             }
             infinispan.put("caches", caches);
+
+            var initialHostsArray = StringUtils.split(initialHosts, ",");
+            var cluster = Map.of(
+                "address", nativeCacheManager.getAddress().toString(),
+                "initialHosts", initialHostsArray,
+                "members", nativeCacheManager.getMembers().stream().map(Address::toString).toList()
+            );
+            infinispan.put("cluster", cluster);
+            if (initialHostsArray.length > nativeCacheManager.getMembers().size()) {
+                health = false;
+            }
+
             builder.withDetail("infinispan", infinispan);
         }
 
