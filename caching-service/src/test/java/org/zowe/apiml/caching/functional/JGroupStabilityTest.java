96ab168eca4ca4d5fbd064b9f503ef9afcc480f3
nxhafa <nxhafa11@gmail.com>
2026-02-12 10:35:22 +0100

    update of unit test
    Signed-off-by: nxhafa <nxhafa11@gmail.com>

--- caching-service/src/test/java/org/zowe/apiml/caching/functional/JGroupStabilityTest.java
+++ caching-service/src/test/java/org/zowe/apiml/caching/functional/JGroupStabilityTest.java
@@ -11,26 +11,17 @@
 package org.zowe.apiml.caching.functional;
 
 import com.jayway.jsonpath.JsonPath;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.infinispan.configuration.ConfigurationManager;
-import org.infinispan.manager.DefaultCacheManager;
-import org.jgroups.stack.Protocol;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.net.ssl.*;
 import java.io.*;
@@ -40,168 +31,62 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @Slf4j
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@SpringBootTest(
-    properties = {
-        "server.port=27025",
-        "apiml.service.port=27025",
-        "jgroups.bind.port=27600",
-        "jgroups.bind.address=localhost",
-        "jgroups.keyExchange.port=17601",
-        "caching.storage.infinispan.initialHosts=localhost[17600],localhost[27600]",
-        "caching.storage.mode=infinispan",
-        "infinispan.embedded.enabled=true",
-
-        /*"server.ssl.keyStore=../keystore/localhost/localhost.keystore.p12",
-        "server.ssl.keyStoreType=PKCS12",
-        "server.ssl.keyStorePassword=password",
-        "server.ssl.keyAlias=localhost",
-        "server.ssl.keyPassword=password",
-        "server.ssl.trustStore=../keystore/localhost/localhost.truststore.p12",
-        "server.ssl.trustStoreType=PKCS12",
-        "server.ssl.trustStorePassword=password",*/
-
-        "apiml.health.protected=false"
-    },
-    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
-)
 public class JGroupStabilityTest {
-
-    @Autowired
-    private DefaultCacheManager defaultCacheManager;
-
-    private Process terminalCommandProcess;
-
-    private void executeCommand(ProcessBuilder pb) {
-        try {
-            terminalCommandProcess = pb.start();
-            try (
-                InputStream inputStream = terminalCommandProcess.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
-            ) {
-
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    log.info(line);
-                }
-            }
-        } catch (IOException ioException) {
-            fail(ioException);
-        }
-    }
-
-    @BeforeAll
-    void startCachingService() {
-        var env = new HashMap<String, String>();
-        env.put("LAUNCH_COMPONENT", "caching-service/build/libs");
-
-        env.put("ZWE_configs_port", "17025");
-
-        env.put("ZWE_configs_storage_infinispan_jgroups_port", "17600");
-        env.put("ZWE_configs_storage_infinispan_jgroups_keyExchange_port", "17601");
-        env.put("ZWE_configs_storage_infinispan_initialHosts", "localhost[17600],localhost[27600]");
-        env.put("ZWE_configs_storage_mode", "infinispan");
-
-        env.put("ZWE_zowe_certificate_keystore_file", "keystore/localhost/localhost.keystore.p12");
-        env.put("ZWE_zowe_certificate_keystore_password", "password");
-        env.put("ZWE_zowe_certificate_keystore_alias", "localhost");
-        env.put("ZWE_zowe_certificate_key_password", "password");
-
-        env.put("ZWE_zowe_certificate_truststore_file", "keystore/localhost/localhost.truststore.p12");
-        env.put("ZWE_zowe_certificate_truststore_password", "password");
-
-        env.put("ZWE_configs_apiml_health_protected", "false");
-
-        ProcessBuilder builder = new ProcessBuilder("caching-service-package/src/main/resources/bin/start.sh");
-        builder.environment().putAll(env);
-
-        File binFolder = new File("../");
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
-        builder.directory(binFolder);
-        executorService.submit(() -> executeCommand(builder));
-    }
-
-    @AfterAll
-    void stopCachingService() {
-        if (terminalCommandProcess != null) {
-            terminalCommandProcess.destroy();
-        }
-    }
-
-    private boolean isUp(boolean local) {
-        HttpGet request = new HttpGet(local ? "https://localhost:27025/cachingservice/application/health" : "https://localhost:17025/cachingservice/application/health");
-        request.addHeader(HttpHeaders.ACCEPT, APPLICATION_JSON);
-        try (CloseableHttpClient client = HttpClients.custom().setSSLContext(ignoreSslContext()).setSSLHostnameVerifier(new NoopHostnameVerifier()).build()) {
-            final HttpResponse response = client.execute(request);
-            final String jsonResponse = EntityUtils.toString(response.getEntity());
-            log.debug("URI: {}, JsonResponse is {}", request.getURI().toString(), jsonResponse);
-
-            if (StringUtils.isNotEmpty(jsonResponse)) {
-                var status = JsonPath.parse(jsonResponse).read("components.caches.details.infinispan.cluster.status.status", String.class);
-                return "UP".equals(status);
-            }
-            return false;
-        } catch (IOException e) {
-            log.warn("Check failed on getting the document: {}", e.getMessage());
-            return false;
-        }
-    }
-
-    private boolean isUp() {
-        return isUp(true) && isUp(false);
-    }
-
-    private boolean isDown() {
-        return !isUp(false);
-    }
 
     /**
      * TODO:
      * run as: caching vs. apiml
-     * run ion HTTP to verify AT-TLS
+     * run ion HTTP to verify AT-TLS (update test as parametrized)
      *
      * @throws Exception
      */
-    //@Test
+    @Test
     void givenTwoInstances_whenOneHasADelay_thenClusterIsRebuilt() throws Exception {
-        // wait to establish the cluster
-        await()
-            .pollDelay(10, TimeUnit.SECONDS)
-            .timeout(5, TimeUnit.MINUTES)
-            .until(this::isUp);
 
-        // create a delay in local instance
-        //defaultCacheManager.getConfigurationManager().getGlobalConfiguration().transport().jgroups().stackConfigurations.get(0).configurator().getProtocolStack()
-        var delay = new DelayProtocol();
-        ConfigurationManager configurationManager = ReflectionTestUtils.invokeMethod(defaultCacheManager, "getConfigurationManager");
-        for (var stack : configurationManager.getGlobalConfiguration().transport().jgroups().stacks()) {
-            var protocols = stack.configurator().getUncombinedProtocolStack();
-            //protocols.add(0, delay);
+        CachingService[] cachingServices = new CachingService[]{
+            new CachingService(17000, new int[]{17000, 27000}),
+            new CachingService(27000, new int[]{17000, 27000})
+        };
+
+        try {
+            Arrays.stream(cachingServices).forEach(CachingService::start);
+            Arrays.stream(cachingServices).forEach(cachingService -> {
+                await()
+                    .pollDelay(10, TimeUnit.SECONDS)
+                    .timeout(1, TimeUnit.MINUTES)
+                    .until(cachingService::isUp);
+            });
+
+            cachingServices[1].pause();
+
+            await()
+                .pollDelay(10, TimeUnit.SECONDS)
+                .timeout(1, TimeUnit.MINUTES)
+                .until(() -> cachingServices[0].isDown());
+
+            cachingServices[1].resume();
+
+            Arrays.stream(cachingServices).forEach(cachingService -> {
+                await()
+                    .pollDelay(10, TimeUnit.SECONDS)
+                    .timeout(1, TimeUnit.MINUTES)
+                    .until(cachingService::isUp);
+            });
+        } finally {
+            Arrays.stream(cachingServices).forEach(CachingService::kill);
         }
-
-        // wait till cluster is down
-        await()
-            .pollDelay(10, TimeUnit.SECONDS)
-            .timeout(1, TimeUnit.MINUTES)
-            .until(this::isDown);
-
-        // fix the local instance
-
-        // wait to establish the cluster
-        await()
-            .pollDelay(10, TimeUnit.SECONDS)
-            .timeout(1, TimeUnit.MINUTES)
-            .until(this::isUp);
     }
 
     public static SSLContext ignoreSslContext() {
@@ -256,7 +141,117 @@ public class JGroupStabilityTest {
         }
     }
 
-    static class DelayProtocol extends Protocol {
+    @RequiredArgsConstructor
+    static class CachingService {
+
+        private final int basePort;
+        private final int[] allBasePorts;
+
+        private Process terminalCommandProcess;
+        private String pid;
+
+        public void start() {
+            var env = new HashMap<String, String>();
+            env.put("ZWE_haInstance_id", "localhost" + basePort);
+            env.put("logbackService", "ZWEAGW" + basePort);
+            env.put("LAUNCH_COMPONENT", "caching-service/build/libs");
+
+            env.put("ZWE_configs_port", String.valueOf(basePort + 25));
+
+            env.put("ZWE_configs_storage_infinispan_jgroups_port", String.valueOf(basePort + 600));
+            env.put("ZWE_configs_storage_infinispan_jgroups_keyExchange_port", String.valueOf(allBasePorts[0] + 601));
+            env.put("ZWE_configs_storage_infinispan_initialHosts", Arrays.stream(allBasePorts).mapToObj(bp -> "localhost[" + (bp + 600) + "]").collect(Collectors.joining(",")));
+            env.put("ZWE_configs_storage_mode", "infinispan");
+
+            env.put("ZWE_zowe_certificate_keystore_file", "keystore/localhost/localhost.keystore.p12");
+            env.put("ZWE_zowe_certificate_keystore_password", "password");
+            env.put("ZWE_zowe_certificate_keystore_alias", "localhost");
+            env.put("ZWE_zowe_certificate_key_password", "password");
+
+            env.put("ZWE_zowe_certificate_truststore_file", "keystore/localhost/localhost.truststore.p12");
+            env.put("ZWE_zowe_certificate_truststore_password", "password");
+
+            env.put("ZWE_configs_apiml_health_protected", "false");
+            env.put("ZWE_configs_debug", "true");
+
+            ProcessBuilder builder = new ProcessBuilder("caching-service-package/src/main/resources/bin/start.sh");
+            builder.environment().putAll(env);
+
+            File binFolder = new File("../");
+            ExecutorService executorService = Executors.newFixedThreadPool(1);
+            builder.directory(binFolder);
+            executorService.submit(() -> executeCommand(builder));
+        }
+
+        void executeCommand(ProcessBuilder pb) {
+            try {
+                terminalCommandProcess = pb.start();
+                try (
+                    InputStream inputStream = terminalCommandProcess.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
+                ) {
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        if (line.startsWith("pid=")) {
+                            pid = line.substring("pid=".length());
+                        }
+                        log.info(line);
+
+                    }
+                }
+            } catch (IOException ioException) {
+                fail(ioException);
+            }
+        }
+
+        void issue(String... parts) {
+            ProcessBuilder builder = new ProcessBuilder(parts);
+            try {
+                var proccess = builder.start();
+                // TODO try to print the output results in the console
+                int rc = proccess.waitFor();
+                log.info("Command {} ends with RC={}", rc);
+                proccess.destroy();
+            } catch (IOException | InterruptedException e) {
+                log.warn("cannot issue the command {}", StringUtils.join(parts, " "), e);
+            }
+        }
+
+        public void pause() {
+            issue("kill", "-STOP", pid);
+        }
+
+        public void resume() {
+            issue("kill", "-CONT", pid);
+        }
+
+        public void kill() {
+            issue("kill", "-9", pid);
+            terminalCommandProcess.destroy();
+        }
+
+        public boolean isUp() {
+            HttpGet request = new HttpGet("https://localhost:" + (basePort + 25) + "/cachingservice/application/health");
+            request.addHeader(HttpHeaders.ACCEPT, APPLICATION_JSON);
+            try (CloseableHttpClient client = HttpClients.custom().setSSLContext(ignoreSslContext()).setSSLHostnameVerifier(new NoopHostnameVerifier()).build()) {
+                final HttpResponse response = client.execute(request);
+                final String jsonResponse = EntityUtils.toString(response.getEntity());
+                log.debug("URI: {}, JsonResponse is {}", request.getURI().toString(), jsonResponse);
+
+                if (StringUtils.isNotEmpty(jsonResponse)) {
+                    var status = JsonPath.parse(jsonResponse).read("components.caches.details.infinispan.cluster.status.status", String.class);
+                    return "UP".equals(status);
+                }
+                return false;
+            } catch (Exception e) {
+                log.warn("Check failed on getting the document: {}", e.getMessage());
+                return false;
+            }
+        }
+
+        public boolean isDown() {
+            return !isUp();
+        }
 
     }
 
