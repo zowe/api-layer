93b1ba9c44118c2ad4b43e9b036dbfc1ae8cc63d
ac892247 <a.chmelo@gmail.com>
2026-02-18 09:28:02 +0100

    client cert store
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- apiml-common/src/main/java/org/zowe/apiml/product/web/HttpConfig.java
+++ apiml-common/src/main/java/org/zowe/apiml/product/web/HttpConfig.java
@@ -53,37 +53,37 @@ public class HttpConfig implements InitializingBean {
 
     private static final char[] KEYRING_PASSWORD = "password".toCharArray();
 
-    @Value("${server.ssl.protocol:TLSv1.2}")
+    @Value("${apiml.httpclient.ssl.protocol:${server.ssl.protocol:TLSv1.2}}")
     private String protocol;
 
     @Value("${apiml.httpclient.ssl.enabled-protocols:TLSv1.2,TLSv1.3}")
     private String[] supportedProtocols;
 
-    @Value("${server.ssl.ciphers:TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,TLS_AES_128_GCM_SHA256,TLS_AES_256_GCM_SHA384}")
+    @Value("${apiml.httpclient.ssl.ciphers:${server.ssl.ciphers:TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,TLS_AES_128_GCM_SHA256,TLS_AES_256_GCM_SHA384}}")
     private String[] ciphers;
 
-    @Value("${server.ssl.trustStore:#{null}}")
+    @Value("${apiml.httpclient.ssl.trust-store:${server.ssl.trustStore:#{null}}}")
     private String trustStorePath;
 
-    @Value("${server.ssl.trustStorePassword:#{null}}")
+    @Value("${apiml.httpclient.ssl.trust-store-password:${server.ssl.trustStorePassword:#{null}}}")
     private char[] trustStorePassword;
 
-    @Value("${server.ssl.trustStoreType:PKCS12}")
+    @Value("${apiml.httpclient.ssl.trust-store-type:${server.ssl.trustStoreType:PKCS12}}")
     private String trustStoreType;
 
-    @Value("${server.ssl.keyAlias:#{null}}")
+    @Value("${apiml.httpclient.ssl.key-alias:${server.ssl.keyAlias:#{null}}}")
     private String keyAlias;
 
-    @Value("${server.ssl.keyStore:#{null}}")
+    @Value("${apiml.httpclient.ssl.key-store:${server.ssl.keyStore:#{null}}}")
     private String keyStorePath;
 
-    @Value("${server.ssl.keyStorePassword:#{null}}")
+    @Value("${apiml.httpclient.ssl.key-store-password:${server.ssl.keyStorePassword:#{null}}}")
     private char[] keyStorePassword;
 
-    @Value("${server.ssl.keyPassword:#{null}}")
+    @Value("${apiml.httpclient.ssl.key-password:${server.ssl.keyPassword:#{null}}}")
     private char[] keyPassword;
 
-    @Value("${server.ssl.keyStoreType:PKCS12}")
+    @Value("${apiml.httpclient.ssl.key-store-type:${server.ssl.keyStoreType:PKCS12}}")
     private String keyStoreType;
 
     @Value("${apiml.security.ssl.verifySslCertificatesOfServices:true}")
@@ -92,7 +92,7 @@ public class HttpConfig implements InitializingBean {
     @Value("${apiml.security.ssl.nonStrictVerifySslCertificatesOfServices:false}")
     private boolean nonStrictVerifySslCertificatesOfServices;
 
-    @Value("${server.ssl.trustStoreRequired:false}")
+    @Value("${apiml.httpclient.ssl.trust-store-required:${server.ssl.trustStoreRequired:false}}")
     private boolean trustStoreRequired;
 
     @Value("${server.maxConnectionsPerRoute:#{10}}")
@@ -123,16 +123,25 @@ public class HttpConfig implements InitializingBean {
     private final ApplicationContext context;
 
     void updateStorePaths() {
-        ServerProperties serverProperties = context.getBean(ServerProperties.class);
         if (SecurityUtils.isKeyring(keyStorePath)) {
             keyStorePath = SecurityUtils.formatKeyringUrl(keyStorePath);
-            serverProperties.getSsl().setKeyStore(keyStorePath);
             if (keyStorePassword == null) keyStorePassword = KEYRING_PASSWORD;
         }
         if (SecurityUtils.isKeyring(trustStorePath)) {
             trustStorePath = SecurityUtils.formatKeyringUrl(trustStorePath);
-            serverProperties.getSsl().setTrustStore(trustStorePath);
             if (trustStorePassword == null) trustStorePassword = KEYRING_PASSWORD;
+        }
+
+        ServerProperties serverProperties = context.getBean(ServerProperties.class);
+        if (serverProperties.getSsl() != null) {
+            String serverKeyStore = serverProperties.getSsl().getKeyStore();
+            if (SecurityUtils.isKeyring(serverKeyStore)) {
+                serverProperties.getSsl().setKeyStore(SecurityUtils.formatKeyringUrl(serverKeyStore));
+            }
+            String serverTrustStore = serverProperties.getSsl().getTrustStore();
+            if (SecurityUtils.isKeyring(serverTrustStore)) {
+                serverProperties.getSsl().setTrustStore(SecurityUtils.formatKeyringUrl(serverTrustStore));
+            }
         }
     }
 
