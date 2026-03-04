1e899d2848f06853abb7ce4a55e1f87795913e6e
Pablo Carle <pablo.carle@broadcom.com>
2026-03-04 17:17:41 +0100

    remove branch
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- onboarding-enabler-python/release.config.js
+++ onboarding-enabler-python/release.config.js
@@ -7,10 +7,6 @@ module.exports = {
         {
             name: "zowe-v?-lts",
             level: "patch"
-        },
-        {
-            name: "reboot/fix/python-enabler-release",
-            level: "patch"
         }
     ],
     plugins: [
