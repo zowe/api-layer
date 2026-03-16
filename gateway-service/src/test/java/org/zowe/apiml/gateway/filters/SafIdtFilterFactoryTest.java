0183a1d522e2474e4aaa65ce13261d0bcc721705
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-03-16 16:12:20 +0100

    org.zowe.apiml.gateway.filters.SafIdtFilterFactory.getAuthenticationScheme tests
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

---
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/filters/SafIdtFilterFactoryTest.java
@@ -0,0 +1,26 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.gateway.filters;
+
+import org.junit.jupiter.api.Test;
+import org.zowe.apiml.auth.AuthenticationScheme;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SafIdtFilterFactoryTest {
+
+    @Test
+    void givenSafIdtFilterFactory_whenGetAuthenticationScheme_thenReturnSafIdt() {
+        var safIdtFilterFactory = new SafIdtFilterFactory(null, null, null);
+        assertEquals(AuthenticationScheme.SAF_IDT, safIdtFilterFactory.getAuthenticationScheme());
+    }
+
+}
