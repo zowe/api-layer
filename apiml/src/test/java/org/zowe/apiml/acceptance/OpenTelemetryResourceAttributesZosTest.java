83d11f280803e6e06293c4ea41ec5fdeb5f49e37
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-03-17 10:29:00 +0100

    fix auth status in test
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- apiml/src/test/java/org/zowe/apiml/acceptance/OpenTelemetryResourceAttributesZosTest.java
+++ apiml/src/test/java/org/zowe/apiml/acceptance/OpenTelemetryResourceAttributesZosTest.java
@@ -19,11 +19,7 @@ import io.opentelemetry.sdk.testing.exporter.InMemoryLogRecordExporter;
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader;
 import io.restassured.http.ContentType;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
@@ -48,11 +44,7 @@ import java.util.Map;
 import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static io.restassured.RestAssured.given;
 import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OpenTelemetryResourceAttributesZosTest {
 
@@ -141,7 +133,7 @@ class OpenTelemetryResourceAttributesZosTest {
 
         @BeforeAll
         void startMockServices() {
-            mockServiceZoweJwt = mockService("testserviceXXXXXXXXXXXX")
+            mockServiceZoweJwt = mockService("testservice")
                 .scope(Scope.CLASS)
                 .authenticationScheme(AuthenticationScheme.ZOWE_JWT)
                 .addEndpoint("/testservice/200")
@@ -328,7 +320,7 @@ class OpenTelemetryResourceAttributesZosTest {
             assertEquals("INFO", logRecord.getSeverityText(), "Expected INFO log level, was " + logRecord.getSeverityText());
             assertEquals("testservicept", getAttribute(logBody, "service.id"));
             assertEquals("GET", getAttribute(logBody, "http.request.method"));
-            assertNull(getAttribute(logBody, "auth.status"));
+            assertEquals("OK", getAttribute(logBody, "auth.status"));
             assertEquals("localhost:testservicept:" + mockServicePassTicket.getPort(), getAttribute(logBody, "service.instance.id"));
             assertEquals("200", getAttribute(logBody, "service.response_code"));
             assertEquals("/testservicept/api/v1/200", getAttribute(logBody, "url.path"));
