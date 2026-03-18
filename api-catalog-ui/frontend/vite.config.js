28b112753599eee1527cea98c10133c9dc961626
ac892247 <a.chmelo@gmail.com>
2026-03-18 10:16:26 +0100

    url path for build
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- api-catalog-ui/frontend/vite.config.js
+++ api-catalog-ui/frontend/vite.config.js
@@ -10,8 +10,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+    root: __dirname,
     plugins: [
         react(),
         nodePolyfills({
