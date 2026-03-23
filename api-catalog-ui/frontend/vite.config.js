7bd83c121bd6f278c5bddd659c069afa54833012
ac892247 <a.chmelo@gmail.com>
2026-03-23 10:01:23 +0100

    fix dev build
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- api-catalog-ui/frontend/vite.config.js
+++ api-catalog-ui/frontend/vite.config.js
@@ -7,7 +7,7 @@
  *
  * Copyright Contributors to the Zowe Project.
  */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { fileURLToPath } from 'url';
@@ -15,39 +15,45 @@ import { dirname } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-    root: __dirname,
-    plugins: [
-        react(),
-        nodePolyfills({
-            include: ['buffer', 'process', 'stream', 'util', 'url', 'querystring'],
-            globals: {
-                Buffer: true,
-                global: true,
-                process: true,
-            },
-        }),
-    ],
-    base: './',
-    build: {
-        outDir: 'build',
-        sourcemap: false,
-        assetsDir: 'static',
-    },
-    server: {
-        https: true,
-        proxy: {
-            '/apicatalog': {
-                target: 'https://localhost:10010',
-                secure: false,
-                changeOrigin: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const defaultTarget = mode === 'development' ? 'http://localhost:8000' : 'https://localhost:10010';
+    const gatewayUrl = env.VITE_GATEWAY_URL || defaultTarget;
+
+    return {
+        root: __dirname,
+        plugins: [
+            react(),
+            nodePolyfills({
+                include: ['buffer', 'process', 'stream', 'util', 'url', 'querystring'],
+                globals: {
+                    Buffer: true,
+                    global: true,
+                    process: true,
+                },
+            }),
+        ],
+        base: './',
+        build: {
+            outDir: 'build',
+            sourcemap: false,
+            assetsDir: 'static',
+        },
+        server: {
+            https: false,
+            proxy: {
+                '/apicatalog': {
+                    target: gatewayUrl,
+                    secure: false,
+                    changeOrigin: true,
+                },
             },
         },
-    },
-    optimizeDeps: {
-        include: ['swagger-ui-react'],
-    },
-    define: {
-        'process.env': {},
-    },
+        optimizeDeps: {
+            include: ['swagger-ui-react'],
+        },
+        define: {
+            'process.env': {},
+        },
+    };
 });
