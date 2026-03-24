8e54689e6d7d54ebe51457a0b85af93dad0628a8
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-03-24 13:37:42 +0100

    test on GW
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

---
+++ gateway-service/src/test/java/org/zowe/apiml/acceptance/ResponseHeaderFixTest.java
@@ -0,0 +1,112 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.acceptance;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.zowe.apiml.gateway.GatewayApplication;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static io.restassured.RestAssured.given;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@SpringBootTest(
+    classes = {
+        GatewayApplication.class,
+        ResponseHeaderFixTest.TestController.class
+    },
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@ActiveProfiles("ResponseHeaderFixTest")
+public class ResponseHeaderFixTest {
+
+    private static final int TEST_CONTENT_LENGTH = 101;
+    private static final String CONTENT_LENGHT = "Content-Length";
+
+    @LocalServerPort
+    protected int port;
+
+    @ParameterizedTest(name = "Test handling setting context-type using {1}")
+    @CsvSource({
+        "0,addHeader(String name, String value)",
+        "1,setHeader(String name, String value)",
+        "2,setIntHeader(String name, int value)",
+        "3,addIntHeader(String name, int value)"
+    })
+    void givenRequest_whenSetContentLength_thenIsPropagated(int method, String description) {
+        given()
+            .relaxedHTTPSValidation()
+        .when()
+            .get(String.format("https://localhost:%d/test/%d/%s", port, method, CONTENT_LENGHT))
+        .then()
+            .statusCode(SC_OK)
+            .header(CONTENT_LENGHT, String.valueOf(TEST_CONTENT_LENGTH))
+            .header("Strict-Transport-Security", is(notNullValue()))
+            .header("X-XSS-Protection", is(notNullValue()));
+    }
+
+    @ParameterizedTest(name = "Test handling headers without content-type using {1}")
+    @CsvSource({
+        "0,addHeader(String name, String value)",
+        "1,setHeader(String name, String value)",
+        "2,setIntHeader(String name, int value)",
+        "3,addIntHeader(String name, int value)"
+    })
+    void givenRequest_whenDontSetContentLength_thenIsMissing(int method, String description) {
+        given()
+            .relaxedHTTPSValidation()
+        .when()
+            .get(String.format("https://localhost:%d/test/%d/%s", port, method, "otherHeaderName"))
+        .then()
+            .statusCode(SC_OK)
+            .header(CONTENT_LENGHT,"0")
+            .header("Strict-Transport-Security", is(notNullValue()))
+            .header("X-XSS-Protection", is(notNullValue()));
+    }
+
+    @RestController
+    @Profile("ResponseHeaderFixTest")
+    static class TestController {
+
+        @GetMapping(value = "/test/{method}/{headerName}")
+        public void getApiDoc(@PathVariable("method") int method, @PathVariable("headerName") String headerName, HttpServletResponse response) {
+            switch(method) {
+                case 0:
+                    response.addHeader(headerName, String.valueOf(TEST_CONTENT_LENGTH));
+                    break;
+                case 1:
+                    response.setHeader(headerName, String.valueOf(TEST_CONTENT_LENGTH));
+                    break;
+                case 2:
+                    response.setIntHeader(headerName, TEST_CONTENT_LENGTH);
+                    break;
+                case 3:
+                    response.addIntHeader(headerName, TEST_CONTENT_LENGTH);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Uknown method: " + method);
+            }
+
+        }
+
+    }
+
+}
