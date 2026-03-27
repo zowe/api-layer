70264fbaf6ecdefcfd8faa00a07222f13dad4db2
nxhafa <nxhafa11@gmail.com>
2026-03-27 16:14:57 +0100

    rename variables
    Signed-off-by: nxhafa <nxhafa11@gmail.com>

--- caching-service/src/test/java/org/zowe/apiml/caching/health/InfinispanHealthIndicatorTest.java
+++ caching-service/src/test/java/org/zowe/apiml/caching/health/InfinispanHealthIndicatorTest.java
@@ -112,12 +112,12 @@ class InfinispanHealthIndicatorTest {
             doReturn(cache).when(nativeCacheManager).getCache(CACHE_1);
             doReturn(cacheStatus).when(cache).getStatus();
 
-            var cachesHealthIndicator = new InfinispanHealthIndicator();
-            ((AtomicReference<CacheManager>) ReflectionTestUtils.getField(cachesHealthIndicator, "cacheManager"))
+            var infinispanHealthIndicator = new InfinispanHealthIndicator();
+            ((AtomicReference<CacheManager>) ReflectionTestUtils.getField(infinispanHealthIndicator, "cacheManager"))
                 .set(cacheManager);
-            ReflectionTestUtils.setField(cachesHealthIndicator, "initialHosts", "");
+            ReflectionTestUtils.setField(infinispanHealthIndicator, "initialHosts", "");
             var builder = mock(Health.Builder.class);
-            cachesHealthIndicator.doHealthCheck(builder);
+            infinispanHealthIndicator.doHealthCheck(builder);
 
             verify(builder).withDetail(eq(INFINISPAN), mapCaptor.capture());
             var mapDetails = mapCaptor.getValue();
