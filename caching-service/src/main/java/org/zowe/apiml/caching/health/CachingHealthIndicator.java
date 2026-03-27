878d4d73b3597c24fefb0e597e84432a88163d25
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-03-27 13:21:19 +0100

    code review
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- caching-service/src/main/java/org/zowe/apiml/caching/health/CachingHealthIndicator.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/health/CachingHealthIndicator.java
@@ -22,7 +22,7 @@ import org.zowe.apiml.eurekaservice.client.ApiMediationClient;
 import org.zowe.apiml.product.constants.CoreService;
 
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Caching service health information (/cachingservice/application/health)
@@ -31,7 +31,7 @@ import java.util.concurrent.atomic.AtomicReference;
 @RequiredArgsConstructor
 public class CachingHealthIndicator extends AbstractHealthIndicator implements ApplicationListener<ApplicationReadyEvent> {
 
-    private final AtomicReference<Boolean> serviceUp = new AtomicReference<>(false);
+    private final AtomicBoolean serviceUp = new AtomicBoolean();
 
     private final ApiMediationClient apiMediationClient;
     private final Optional<CachesHealthIndicator> cachesHealthIndicator;
