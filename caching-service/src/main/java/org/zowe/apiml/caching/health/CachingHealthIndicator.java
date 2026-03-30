465de3e00b33011dbe28d1514ab5164fa28e69e3
Pavel Jareš <58428711+pavel-jares-bcm@users.noreply.github.com>
2026-03-30 09:16:36 +0200

    fix: Cherry-pick fixes from v3 (#4524)
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>
    Signed-off-by: nxhafa <nxhafa11@gmail.com>
    Co-authored-by: nxhafa <nxhafa11@gmail.com>

---
+++ caching-service/src/main/java/org/zowe/apiml/caching/health/CachingHealthIndicator.java
@@ -0,0 +1,62 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml.caching.health;
+
+import com.netflix.discovery.shared.Application;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.zowe.apiml.eurekaservice.client.ApiMediationClient;
+import org.zowe.apiml.product.constants.CoreService;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Caching service health information (/cachingservice/application/health)
+ */
+@Component
+@RequiredArgsConstructor
+public class CachingHealthIndicator extends AbstractHealthIndicator implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final AtomicBoolean serviceUp = new AtomicBoolean();
+
+    private final ApiMediationClient apiMediationClient;
+    private final Optional<InfinispanHealthIndicator> infinispanHealthIndicator;
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) {
+        builder.up();
+
+        boolean gatewayUp = Optional.ofNullable(apiMediationClient.getEurekaClient())
+            .map(eurekaClient -> eurekaClient.getApplication(CoreService.GATEWAY.getServiceId()))
+            .map(Application::getInstances)
+            .map(instanceList -> !instanceList.isEmpty())
+            .orElse(false);
+        builder.withDetail(CoreService.GATEWAY.getServiceId(), gatewayUp ? Status.UP : Status.DOWN);
+
+        infinispanHealthIndicator.ifPresent(healthIndicator -> healthIndicator.doHealthCheck(builder));
+        boolean isCachingUp = this.serviceUp.get();
+        if (!(isCachingUp && gatewayUp)) {
+            builder.down();
+        }
+    }
+
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+        serviceUp.set(true);
+    }
+
+}
