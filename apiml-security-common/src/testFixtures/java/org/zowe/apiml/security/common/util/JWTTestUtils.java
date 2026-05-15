a2690fc012ca6de533eeca30a2cb83a4cd4f39e2
Pablo Carle <pablo.carle@broadcom.com>
2026-05-15 14:41:34 +0200

    remove duplicate method
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- apiml-security-common/src/testFixtures/java/org/zowe/apiml/security/common/util/JWTTestUtils.java
+++ apiml-security-common/src/testFixtures/java/org/zowe/apiml/security/common/util/JWTTestUtils.java
@@ -48,10 +48,6 @@ public class JWTTestUtils {
         return createToken(username, domain, null, null, scopes, config, "APIML_PAT");
     }
 
-    public static String createZoweJwtToken(String username, String domain, String ltpaToken, HttpsConfig config) {
-        return createToken(username, domain, ltpaToken, null, null, config, "APIML");
-    }
-
     public static String createToken(String username, String domain, String ltpaToken, Long expiration, List<String> scopes, HttpsConfig config, String issuer) {
         long now = System.currentTimeMillis();
         if (expiration == null) {
