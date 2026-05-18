893979658ed7f7b5bf21e03f8a2ebf3269102d4e
Pablo Carle <pablo.carle@broadcom.com>
2026-05-18 16:48:47 +0200

    fix spel
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- gateway-service/src/main/java/org/zowe/apiml/gateway/config/GatewayHealthIndicator.java
+++ gateway-service/src/main/java/org/zowe/apiml/gateway/config/GatewayHealthIndicator.java
@@ -41,7 +41,7 @@ import static org.springframework.boot.actuate.health.Status.UP;
 @Slf4j
 public class GatewayHealthIndicator extends AbstractHealthIndicator {
 
-    @Value("${apiml.service.hostname")
+    @Value("${apiml.service.hostname}")
     private String hostname;
 
     protected final DiscoveryClient discoveryClient;
