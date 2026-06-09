d733981015cd3badc49a317901f3b96c7a46cae8
Pablo Carle <pablo.carle@broadcom.com>
2026-06-09 16:02:43 +0200

    update cloud gateway test
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- integration-tests/src/test/java/org/zowe/apiml/integration/proxy/XForwardHeadersProxyTest.java
+++ integration-tests/src/test/java/org/zowe/apiml/integration/proxy/XForwardHeadersProxyTest.java
@@ -19,15 +19,21 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.zowe.apiml.util.config.*;
+import org.zowe.apiml.util.config.CloudGatewayConfiguration;
+import org.zowe.apiml.util.config.ConfigReader;
+import org.zowe.apiml.util.config.GatewayServiceConfiguration;
+import org.zowe.apiml.util.config.ItSslConfigFactory;
+import org.zowe.apiml.util.config.SslContext;
 
 import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.emptyOrNullString;
 import static org.zowe.apiml.util.SecurityUtils.COOKIE_NAME;
 import static org.zowe.apiml.util.SecurityUtils.gatewayToken;
 import static org.zowe.apiml.util.requests.Endpoints.REQUEST_INFO_ENDPOINT;
@@ -88,7 +94,7 @@ class XForwardHeadersProxyTest {
             .statusCode(HttpStatus.SC_OK)
             .body("headers.x-forwarded-proto", is("https,https"))
             .body("headers.x-forwarded-prefix", is("/dcpassticket/api/v1"))
-            .body("headers.x-forwarded-port", is(cgwConf.getPort() + "," + gwConf.getInternalPorts()))
+            .body("headers.x-forwarded-port", is(cgwConf.getPort() + "," + gwConf.getPort()))
             .body("headers.x-forwarded-for", containsString(cgwIp))
             .body("headers.x-forwarded-for", containsString(localIp))
             .body("headers.x-forwarded-host", containsString(cgwConf.getHost()))
@@ -111,7 +117,7 @@ class XForwardHeadersProxyTest {
             .statusCode(HttpStatus.SC_OK)
             .body("headers.x-forwarded-proto", is("https,https"))
             .body("headers.x-forwarded-prefix", is("/dcpassticket/api/v1"))
-            .body("headers.x-forwarded-port", is(cgwConf.getPort() + "," + gwConf.getInternalPorts()))
+            .body("headers.x-forwarded-port", is(cgwConf.getPort() + "," + gwConf.getPort()))
             .body("headers.x-forwarded-for", not(containsString("6.6.6.6")))
             .body("headers.x-forwarded-for", is(cgwIp))
             .body("headers.x-forwarded-host", not(containsString("9.9.9.9")))
@@ -135,7 +141,7 @@ class XForwardHeadersProxyTest {
             .body("headers.x-forwarded-proto", is("https"))
             .body("headers.x-forwarded-prefix", is("/dcpassticket/api/v1"))
             .body("headers.x-forwarded-port", is(String.valueOf(gwConf.getPort())))
-            .body("headers.x-forwarded-for", isEmptyOrNullString())
+            .body("headers.x-forwarded-for", is(emptyOrNullString()))
             .body("headers.x-forwarded-host", not(containsString("9.9.9.9")))
             .body("headers.x-forwarded-host", containsString(gwConf.getHost()));
     }
