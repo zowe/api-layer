e80e2a55c53bbd1d164290f156b2b794362709aa
Jakub Balhar <jakub.balhar@broadcom.com>
2026-06-10 15:25:19 +0200

    feat: expand CachingControllerAdvice with individual typed exception handlers
    Add @ExceptionHandler methods for all 7 typed exception subclasses:
    KeyNotFoundException, KeyNotProvidedException, InvalidPayloadException,
    DuplicateKeyException, CacheNotAvailableException,
    InsufficientStorageException, IncompatibleStorageMethodException.
    
    Add exceptionToMonoResponse() helper for consistent error response
    formatting. Keep StorageException handler as fallback. Add debug logging
    with request URI context. Use RestControllerAdvice(assignableTypes) to
    scope advice to CachingController.
    
    #4426

--- caching-service/src/main/java/org/zowe/apiml/caching/api/CachingControllerAdvice.java
+++ caching-service/src/main/java/org/zowe/apiml/caching/api/CachingControllerAdvice.java
@@ -12,33 +12,88 @@ package org.zowe.apiml.caching.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ServerWebExchange;
+import org.zowe.apiml.cache.CacheNotAvailableException;
+import org.zowe.apiml.cache.DuplicateKeyException;
+import org.zowe.apiml.cache.IncompatibleStorageMethodException;
+import org.zowe.apiml.cache.InsufficientStorageException;
+import org.zowe.apiml.cache.InvalidPayloadException;
+import org.zowe.apiml.cache.KeyNotFoundException;
+import org.zowe.apiml.cache.KeyNotProvidedException;
 import org.zowe.apiml.cache.StorageException;
 import org.zowe.apiml.message.core.Message;
 import org.zowe.apiml.message.core.MessageService;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = {CachingController.class})
 @RequiredArgsConstructor
 public class CachingControllerAdvice {
 
     private final MessageService messageService;
 
+    @ExceptionHandler(KeyNotFoundException.class)
+    public Mono<ResponseEntity<Object>> handleKeyNotFound(ServerWebExchange exchange, KeyNotFoundException ex) {
+        log.debug("Key not found on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
+    }
+
+    @ExceptionHandler(KeyNotProvidedException.class)
+    public Mono<ResponseEntity<Object>> handleKeyNotProvided(ServerWebExchange exchange, KeyNotProvidedException ex) {
+        log.debug("Key not provided on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
+    }
+
+    @ExceptionHandler(InvalidPayloadException.class)
+    public Mono<ResponseEntity<Object>> handleInvalidPayload(ServerWebExchange exchange, InvalidPayloadException ex) {
+        log.debug("Invalid payload on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public Mono<ResponseEntity<Object>> handleDuplicateKey(ServerWebExchange exchange, DuplicateKeyException ex) {
+        log.debug("Duplicate key on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
+    }
+
+    @ExceptionHandler(CacheNotAvailableException.class)
+    public Mono<ResponseEntity<Object>> handleCacheNotAvailable(ServerWebExchange exchange, CacheNotAvailableException ex) {
+        log.debug("Cache not available on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
+    }
+
+    @ExceptionHandler(InsufficientStorageException.class)
+    public Mono<ResponseEntity<Object>> handleInsufficientStorage(ServerWebExchange exchange, InsufficientStorageException ex) {
+        log.debug("Insufficient storage on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
+    }
+
+    @ExceptionHandler(IncompatibleStorageMethodException.class)
+    public Mono<ResponseEntity<Object>> handleIncompatibleStorageMethod(ServerWebExchange exchange, IncompatibleStorageMethodException ex) {
+        log.debug("Incompatible storage method on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
+    }
+
     @ExceptionHandler(StorageException.class)
-    public Mono<ResponseEntity<Object>> handleStorageException(StorageException exception) {
-        log.debug("Storage exception", exception);
-        Message message = messageService.createMessage(exception.getKey(), (Object[]) exception.getParameters());
-        return Mono.just(ResponseEntity.status(exception.getStatus()).body(message.mapToView()));
+    public Mono<ResponseEntity<Object>> handleStorageException(ServerWebExchange exchange, StorageException ex) {
+        log.debug("Storage exception on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        return exceptionToMonoResponse(ex);
     }
 
     @ExceptionHandler(Exception.class)
-    public Mono<ResponseEntity<Object>> handleException(Exception exception) {
-        log.debug("Internal error occurred", exception);
-        Message message = messageService.createMessage("org.zowe.apiml.common.internalRequestError",
-            exception.getMessage(), exception.toString());
-        return Mono.just(ResponseEntity.status(500).body(message.mapToView()));
+    public Mono<ResponseEntity<Object>> handleInternal(ServerWebExchange exchange, Exception ex) {
+        log.debug("Internal error on {}: {}", exchange.getRequest().getURI(), ex.getMessage());
+        Message message = messageService.createMessage(
+            "org.zowe.apiml.common.internalServerError", ex.getMessage());
+        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message.mapToView()));
+    }
+
+    private Mono<ResponseEntity<Object>> exceptionToMonoResponse(StorageException ex) {
+        Message message = messageService.createMessage(ex.getKey(), (Object[]) ex.getParameters());
+        return Mono.just(ResponseEntity.status(ex.getStatus()).body(message.mapToView()));
     }
 }
