81ba65ce3ed04e4b5f0679fd64ba2ef3977e481a
Andrea Tabone <andrea.tabone@broadcom.com>
2026-06-22 12:50:46 +0200

    disable virtual threads and temporarily comment the SSL_KEY_EXCHANGE and ASYM_ENCRYPT config
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

--- caching-service-package/src/main/resources/bin/start.sh
+++ caching-service-package/src/main/resources/bin/start.sh
@@ -166,6 +166,7 @@ _BPX_JOBNAME=${ZWE_zowe_job_prefix}${CACHING_CODE} ${JAVA_BIN_DIR}java \
   -Djgroups.tcp.diag.enabled=${ZWE_configs_storage_infinispan_jgroups_tcp_diag_enabled:-false} \
   -Djgroups.keyExchange.socketTimeout=${ZWE_configs_storage_infinispan_jgroups_keyExchange_socketTimeout:-5000} \
   -Dcaching.storage.infinispan.initialHosts=${INITIAL_HOSTS} \
+  -Dorg.infinispan.threads.virtual=false \
   -Dserver.address=${ZWE_configs_zowe_network_server_listenAddresses:-${ZWE_zowe_network_server_listenAddresses:-"0.0.0.0"}} \
   -Dserver.ssl.enabled=${ZWE_configs_server_ssl_enabled:-true}  \
   -Dserver.ssl.keyStore="${keystore_location}" \
