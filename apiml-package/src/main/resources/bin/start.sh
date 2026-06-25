36146e91ea86d289f9bec698c13117a18b6ed76f
Andrea Tabone <andrea.tabone@broadcom.com>
2026-06-25 17:05:23 +0200

    disable virtual thread for version above 21 and disable it by default
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

--- apiml-package/src/main/resources/bin/start.sh
+++ apiml-package/src/main/resources/bin/start.sh
@@ -195,9 +195,9 @@ fi
 JAVA_VERSION=$(${JAVA_HOME}/bin/javap -verbose java.lang.String \
     | grep "major version" \
     | cut -d " " -f5)
-INFINISPAN_VTHREADS=${ZWE_components_caching_service_storage_infinispan_useVirtualThreads:-${ZWE_configs_storage_infinispan_useVirtualThreads:-true}}
+INFINISPAN_VTHREADS=${ZWE_components_caching_service_storage_infinispan_useVirtualThreads:-${ZWE_configs_storage_infinispan_useVirtualThreads:-false}}
 if [ "$(uname)" = "OS/390" ]; then # z/OS
-    if [ $JAVA_VERSION -eq 65 ]; then # Java 21
+    if [ $JAVA_VERSION -ge 65 ]; then # Java 21+
         INFINISPAN_VTHREADS="false"
     fi
 fi
