a922ef33353c35610d3d4b094b3643f7ce70fd35
Andrea Tabone <andrea.tabone@broadcom.com>
2026-07-09 16:22:52 +0200

    poc - use StoreMigrator utility class from Infinispan-tools to migrate the distributed caches
    Signed-off-by: Andrea Tabone <andrea.tabone@broadcom.com>

---
+++ migrator-debug/src/main/java/org/zowe/apiml/Main.java
@@ -0,0 +1,26 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.apiml;
+
+import org.infinispan.tools.store.migrator.StoreMigrator;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+       migrate("migrator-debug/config/migrator-invalidatedJwtTokens.properties");
+       migrate("migrator-debug/config/migrator-zoweCache.properties");
+       migrate("migrator-debug/config/migrator-zoweInvalidatedTokenCache.properties");
+    }
+
+    private static void migrate(String properties) throws Exception {
+        System.out.println("Migrating " + properties + "...");
+        StoreMigrator.main(new String[]{properties});
+    }
+}
