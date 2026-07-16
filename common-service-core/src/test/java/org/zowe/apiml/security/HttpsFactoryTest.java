20ca0368a65a48b30b8d32fda71aaeb9754b7ea8
Pablo Carle <pablo.carle@broadcom.com>
2026-07-16 13:51:46 +0200

    add tests
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- common-service-core/src/test/java/org/zowe/apiml/security/HttpsFactoryTest.java
+++ common-service-core/src/test/java/org/zowe/apiml/security/HttpsFactoryTest.java
@@ -10,20 +10,46 @@
 
 package org.zowe.apiml.security;
 
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
 import java.security.KeyStoreException;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mockStatic;
 
+@ExtendWith(MockitoExtension.class)
 class HttpsFactoryTest {
+
     private static final String INCORRECT_PARAMETER_VALUE = "WRONG";
 
     private HttpsConfig.HttpsConfigBuilder httpsConfigBuilder;
@@ -121,4 +147,95 @@ class HttpsFactoryTest {
         HostnameVerifier hostnameVerifier = httpsFactory.getHostnameVerifier();
         assertEquals(NoopHostnameVerifier.class, hostnameVerifier.getClass());
     }
+
+    @Nested
+    class OverrideLocalAddress {
+
+        private static final String OWNED_ADDRESS = "127.0.0.1";
+        private static final String NOT_OWNED_ADDRESS = "192.0.2.1";
+
+        @Mock
+        private NetworkInterface interface0;
+        @Mock
+        private NetworkInterface interface1;
+        @Mock
+        private InterfaceAddress interfaceAddress0;
+        @Mock
+        private InterfaceAddress interfaceAddress1;
+
+        private HttpServer localServer;
+
+        @BeforeEach
+        void setUp() throws IOException {
+            lenient().when(interface0.getInterfaceAddresses()).thenReturn(List.of(interfaceAddress0));
+            lenient().when(interface1.getInterfaceAddresses()).thenReturn(List.of(interfaceAddress1));
+            lenient().when(interfaceAddress0.getAddress()).thenReturn(InetAddress.getByName(NOT_OWNED_ADDRESS));
+            lenient().when(interfaceAddress1.getAddress()).thenReturn(InetAddress.getByName(OWNED_ADDRESS));
+
+            localServer = HttpServer.create(new InetSocketAddress(OWNED_ADDRESS, 0), 0);
+            localServer.createContext("/", exchange -> {
+                exchange.sendResponseHeaders(200, -1);
+                exchange.close();
+            });
+            localServer.start();
+        }
+
+        @AfterEach
+        void tearDown() {
+            localServer.stop(0);
+        }
+
+        @Test
+        void givenSpecificAddress_whenHttpClientConnects_thenItBindsToTheMatchingInterfaceAndConnects() {
+            try (
+                var mockedNetworkInterface = mockStatic(NetworkInterface.class);
+            ) {
+                mockedNetworkInterface.when(NetworkInterface::getNetworkInterfaces)
+                    .thenReturn(Collections.enumeration(List.of(interface0, interface1)));
+
+                var httpClient = buildHttpClient(OWNED_ADDRESS);
+                var status = connect(httpClient);
+                assertEquals(200, status);
+            }
+        }
+
+        private int connect(CloseableHttpClient httpClient) {
+            var get = new HttpGet("http://" + OWNED_ADDRESS + ":" + localServer.getAddress().getPort() + "/");
+            try {
+                return httpClient.execute(get, r -> {
+                    return r.getCode();
+                });
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Test
+        void givenAddressNotOwnedByThisHost_whenHttpClientConnects_thenBindFails() {
+            try (
+                var mockedNetworkInterface = mockStatic(NetworkInterface.class);
+            ) {
+                mockedNetworkInterface.when(NetworkInterface::getNetworkInterfaces)
+                    .thenReturn(Collections.enumeration(List.of(interface0, interface1)));
+
+                var httpClient = buildHttpClient(NOT_OWNED_ADDRESS);
+
+                var connectionAttempt = assertThrows(Exception.class, () -> connect(httpClient));
+
+                assertInstanceOf(BindException.class, ExceptionUtils.getRootCause(connectionAttempt));
+            }
+        }
+
+        private CloseableHttpClient buildHttpClient(String selectedAddress) {
+            var httpsConfig = httpsConfigBuilder.build();
+            var httpsFactory = new HttpsFactory(httpsConfig, selectedAddress);
+
+            var httpClient = httpsFactory.buildHttpClient(null);
+            assertEquals("org.apache.hc.client5.http.impl.classic.InternalHttpClient", httpClient.getClass().getName());
+
+            return httpClient;
+        }
+
+    }
+
 }
