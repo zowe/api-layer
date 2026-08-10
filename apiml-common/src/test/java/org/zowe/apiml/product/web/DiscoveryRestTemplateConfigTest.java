7e7a4220df48a3fb6450644b7fd957f3dcc24986
Pablo Carle <pablo.carle@broadcom.com>
2026-08-10 14:34:51 +0200

    using connection manager shared
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- apiml-common/src/test/java/org/zowe/apiml/product/web/DiscoveryRestTemplateConfigTest.java
+++ apiml-common/src/test/java/org/zowe/apiml/product/web/DiscoveryRestTemplateConfigTest.java
@@ -10,30 +10,79 @@
 
 package org.zowe.apiml.product.web;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.core5.io.CloseMode;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import javax.net.ssl.SSLContext;
+
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(MockitoExtension.class)
 class DiscoveryRestTemplateConfigTest {
+
+    private DiscoveryRestTemplateConfig discoveryRestTemplateConfig;
+
+    @BeforeEach
+    void setUp() throws NoSuchAlgorithmException {
+        this.discoveryRestTemplateConfig = new DiscoveryRestTemplateConfig();
+    }
 
     @Test
     void giveSecureEurekaUrl_thenCreateSSLConfig() throws NoSuchAlgorithmException {
-
-        var dct = new DiscoveryRestTemplateConfig();
-        var args = dct.defaultArgs("https://localhost:10011", SSLContext.getDefault(), new NoopHostnameVerifier());
+        var connectionManager = discoveryRestTemplateConfig.httpClientConnectionManager(SSLContext.getDefault(), new NoopHostnameVerifier());
+        var args = discoveryRestTemplateConfig.defaultArgs("https://localhost:10011", SSLContext.getDefault(), new NoopHostnameVerifier(), connectionManager);
         assertTrue(args.getSSLContext().isPresent());
     }
 
     @Test
     void giveUnsecureEurekaUrl_thenDontCreateSSLConfig() throws NoSuchAlgorithmException {
-
-        var dct = new DiscoveryRestTemplateConfig();
-        var args = dct.defaultArgs("http://localhost:10011", SSLContext.getDefault(), new NoopHostnameVerifier());
+        var connectionManager = discoveryRestTemplateConfig.httpClientConnectionManager(SSLContext.getDefault(), new NoopHostnameVerifier());
+        var args = discoveryRestTemplateConfig.defaultArgs("http://localhost:10011", SSLContext.getDefault(), new NoopHostnameVerifier(), connectionManager);
         assertFalse(args.getSSLContext().isPresent());
     }
+
+    @Nested
+    class GivenRestTemplateConfig {
+
+        @Test
+        void whenSupplierInvokedRepeatedly_thenEvictorThreadsUnderControl() throws Exception {
+            var connectionManager = discoveryRestTemplateConfig.httpClientConnectionManager(SSLContext.getDefault(), new NoopHostnameVerifier());
+            var supplier = DiscoveryRestTemplateConfig.getDefaultEurekaClientHttpRequestFactorySupplier(connectionManager);
+
+            var before = countThreadsMatching("idle-connection-evictor-.*"); // default name in Apache HTTP Client
+
+            var factories = new ArrayList<>();
+            for (int i = 0; i < 50; i++) {
+                factories.add(supplier.get(SSLContext.getDefault(), new NoopHostnameVerifier()));
+            }
+
+            var after = countThreadsMatching("idle-connection-evictor-.*");
+
+            assertTrue(after - before <= 1, "before threads: " + before + ". after threads: " + after);
+
+            factories
+                .stream()
+                .map(HttpComponentsClientHttpRequestFactory.class::cast)
+                .forEach(f -> ((CloseableHttpClient) f.getHttpClient()).close(CloseMode.IMMEDIATE));
+        }
+
+        private long countThreadsMatching(String regex) {
+            return Thread.getAllStackTraces().keySet().stream()
+                .filter(t -> t.getName().matches(regex))
+                .count();
+        }
+
+    }
+
 }
