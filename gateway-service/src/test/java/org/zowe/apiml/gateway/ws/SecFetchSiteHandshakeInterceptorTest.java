0419e978458bafd7f90d67560091cdb628dfc9a8
ac892247 <a.chmelo@gmail.com>
2026-08-18 17:17:00 +0200

    servlet object
    Signed-off-by: ac892247 <a.chmelo@gmail.com>

--- gateway-service/src/test/java/org/zowe/apiml/gateway/ws/SecFetchSiteHandshakeInterceptorTest.java
+++ gateway-service/src/test/java/org/zowe/apiml/gateway/ws/SecFetchSiteHandshakeInterceptorTest.java
@@ -18,7 +18,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -31,10 +30,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SecFetchSiteHandshakeInterceptorTest {
@@ -42,7 +38,7 @@ class SecFetchSiteHandshakeInterceptorTest {
     @Mock
     private SecFetchSitePolicy secFetchSitePolicy;
     @Mock
-    private ServerHttpRequest request;
+    private ServletServerHttpRequest request;
     @Mock
     private ServerHttpResponse response;
     @Mock
