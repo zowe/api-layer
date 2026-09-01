2a707feed54c525e409372ea2f8c715bbc8ee54b
Ian Sergeant <ian.sergeant@broadcom.com>
2026-09-01 12:33:37 +0200

    Add Lifecycle.PER_CLASS to allow tests to run together
    Signed-off-by: Ian Sergeant <ian.sergeant@broadcom.com>

--- api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/ApiCatalogUiSecurityHeaderTest.java
+++ api-catalog-services/src/test/java/org/zowe/apiml/apicatalog/ApiCatalogUiSecurityHeaderTest.java
@@ -11,6 +11,7 @@
 package org.zowe.apiml.apicatalog;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @TestPropertySource(properties = {
