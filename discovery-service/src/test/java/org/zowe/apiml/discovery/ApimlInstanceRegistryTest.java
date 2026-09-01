74131d65ffe03928bfdd08981564edc0d00801ca
Jakub Balhar <jakub@balhar.net>
2026-09-01 07:34:57 +0200

    fix: remove unused Mockito imports
    Signed-off-by: Jakub Balhar <jakub@balhar.net>

--- discovery-service/src/test/java/org/zowe/apiml/discovery/ApimlInstanceRegistryTest.java
+++ discovery-service/src/test/java/org/zowe/apiml/discovery/ApimlInstanceRegistryTest.java
@@ -62,8 +62,6 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
