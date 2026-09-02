e7f035ec2607b19e4e12b464fc92bc2fe4cc6e13
Ian Sergeant <ian.sergeant@broadcom.com>
2026-09-02 08:40:39 +0200

    Add @AcceptanceTest
    Signed-off-by: Ian Sergeant <ian.sergeant@broadcom.com>

--- apiml/src/test/java/org/zowe/apiml/acceptance/ApiCatalogUiSecurityHeaderTest.java
+++ apiml/src/test/java/org/zowe/apiml/acceptance/ApiCatalogUiSecurityHeaderTest.java
@@ -12,17 +12,10 @@ package org.zowe.apiml.acceptance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
-@ActiveProfiles("test")
-@DirtiesContext
+@AcceptanceTest
 
 class ApiCatalogUiSecurityHeaderTest {
 
