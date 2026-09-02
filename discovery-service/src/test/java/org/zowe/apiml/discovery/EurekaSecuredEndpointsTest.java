0de038a3fe8a3b0584d212f43cde719e00fad15c
Pavel Jareš <Pavel.Jares@broadcom.com>
2026-09-02 14:39:56 +0200

    code review
    Signed-off-by: Pavel Jareš <Pavel.Jares@broadcom.com>

--- discovery-service/src/test/java/org/zowe/apiml/discovery/EurekaSecuredEndpointsTest.java
+++ discovery-service/src/test/java/org/zowe/apiml/discovery/EurekaSecuredEndpointsTest.java
@@ -14,10 +14,9 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.zowe.apiml.discovery.config.EurekaConfig;
 import org.zowe.apiml.discovery.functional.DiscoveryFunctionalTest;
 
 import java.util.Base64;
@@ -25,17 +24,14 @@ import java.util.Base64;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = {
-        "eureka.client.fetchRegistry=false",
-        "eureka.client.registerWithEureka=false",
-        "apiml.discovery.userid=eureka",
-        "apiml.discovery.password=password",
-        "apiml.security.ssl.verifySslCertificatesOfServices=false",
-        "apiml.security.ssl.nonStrictVerifySslCertificatesOfServices=true"
-    },
-    classes = {DiscoveryServiceApplication.class, EurekaConfig.class}
+@TestPropertySource(properties = {
+    "eureka.client.fetchRegistry=false",
+    "eureka.client.registerWithEureka=false",
+    "apiml.discovery.userid=eureka",
+    "apiml.discovery.password=password",
+    "apiml.security.ssl.verifySslCertificatesOfServices=false",
+    "apiml.security.ssl.nonStrictVerifySslCertificatesOfServices=true"
+    }
 )
 @AutoConfigureMockMvc
 class EurekaSecuredEndpointsTest extends DiscoveryFunctionalTest {
