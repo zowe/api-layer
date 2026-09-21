5166d8a609c1f68f94c8134d1efa6c671d6d0b2a
Elena Kubantseva <elena.kubantseva@broadcom.com>
2026-09-21 16:13:26 +0200

    added tests
    Signed-off-by: Elena Kubantseva <elena.kubantseva@broadcom.com>

--- zaas-service/src/test/java/org/zowe/apiml/zaas/security/service/AuthenticationServiceTest.java
+++ zaas-service/src/test/java/org/zowe/apiml/zaas/security/service/AuthenticationServiceTest.java
@@ -14,10 +14,11 @@ import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.crypto.RSASSAVerifier;
-
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.proc.BadJWTException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
@@ -45,7 +46,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.zowe.apiml.constants.ApimlConstants;
@@ -71,7 +71,15 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.interfaces.RSAPublicKey;
 import java.text.ParseException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import static org.apache.http.HttpHeaders.AUTHORIZATION;
@@ -271,6 +279,21 @@ public class AuthenticationServiceTest { //NOSONAR, needs to be public
                 () -> authService.parseJwtWithSignature(invalidToken));
         }
 
+        @Test
+        void whenSignatureNotVerified_thenThrowsTokenNotValidException() throws JOSEException {
+            stubJWTSecurityForSign();
+            var verifier = mock(RSASSAVerifier.class);
+            lenient().when(verifier.getPublicKey()).thenReturn((RSAPublicKey) publicKey);
+            lenient().when(verifier.verify(any(), any(), any())).thenReturn(false);
+            lenient().when(jwtSecurityInitializer.getJwtVerifier()).thenReturn(verifier);
+
+            String pat = authService.createLongLivedJwtToken(USER, 60, scopes);
+            var exception = assertThrows(TokenNotValidException.class,
+                () -> authService.parseJwtWithSignature(pat),
+                "Expected exception is not TokenNotValidException");
+            assertEquals(BadJWTException.class, exception.getCause().getClass());
+            assertTrue(exception.getCause().getMessage().startsWith("Token signature is invalid for public key:"));
+        }
     }
 
     @Nested
